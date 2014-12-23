5ac4df4832cab7b9cbf2f9ee7de8cf467ff11f5b
Nate Bailey <nate@wintr.us>
2014-12-23 11:21:11 -0500

    Route fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   controller :courses do
-    get "courses/:id" => "courses#show"
     get "courses/:id/students" => "courses#students", :as => :students
     get "courses/:id/articles" => "courses#articles", :as => :path_save
   end
