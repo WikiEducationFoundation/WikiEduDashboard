9301bc93e170cc66c1422c95b7a2ff7d3ef059b2
Nate Bailey <nate@wintr.us>
2014-12-22 19:52:05 -0500

    Route change

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   resources :courses
   controller :courses do
-    get "courses/:id/show" => "courses#show", :as => :show
     get "courses/:id/students" => "courses#students", :as => :students
     get "courses/:id/articles" => "courses#articles", :as => :path_save
   end
