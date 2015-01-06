8226d4b61c520a5c488aed7ede70dcbdecaf8eb1
Nate Bailey <nate@wintr.us>
2015-01-06 15:56:16 -0800

    Updated Capistrano deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :ssh_options, { :forward_agent => true }
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('application.yml', 'config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/application.yml', 'config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp')
