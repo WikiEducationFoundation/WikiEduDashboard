41dde7bdc9db7144b7c468ca977468075e88ae6e
Nate Bailey <nate@wintr.us>
2015-01-09 12:27:55 -0800

    Clean up log tags

--- config/initializers/log_formatting.rb
+++ config/initializers/log_formatting.rb
@@ -13,6 +13,10 @@ class ActiveSupport::Logger::SimpleFormatter
     formatted_time = time.strftime("%Y-%m-%d %H:%M:%S.") << time.usec.to_s[0..2].rjust(3)
     color = SEVERITY_TO_COLOR_MAP[severity]
 
-    "\033[0;37m#{formatted_time}\033[0m [\033[#{color}m#{formatted_severity}\033[0m] #{msg.strip} (pid:#{$$})\n"
+    # This version omits colors
+    "[#{formatted_time} #{formatted_severity}] #{msg.strip}\n"
+
+    # This version uses colors
+    # "\033[0;37m#{formatted_time}\033[0m [\033[#{color}m#{formatted_severity}\033[0m] #{msg.strip} (pid:#{$$})\n"
   end
 end
