eaebcb1bfae2d7a2dba0bffddf5a7eba3b386dd7
Nate Bailey <nate@wintr.us>
2015-01-12 17:39:11 -0800

    Limit stats.grok.se API requests to 50 at a time

--- app/models/article.rb
+++ app/models/article.rb
@@ -101,17 +101,18 @@ class Article < ActiveRecord::Base
     require "./lib/course_list"
     require "./lib/grok"
     views = {}
-    threads = Article.all.each_with_index.map do |a, i|
-      Thread.new(i) do |i|
-        views_updated_at = a.views_updated_at || CourseList.start.to_date
-        if(views_updated_at < Date.today)
-          since = all_time ? CourseList.start.to_date : views_updated_at + 1.day
-          views[a.id] = Grok.get_views_since_date_for_article(a.title, since)
+    Article.find_in_batches(batch_size: 50).with_index do |group, batch|
+      threads = group.each_with_index.map do |a, i|
+        Thread.new(i) do |i|
+          views_updated_at = a.views_updated_at || CourseList.start.to_date
+          if(views_updated_at < Date.today)
+            since = all_time ? CourseList.start.to_date : views_updated_at + 1.day
+            views[a.id] = Grok.get_views_since_date_for_article(a.title, since)
+          end
         end
       end
+      threads.each { |t| t.join }
     end
-
-    threads.each { |t| t.join }
 
     Article.all.each do |a|
       a.update_views(all_time, views[a.id])
