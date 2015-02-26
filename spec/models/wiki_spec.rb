c7a5f99dd4f8fe6cf59f6ad01fcd4eac47e91dab
ragesoss <ragesoss@gmail.com>
2015-02-26 15:30:00 -0800

    Fix wiki spec

--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -6,16 +6,20 @@ describe Wiki do
     it "should return the content of a page" do
       VCR.use_cassette "wiki/course_list" do
         response = Wiki.get_page_content('Wikipedia:Education program/Dashboard/test_ids')
-        expect(response).to_not be_empty
+        expect(response).to eq("439\n456\n351")
+
       end
     end
 
     it "should return course info for a certain course" do
       VCR.use_cassette "wiki/course_data" do
         response = Wiki.get_course_info 351
-        expect(response["name"]).to eq("Education Program:University of Oklahoma/HSCI 3013: History of Science to the Age of Newton (Summer 2014)")
-        expect(response["start"]).to eq("2014-05-12")
-        expect(response["end"]).to eq("2014-06-25")
+        expect(response[0]["course"]["title"]).to eq("HSCI 3013: History of Science to the Age of Newton")
+        expect(response[0]["course"]["term"]).to eq("Summer 2014")
+        expect(response[0]["course"]["slug"]).to eq("University_of_Oklahoma/HSCI_3013:_History_of_Science_to_the_Age_of_Newton_(Summer_2014)")
+        expect(response[0]["course"]["school"]).to eq("University of Oklahoma")
+        expect(response[0]["course"]["start"]).to eq("2014-05-12".to_date)
+        expect(response[0]["course"]["end"]).to eq("2014-06-25".to_date)
       end
     end
   end
@@ -25,7 +29,7 @@ describe Wiki do
     it "should return the list of courses" do
       VCR.use_cassette "wiki/course_list" do
         response = Wiki.get_course_list
-        expect(response.count).to eq(3)
+        expect(response.count).to be >= 1
       end
     end
   end
