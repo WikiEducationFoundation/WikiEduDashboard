d8b4ba7fb323e3ea0d6bacfa28df4fcdeaac7a19
ragesoss <ragesoss@gmail.com>
2015-02-28 14:25:16 -0800

    Factories and trivial tests for assignments model

--- spec/factories/assignments.rb
+++ spec/factories/assignments.rb
@@ -1,6 +1,24 @@
 FactoryGirl.define do
+  # article that exists
   factory :assignment do
-    
+    id 1
+    created_at "2015-02-18 18:02:50"
+    updated_at "2015-02-18 18:03:01"
+    user_id 23682032
+    course_id 481
+    article_id 12488499
+    article_title "Siderocalin"
+  end
+
+  # article that does not exist
+  factory :redlink, class: Assignment do
+    id 2
+    created_at "2015-02-18 18:02:50"
+    updated_at "2015-02-18 18:02:50"
+    user_id 22497786
+    course_id 481
+    article_id nil
+    article_title "Faecal calprotectin"
   end
 
 end
--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -1,1 +1,16 @@
 require 'rails_helper'
+
+describe Assignment do
+
+  describe "assignment creation" do
+    it "should create Assignment objects" do
+      assignment = build(:assignment)
+      assignment2 = build(:redlink)
+      
+      expect(assignment.id).to be_kind_of(Integer)
+      expect(assignment2.article_id).to be_nil
+    end
+  end
+
+
+end
