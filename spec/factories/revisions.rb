9151bb8a2e0d236c610d9c28a219c53b7de5b027
Sage Ross <ragesoss@gmail.com>
2015-02-28 21:03:50 -0800

    Start tests for revision model

--- spec/factories/revisions.rb
+++ spec/factories/revisions.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :revision do
     date "2014-12-17"
-bytes 1
+    characters 1
   end
 
 end
--- spec/models/revision_spec.rb
+++ spec/models/revision_spec.rb
@@ -1,1 +1,13 @@
 require 'rails_helper'
+
+describe Revision do
+
+  describe "revision methods" do
+    it "should create Revision objects" do
+      revision = build(:revision)
+      revision.update
+      expect(revision).to be
+    end
+  end
+  
+end
