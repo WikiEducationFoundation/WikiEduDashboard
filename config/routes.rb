118c7952fb0559e4256f4ce329fcfd3a3a006f15
ragesoss <ragesoss@gmail.com>
2015-03-12 11:13:44 -0700

    Fix for issue #43 to allow dots in course slugs

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   controller :courses do
     get "courses/*id/students" => "courses#students", :as => :students
     get "courses/*id/articles" => "courses#articles", :as => :path_save
-    get "courses/*id" => "courses#students"
+    get "courses/*id" => "courses#students", :constraints => { :id => /.*/ } 
   end
 
   resources :courses
