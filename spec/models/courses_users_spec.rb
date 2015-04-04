9cc4f9456b94da694043de27762f110647bb460d
Sage Ross <ragesoss@gmail.com>
2015-04-03 22:14:45 -0700

    Improve coverage of CoursesUsers

--- spec/models/courses_users_spec.rb
+++ spec/models/courses_users_spec.rb
@@ -4,57 +4,57 @@ describe CoursesUsers, type: :model do
   describe '.update_all_caches' do
     it 'should update data for course-user relationships' do
       # Add a user, a course, an article, and a revision.
-      build(:user,
+      create(:user,
             id: 1,
             wiki_id: 'Ragesoss'
-      ).save
+      )
 
-      build(:course,
+      create(:course,
             id: 1,
             start: '2015-01-01'.to_date,
             end: '2015-07-01'.to_date,
             title: 'Underwater basket-weaving'
-      ).save
+      )
 
-      build(:article,
+      create(:article,
             id: 1,
             title: 'Selfie',
             namespace: 0
-      ).save
+      )
 
-      build(:revision,
+      create(:revision,
             id: 1,
             user_id: 1,
             article_id: 1,
             date: '2015-03-01'.to_date,
             characters: 9000
-      ).save
+      )
 
       # Assign the article to the user.
-      build(:assignment,
+      create(:assignment,
             course_id: 1,
             user_id: 1,
             article_id: 1,
             article_title: 'Selfie'
-      ).save
+      )
 
       # Make a course-user and save it.
-      build(:courses_user,
+      create(:courses_user,
             id: 1,
             course_id: 1,
             user_id: 1,
             assigned_article_title: 'Selfie'
-      ).save
+      )
 
       # Make an article-course.
-      build(:articles_course,
+      create(:articles_course,
             id: 1,
             article_id: 1,
             course_id: 1
-      ).save
+      )
 
       # Update caches for all CoursesUsers
-      CoursesUsers.update_all_caches
+      CoursesUsers.update_all_caches(CoursesUsers.where(id: 1).first)
 
       # Fetch the created CoursesUsers entry
       course_user = CoursesUsers.all.first
