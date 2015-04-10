59678a33cc33f6106658ad4fcc367dee050aea35
Nate Bailey <nate@wintr.us>
2015-04-10 11:04:03 -0700

    Resolves #43 Handle dots in course titles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 # Page titles on Wikipedia may include dots, so this constraint is needed.
-@cstr = { id: /.*/ }
 
 Rails.application.routes.draw do
   get 'errors/file_not_found'
@@ -12,7 +11,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'sign_in', to: 'devise/sessions#new', as: :new_user_session
     get 'sign_out', to: 'users#signout', as: :destroy_user_session
-    get 'sign_out_oauth', to: 'devise/sessions#destroy', as: :true_destroy_user_session
+    get 'sign_out_oauth', to: 'devise/sessions#destroy',
+                          as: :true_destroy_user_session
   end
 
   controller :users do
@@ -20,12 +20,16 @@ Rails.application.routes.draw do
   end
 
   controller :courses do
-    get 'courses/*id/manual_update' => 'courses#manual_update', :as => :manual_update, constraints: @cstr
-    get 'courses/*id/notify_untrained' => 'courses#notify_untrained', :as => :notify_untrained, constraints: @cstr
+    get 'courses/*id/manual_update' => 'courses#manual_update',
+        :as => :manual_update, constraints: { id: /.*/ }
+    get 'courses/*id/notify_untrained' => 'courses#notify_untrained',
+        :as => :notify_untrained, constraints: { id: /.*/ }
 
-    get 'courses/*id/students' => 'courses#students', :as => :students, constraints: @cstr
-    get 'courses/*id/articles' => 'courses#articles', :as => :path_save, constraints: @cstr
-    get 'courses/*id' => 'courses#students', constraints: @cstr
+    get 'courses/*id/students' => 'courses#students',
+        :as => :students, constraints: { id: /.*/ }
+    get 'courses/*id/articles' => 'courses#articles',
+        :as => :path_save, constraints: { id: /.*/ }
+    get 'courses/*id' => 'courses#students', constraints: { id: /.*/ }
     get 'courses' => 'courses#index'
     get 'talk' => 'courses#talk'
   end
