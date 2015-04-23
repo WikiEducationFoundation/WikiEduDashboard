328bec8118e18b6fb47917fe8b4843c50681dd84
Nate Bailey <nate@wintr.us>
2015-04-23 14:35:01 -0700

    Gradeables properly cleaned up on block destroy

--- app/models/block.rb
+++ app/models/block.rb
@@ -2,9 +2,14 @@
 class Block < ActiveRecord::Base
   belongs_to :week
   has_one :gradeable, as: :gradeable_item
+  before_destroy :cleanup
 
   def pretty_kind
     index = %w(Assignment Milestone Class Custom)
     index[kind]
+  end
+
+  def cleanup
+    Gradeable.destroy gradeable_id unless gradeable_id.nil?
   end
 end
