a933fef1e3850f45e678e6ea31aa4b027d5d222b
Sage Ross <ragesoss@gmail.com>
2015-05-01 21:38:34 -0700

    Add test for Utils.parse_json error handling

---
+++ spec/lib/utils_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe Utils do
+  describe '.parse_json' do
+    it 'should handle unparseable json' do
+      not_json = "<xml_is_great>Wat?</xml_is_great>"
+      Utils.parse_json(not_json)
+    end
+  end
+end
