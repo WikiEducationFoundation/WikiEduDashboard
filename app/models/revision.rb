c040722ec782265b7707a31ddbd5b70a6b74feef
Nate Bailey <nate@wintr.us>
2015-05-11 16:39:58 -0700

    Update user links on Students panel Add dif links for revisions

--- app/models/revision.rb
+++ app/models/revision.rb
@@ -7,6 +7,15 @@ class Revision < ActiveRecord::Base
   ####################
   # Instance methods #
   ####################
+  def url
+    # https://en.wikipedia.org/w/index.php?title=Eva_Hesse&diff=cur&oldid=655980945
+
+    escaped_title = article.title.gsub(' ', '_')
+    language = Figaro.env.wiki_language
+
+    "https://#{language}.wikipedia.org/w/index.php?title=#{escaped_title}&diff=cur&oldid=#{id}"
+  end
+
   def update(data={}, save=true)
     self.attributes = data
     self.save if save
