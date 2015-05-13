17ee28e0f527851e7ca0d33135a9893f9aacae4d
ragesoss <ragesoss@gmail.com>
2015-05-12 21:58:44 -0700

    Further refactor RevisionImporter to break up complex .update_all_revisions

--- lib/importers/revision_importer.rb
+++ lib/importers/revision_importer.rb
@@ -14,22 +14,7 @@ class RevisionImporter
     courses = [courses] if courses.is_a? Course
     courses ||= all_time ? Course.all : Course.current
     courses.each do |c|
-      next if c.students.empty?
-      start = c.start.strftime('%Y%m%d')
-      end_date = c.end.strftime('%Y%m%d')
-      new_users = c.users.role('student').where(revision_count: 0)
-
-      old_users = c.students - new_users
-
-      unless new_users.empty?
-        results += get_revisions(new_users, start, end_date)
-      end
-
-      unless old_users.empty?
-        first_rev = c.revisions.order('date DESC').first
-        start = first_rev.date.strftime('%Y%m%d') unless first_rev.blank?
-        results += get_revisions(old_users, start, end_date)
-      end
+      results += get_revisions_for_course(c)
     end
 
     import_revisions(results)
@@ -43,6 +28,29 @@ class RevisionImporter
     ArticlesCourses.update_from_revisions result_revs
   end
 
+  # Given a Course, get new revisions for the users in that course.
+  def self.get_revisions_for_course(c)
+    results = []
+    return results if c.students.empty?
+    start = c.start.strftime('%Y%m%d')
+    end_date = c.end.strftime('%Y%m%d')
+    new_users = c.users.role('student').where(revision_count: 0)
+
+    old_users = c.students - new_users
+
+    unless new_users.empty?
+      results += get_revisions(new_users, start, end_date)
+    end
+
+    unless old_users.empty?
+      first_rev = c.revisions.order('date DESC').first
+      start = first_rev.date.strftime('%Y%m%d') unless first_rev.blank?
+      results += get_revisions(old_users, start, end_date)
+    end
+    results
+  end
+
+  # Get revisions made by a set of users between two dates.
   def self.get_revisions(users, start, end_date)
     Utils.chunk_requests(users, 40) do |block|
       Replica.get_revisions block, start, end_date
