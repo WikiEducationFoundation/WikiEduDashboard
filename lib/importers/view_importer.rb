200bf54462cd9fffe1ae5142118116563f6e4de8
Sage Ross <ragesoss@gmail.com>
2015-05-22 21:25:41 -0700

    Refactor ViewImporter

--- lib/importers/view_importer.rb
+++ lib/importers/view_importer.rb
@@ -1,3 +1,5 @@
+require "#{Rails.root}/lib/grok"
+
 #= Imports and updates views for articles, revisions, and join tables
 class ViewImporter
   ################
@@ -22,25 +24,27 @@ class ViewImporter
   # API Access #
   ##############
   def self.update_views(articles, all_time=false)
-    require './lib/grok'
-    views, vua = {}, {}
     articles.with_index do |group, _batch|
-      threads = group.each_with_index.map do |a, i|
-        start = a.courses.order(:start).first.start.to_date
-        Thread.new(i) do
-          vua[a.id] = a.views_updated_at || start
-          if vua[a.id] < Date.today
-            since = all_time ? start : vua[a.id] + 1.day
-            views[a.id] = Grok.views_for_article a.title, since
-          end
+      update_views_for_batch(group, all_time)
+    end
+  end
+
+  def self.update_views_for_batch(group, all_time)
+    views, vua = {}, {}
+    threads = group.each_with_index.map do |a, i|
+      start = a.courses.order(:start).first.start.to_date
+      Thread.new(i) do
+        vua[a.id] = a.views_updated_at || start
+        if vua[a.id] < Date.today
+          since = all_time ? start : vua[a.id] + 1.day
+          views[a.id] = Grok.views_for_article a.title, since
         end
       end
-      threads.each(&:join)
-      group.each do |a|
-        a.views_updated_at = vua[a.id]
-        update_views_for_article(a, all_time, views[a.id])
-      end
-      views, vua = {}, {}
+    end
+    threads.each(&:join)
+    group.each do |a|
+      a.views_updated_at = vua[a.id]
+      update_views_for_article(a, all_time, views[a.id])
     end
   end
 
@@ -48,29 +52,32 @@ class ViewImporter
   # Helpers #
   ###########
   def self.update_views_for_article(article, all_time=false, views=nil)
-    require './lib/grok'
     return unless article.views_updated_at < Date.today
 
     since = article.views_updated_at + 1.day
     since = article.courses.order(:start).first.start.to_date if all_time
 
     # Update views on all revisions and the article
-    nv = views || Grok.views_for_article(article.title, since)
+    views ||= Grok.views_for_article(article.title, since)
+
+    add_views_to_revisions(article, views, all_time)
 
     last = since
-    ActiveRecord::Base.transaction do
-      article.revisions.each do |r|
-        r.views = all_time ? 0 : r.views
-        r.views += nv.reduce(0) do |sum, (d, v)|
-          sum + (d.to_date >= r.date ? v : 0)
-        end
-        r.save
-      end
-      last = nv.sort_by { |(d)| d }.last.first.to_date unless nv.empty?
-    end
-
+    last = views.sort_by { |(d)| d }.last.first.to_date unless views.empty?
     article.views_updated_at = last.nil? ? article.views_updated_at : last
     article.views = article.revisions.order('date ASC').first.views
     article.save
+  end
+
+  def self.add_views_to_revisions(article, views, all_time)
+    ActiveRecord::Base.transaction do
+      article.revisions.each do |rev|
+        rev.views = all_time ? 0 : rev.views
+        rev.views += views.reduce(0) do |sum, (d, v)|
+          sum + (d.to_date >= rev.date ? v : 0)
+        end
+        rev.save
+      end
+    end
   end
 end
