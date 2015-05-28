c45d7f409d4ac5503d2a9d78ff1c4d4f018ce622
ragesoss <ragesoss@gmail.com>
2015-05-27 20:47:15 -0700

    Diff urls for revisions should be against the previous rev, not the current one.
    Also, add spec for Revision#url and move some of the RevisionImporter specs to the corresponding spec file.

--- spec/lib/importers/revision_importer_spec.rb
+++ spec/lib/importers/revision_importer_spec.rb
@@ -3,6 +3,94 @@ require "#{Rails.root}/lib/importers/revision_importer"
 require "#{Rails.root}/lib/cleaners"
 
 describe RevisionImporter do
+  describe '.update_all_revisions' do
+    it 'should fetch revisions for existing courses' do
+      VCR.use_cassette 'revisions/update_all_revisions' do
+        # Try it with no courses.
+        RevisionImporter.update_all_revisions
+        expect(Revision.all.count).to eq(0)
+
+        # Now add a course with users
+        VCR.use_cassette 'wiki/course_data' do
+          CourseImporter.update_all_courses(false, cohort: [351])
+        end
+
+        RevisionImporter.update_all_revisions nil, true
+        # When the non-students are included, Revisions count is 1919.
+        expect(Revision.all.count).to eq(519)
+        # When the non-students are included, ArticlesCourses count is 224.
+        expect(ArticlesCourses.all.count).to eq(10)
+
+        # Update the revision counts, then try update_all_revisions again
+        # to test how it handles old users.
+        User.update_all_caches(Course.find(351).users)
+        RevisionImporter.update_all_revisions nil, true
+        expect(Revision.all.count).to eq(519)
+        expect(ArticlesCourses.all.count).to eq(10)
+      end
+    end
+
+    it 'should not fail with returning students' do
+      VCR.use_cassette 'revisions/returning_students' do
+        # Create a user who has a revision from long ago
+        create(:trained) # This is user 319203, with edits since 2015.
+        create(:revision,
+               user_id: 319203,
+               date: '2003-03-01'.to_date)
+        # Create a recent course and add the user to it.
+        create(:course,
+               id: 1,
+               start: '2015-01-01'.to_date,
+               end: '2030-01-01'.to_date)
+        create(:courses_user,
+               course_id: 1,
+               user_id: 319203,
+               role: 0)
+        User.update_all_caches(Course.find(1).users)
+        RevisionImporter.update_all_revisions nil, true
+        expect(Revision.all.count > 1).to be true
+      end
+    end
+  end
+
+  describe '.update_assignment_article_ids' do
+    it 'should add article ids to assignments after importing revisions' do
+      VCR.use_cassette 'revisions/update_all_revisions' do
+        VCR.use_cassette 'wiki/course_data' do
+          CourseImporter.update_all_courses(false, cohort: [351])
+        end
+        # .update_all_revisions calls .update_assignment_article_ids at the end.
+        RevisionImporter.update_all_revisions nil, true
+        # Only assignments that had revisions by course participants should have
+        # an article_id.
+        expect(Assignment.all.count).to eq(11)
+        expect(Assignment.where.not(article_id: nil).count).to eq(10)
+      end
+    end
+
+    it 'should only update article_ids for mainspace titles' do
+      create(:assignment,
+             id: 1,
+             article_title: 'Foo',
+             article_id: nil)
+      create(:assignment,
+             id: 2,
+             article_title: 'Bar',
+             article_id: nil)
+      create(:article,
+             id: 123,
+             title: 'Foo',
+             namespace: 0)
+      create(:article,
+             id: 456,
+             title: 'Bar',
+             namespace: 2)
+      expect(Assignment.where.not(article_id: nil).count).to eq(0)
+      AssignmentImporter.update_assignment_article_ids
+      expect(Assignment.where.not(article_id: nil).count).to eq(1)
+    end
+  end
+
   describe '.repair_orphan_revisions' do
     it 'should import articles for orphaned revisions' do
       # We start with revision and article
--- spec/models/revision_spec.rb
+++ spec/models/revision_spec.rb
@@ -1,6 +1,4 @@
 require 'rails_helper'
-require "#{Rails.root}/lib/importers/course_importer"
-require "#{Rails.root}/lib/importers/revision_importer"
 
 describe Revision do
   describe '#update' do
@@ -17,91 +15,18 @@ describe Revision do
     end
   end
 
-  describe '.update_all_revisions' do
-    it 'should fetch revisions for existing courses' do
-      VCR.use_cassette 'revisions/update_all_revisions' do
-        # Try it with no courses.
-        RevisionImporter.update_all_revisions
-        expect(Revision.all.count).to eq(0)
-
-        # Now add a course with users
-        VCR.use_cassette 'wiki/course_data' do
-          CourseImporter.update_all_courses(false, cohort: [351])
-        end
-
-        RevisionImporter.update_all_revisions nil, true
-        # When the non-students are included, Revisions count is 1919.
-        expect(Revision.all.count).to eq(519)
-        # When the non-students are included, ArticlesCourses count is 224.
-        expect(ArticlesCourses.all.count).to eq(10)
-
-        # Update the revision counts, then try update_all_revisions again
-        # to test how it handles old users.
-        User.update_all_caches(Course.find(351).users)
-        RevisionImporter.update_all_revisions nil, true
-        expect(Revision.all.count).to eq(519)
-        expect(ArticlesCourses.all.count).to eq(10)
-      end
-    end
-
-    it 'should not fail with returning students' do
-      VCR.use_cassette 'revisions/returning_students' do
-        # Create a user who has a revision from long ago
-        create(:trained) # This is user 319203, with edits since 2015.
-        create(:revision,
-               user_id: 319203,
-               date: '2003-03-01'.to_date)
-        # Create a recent course and add the user to it.
-        create(:course,
-               id: 1,
-               start: '2015-01-01'.to_date,
-               end: '2030-01-01'.to_date)
-        create(:courses_user,
-               course_id: 1,
-               user_id: 319203,
-               role: 0)
-        User.update_all_caches(Course.find(1).users)
-        RevisionImporter.update_all_revisions nil, true
-        expect(Revision.all.count > 1).to be true
-      end
-    end
-  end
-
-  describe 'RevisionImporter.update_assignment_article_ids' do
-    it 'should add article ids to assignments after importing revisions' do
-      VCR.use_cassette 'revisions/update_all_revisions' do
-        VCR.use_cassette 'wiki/course_data' do
-          CourseImporter.update_all_courses(false, cohort: [351])
-        end
-        # .update_all_revisions calls .update_assignment_article_ids at the end.
-        RevisionImporter.update_all_revisions nil, true
-        # Only assignments that had revisions by course participants should have
-        # an article_id.
-        expect(Assignment.all.count).to eq(11)
-        expect(Assignment.where.not(article_id: nil).count).to eq(10)
-      end
-    end
-
-    it 'should only update article_ids for mainspace titles' do
-      create(:assignment,
+  describe '#url' do
+    it 'should generate a diff url for the revision' do
+      create(:article,
              id: 1,
-             article_title: 'Foo',
-             article_id: nil)
-      create(:assignment,
-             id: 2,
-             article_title: 'Bar',
-             article_id: nil)
-      create(:article,
-             id: 123,
-             title: 'Foo',
-             namespace: 0)
-      create(:article,
-             id: 456,
-             title: 'Bar',
-             namespace: 2)
-      expect(Assignment.where.not(article_id: nil).count).to eq(0)
-      AssignmentImporter.update_assignment_article_ids
-      expect(Assignment.where.not(article_id: nil).count).to eq(1)
+             title: 'Vectors_in_gene_therapy')
+      create(:revision,
+             id: 637221390,
+             article_id: 1)
+      url = Revision.find(637221390).url
+      # rubocop:disable Metrics/LineLength
+      expect(url).to eq('https://en.wikipedia.org/w/index.php?title=Vectors_in_gene_therapy&diff=prev&oldid=637221390')
+      # rubocop:enable Metrics/LineLength
     end
   end
 end
