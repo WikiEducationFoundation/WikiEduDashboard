01d13d6d3dc1742cc31406fdbc7b2379132686bd
Sage Ross <sage@ragesoss.com>
2015-06-08 21:14:04 -0700

    Mark a FIXME in CourseHelper

--- app/helpers/course_helper.rb
+++ app/helpers/course_helper.rb
@@ -10,6 +10,8 @@ module CourseHelper
   end
 
   def current?(course)
+    # FIXME: 'current' should only be defined in one place. It's also defined
+    # from an application.yml parameter in course.rb
     month = 2_592_000 # number of seconds in 30 days
     course.start < Time.now && course.end > Time.now - month
   end
