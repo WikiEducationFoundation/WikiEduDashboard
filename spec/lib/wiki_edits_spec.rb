d4558cdabd76bd02784276e50d42c32e5cc94442
Sage Ross <sage@ragesoss.com>
2015-06-12 16:06:31 -0700

    Improve coverage of WikiEdits

--- spec/lib/wiki_edits_spec.rb
+++ spec/lib/wiki_edits_spec.rb
@@ -5,7 +5,17 @@ describe WikiEdits do
   # We're not testing any of the network stuff, nor whether the requests are
   # well-formatted, but at least this verifies that the flow is parsing tokens
   # in the expected way.
-  describe '.notify_untrained' do
+  # rubocop:disable Metrics/LineLength
+  before :each do
+    fake_tokens = "{\"query\":{\"tokens\":{\"csrftoken\":\"myfaketoken+\\\\\"}}}"
+    # rubocop:enable Metrics/LineLength
+    stub_request(:get, /.*wikipedia.*/)
+      .to_return(status: 200, body: fake_tokens, headers: {})
+    stub_request(:post, /.*wikipedia.*/)
+      .to_return(status: 200, body: 'success', headers: {})
+  end
+
+  describe '.notify_untrained and .notify_students' do
     it 'should post talk page messages on Wikipedia' do
       create(:course,
              id: 1)
@@ -23,14 +33,11 @@ describe WikiEdits do
       create(:courses_user,
              course_id: 1,
              user_id: 2)
-      # rubocop:disable Metrics/LineLength
-      fake_tokens = "{\"query\":{\"tokens\":{\"csrftoken\":\"myfaketoken+\\\\\"}}}"
-      # rubocop:enable Metrics/LineLength
-      stub_request(:get, /.*wikipedia.*/)
-        .to_return(status: 200, body: fake_tokens, headers: {})
-      stub_request(:post, /.*wikipedia.*/)
-        .to_return(status: 200, body: 'success', headers: {})
       WikiEdits.notify_untrained(1, User.first)
+      params = { sectiontitle: 'My message headline',
+                 text: 'My message to you',
+                 summary: 'My edit summary' }
+      WikiEdits.notify_students(1, User.first, User.all, params)
     end
   end
 end
