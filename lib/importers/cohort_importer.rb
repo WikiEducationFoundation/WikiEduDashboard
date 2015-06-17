9b99c89aa9d38a31d832ce7ae1b36a11e0453506
Sage Ross <ragesoss@gmail.com>
2015-06-16 22:35:41 -0700

    One more pass at refactoring CohortImporter

--- lib/importers/cohort_importer.rb
+++ lib/importers/cohort_importer.rb
@@ -8,26 +8,27 @@ class CohortImporter
       raw_ids.each do |slug, course_ids|
         cohort = Cohort.find_or_create_by(slug: slug)
         ids_in_cohort = cohort.courses.map(&:id)
-        new_course_ids = course_ids - ids_in_cohort
-        removed_course_ids = ids_in_cohort - course_ids
 
-        add_courses_to_cohort(new_course_ids, cohort)
-        remove_courses_from_cohort(removed_course_ids, cohort)
+        new_course_ids = course_ids - ids_in_cohort
+        new_courses = Course.where(id: new_course_ids)
+        add_courses_to_cohort(new_courses, cohort)
+
+        removed_course_ids = ids_in_cohort - course_ids
+        removed_courses = Course.where(id: removed_course_ids)
+        remove_courses_from_cohort(removed_courses, cohort)
       end
     end
   end
 
-  def self.add_courses_to_cohort(new_course_ids, cohort)
-    new_course_ids.each do |course_id|
-      new_course = Course.find_by_id(course_id)
-      new_course.cohorts << cohort if new_course
+  def self.add_courses_to_cohort(courses, cohort)
+    courses.each do |course|
+      course.cohorts << cohort
     end
   end
 
-  def self.remove_courses_from_cohort(removed_course_ids, cohort)
-    removed_course_ids.each do |course_id|
-      removed_course = Course.find_by_id(course_id)
-      removed_course.cohorts.delete(cohort) if removed_course
+  def self.remove_courses_from_cohort(courses, cohort)
+    courses.each do |course|
+      course.cohorts.delete(cohort)
     end
   end
 end
