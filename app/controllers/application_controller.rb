fcc335696b8532d9d0b4d1f9800e6f6d1f60bbf4
Nate Bailey <nate@wintr.us>
2015-06-17 17:39:09 -0700

    Location is preserved on login/logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,16 @@ class ApplicationController < ActionController::Base
 
   before_action :set_locale
 
+  def after_sign_out_path_for(resource_or_scope)
+    request.referrer
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    if request.env['omniauth.origin']
+      request.env['omniauth.origin']
+    end
+  end
+
   def require_permissions
     course = Course.find_by_slug(params[:id])
     exception = ActionController::InvalidAuthenticityToken.new('Unauthorized')
