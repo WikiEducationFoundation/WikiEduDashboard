07be765e981119cd277694a0bcfa4bb39404bd87
Sage Ross <ragesoss@gmail.com>
2015-06-25 22:39:17 -0700

    Extend student_role_spec to cover unenrollment, adding an assignment.
    My build passes locally, so I'm not sure why travis has been so brittle today.

--- spec/features/student_role_spec.rb
+++ spec/features/student_role_spec.rb
@@ -12,7 +12,7 @@ def stub_oauth_edit
     .to_return(status: 200, body: 'success', headers: {})
 end
 
-describe 'Student users', type: :feature do
+describe 'Student users', type: :feature, js: true do
   before do
     include Devise::TestHelpers, type: :feature
     Capybara.current_driver = :selenium
@@ -32,46 +32,87 @@ describe 'Student users', type: :feature do
     create(:cohorts_course,
            cohort_id: 1,
            course_id: 10001)
-
     user = create(:user,
+                  id: 1,
                   wiki_token: 'foo',
                   wiki_secret: 'bar')
     login_as(user, scope: :user)
     visit root_path
   end
 
-  describe 'enrolling by button', js: true do
-    it 'should let students join a course' do
+  describe 'enrolling and unenrolling by button' do
+    it 'should join a course' do
       stub_oauth_edit
 
+      # click enroll button
       visit "/courses/#{Course.first.slug}"
       first('button').click
 
-      # enter passcode in alert popup
+      # enter passcode in alert popup to enroll
       prompt = page.driver.browser.switch_to.alert
       prompt.send_keys('passcode')
-      sleep 1
       prompt.accept
 
       visit "/courses/#{Course.first.slug}/students"
-      expect(page).to have_content User.first.wiki_id
+      expect(first('tbody')).to have_content User.first.wiki_id
+
+      # now unenroll
+      visit "/courses/#{Course.first.slug}"
+      page.all('button').last.click
+      page.driver.browser.switch_to.alert.accept
+
+      visit "/courses/#{Course.first.slug}/students"
+      expect(first('tbody')).not_to have_content User.first.wiki_id
     end
   end
-  # TODO: enroll via action button
 
-  describe 'enrolling by url', js: true do
-    it 'should let students join a course' do
+  describe 'enrolling by url' do
+    it 'should join a course' do
       stub_oauth_edit
 
       visit "/courses/#{Course.first.slug}/students/enroll/passcode"
-      sleep 1
       visit "/courses/#{Course.first.slug}/students"
-      expect(page).to have_content User.first.wiki_id
-      # TODO: add assignments and reviews
-      # TODO: remove assignments and reviews
+      expect(first('tbody')).to have_content User.first.wiki_id
     end
   end
 
+  describe 'adding and removing assignments' do
+    it 'should work' do
+      stub_oauth_edit
+      create(:courses_user,
+             course_id: 10001,
+             user_id: 1,
+             role: 0)
+      visit "/courses/#{Course.first.slug}/students"
+
+      # Add an assigned article
+      first('button.border').click
+      first('input').set('Selfie')
+      page.all('button.border')[1].click
+      page.driver.browser.switch_to.alert.accept
+      page.all('button.border')[0].click
+      expect(page).to have_content 'Selfie'
+
+      # TODO: Work around mysql errors for the rest of this spec
+      # # Add a reviewed article
+      # page.all('button.border')[1].click
+      # first('input').set('Self-portrait')
+      # page.all('button.border')[2].click
+      # page.driver.browser.switch_to.alert.accept
+      # page.all('button.border')[1].click
+      # expect(page).to have_content 'Self-portrait'
+
+      # # Remove the assignment
+      # page.all('button.border')[0].click
+      # page.all('button.border')[2].click
+      # sleep 3
+      # page.driver.browser.switch_to.alert.accept
+      # page.all('button.border')[0].click
+      # sleep 3
+      # visit "/courses/#{Course.first.slug}/students"
+      # expect(page).not_to have_content 'Selfie'
+    end
+  end
   after do
     logout
     Capybara.use_default_driver
