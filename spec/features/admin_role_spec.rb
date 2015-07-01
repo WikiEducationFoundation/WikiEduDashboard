442b3933e5b9dac229f44ed7d2802a510ccb3b78
Sage Ross <ragesoss@gmail.com>
2015-07-01 16:27:39 -0700

    Extend admin role spec to cover Submitted and Unsubmitted courses

--- spec/features/admin_role_spec.rb
+++ spec/features/admin_role_spec.rb
@@ -19,12 +19,13 @@ describe 'Admin users', type: :feature, js: true do
   end
 
   before :each do
-    create(:cohort,
-           id: 1,
-           title: 'Fall 2015')
+    create(:user,
+           id: 100,
+           wiki_id: 'Professor Sage')
+
     create(:course,
            id: 10001,
-           title: 'Course',
+           title: 'My Submitted Course',
            school: 'University',
            term: 'Term',
            slug: 'University/Course_(Term)',
@@ -33,18 +34,44 @@ describe 'Admin users', type: :feature, js: true do
            passcode: 'passcode',
            start: '2015-01-01'.to_date,
            end: '2020-01-01'.to_date)
-    create(:user,
-           id: 100,
-           wiki_id: 'Professor Sage')
     create(:courses_user,
            user_id: 100,
            course_id: 10001,
            role: 1)
+
+    create(:course,
+           id: 10002,
+           title: 'My Unsubmitted Course',
+           school: 'University',
+           term: 'Term',
+           slug: 'University/Course_(Term)',
+           submitted: 0,
+           listed: true,
+           passcode: 'passcode',
+           start: '2015-01-01'.to_date,
+           end: '2020-01-01'.to_date)
+    create(:courses_user,
+           user_id: 100,
+           course_id: 10002,
+           role: 1)
+
+    create(:cohort,
+           id: 1,
+           title: 'Fall 2015')
     user = create(:admin,
                   id: 200,
                   wiki_token: 'foo',
                   wiki_secret: 'bar')
     login_as(user, scope: :user)
+  end
+
+  describe 'visiting the homepage' do
+    it 'should see submitted courses awaiting approval' do
+      visit root_path
+      sleep 1
+      expect(page).to have_content 'Submitted Courses'
+      expect(page).to have_content 'My Submitted Course'
+    end
   end
 
   describe 'adding a course to cohort' do
@@ -64,6 +91,19 @@ describe 'Admin users', type: :feature, js: true do
       sleep 1
 
       expect(page).to have_content 'Your course has been published'
+
+      visit root_path
+      sleep 1
+      expect(page).not_to have_content 'Submitted Courses'
+    end
+  end
+
+  describe 'visiting the None cohort' do
+    it 'should see unsubmitted courses' do
+      visit '/courses?cohort=none'
+      sleep 1
+      expect(page).to have_content 'Unsubmitted Courses'
+      expect(page).to have_content 'My Unsubmitted Course'
     end
   end
 
