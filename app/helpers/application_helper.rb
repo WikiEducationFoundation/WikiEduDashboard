f9b506f31d28bdc14ba725f910222474ec85240e
Sage Ross <ragesoss@gmail.com>
2015-07-15 18:39:24 -0700

    Revert "Make logo and favicon configurable"
    This reverts commit f744f9916cec65a8de8858d6bebea18f8bea7b00.
    
    This doesn't quite work yet (although for reasons I can't figure out, it passed on travis after I added the relevant environment variables).

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,3 @@
 #= Root-level helpers
 module ApplicationHelper
-  def logo_tag
-    logo_path = "/images/" + Figaro.env.logo_file
-    image_tag logo_path
-  end
-
-  def logo_favicon_tag
-    if Rails.env == 'development' || Rails.env == 'developmentcp'
-      favicon_path = "/images/" + Figaro.env.favicon_dev_file
-    else
-      favicon_path = "/images/" + Figaro.env.favicon_file
-    end
-
-    favicon_link_tag favicon_path
-  end
 end
