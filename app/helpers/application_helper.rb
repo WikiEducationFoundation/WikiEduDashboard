73ecc802b1e9933e226c8a3bce03ae993d5f03b4
Nate Bailey <nate@wintr.us>
2015-07-20 09:13:40 -0700

    Fix string concatenation formatting Enforce optional title rendering on markdown links Resolves #209

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,15 @@
 #= Root-level helpers
 module ApplicationHelper
   def logo_tag
-    logo_path = "/images/" + Figaro.env.logo_file
+    logo_path = "/images/#{Figaro.env.logo_file}"
     image_tag logo_path
   end
 
   def logo_favicon_tag
     if Rails.env == 'development' || Rails.env == 'developmentcp'
-      favicon_path = "/images/" + Figaro.env.favicon_dev_file
+      favicon_path = "/images/#{Figaro.env.favicon_dev_file}"
     else
-      favicon_path = "/images/" + Figaro.env.favicon_file
+      favicon_path = "/images/#{Figaro.env.favicon_file}"
     end
 
     favicon_link_tag favicon_path
