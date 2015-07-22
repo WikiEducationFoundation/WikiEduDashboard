5d9d926efa1e280af2dbe433cab9e758e283783c
Sage Ross <ragesoss@gmail.com>
2015-07-22 14:50:41 -0700

    Fix admin_role_spec.
    This approach of searching for the button text instead of counting the all the buttons with the relevant class, would probably help the rest of the feature tests less brittle.

--- spec/features/admin_role_spec.rb
+++ spec/features/admin_role_spec.rb
@@ -70,7 +70,7 @@ describe 'Admin users', type: :feature, js: true do
       sleep 1
 
       # Edit details and add cohort
-      page.all('.button.dark')[1].click
+      click_button('Edit Details')
       page.all('.button.border.plus')[4].click
       find('.pop input', visible: true).set('Fall 2015')
       find('.pop button', visible: true).click
