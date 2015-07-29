01a3f180fdc8c1233a0f181f896a9088d3d080b1
Nate Bailey <nate@wintr.us>
2015-07-29 13:19:57 -0700

    Update course submitted check to match `submitted` column type (boolean) Resolves #232

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -121,7 +121,7 @@ class CoursesController < ApplicationController
     params = {}
     params['course'] = course_params
 
-    if !(@course.submitted == 1) && params['course']['submitted'] == true
+    if !@course.submitted && params['course']['submitted'] == true
       instructor = @course.instructors.first
       WikiEdits.announce_course(@course, current_user, instructor)
     end
