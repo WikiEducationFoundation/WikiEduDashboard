d194efdd040358bc850fbfb32ed909de0fcf8632
Sage Ross <ragesoss@gmail.com>
2015-08-06 11:19:25 -0700

    Fix course creation spec
    A recent change made the validation css more specific. Now the spec will look for that.

--- spec/features/course_creation_spec.rb
+++ spec/features/course_creation_spec.rb
@@ -77,9 +77,9 @@ describe 'New course creation and editing', type: :feature do
       # If we click before filling out all require fields, only the invalid
       # fields get restyled to indicate the problem.
       find('button.dark').click
-      expect(find('#course_title')['class']).not_to eq('invalid')
-      expect(find('#course_school')['class']).to eq('invalid')
-      expect(find('#course_term')['class']).to eq('invalid')
+      expect(find('#course_title')['class']).not_to eq('invalid title')
+      expect(find('#course_school')['class']).to eq('invalid school')
+      expect(find('#course_term')['class']).to eq('invalid term')
 
       # Now we fill out all the fields and continue.
       find('#instructor_name').set('Mr. Capybara')
