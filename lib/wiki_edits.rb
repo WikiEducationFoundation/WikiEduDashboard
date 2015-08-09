833ddd09132c1297e1be9a15397f83e2eed051a2
Sage Ross <ragesoss@gmail.com>
2015-08-09 15:59:15 -0700

    Simplify the logic of WikiEdits a little more.
    This will no longer remove the entire line when an assignment is being deleted, it will just remove the tag but (in most cases) leavea  blank line. I think this is the better behavior, in case the removed template is not actually on its own line.

--- lib/wiki_edits.rb
+++ lib/wiki_edits.rb
@@ -186,13 +186,8 @@ class WikiEdits
     # Check for existing tags and replace
     old_tag_ex = "{{course assignment | course = #{course_page}"
     new_tag_ex = "{{#{dashboard_url} assignment | course = #{course_page}"
-    if new_tag.blank?
-      page_content.gsub!(/#{Regexp.quote(old_tag_ex)}[^\}]*\}\}[\n]?/, new_tag)
-      page_content.gsub!(/#{Regexp.quote(new_tag_ex)}[^\}]*\}\}[\n]?/, new_tag)
-    else
-      page_content.gsub!(/#{Regexp.quote(old_tag_ex)}[^\}]*\}\}/, new_tag)
-      page_content.gsub!(/#{Regexp.quote(new_tag_ex)}[^\}]*\}\}/, new_tag)
-    end
+    page_content.gsub!(/#{Regexp.quote(old_tag_ex)}[^\}]*\}\}/, new_tag)
+    page_content.gsub!(/#{Regexp.quote(new_tag_ex)}[^\}]*\}\}/, new_tag)
 
     # Add new tag at top (if there wasn't an existing tag already)
     if !page_content.include?(new_tag) && !new_tag.blank?
