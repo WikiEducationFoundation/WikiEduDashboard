38579b88e6f400ba50a6089953b06528a50c30e2
Nick Cox <nick@nickcox.me>
2015-08-28 11:43:13 -0700

    Styling for DYK articles table

--- lib/revision_analytics_service.rb
+++ lib/revision_analytics_service.rb
@@ -1,7 +1,11 @@
 class RevisionAnalyticsService
 
   def self.dyk_eligible
-    Article.joins(:revisions).includes(:revisions).joins(:courses).where{ revisions.wp10 > 40 }.where{(namespace == 118) | ((namespace == 2) & (title !~ '%/%'))}
+    Article
+      .eager_load(:revisions)
+      .eager_load(:courses)
+      .where{ revisions.wp10 > 40 }
+      .where{(namespace == 118) | ((namespace == 2) & (title !~ '%/%'))}
   end
 
 end
