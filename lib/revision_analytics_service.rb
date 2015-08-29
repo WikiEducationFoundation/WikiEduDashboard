328b9f9256e782b13b030313efa27411e453ea3b
Sage Ross <ragesoss@gmail.com>
2015-08-28 21:54:25 -0700

    One my try at a DYK query that gets the articles we want

--- lib/revision_analytics_service.rb
+++ lib/revision_analytics_service.rb
@@ -1,14 +1,15 @@
 class RevisionAnalyticsService
-
   def self.dyk_eligible
     current_course_ids = Course.current.pluck(:id)
     current_student_ids = CoursesUsers
                           .where(course_id: current_course_ids, role: 0)
                           .pluck(:user_id)
-    Article
-      .eager_load(:revisions)
-      .where { revisions.wp10 > 30 }
-      .where(revisions: { user_id: current_student_ids })
-      .where { (namespace == 118) | ((namespace == 2) & ('title like %/%')) }
+    good_student_revisions = Revision
+                             .where(user_id: current_student_ids)
+                             .where('wp10 > ?', 30)
+    good_article_ids = good_student_revisions.pluck(:article_id)
+    good_drafts = Article.where(id: good_article_ids)
+                 .where { (namespace == 118) | ((namespace == 2) & ('title like %/%')) }
+    good_drafts
   end
 end
