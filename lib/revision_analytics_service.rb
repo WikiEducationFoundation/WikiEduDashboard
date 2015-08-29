7863166172a2cdb0143931b2c6c306f547e88dca
Sage Ross <ragesoss@gmail.com>
2015-08-28 21:22:34 -0700

    Update query for DYK feed
    ArticlesCourses generally only get created for mainspace pages, so we can't use them as an indicator that a page is connected to a current course.

--- lib/revision_analytics_service.rb
+++ lib/revision_analytics_service.rb
@@ -1,12 +1,14 @@
 class RevisionAnalyticsService
 
   def self.dyk_eligible
+    current_course_ids = Course.current.pluck(:id)
+    current_student_ids = CoursesUsers
+                          .where(course_id: current_course_ids, role: 0)
+                          .pluck(:user_id)
     Article
       .eager_load(:revisions)
-      .eager_load(:courses)
-      .references(:all)
-      .where{ revisions.wp10 > 5 }
-      .where{(namespace == 118) | ((namespace == 2) & (title !~ '%/%'))}
+      .where { revisions.wp10 > 30 }
+      .where(revisions: { user_id: current_student_ids })
+      .where { (namespace == 118) | ((namespace == 2) & ('title like %/%')) }
   end
-
 end
