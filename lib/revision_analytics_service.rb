c4ae2ea7911f7d57240599fd04fde9334ab068b2
Nick Cox <nick@nickcox.me>
2015-09-01 11:24:29 -0700

    refactor revision analytics service (closes #344)

--- lib/revision_analytics_service.rb
+++ lib/revision_analytics_service.rb
@@ -1,22 +1,11 @@
 class RevisionAnalyticsService
   def self.dyk_eligible
-    current_course_ids = Course.current.pluck(:id)
-    current_student_ids = CoursesUsers
-                          .where(course_id: current_course_ids, role: 0)
-                          .pluck(:user_id)
-    good_student_revisions = Revision
-                             .where(user_id: current_student_ids)
-                             .where('wp10 > ?', 50)
-    good_article_ids = good_student_revisions.pluck(:article_id)
-    good_user_space = Article.where(id: good_article_ids)
-                             .where(namespace: 2)
-                             .where('title LIKE ?', '%/%')
-                             .pluck(:id)
-    good_draft_space = Article.where(id: good_article_ids)
-                              .where(namespace: 118)
-                              .pluck(:id)
-
-    good_drafts = Article.where(id: good_draft_space + good_user_space)
-    good_drafts
+    Article
+      .joins(:revisions)
+      .joins(courses: { courses_users: :user })
+      .references(:all)
+      .merge(Course.current)
+      .where{ revisions.wp10 > 50 }
+      .where{(namespace == 118) | ((namespace == 2) & (title =~ '%/%'))}
   end
 end
