93510c77e540aeefce8900d618052975ffe77239
Sage Ross <ragesoss@gmail.com>
2015-09-01 17:34:11 -0700

    Use a less prolific user as the example in UploadImporter spec.
    This should reduce the amount of time it takes to pull in data during builds.

--- spec/lib/importers/upload_importer_spec.rb
+++ spec/lib/importers/upload_importer_spec.rb
@@ -5,10 +5,10 @@ describe UploadImporter do
   describe '.import_all_uploads' do
     it 'should find and record files uploaded to Commons' do
       create(:user,
-             wiki_id: 'Ragesoss')
+             wiki_id: 'Guettarda')
       VCR.use_cassette 'commons/import_all_uploads' do
         UploadImporter.import_all_uploads(User.all)
-        expect(CommonsUpload.all.count).to be > 3000
+        expect(CommonsUpload.all.count).to be > 50
       end
     end
   end
@@ -16,14 +16,14 @@ describe UploadImporter do
   describe '.update_usage_count' do
     it 'should count and record how many times files are used' do
       create(:user,
-             wiki_id: 'Ragesoss')
+             wiki_id: 'Guettarda')
       VCR.use_cassette 'commons/import_all_uploads' do
         UploadImporter.import_all_uploads(User.all)
       end
       VCR.use_cassette 'commons/update_usage_count' do
         UploadImporter.update_usage_count(CommonsUpload.all)
-        library_photo = CommonsUpload.find(4699576)
-        expect(library_photo.usage_count).to be > 1
+        peas_photo = CommonsUpload.find(543972)
+        expect(peas_photo.usage_count).to be > 1
       end
     end
   end
@@ -31,14 +31,14 @@ describe UploadImporter do
   describe '.import_urls_in_batches' do
     it 'should find and record Commons thumbnail urls' do
       create(:user,
-             wiki_id: 'Ragesoss')
+             wiki_id: 'Guettarda')
       VCR.use_cassette 'commons/import_all_uploads' do
         UploadImporter.import_all_uploads(User.all)
       end
       VCR.use_cassette 'commons/import_urls_in_batches' do
-        UploadImporter.import_urls_in_batches([CommonsUpload.find(4699576)])
-        library_photo = CommonsUpload.find(4699576)
-        expect(library_photo.thumburl[0...24]).to eq('https://upload.wikimedia')
+        UploadImporter.import_urls_in_batches([CommonsUpload.find(543972)])
+        peas_photo = CommonsUpload.find(543972)
+        expect(peas_photo.thumburl[0...24]).to eq('https://upload.wikimedia')
       end
     end
   end
