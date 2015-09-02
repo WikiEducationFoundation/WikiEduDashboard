ec348cc8a4aff1e843bb272b98eac275bb3f3f50
Sage Ross <ragesoss@gmail.com>
2015-09-01 17:47:28 -0700

    Only do a depth 1 recursive category query for CategoryImporter spec.
    Using depth 2 in the example takes too long, and depth 1 should hit all the code still.

--- spec/lib/importers/category_importer_spec.rb
+++ spec/lib/importers/category_importer_spec.rb
@@ -6,23 +6,23 @@ describe CategoryImporter do
     VCR.use_cassette 'category_importer/import' do
       CategoryImporter.import_category('Category:Dark Web')
       expect(Article.exists?(title: 'Tor_(anonymity_network)')).to be true
-      expect(Article.exists?(title: 'Guy_Fawkes_mask')).to be false # depth 2
+      expect(Article.exists?(title: 'Riseup')).to be false # depth 2
     end
   end
 
   it 'should import subcategories recursively' do
     VCR.use_cassette 'category_importer/import' do
-      CategoryImporter.import_category('Category:Dark Web', 2)
+      CategoryImporter.import_category('Category:Dark Web', 1)
       expect(Article.exists?(title: 'Tor_(anonymity_network)')) # depth 0
         .to be true
       expect(Article.exists?(title: 'Riseup')).to be true # depth 1
-      expect(Article.exists?(title: 'Guy_Fawkes_mask')).to be true # depth 2
+      # expect(Article.exists?(title: 'Guy_Fawkes_mask')).to be true # depth 2
     end
   end
 
   it 'should output filtered data about a category' do
     VCR.use_cassette 'category_importer/import' do
-      CategoryImporter.import_category('Category:Dark Web', 2)
+      CategoryImporter.import_category('Category:Dark Web', 1)
     end
 
     VCR.use_cassette 'category_importer/report_on_category' do
