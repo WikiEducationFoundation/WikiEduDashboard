e80e1d5dfee06c6e0b6a0e2c5fa0877f339bb5d0
Sage Ross <ragesoss@gmail.com>
2015-09-01 21:12:38 -0700

    Use very small category for CategoryImporter to reduce build time

--- spec/lib/importers/category_importer_spec.rb
+++ spec/lib/importers/category_importer_spec.rb
@@ -4,37 +4,43 @@ require "#{Rails.root}/lib/importers/category_importer"
 describe CategoryImporter do
   it 'should import data for articles in a category' do
     VCR.use_cassette 'category_importer/import' do
-      CategoryImporter.import_category('Category:Dark Web')
-      expect(Article.exists?(title: 'Tor_(anonymity_network)')).to be true
-      expect(Article.exists?(title: 'Riseup')).to be false # depth 2
+      CategoryImporter
+        .import_category('Category:"Crocodile" Dundee')
+      expect(Article.exists?(title: 'Michael_"Crocodile"_Dundee'))  # depth 0
+        .to be true
+      expect(Article.exists?(title: 'Crocodile_Dundee_in_Los_Angeles'))  # depth 1
+        .to be false
     end
   end
 
   it 'should import subcategories recursively' do
     VCR.use_cassette 'category_importer/import' do
-      CategoryImporter.import_category('Category:Dark Web', 1)
-      expect(Article.exists?(title: 'Tor_(anonymity_network)')) # depth 0
+      CategoryImporter
+        .import_category('Category:"Crocodile" Dundee', 1)
+      expect(Article.exists?(title: 'Michael_"Crocodile"_Dundee')) # depth 0
         .to be true
-      expect(Article.exists?(title: 'Riseup')).to be true # depth 1
-      # expect(Article.exists?(title: 'Guy_Fawkes_mask')).to be true # depth 2
+      expect(Article.exists?(title: 'Crocodile_Dundee_in_Los_Angeles'))
+        .to be true # depth 1
     end
   end
 
   it 'should output filtered data about a category' do
     VCR.use_cassette 'category_importer/import' do
-      CategoryImporter.import_category('Category:Dark Web', 1)
+      CategoryImporter
+        .import_category('Category:"Crocodile" Dundee', 1)
     end
-
     VCR.use_cassette 'category_importer/report_on_category' do
-      output = CategoryImporter.report_on_category('Category:Dark Web')
-      expect(output).to include 'Tor_(anonymity_network)'
+      output = CategoryImporter
+               .report_on_category('Category:"Crocodile" Dundee')
+      expect(output).to include 'Michael_"Crocodile"_Dundee'
     end
   end
 
   it 'should pull importin missing data for category' do
     VCR.use_cassette 'category_importer/report_on_category_incomplete' do
-      output = CategoryImporter.report_on_category('Category:Dark Web')
-      expect(output).to include 'Tor_(anonymity_network)'
+      output = CategoryImporter
+               .report_on_category('Category:"Crocodile" Dundee')
+      expect(output).to include 'Michael_"Crocodile"_Dundee'
     end
   end
 end
