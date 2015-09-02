27c7c305a1e25e239cb54fe953beda187374b0d1
Sage Ross <ragesoss@gmail.com>
2015-09-02 11:44:20 -0700

    Lower the threshold for DYK completeness scores, and make it a configuration variable

--- lib/revision_analytics_service.rb
+++ lib/revision_analytics_service.rb
@@ -1,11 +1,12 @@
 class RevisionAnalyticsService
   def self.dyk_eligible
+    wm10_limit = ENV['dyk_wp10_limit'] || 30
     Article
       .joins(:revisions)
       .joins(courses: { courses_users: :user })
       .references(:all)
       .merge(Course.current)
-      .where{ revisions.wp10 > 50 }
-      .where{(namespace == 118) | ((namespace == 2) & (title =~ '%/%'))}
+      .where { revisions.wp10 > wm10_limit }
+      .where { (namespace == 118) | ((namespace == 2) & (title =~ '%/%')) }
   end
 end
