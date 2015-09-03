33dbb064667d989e549d6b65efd5fb1a0721d093
Brian Mathews <brian@wintr.us>
2015-09-03 14:16:44 -0700

    From travis, deploy the exact commit rather than the _potentially_ changed branch

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-set :branch, 'production'
+set :branch, ENV["TRAVIS_COMMIT"] || 'production'
 set :rails_env, 'production'
 
 role :app, %w(root@dashboard.wikiedu.org)
