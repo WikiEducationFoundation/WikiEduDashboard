5ca81447c0757e70a87ecd17220bfd39403bdb58
Nick Cox <nick@nickcox.me>
2015-09-09 14:02:14 -0700

    improve tests around recent activity

--- spec/features/activity_page_spec.rb
+++ spec/features/activity_page_spec.rb
@@ -37,21 +37,60 @@ describe 'activity page', type: :feature, js: true do
       create(:revision, article_id: article.id, wp10: 50, user_id: user.id)
     end
 
-    before do
-      allow(RevisionAnalyticsService).to receive(:dyk_eligible)
-        .and_return([article])
-    end
-
     it 'should be viewable by admins' do
       within '.container .home' do
         expect(page).to have_content 'Activity'
       end
     end
 
-    it 'displays a list of DYK-eligible articles' do
-      click_link 'Recent Activity'
-      sleep 1
-      expect(page).to have_content article.title.gsub('_', ' ')
+    context 'dyk eligible' do
+      before do
+        allow(RevisionAnalyticsService).to receive(:dyk_eligible)
+          .and_return([article])
+      end
+
+      it 'displays a list of DYK-eligible articles' do
+        click_link 'Recent Activity'
+        sleep 1
+        expect(page).to have_content article.title.gsub('_', ' ')
+      end
     end
+
+    context 'suspected plagiarism' do
+      context 'no plagiarism revisions' do
+        before do
+          allow(RevisionAnalyticsService).to receive(:suspected_plagiarism)
+            .and_return([])
+        end
+
+        it 'displays a list of revisions suspected of plagiarism' do
+          view_plagiarism_page
+          assert_page_content "There are not currently any recent revisions suspected of plagiarism."
+        end
+      end
+
+      context 'no plagiarism revisions' do
+        before do
+          allow(RevisionAnalyticsService).to receive(:suspected_plagiarism)
+            .and_return([revision])
+        end
+
+        it 'displays a list of revisions suspected of plagiarism' do
+          view_plagiarism_page
+          assert_page_content article.title.gsub('_', ' ')
+        end
+      end
+    end
+  end
+
+  def view_plagiarism_page
+    click_link 'Recent Activity'
+    sleep 1
+    click_link 'Plagiarism Flag'
+    sleep 1
+  end
+
+  def assert_page_content(content)
+    expect(page).to have_content content
   end
 end
--- spec/lib/revision_analytics_service_spec.rb
+++ spec/lib/revision_analytics_service_spec.rb
@@ -2,49 +2,53 @@ require 'rails_helper'
 require "#{Rails.root}/lib/revision_analytics_service"
 
 describe RevisionAnalyticsService do
-  before do
-    # build revisions and courses and stuff
-    create(:course,
-           id: 10001,
-           start: 1.month.ago,
-           end: Time.now + 1.month)
-    create(:user,
-           id: 1,
-           wiki_id: 'Student_1')
-    create(:courses_user,
-           user_id: 1,
-           course_id: 10001,
-           role: 0)
-    create(:article,
-           id: 1,
-           title: 'Student_1/A_great_draft',
-           namespace: 2)
-    create(:revision,
-           id: 1,
-           user_id: 1,
-           article_id: 1,
-           date: 1.day.ago,
-           wp10: 60)
-    create(:article,
-           id: 2,
-           title: 'Student_1/A_poor_draft',
-           namespace: 2)
-    create(:revision,
-           id: 2,
-           user_id: 1,
-           article_id: 2,
-           date: 1.day.ago,
-           wp10: 20)
-  end
-  describe '.dyk_eligible' do
-    it 'should return relevant revisions' do
-      articles = RevisionAnalyticsService.dyk_eligible
-      expect(articles).to include Article.find(1)
+  let!(:revision)  { create(:revision,
+                             id: 1,
+                             user_id: 1,
+                             article_id: 1,
+                             date: 1.day.ago,
+                             wp10: 60,
+                             ithenticate_id: r1_id) }
+  let(:r1_id) { nil }
+
+  describe '#dyk_eligible' do
+    let!(:course)    { create(:course, id: 10001, start: 1.month.ago, end: 1.month.from_now) }
+    let!(:user)      { create(:user, id: 1, wiki_id: 'Student_1') }
+    let!(:c_user)    { create(:courses_user, user_id: 1, course_id: 10001, role: 0) }
+    let!(:article)   { create(:article, id: 1, title: 'Student_1/A_great_draft', namespace: 2) }
+    let!(:article2)  { create(:article, id: 2, title: 'Student_1/A_poor_draft', namespace: 2) }
+    let!(:revision2) { create(:revision, id: 2, user_id: 1, article_id: 2, date: 1.day.ago, wp10: 20) }
+
+    subject { described_class.dyk_eligible }
+
+    context 'sufficient wp10' do
+      it 'should return relevant revisions' do
+        expect(subject).to include(article)
+      end
     end
 
-    it 'should not return irrelevant revisions' do
-      articles = RevisionAnalyticsService.dyk_eligible
-      expect(articles).not_to include Article.find(2)
+    context 'insufficient wp10' do
+      it 'should not return irrelevant revisions' do
+        expect(subject).not_to include(article2)
+      end
+    end
+  end
+
+
+  describe '#suspected_plagiarism' do
+    subject { described_class.suspected_plagiarism }
+    context 'no ithenticate_id' do
+      let(:r1_id) { nil }
+      it 'does not include revision' do
+        expect(subject).not_to include(revision)
+      end
+    end
+
+    context 'ithenticate_id present' do
+      let(:r1_id) { 5 }
+      it 'does include revision' do
+        expect(subject).to include(revision)
+      end
     end
   end
 end
