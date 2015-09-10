bbfcb6b2abf8ff888afb9334268c7202f12fc0f5
Sage Ross <ragesoss@gmail.com>
2015-09-09 20:00:08 -0700

    Fix default for Article Finder max_wp10

--- app/controllers/article_finder_controller.rb
+++ app/controllers/article_finder_controller.rb
@@ -8,7 +8,7 @@ class ArticleFinderController < ApplicationController
     @category = params[:category]
     @depth = params[:depth].to_i
     @min_views = params[:minviews].to_i
-    @max_wp10 = params[:maxwp10].to_i
+    @max_wp10 = (params[:maxwp10] || 100).to_i
     @articles = CategoryImporter.show_category(@category, depth: @depth,
                                                           min_views: @min_views,
                                                           max_wp10: @max_wp10)
