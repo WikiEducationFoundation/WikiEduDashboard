41deb1ccbd90b8be2b3649ffe09d3b9537d4c3f8
Sage Ross <ragesoss@gmail.com>
2015-09-20 16:14:55 -0700

    Adjust unsupported browser rules to be more conservative
    Only block known incompatible browsers: IE 9 and below.

--- config/initializers/browsers.rb
+++ config/initializers/browsers.rb
@@ -1,6 +1,6 @@
 require 'browser'
-Browser.modern_rules << -> b { b.ie? && b.version.to_i >= 10 }
 
 Rails.configuration.middleware.use Browser::Middleware do
-  redirect_to '/unsupported_browser' unless browser.modern?
+  supported = !browser.ie? || browser.version.to_i >= 10
+  redirect_to '/unsupported_browser' unless supported
 end
