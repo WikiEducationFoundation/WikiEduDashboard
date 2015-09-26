308119dbd23944795f38494b0f044aa68c55ce51
Sage Ross <ragesoss@gmail.com>
2015-09-25 19:22:14 -0700

    Remove dead code from LookupsController.
    The course is not used at all, and the user method doesn't get called from anywhere. These all get called through the Lookup component, which is ultimately called from the cohort_button (for cohort lookup), the tag_button for tag lookup), and the assign_button (for article_lookup). The user lookup is not used anywhere that I can find (and the code does not get hit by the specs).

--- app/controllers/lookups_controller.rb
+++ app/controllers/lookups_controller.rb
@@ -5,13 +5,7 @@ class LookupsController < ApplicationController
   before_action :require_permissions
   respond_to :json
 
-  def fetch_course
-    return unless params.key? :course
-    @course = find_course_by_slug(params[:course])
-  end
-
   def cohort
-    fetch_course
     @model = 'cohort'
     @key = 'title'
     @values = Cohort.all.pluck(:title)
@@ -19,7 +13,6 @@ class LookupsController < ApplicationController
   end
 
   def tag
-    fetch_course
     @model = 'tag'
     @key = 'tag'
     @values = Tag.all.pluck(:tag)
@@ -27,19 +20,10 @@ class LookupsController < ApplicationController
   end
 
   def article
-    fetch_course
     @model = 'article'
     @key = 'title'
     @values = Article.live.namespace(0).select('title').pluck(:title)
     @values.map! { |a| a.gsub('_', ' ') }
-    render 'index'
-  end
-
-  def user
-    fetch_course
-    @model = 'user'
-    @key = 'wiki_id'
-    @values = User.all.select('wiki_id').pluck(:wiki_id)
     render 'index'
   end
 end
