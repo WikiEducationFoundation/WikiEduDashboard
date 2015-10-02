42f6e8f7cfb2a3a5f23cae244a6f9b0a714029d7
Nick Cox <nick@nickcox.me>
2015-10-02 13:59:24 -0700

    make routing conform to user's mental model

--- lib/training_library.rb
+++ lib/training_library.rb
@@ -5,7 +5,15 @@ class TrainingLibrary
     @@libs.any? ? @@libs : self.load
   end
 
-  def self.find(opts)
+  def self.find_library(opts)
+    library_id = opts[:library]
+    YAML.load_file("#{Rails.root}/training_content/libraries/#{library_id}.yml")
+  end
+
+  def self.find_module(opts)
+    library_id = opts[:library]
+    module_id = opts[:module]
+    YAML.load_file("#{Rails.root}/training_content/libraries/#{library_id}/modules/#{module_id}.yml")
   end
 
   private
