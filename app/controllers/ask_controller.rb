07ba12ae6d55d4d5146b65841ea1000fc8ed2ad1
Sage Ross <ragesoss@gmail.com>
2015-10-06 21:23:27 -0700

    Handle empty queries for ask form

--- app/controllers/ask_controller.rb
+++ app/controllers/ask_controller.rb
@@ -3,6 +3,11 @@ require 'uri'
 # Controller for ask.wikiedu.org search form
 class AskController < ApplicationController
   def search
+    if params[:q].blank?
+      redirect_to 'http://ask.wikiedu.org'
+      return
+    end
+
     query = URI.encode(params[:q])
     redirect_to "http://ask.wikiedu.org/questions/scope:all/sort:activity-desc/page:1/query:#{query}/"
   end
--- spec/controllers/ask_controller_spec.rb
+++ spec/controllers/ask_controller_spec.rb
@@ -6,8 +6,13 @@ describe AskController do
   end
 
   describe '#search' do
-    it 'should redirect to ask.wikiedu.org' do
+    it 'should redirect to ask.wikiedu.org for a search' do
       query = { q: 'Help! I cannot enroll!' }
+      expect(get 'search', query).to redirect_to(/.*ask\.wikiedu\.org\.*/)
+    end
+
+    it 'should redirect to ask.wikiedu.org homepage for empty query' do
+      query = { q: '' }
       expect(get 'search', query).to redirect_to(/.*ask\.wikiedu\.org.*/)
     end
   end
