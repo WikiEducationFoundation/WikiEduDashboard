a149e5bb4b57b3744a98bc0143f6ae599e3f8c73
Sage Ross <ragesoss@gmail.com>
2015-10-08 10:36:38 -0700

    Refactor AskController

--- app/controllers/ask_controller.rb
+++ app/controllers/ask_controller.rb
@@ -3,12 +3,14 @@ require 'uri'
 # Controller for ask.wikiedu.org search form
 class AskController < ApplicationController
   def search
-    if params[:q].blank?
-      redirect_to 'http://ask.wikiedu.org/questions/scope:all/sort:activity-desc/tags:student/page:1/'
-      return
-    end
+    ask_root = 'http://ask.wikiedu.org/questions/scope:all/sort:activity-desc/'
 
-    query = URI.encode(params[:q])
-    redirect_to "http://ask.wikiedu.org/questions/scope:all/sort:activity-desc/page:1/query:#{query}/"
+    if params[:q].blank?
+      # Default to the 'student' tag
+      redirect_to "#{ask_root}tags:student/page:1/"
+    else
+      query = URI.encode(params[:q])
+      redirect_to "#{ask_root}page:1/query:#{query}/"
+    end
   end
 end
