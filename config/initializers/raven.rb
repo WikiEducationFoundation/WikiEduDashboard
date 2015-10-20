da1b94dd323c6692ae6b0d0fcb77298f40d958a2
Matt Fordham <matt@wintr.us>
2015-10-20 10:50:35 -0700

    Reverted conditional loading of Raven. Oops

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,10 +1,6 @@
 require 'raven'
 
-unless Figaro.env.sentry_dsn.empty?
-
-  Raven.configure do |config|
-    config.dsn = Figaro.env.sentry_dsn
-    config.silence_ready = true
-  end
-
+Raven.configure do |config|
+  config.dsn = Figaro.env.sentry_dsn
+  config.silence_ready = true
 end
