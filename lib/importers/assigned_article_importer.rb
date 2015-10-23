d90e9f1d6c6000caea270642f896cfd79be5ee0c
Sage Ross <ragesoss@gmail.com>
2015-10-22 21:53:00 -0700

    Create importer for creating Articles based on assignment titles
    This way, we can import articles corresponding to assignments even if we don't have any revisions for them yet.
    
    TODO: add tests and incorporate this into the batch upates.

---
+++ lib/importers/assigned_article_importer.rb
@@ -0,0 +1,20 @@
+require "#{Rails.root}/lib/importers/article_importer"
+
+class AssignedArticleImporter
+  ################
+  # Entry points #
+  ################
+
+  def self.import_articles_for_assignments
+    titles = Assignment.where(article_id: nil).pluck(:article_title).uniq
+    pp titles
+    ArticleImporter.import_articles_by_title(titles)
+    new_article_titles = Article.where(namespace: 0, title: titles).pluck(:title)
+    Assignment.where(article_title: new_article_titles).each do |assignment|
+      article = Article.find_by(title: assignment.article_title)
+      next unless article.title == assignment.article_title # guard against case variants
+      assignment.article_id = article.id
+      assignment.save
+    end
+  end
+end
