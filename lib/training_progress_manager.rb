3162246bcb4af8295ee2a3b0871ab7d9209f56ff
Nick Cox <nick@nickcox.me>
2015-10-30 13:28:41 -0700

    Display completion percentage on module library

--- lib/training_progress_manager.rb
+++ lib/training_progress_manager.rb
@@ -5,7 +5,7 @@ class TrainingProgressManager
     @training_module = training_module
     @slide = slide
     if @user
-      @utm = TrainingModulesUsers.find_by(
+      @tmu = TrainingModulesUsers.find_by(
         user_id: @user.id,
         training_module_id: @training_module.id
       )
@@ -13,22 +13,31 @@ class TrainingProgressManager
   end
 
   def slide_completed?
-    last_slide = @utm.present? ? TrainingSlide.find_by(slug: @utm.last_slide_completed) : nil
+    last_slide = @tmu.present? ? TrainingSlide.find_by(slug: @tmu.last_slide_completed) : nil
     return false unless last_slide.present?
     slug_index(last_slide) >= slug_index(@slide)
   end
 
   def slide_enabled?
     return true if slide_completed? || @user.nil?
-    (@utm.nil? || @utm.last_slide_completed.nil?) && slug_index(@slide) == 0
+    (@tmu.nil? || @tmu.last_slide_completed.nil?) && slug_index(@slide) == 0
   end
 
   def module_completed?
-    @utm.completed_at.present?
+    @tmu.completed_at.present?
   end
 
   def current_slide_further_than_previous?(previous_slug)
     slug_index(@slide) > slug_index(previous_slug)
+  end
+
+  def module_progress
+    return unless @user
+    return 'Completed' if module_completed?
+    return '' unless @tmu.last_slide_completed.present?
+    quotient = (slug_index(@tmu.last_slide_completed) + 1) / @training_module.slides.length.to_f
+    percentage = (quotient * 100).round
+    "#{percentage}%"
   end
 
   private
--- spec/lib/training_progress_manager_spec.rb
+++ spec/lib/training_progress_manager_spec.rb
@@ -7,15 +7,15 @@ describe TrainingProgressManager do
   let(:slides)   { [t_module.slides.first, t_module.slides.last] }
   let(:slide)    { slides.first }
   let(:last_slide_completed) { slides.first.slug }
-  let(:utm)      { create(:training_modules_users, user_id: user.try(:id), training_module_id: t_module.id, last_slide_completed: last_slide_completed, completed_at: completed_at ) }
+  let(:tmu)      { create(:training_modules_users, user_id: user.try(:id), training_module_id: t_module.id, last_slide_completed: last_slide_completed, completed_at: completed_at ) }
   let(:completed_at) { nil }
 
-  before  { utm }
+  before  { tmu }
   subject { described_class.new(user, t_module, slide) }
 
   describe '#slide_completed?' do
-    context 'utm is nil' do
-      let(:utm) { nil }
+    context 'tmu is nil' do
+      let(:tmu) { nil }
       it 'returns false' do
         expect(subject.slide_completed?).to eq(false)
       end
@@ -51,8 +51,8 @@ describe TrainingProgressManager do
       end
     end
 
-    context 'utm is nil (user has not started module yet)' do
-      let(:utm) { nil }
+    context 'tmu is nil (user has not started module yet)' do
+      let(:tmu) { nil }
       context 'slide is first slide' do
         it 'returns true' do
           expect(subject.slide_enabled?).to eq(true)
@@ -91,16 +91,67 @@ describe TrainingProgressManager do
   end
 
   describe '#module_completed?' do
-    context 'completed_at is nil for utm' do
+    context 'completed_at is nil for tmu' do
       it 'returns false' do
         expect(subject.module_completed?).to eq(false)
       end
     end
 
-    context 'completed_at is present for utm' do
+    context 'completed_at is present for tmu' do
       let(:completed_at) { Time.now }
       it 'returns true' do
         expect(subject.module_completed?).to eq(true)
+      end
+    end
+  end
+
+  describe '#module_progress' do
+    context 'user is nil' do
+      let(:user) { nil }
+      it 'returns nil' do
+        expect(subject.module_progress).to be_nil
+      end
+    end
+
+    context 'completed' do
+      let(:completed_at) { Time.now }
+      it 'returns "completed"' do
+        expect(subject.module_progress).to eq('Completed')
+      end
+    end
+
+    context 'not started' do
+      let(:completed_at) { nil }
+      context '0 of 35' do
+        # module has 35 slides
+        let(:last_slide_completed) { nil }
+        it 'returns empty string' do
+          expect(subject.module_progress).to eq('')
+        end
+      end
+    end
+
+    context 'partial completion' do
+      context 'round down' do
+        let(:completed_at) { nil }
+        context '12 of 35' do
+          # module has 35 slides
+          let(:last_slide_completed) { t_module.slides[11].slug }
+          it 'returns 34%' do
+            expect(subject.module_progress).to eq('34%')
+          end
+        end
+      end
+
+      context 'round up' do
+        let(:completed_at) { nil }
+        context '23 of 35' do
+          # module has 35 slides
+          let(:last_slide_completed) { t_module.slides[22].slug }
+          it 'returns 66%' do
+            expect(subject.module_progress).to eq('66%')
+          end
+        end
       end
     end
   end
