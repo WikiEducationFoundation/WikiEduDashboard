f85572b0e1eec939de802986d0c7539d20770ef0
Sage Ross <ragesoss@gmail.com>
2015-11-06 21:04:29 -0800

    Extend training_tool_spec to loop through each available module.
    This will verify that all loaded modules are functional and can be completed.

--- spec/features/training_tool_spec.rb
+++ spec/features/training_tool_spec.rb
@@ -1,34 +1,10 @@
 require 'rails_helper'
 require "#{Rails.root}/lib/training_module"
 
-def check_slide_contents(slide, slide_number, slide_count)
-  expect(page).to have_content training_module.slides[slide_number].title
-  expect(page).to have_content "Page #{slide_number + 1} of #{slide_count}"
-  pp "slide #{slide_number} looks good"
-end
-
-def proceed_to_next_slide
-  button = page.first('button.ghost-button')
-  find_correct_answer_by_trial_and_error unless button.nil?
-  pp "ready to move on"
-  click_link 'Next Page'
-end
-
-def find_correct_answer_by_trial_and_error
-  correct_answer_found = false
-  (1..10).each do |current_answer|
-    pp "trying answer #{current_answer}"
-    page.all('input')[current_answer].click
-    click_button 'Check Answer'
-    next_button = page.first('a.slide-nav.btn.btn-primary')
-    return unless next_button['disabled'] == 'true'
-  end
-end
-
 describe 'A training module', type: :feature, js: true do
   let(:cohort) { create(:cohort) }
   let(:user)   { create(:user, id: 1) }
-  let(:training_module) { TrainingModule.find(2) } # Policies and Guidelines module
+  let(:module_2) { TrainingModule.find(2) } # Policies and Guidelines module
 
   before do
     login_as(user, scope: :user)
@@ -37,28 +13,28 @@ describe 'A training module', type: :feature, js: true do
 
   describe 'index page' do
     before do
-      visit "/training/students/#{training_module.slug}"
+      visit "/training/students/#{module_2.slug}"
     end
 
     it 'describes the module' do
-      expect(page).to have_content training_module.name
+      expect(page).to have_content module_2.name
       expect(page).to have_content 'Estimated time to complete'
-      expect(page).to have_content training_module.estimated_ttc
+      expect(page).to have_content module_2.estimated_ttc
     end
 
     it 'renders the table of contents' do
       expect(page).to have_content 'TABLE OF CONTENTS'
-      expect(page).to have_content training_module.slides[0].title
-      expect(page).to have_content training_module.slides[-1].title
+      expect(page).to have_content module_2.slides[0].title
+      expect(page).to have_content module_2.slides[-1].title
     end
 
     it 'lets the user start the module' do
       click_link 'Start'
-      slide_count = training_module.slides.count
-      #expect(page).to have_content "Page 1 of #{slide_count}"
+      slide_count = module_2.slides.count
+      expect(page).to have_content "Page 1 of #{slide_count}"
       expect(TrainingModulesUsers.find_by(
-        user_id: user.id,
-        training_module_id: training_module.id
+               user_id: user.id,
+               training_module_id: module_2.id
       )).not_to be_nil
     end
 
@@ -71,21 +47,50 @@ describe 'A training module', type: :feature, js: true do
   end
 
   describe 'slide sequence' do
-    it 'lets the user go from start to finish' do
-      slide_count = training_module.slides.count
-
-      visit "/training/students/#{training_module.slug}"
-      click_link 'Start'
-      training_module.slides.each_with_index do |slide, i|
-        pp "slide #{i}"
-        check_slide_contents(slide, i, slide_count)
-        unless i == slide_count - 1
-          proceed_to_next_slide
-          next
-        end
-        click_link 'Done!'
-        expect(TrainingModulesUsers.last.completed_at).not_to be_nil
+    it 'lets the user go from start to finish for every module' do
+      module_ids = TrainingModule.all.map(&:id)
+      module_ids.each do |module_id|
+        training_module = TrainingModule.find(module_id)
+        go_through_module_from_start_to_finish(training_module)
       end
     end
+  end
+end
+
+def go_through_module_from_start_to_finish(training_module)
+  slide_count = training_module.slides.count
+  visit "/training/students/#{training_module.slug}"
+  click_link 'Start'
+  training_module.slides.each_with_index do |slide, i|
+    check_slide_contents(slide, i, slide_count)
+    unless i == slide_count - 1
+      proceed_to_next_slide
+      next
+    end
+    click_link 'Done!'
+    expect(TrainingModulesUsers.find_by(
+      user_id: 1,
+      training_module_id: training_module.id
+    ).completed_at).not_to be_nil
+  end
+end
+
+def check_slide_contents(slide, slide_number, slide_count)
+  expect(page).to have_content slide.title
+  expect(page).to have_content "Page #{slide_number + 1} of #{slide_count}"
+end
+
+def proceed_to_next_slide
+  button = page.first('button.ghost-button')
+  find_correct_answer_by_trial_and_error unless button.nil?
+  click_link 'Next Page'
+end
+
+def find_correct_answer_by_trial_and_error
+  (1..10).each do |current_answer|
+    page.all('input')[current_answer].click
+    click_button 'Check Answer'
+    next_button = page.first('a.slide-nav.btn.btn-primary')
+    break unless next_button['disabled'] == 'true'
   end
 end
