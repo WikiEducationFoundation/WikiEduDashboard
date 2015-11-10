0631cf1707a0bb8c2405c60e87c83e459db8579f
Brian Mathews <brian@wintr.us>
2015-11-09 17:51:25 -0800

    Add target destination for tar x

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -2,7 +2,7 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ]
   then
 
     openssl aes-256-cbc -K $encrypted_f7a019a5ba96_key -iv $encrypted_f7a019a5ba96_iv -in config/deploy-keys.tar.enc -out config/deploy-keys.tar -d
-    tar xvf config/deploy-keys.tar
+    tar xvf config/deploy-keys.tar -C config
     chmod 600 config/github_deploy
     chmod 600 config/travis_deploy
     eval `ssh-agent -s`
