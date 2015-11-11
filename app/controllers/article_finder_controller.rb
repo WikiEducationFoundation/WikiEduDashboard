3629a47985f00d4989b90d75f5ccf1d3363d071e
Sage Ross <ragesoss@gmail.com>
2015-11-10 20:57:22 -0800

    Fix default values for article_finder
    It was setting the default completeness to zero, which would then return no results.

--- app/controllers/article_finder_controller.rb
+++ app/controllers/article_finder_controller.rb
@@ -5,6 +5,9 @@ class ArticleFinderController < ApplicationController
   DEFAULT_MAX_WP10_SCORE = 100
 
   def index
+    @depth ||= 0
+    @min_views ||= 0
+    @max_wp10 ||= DEFAULT_MAX_WP10_SCORE
   end
 
   def results
@@ -14,7 +17,7 @@ class ArticleFinderController < ApplicationController
     cat_name = 'Category:' + @category
     @depth = params[:depth].to_i
     @min_views = params[:minviews].to_i
-    @max_wp10 = (params[:maxwp10] || DEFAULT_MAX_WP10_SCORE).to_i
+    @max_wp10 = params[:maxwp10].to_i
     @articles = CategoryImporter.show_category(cat_name, depth: @depth,
                                                          min_views: @min_views,
                                                          max_wp10: @max_wp10)
