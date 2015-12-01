0593ffb2bf9b5ec90a90bfeadff913a92beba848
Brian Mathews <brian@wintr.us>
2015-11-30 16:34:18 -0800

    home_path -> root_path

--- app/controllers/onboarding_controller.rb
+++ app/controllers/onboarding_controller.rb
@@ -6,13 +6,13 @@ class OnboardingController < ApplicationController
   def index
     # Require authentication
     if !user_signed_in?
-      redirect_to home_path
+      redirect_to root_path
       return
     end
 
     # Redirect to dashboard if they're already onboarded
     if current_user.onboarded
-      redirect_to home_path
+      redirect_to root_path
     end
 
   end
