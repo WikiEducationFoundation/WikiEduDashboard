3f817612d318e987fda905756478385567a87986
Brian Mathews <brian@wintr.us>
2015-12-03 16:32:21 -0800

    Add plus icon to create course button

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,9 @@ class DashboardController < ApplicationController
   respond_to :html
 
   def index
+    require 'pry'
+    binding.pry
+
     courses = current_user.courses.current_and_future.listed
     current = courses.select { |c| c.end >= Time.now }
     past = courses.select { |c| c.end < Time.now }
