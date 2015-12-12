e58147692e217bd372362d4b14869c6dc6c88c2d
Nick Cox <nick@nickcox.me>
2015-12-11 16:24:40 -0800

    Migration to convert markdown to HTML in block content

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151115182730) do
+ActiveRecord::Schema.define(version: 20151212001317) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
