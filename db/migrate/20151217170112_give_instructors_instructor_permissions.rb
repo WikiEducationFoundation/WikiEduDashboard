d7ff9881c49615a6abd174a226c2d42e42b7ecd9
Nick Cox <nick@nickcox.me>
2015-12-17 10:28:18 -0800

    give instructors instructor permissions, not admin

--- db/migrate/20151217170112_give_instructors_instructor_permissions.rb
+++ db/migrate/20151217170112_give_instructors_instructor_permissions.rb
@@ -1,5 +1,5 @@
 class GiveInstructorsInstructorPermissions < ActiveRecord::Migration
   def change
-    User.joins(:courses_users).where(courses_users: { role: 1 }).update_all(permissions: 1)
+    User.joins(:courses_users).where(courses_users: { role: 1 }).update_all(permissions: 2)
   end
 end
