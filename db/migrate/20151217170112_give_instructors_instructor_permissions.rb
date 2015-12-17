052e1ed2d2834e1d79c9c8cf21301af03df3395d
Nick Cox <nick@nickcox.me>
2015-12-17 09:03:02 -0800

    give all instructors instructor permissions

---
+++ db/migrate/20151217170112_give_instructors_instructor_permissions.rb
@@ -0,0 +1,5 @@
+class GiveInstructorsInstructorPermissions < ActiveRecord::Migration
+  def change
+    User.joins(:courses_users).where(courses_users: { role: 1 }).update_all(permissions: 1)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151217164421) do
+ActiveRecord::Schema.define(version: 20151217170112) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",                    limit: 255
