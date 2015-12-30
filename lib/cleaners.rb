00a5e452a1473e0a790d01bdde77b1f0a5ed16ea
Sage Ross <ragesoss@gmail.com>
2015-12-30 15:29:23 -0800

    Remove logging, now that I'm confident this works.

--- lib/cleaners.rb
+++ lib/cleaners.rb
@@ -18,7 +18,6 @@ class Cleaners
       user_ids = course.students.pluck(:id)
       revisions = Revision.where(user_id: user_ids)
       ArticlesCourses.update_from_revisions revisions
-      puts( "Updated ArticlesCourses for #{user_ids.count} users in #{course.title}")
     end
   end
 
