146a6be53c5cf7ec5a65bc7ff5053d6b7c0dd088
Sage Ross <ragesoss@gmail.com>
2016-01-04 12:25:54 -0800

    Document a script for creating student username CSVs by cohort

--- lib/analytics/scripts/usernames_to_csv.rb
+++ lib/analytics/scripts/usernames_to_csv.rb
@@ -3,9 +3,19 @@
 
 require 'csv'
 
+# all the usernames
 usernames = User.joins(:courses_users).uniq.pluck(:wiki_id)
 CSV.open('/root/all_course_participants.csv', 'wb') do |csv|
   usernames.each do |username|
     csv << [username]
+  end
+end
+
+# student usernames by cohort
+Cohort.all.each do |cohort|
+  CSV.open("/root/#{cohort.slug}_students.csv", 'wb') do |csv|
+    cohort.students.each do |student|
+      csv << [student.wiki_id]
+    end
   end
 end
