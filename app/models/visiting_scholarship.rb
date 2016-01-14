343fa431302a59e9a945da6b16ab4e2595efc82c
Sage Ross <ragesoss@gmail.com>
2016-01-14 15:12:43 -0800

    Start of handling stats differently for VisitingScholarships

--- app/models/visiting_scholarship.rb
+++ app/models/visiting_scholarship.rb
@@ -1,4 +1,10 @@
 class VisitingScholarship < Course
+  has_many(:revisions, lambda do |course|
+    where('date >= ?', course.start)
+    .where('date <= ?', course.end)
+    .where(article_id: course.assignments.pluck(:article_id))
+  end, through: :students)
+
   def wiki_edits_enabled?
     false
   end
