3be73d4449dcbf9e8d8261292a1beebecec85dcc
Sage Ross <ragesoss@gmail.com>
2016-01-20 10:02:29 -0800

    Refactor AssignmentsManager
    It's too complex, and dragging down the CodeClimate score.

--- lib/assignments_manager.rb
+++ lib/assignments_manager.rb
@@ -4,35 +4,48 @@ require "#{Rails.root}/lib/utils"
 
 # Handles assignment data submitted by users
 class AssignmentsManager
+  ###############
+  # Entry point #
+  ###############
   def self.update_assignments(course, user_params, current_user)
     # Course may have been saved with no assignments, in which case there is no
     # assignment data to handle.
     return if user_params['assignments'].nil?
 
     user_params['assignments'].each do |assignment_data|
-      assignment_data['course_id'] = course.id
-      assignment_data['article_title'] =
-        Utils.format_article_title(assignment_data['article_title'])
-      assigned = Article.find_by(title: assignment_data['article_title'], namespace: 0)
-      if assigned
-        # We double check that the titles are equal to avoid false matches of case variants.
-        # We can revise this once the database is set to use case-sensitive collation.
-        assignment_data['article_id'] =
-          assigned.id if assigned.title == assignment_data['article_title']
-      end
-
-      assignment = nil
-      assignment = Assignment.find_by(id: assignment_data['id']) if assignment_data['id']
-      update_assignment assignment_data
-
-      WikiCourseEdits.new(action: :remove_assignment,
-                          course: course,
-                          current_user: current_user,
-                          assignment: assignment) if assignment_data['deleted'] && assignment
+      handle_assignment_data(course, assignment_data, current_user)
     end
 
     WikiCourseEdits.new(action: :update_assignments, course: course, current_user: current_user)
     WikiCourseEdits.new(action: :update_course, course: course, current_user: current_user)
+  end
+
+  ####################
+  # Internal methods #
+  ####################
+  def self.handle_assignment_data(course, assignment_data, current_user)
+    assignment_data['course_id'] = course.id
+    assignment_data['article_title'] = Utils.format_article_title(assignment_data['article_title'])
+
+    assigned = Article.find_by(title: assignment_data['article_title'], namespace: 0)
+    # We double check that the titles are equal to avoid false matches of case variants.
+    # We can revise this once the database is set to use case-sensitive collation.
+    if assigned && assigned.title == assignment_data['article_title']
+      assignment_data['article_id'] = assigned.id
+    end
+
+    assignment = assignment_data['id'] ? Assignment.find_by(id: assignment_data['id']) : nil
+    update_assignment assignment_data
+
+    return unless assignment_data['deleted'] && assignment
+    remove_assignment_template(course, assignment, current_user)
+  end
+
+  def self.remove_assignment_template(course, assignment, current_user)
+    WikiCourseEdits.new(action: :remove_assignment,
+                        course: course,
+                        current_user: current_user,
+                        assignment: assignment)
   end
 
   def self.update_assignment(assignment_object)
