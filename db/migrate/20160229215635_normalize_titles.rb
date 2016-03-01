d5b254efd838cb2feaebc9f2d7438248c6d1df93
Sage Ross <ragesoss@gmail.com>
2016-03-01 15:48:47 -0800

    Destroy duplicate assignments that differ by underscores

--- db/migrate/20160229215635_normalize_titles.rb
+++ db/migrate/20160229215635_normalize_titles.rb
@@ -1,6 +1,17 @@
 class NormalizeTitles < ActiveRecord::Migration
   def up
     execute "update articles set title = replace(title, ' ', '_')"
-    execute "update assignments set article_title = replace(article_title, ' ', '_')"
+    Assignment.all.each do |assignment|
+      update_assignment(assignment)
+    end
+  end
+
+  private
+
+  def update_assignment(assignment)
+    assignment.article_title = Utils.format_article_title(assignment.article_title)
+    assignment.save
+  rescue ActiveRecord::RecordNotUnique
+    assignment.destroy
   end
 end
