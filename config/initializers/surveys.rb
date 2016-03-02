1f83a693ae4ef14321a650d50ffd1e578f53597f
hudakdidit <jonathan@wintr.us>
2016-03-02 15:04:39 -0800

    Debug Heroku Deploy
    Moved monkey-patch into initializes so its not autoloader

--- config/initializers/surveys.rb
+++ config/initializers/surveys.rb
@@ -1,1 +1,8 @@
-require Rails.root.join("lib/surveys.rb")
+require 'active_support'
+require 'rapidfire'
+
+Rails.application.config.to_prepare do
+  Rapidfire::ApplicationController.class_eval do
+    layout 'surveys'
+  end
+end
