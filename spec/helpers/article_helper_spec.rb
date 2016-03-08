36b0ae1422b8f807adaabe95a33cfdac8886b9d1
Sage Ross <ragesoss@gmail.com>
2016-03-08 10:24:59 -0800

    Don't create articles in article_helpler_spec
    This is the only thing I've changed that might explain key collisions in unrelated specs.

--- spec/helpers/article_helper_spec.rb
+++ spec/helpers/article_helper_spec.rb
@@ -3,13 +3,12 @@ require 'rails_helper'
 describe ArticleHelper, type: :helper do
   before(:all) do
     # Create some articles in different namespaces
-    @article = create(:article,
-                      id: 1,
-                      title: 'Selfie',
-                      namespace: 0,
-                      views_updated_at: '2014-12-31'.to_date)
-    @sandbox = create(:article, namespace: 2, title: 'Ragesoss/sandbox')
-    @draft = create(:article, namespace: 118, title: 'My Awesome Draft!!!')
+    @article = build(:article,
+                     title: 'Selfie',
+                     namespace: 0,
+                     views_updated_at: '2014-12-31'.to_date)
+    @sandbox = build(:article, namespace: 2, title: 'Ragesoss/sandbox')
+    @draft = build(:article, namespace: 118, title: 'My_Awesome_Draft!!!')
   end
 
   describe '.article_url' do
