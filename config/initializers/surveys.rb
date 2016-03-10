f9780f43a420fdf6f75fd561144d5813a88f2b65
hudakdidit <jonathan@wintr.us>
2016-03-09 17:59:22 -0800

    Fixed QuestionGroup Delete: updated foreign_key

--- config/initializers/surveys.rb
+++ config/initializers/surveys.rb
@@ -4,7 +4,7 @@ require 'rapidfire'
 Rails.application.config.to_prepare do
 
   Rapidfire::QuestionGroup.class_eval do
-    has_and_belongs_to_many :surveys, :join_table => "surveys_question_groups", :foreign_key => "rapidfire_question_group"
+    has_and_belongs_to_many :surveys, :join_table => "surveys_question_groups", :foreign_key => "rapidfire_question_group_id"
   end
 
   Rapidfire::ApplicationController.class_eval do
