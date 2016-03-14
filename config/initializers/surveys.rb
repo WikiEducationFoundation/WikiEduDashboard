f0d8a213ebe7f63390bc080e5aaee5672e13df21
hudakdidit <jonathan@wintr.us>
2016-03-14 15:42:16 -0700

    Moved Question model scope to initializer as it is project-specific

--- config/initializers/surveys.rb
+++ config/initializers/surveys.rb
@@ -7,6 +7,12 @@ Rails.application.config.to_prepare do
     has_and_belongs_to_many :surveys, :join_table => "surveys_question_groups", :foreign_key => "rapidfire_question_group_id"
   end
 
+  Rapidfire::Question.class_eval do
+    def self.for_conditionals
+      where("conditionals IS NULL OR conditionals = ''")
+    end
+  end
+
   Rapidfire::ApplicationController.class_eval do
     layout 'surveys'
   end
