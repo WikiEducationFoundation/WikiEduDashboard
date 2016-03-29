36b80c0a076f9177bd39601271a4e044d6f41422
hudakdidit <jonathan@wintr.us>
2016-03-29 12:00:27 -0700

    Added start to send_emails_and_create_notifications method

--- app/models/survey_notification.rb
+++ app/models/survey_notification.rb
@@ -2,4 +2,11 @@ class SurveyNotification < ActiveRecord::Base
   belongs_to :courses_user
   belongs_to :survey_assignment
   belongs_to :course
+
+  def send_emails_and_create_notifications
+    @survey = self.survey_assignment.survey
+    @user = self.courses_user.user
+    @course = self.course
+    
+  end
 end
