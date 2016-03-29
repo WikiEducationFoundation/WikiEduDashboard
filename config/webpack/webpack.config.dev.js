e693b5aec65f0ebc426dee8625378824126f165e
Brian Mathews <brian@wintr.us>
2016-03-29 15:06:26 -0700

    Format webpack config

--- config/webpack/webpack.config.dev.js
+++ config/webpack/webpack.config.dev.js
@@ -4,70 +4,59 @@ var source = __dirname + "/../../app/assets/javascripts/";
 
 
 module.exports = {
-    entry: {
-        main: hotEntry(source + "main.js"),
-        survey: hotEntry(source + "surveys/survey.coffee"),
-        survey_admin: hotEntry(source + "surveys/survey-admin.coffee")
-    },
-    output: {
-        path: __dirname + "/",
-        filename: "[name].js",
-        publicPath: '/'
-    },
-    resolve: {
-        extension: ['', '.js', '.jsx', '.coffee'],
-        root: ["vendor", "node_modules"]
-    },
-    module: {
-        preLoaders: [
-            {
-                test: /\.coffee$/,
-                loader:"coffee-lint-loader",
-                exclude: [/vendor/, /node_modules/]
-            }
-        ],
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: [/vendor/, /node_modules/],
-                loader: 'babel',
-                query: {
-                    cacheDirectory: true
-                }
-            },
-            {
-                test: /\.coffee$/,
-                loaders: ["coffee-loader"]
-            },
-            {
-                test: /\.cjsx$/,
-                loaders: ['coffee', 'cjsx']
-            },
-            ,
-            {   test: /\.json$/,
-                loader: "json-loader"
-            }
-        ]
-    },
-     externals: {
-        "jquery": "jQuery",
-        "jquery": "$"
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    coffeelint: {
-        configFile: __dirname + "/coffeelint.json"
-    },
-    devtool: 'inline-source-map'
+  entry: {
+    main: hotEntry(source + "main.js"),
+    survey: hotEntry(source + "surveys/survey.coffee"),
+    survey_admin: hotEntry(source + "surveys/survey-admin.coffee")
+  },
+  output: {
+    path: __dirname + "/",
+    filename: "[name].js",
+    publicPath: '/'
+  },
+  resolve: {
+    extension: ['', '.js', '.jsx', '.coffee', '.cjsx'],
+    root: [__dirname + "vendor", __dirname + "node_modules"]
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: [/vendor/, /node_modules/],
+      loader: 'babel',
+      query: {
+        cacheDirectory: true
+      }
+    }, {
+      test: /\.coffee$/,
+      loaders: ["coffee-loader"]
+    }, {
+      test: /\.cjsx$/,
+      loaders: ['coffee', 'cjsx']
+    }, , {
+      test: /\.json$/,
+      loader: "json-loader"
+    }]
+  },
+  externals: {
+    "jquery": "jQuery",
+    "jquery": "$",
+    "i18n-js": 'I18n'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  coffeelint: {
+    configFile: __dirname + "/coffeelint.json"
+  },
+  devtool: 'inline-source-map'
 };
 
 
 
 function hotEntry(entry) {
-    return [
-        'webpack-dev-server/client?http://localhost:8080',
-        'webpack/hot/only-dev-server',
-        entry
-    ]
+  return [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    entry
+  ]
 }
