e4d60c02c7b806dfc3193fda14c643a3be33bf5f
Sage Ross <ragesoss@gmail.com>
2016-03-31 15:16:38 -0700

    Refactor CourseCloneManager

--- lib/course_clone_manager.rb
+++ lib/course_clone_manager.rb
@@ -7,15 +7,25 @@ class CourseCloneManager
 
   def clone!
     @clone = @course.deep_clone include: [{ weeks:  { blocks: :gradeable } }]
+    set_placeholder_start_and_end_dates
     sanitize_clone_info
     update_title_and_slug
-    clear_dates
+    clear_meeting_days_and_due_dates
     set_instructor
     tag_course
     return @clone
   end
 
   private
+
+  def set_placeholder_start_and_end_dates
+    # The datepickers require an ititial date, so we set these to today's date
+    today = Time.zone.today
+    @clone.start = today
+    @clone.end = today
+    @clone.timeline_start = today
+    @clone.timeline_end = today
+  end
 
   def sanitize_clone_info
     @clone.term = "CLONED FROM #{@course.term}"
@@ -25,14 +35,6 @@ class CourseCloneManager
     @clone.submitted = false
     # If a legacy course is cloned, switch the type to ClassroomProgramCourse.
     @clone.type = 'ClassroomProgramCourse' if @clone.legacy?
-
-    # The datepickers require an ititial date, so we set these to today's date
-    today = Time.zone.today
-    @clone.start = today
-    @clone.end = today
-    @clone.timeline_start = today
-    @clone.timeline_end = today
-
     @clone.save
     @clone = Course.find(@clone.id) # Re-load the course to ensure correct course type
     @clone.update_cache
@@ -45,7 +47,7 @@ class CourseCloneManager
     )
   end
 
-  def clear_dates
+  def clear_meeting_days_and_due_dates
     @clone.update_attributes(day_exceptions: '',
                              weekdays: '0000000',
                              no_day_exceptions: false)
