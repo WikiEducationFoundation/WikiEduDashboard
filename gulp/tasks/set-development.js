5be626b98d5ce19d4c81fe0a47c5421829c9ba32
hudakdidit <jonathan@wintr.us>
2016-04-01 14:30:55 -0700

    Updated Gulp Dev
    Gulp Dev runs webpack-build by default now which creates the
    fingerprinted assets and creates the rev-manifest file. The gulp-watch
    task re-runs webpack-build when any js files are changed.
    
    To run development with hot-loading (for js) and live-reloading (for
    css) run `gulp hot-dev` or `npm start`.

--- gulp/tasks/set-development.js
+++ gulp/tasks/set-development.js
@@ -2,3 +2,5 @@ import gulp from 'gulp';
 import config from '../config.js';
 
 gulp.task('set-development', () => config.development = true);
+
+gulp.task('set-watch-js', () => config.watch_js = true);
