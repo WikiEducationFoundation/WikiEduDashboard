171c27232be3614be4b1e42518b50ac1411bb965
Brian Mathews <brian@wintr.us>
2016-04-01 09:23:22 -0700

    Simplify activity table row test

--- test/components/activity/activity_table_row.jsx
+++ test/components/activity/activity_table_row.jsx
@@ -4,40 +4,36 @@ import ReactTestUtils from 'react-addons-test-utils';
 import '../../testHelper';
 
 import ActivityTableRow from '../../../app/assets/javascripts/components/activity/activity_table_row.jsx';
-import ActivityTable from '../../../app/assets/javascripts/components/activity/activity_table.cjsx';
 import { click } from '../../customUtils';
 
 describe('ActivtyTableRow', () => {
   const TestRow = ReactTestUtils.renderIntoDocument(
-    <ActivityTable
-      loading={false}
-      activity={[{ key: '1', revision_score: 0, title: 'Foobar', courses: [{ slug: 'cat' }, { slug: 'dog' }] }]}
-      headers={[{ key: '1' }]}
-      noActivityMessage="Hello world"
-    >
-      <ActivityTableRow
-        key={'23948'}
-        rowId={'675818536'}
-        title="Selfie"
-        articleUrl="https://en.wikipedia.org/wiki/Selfie"
-        author="Wavelength"
-        talkPageLink="https://en.wikipedia.org/wiki/User_talk:Wavelength"
-        diffUrl="https://en.wikipedia.org/w/index.php?title=Selfie&diff=675818536&oldid=675437996"
-        revisionDateTime="2015/08/012 9:43 pm"
-        revisionScore={61}
-      />
-    </ActivityTable>
+    <table>
+      <tbody>
+        <ActivityTableRow
+          key={'23948'}
+          rowId={'675818536'}
+          title="Selfie"
+          articleUrl="https://en.wikipedia.org/wiki/Selfie"
+          author="Wavelength"
+          talkPageLink="https://en.wikipedia.org/wiki/User_talk:Wavelength"
+          diffUrl="https://en.wikipedia.org/w/index.php?title=Selfie&diff=675818536&oldid=675437996"
+          revisionDateTime="2015/08/012 9:43 pm"
+          revisionScore={61}
+        />
+      </tbody>
+    </table>
   );
 
   it('renders a table row with activity-table-row class and closed class', () => {
-    const rows = ReactTestUtils.scryRenderedDOMComponentsWithTag(TestRow, 'tr');
-    // rows[0] is header row
-    expect(rows[1].className).to.eq('activity-table-row closed');
+    expect(TestRow.querySelectorAll('tr')[0].className).to.eq('activity-table-row closed');
   });
 
   it('changes class open to class closed when state is_open', (done) => {
-    const row = ReactTestUtils.scryRenderedDOMComponentsWithClass(TestRow, 'activity-table-row')[0];
+    const row = TestRow.querySelector('.activity-table-row');
+
     expect(row.className).to.eq('activity-table-row closed');
+
     click(row).then(r => {
       expect(r.className).to.eq('activity-table-row open');
       done();
