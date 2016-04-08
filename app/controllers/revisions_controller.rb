fbd1dc2b9ff7d784a459e8e596bfe3bffdf53ca4
Sage Ross <ragesoss@gmail.com>
2016-04-08 13:58:47 -0700

    Fix RevisionsController to rely on course.revisions scope
    This makes the list of revisions for a given user match the revisions that are used to calculate course stats. Related to #752.

--- app/controllers/revisions_controller.rb
+++ app/controllers/revisions_controller.rb
@@ -1,16 +1,16 @@
-#= Controller for revisions functionality
+#=Controller for the Revisions API.
 class RevisionsController < ApplicationController
   respond_to :json
   DEFAULT_REVISION_LIMIT = 10
 
+  # Returns revisions for a single user within the scope of a single course.
   def index
     user = User.find(params[:user_id])
     course = Course.find(params[:course_id])
-    @revisions = user.revisions
-                 .where('date >= ?', course.start)
-                 .where('date <= ?', course.end)
-                 .order('revisions.created_at DESC')
-                 .eager_load(:article)
-                 .limit(params[:limit] || DEFAULT_REVISION_LIMIT)
+
+    @revisions = course.revisions.where(user_id: user.id)
+                       .order('revisions.created_at DESC')
+                       .eager_load(:article)
+                       .limit(params[:limit] || DEFAULT_REVISION_LIMIT)
   end
 end
