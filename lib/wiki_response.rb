a2bf0afaf052d93b67028bf1c87f728c5e2e440d
Sage Ross <ragesoss@gmail.com>
2016-04-10 09:49:12 -0700

    Refactor WikiResponse

--- lib/wiki_response.rb
+++ lib/wiki_response.rb
@@ -3,25 +3,34 @@ class WikiResponse
   ###############
   # Entry point #
   ###############
-  def self.capture(response_data, opts={})
-    current_user = opts[:current_user] || {}
-    post_data = opts[:post_data]
-    type = opts[:type]
+  def self.capture(response_data, opts)
+    message = new(response_data, opts)
+    message.parse_api_response
+    message.send_to_sentry
+  end
 
-    sorting_info = parse_api_response(response_data, type, current_user)
-    Raven.capture_message sorting_info[:title],
-                          level: sorting_info[:level],
-                          tags: { username: current_user[:username],
-                                  action_type: type },
-                          extra: { response_data: response_data,
-                                   post_data: post_data,
-                                   current_user: current_user }
+  def initialize(response_data, opts={})
+    @response_data = response_data
+    @edit_data = response_data['edit']
+    @current_user = opts[:current_user] || {}
+    @post_data = opts[:post_data]
+    @type = opts[:type]
+  end
+
+  def send_to_sentry
+    Raven.capture_message @title,
+                          level: @level,
+                          tags: { username: @current_user[:username],
+                                  action_type: @type },
+                          extra: { response_data: @response_data,
+                                   post_data: @post_data,
+                                   current_user: @current_user }
   end
 
   ###################
   # Parsing methods #
   ###################
-  def self.parse_api_response(response_data, type, current_user)
+  def parse_api_response
     # A successful edit will have response data like this:
     # {"edit"=>
     #   {"result"=>"Success",
@@ -45,49 +54,55 @@ class WikiResponse
     #    "code"=>"abusefilter-warning-email",
     #    "info"=>"Hit AbuseFilter: Adding emails in articles",
     #    "warning"=>"[LOTS OF HTML WARNING TEXT]"}}
-    if response_data['error']
-      title_and_level = parse_api_error_response(response_data, type, current_user)
-    elsif response_data['edit']
-      title_and_level = parse_api_edit_response(response_data, type)
-    elsif response_data['query']
-      title = "#{type} query"
-      level = 'info'
-      title_and_level = { title: title, level: level }
+    if @response_data['error']
+      parse_api_error_response
+    elsif @edit_data
+      parse_api_edit_response
+    elsif @response_data['query']
+      parse_api_query_response
     else
-      title = "Unknown response for #{type}"
-      level = 'error'
-      title_and_level = { title: title, level: level }
+      parse_api_unknown_response
     end
-    title_and_level
   end
 
-  def self.parse_api_edit_response(response_data, type)
-    edit_data = response_data['edit']
-    if edit_data['result'] == 'Success'
-      title = "Successful #{type}"
-      level = 'info'
+  def parse_api_edit_response
+    if @edit_data['result'] == 'Success'
+      @title = "Successful #{@type}"
+      @level = 'info'
     else
-      title = "Failed #{type}"
-      title += ': CAPTCHA' if edit_data['captcha']
-      title += ': spamblacklist' if edit_data['spamblacklist']
-      code = response_data['edit']['code']
-      title += ": #{code}" if edit_data['code']
-      level = 'warning'
+      parse_failed_edit
     end
-    { title: title, level: level }
   end
 
-  def self.parse_api_error_response(response_data, type, current_user)
-    code = response_data['error']['code']
+  def parse_failed_edit
+    @title = "Failed #{@type}"
+    @title += ': CAPTCHA' if @edit_data['captcha']
+    @title += ': spamblacklist' if @edit_data['spamblacklist']
+    code = @response_data['edit']['code']
+    @title += ": #{code}" if @edit_data['code']
+    @level = 'warning'
+  end
+
+  def parse_api_error_response
+    code = @response_data['error']['code']
 
     # If the OAuth credentials are invalid, we need to flag this.
     # It gets handled by application controller.
     if code == 'mwoauth-invalid-authorization'
-      current_user.update_attributes(wiki_token: 'invalid')
+      @current_user.update_attributes(wiki_token: 'invalid')
     end
 
-    title = "Failed #{type}: #{code}"
-    level = 'warning'
-    { title: title, level: level }
+    @title = "Failed #{@type}: #{code}"
+    @level = 'warning'
+  end
+
+  def parse_api_query_response
+    @title = "#{@type} query"
+    @level = 'info'
+  end
+
+  def parse_api_unknown_response
+    @title = "Unknown response for #{@type}"
+    @level = 'error'
   end
 end
