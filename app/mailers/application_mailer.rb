47e3ebbeed40e2efa7096ccc6db1f313675efd7b
hudakdidit <jonathan@wintr.us>
2016-04-12 10:10:49 -0700

    Fix unterminated string error

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "Notifications <sender@#{ENV['SENDER_EMAIL_ADDRESS']>"
+  default from: "Notifications <sender@#{ENV['SENDER_EMAIL_ADDRESS']}>"
   layout 'mailer'
 end
