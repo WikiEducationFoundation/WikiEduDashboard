9672a0a57511d470fc1e34c243498b86ac84bffa
hudakdidit <jonathan@wintr.us>
2016-04-12 10:58:23 -0700

    Fallback to surveys@wikiedu.org email if no default sender email provided in environment vars

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV['SENDER_EMAIL_ADDRESS']
+  default from: ENV['SENDER_EMAIL_ADDRESS'] || 'surveys@wikiedu.org'
   layout 'mailer'
 end
