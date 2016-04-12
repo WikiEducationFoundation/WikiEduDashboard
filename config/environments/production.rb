241f7dbd2272592edf27e5796c5401b385bd2f75
hudakdidit <jonathan@wintr.us>
2016-04-12 09:53:59 -0700

    Use environment var for sender email address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => ENV["SMTP_HOST"] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
