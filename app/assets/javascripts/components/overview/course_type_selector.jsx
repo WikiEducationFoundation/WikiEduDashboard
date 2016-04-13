0312603c1d9bc152018add1425c1c0c4e25f8e0c
Sage Ross <ragesoss@gmail.com>
2016-04-13 12:42:50 -0700

    Add not-yet-functional course type selector
    Admins should be able to change the type of a course. This renders a pulldown to select the course type, but doesn't do anything with the choice yet.

---
+++ app/assets/javascripts/components/overview/course_type_selector.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const CourseTypeSelector = React.createClass({
+  render() {
+    const currentType = this.props.course.type;
+    let selector = currentType;
+    if (this.props.editable && currentType !== 'LegacyCourse') {
+      selector = (
+        <div className="select_wrapper">
+          <select name="course_type" defaultValue={this.props.course.type}>
+            <option value="ClassroomProgramCourse" key="ClassroomProgramCourse">Classroom Program</option>
+            <option value="VisitingScholarship" key="VisitingScholarship">Visiting Scholarship</option>
+            <option value="Editathon" key="Editathon">Edit-a-thon</option>
+            <option value="BasicCourse" key="BasicCourse">Generic Course</option>
+          </select>
+        </div>
+      );
+    }
+    return (
+      <div className="course_type_selector">
+        <span>Type: {selector}</span>
+      </div>
+    );
+  }
+});
+
+export default CourseTypeSelector;
