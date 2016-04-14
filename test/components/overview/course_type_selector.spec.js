033c644a1d0821e0e3317709bd79b4ec87bcf083
Sage Ross <ragesoss@gmail.com>
2016-04-14 10:55:32 -0700

    Fix CourseTypeSelector spec syntax
    Also, rename to .js instead of .jsx, because syntax highlighting is incorrect when it's .jsx

--- test/components/overview/course_type_selector.spec.js
+++ test/components/overview/course_type_selector.spec.js
@@ -1,20 +1,22 @@
 import '../../testHelper';
 import React from 'react';
-import ReactTestUtils, { Simulate } from 'react-addons-test-utils';
+import ReactTestUtils from 'react-addons-test-utils';
 
 import CourseTypeSelector from '../../../app/assets/javascripts/components/overview/course_type_selector.jsx';
 
-describe('CourseTypeSelector', ()=> {
+describe('CourseTypeSelector', () => {
   const course = {
     type: 'ClassroomProgramCourse'
-  }
+  };
+
   it('displays the course type when not editable', () => {
     const NonEditableCourseTypeSelector = ReactTestUtils.renderIntoDocument(
       <CourseTypeSelector
         course={course}
-        editable={false} />
+        editable={false}
+      />
     );
-    const type_listing = ReactTestUtils.findRenderedDOMComponentWithTag(NonEditableCourseTypeSelector, 'div');
-    expect(type_listing.textContent).to.eq('Type: ClassroomProgramCourse')
+    const typeListing = ReactTestUtils.findRenderedDOMComponentWithTag(NonEditableCourseTypeSelector, 'div');
+    expect(typeListing.textContent).to.eq('Type: ClassroomProgramCourse');
   });
 });
