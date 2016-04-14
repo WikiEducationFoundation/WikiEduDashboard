f1c8962b16bfe9033f2f490bec0cf2a66c0f1d07
Sage Ross <ragesoss@gmail.com>
2016-04-14 11:24:00 -0700

    Improve CourseTypeSelector test

--- test/components/overview/course_type_selector.spec.js
+++ test/components/overview/course_type_selector.spec.js
@@ -1,6 +1,7 @@
 import '../../testHelper';
 import React from 'react';
-import ReactTestUtils from 'react-addons-test-utils';
+import ReactTestUtils, { Simulate } from 'react-addons-test-utils';
+import sinon from 'sinon';
 
 import CourseTypeSelector from '../../../app/assets/javascripts/components/overview/course_type_selector.jsx';
 
@@ -18,5 +19,21 @@ describe('CourseTypeSelector', () => {
     );
     const typeListing = ReactTestUtils.findRenderedDOMComponentWithTag(NonEditableCourseTypeSelector, 'div');
     expect(typeListing.textContent).to.eq('Type: ClassroomProgramCourse');
+  });
+
+  it('calls updateCourse when selection changes', () => {
+    const spy = sinon.spy();
+    CourseTypeSelector.__Rewire__('CourseActions', {
+      updateCourse: spy
+    });
+    const EditableCourseTypeSelector = ReactTestUtils.renderIntoDocument(
+      <CourseTypeSelector
+        course={course}
+        editable={true}
+      />
+    );
+    const selector = ReactTestUtils.findRenderedDOMComponentWithTag(EditableCourseTypeSelector, 'select');
+    Simulate.change(selector, { target: { value: 'VisitingScholarship' } });
+    expect(spy.callCount).to.eq(1);
   });
 });
