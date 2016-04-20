7bca09ba161cdf746a94e1342de7aad68c240a96
hudakdidit <jonathan@wintr.us>
2016-04-20 09:25:23 -0700

    Include Course Title in Survey Email Subject

--- app/mailers/survey_mailer.rb
+++ app/mailers/survey_mailer.rb
@@ -4,6 +4,6 @@ class SurveyMailer < ApplicationMailer
     @user = notification.user
     @survey = notification.survey
     @course = notification.course
-    mail(to: @user.email, subject: 'A survey is available for your course')
+    mail(to: @user.email, subject: "A survey is available for your course. #{@course.title}")
   end
 end
