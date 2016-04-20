a9ab85afa849b79c5c1cc3f9b3e099bf14e3a85d
Sage Ross <ragesoss@gmail.com>
2016-04-19 18:36:31 -0700

    Refactor so that email preview still works

--- spec/mailers/previews/course_submission_mailer_preview.rb
+++ spec/mailers/previews/course_submission_mailer_preview.rb
@@ -1,5 +1,5 @@
 class CourseSubmissionMailerPreview < ActionMailer::Preview
   def submission
-    CourseSubmissionMailer.submission(Course.last, User.last)
+    CourseSubmissionMailer.email(Course.last, User.last)
   end
 end
