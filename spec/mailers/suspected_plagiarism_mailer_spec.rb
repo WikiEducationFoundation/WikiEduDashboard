6a581dc732e779a34fa4e5381f8426001228e53f
Sage Ross <ragesoss@gmail.com>
2016-04-21 16:42:22 -0700

    Add spec for SuspectedPlagiarismMailer

---
+++ spec/mailers/suspected_plagiarism_mailer_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe SuspectedPlagiarismMailer do
+  let(:course) { create(:course, title: 'Foo Course') }
+  let(:user) { create(:user) }
+  let!(:courses_user) { create(:courses_user, course_id: course.id, user_id: user.id) }
+  let(:content_expert) { create(:user, greeter: true, email: 'ce@wikiedu.org') }
+  let!(:courses_user2) do
+    create(:courses_user, course_id: course.id, user_id: content_expert.id,
+                          role: CoursesUsers::Roles::WIKI_ED_STAFF_ROLE)
+  end
+  let(:article) { create(:article) }
+  let(:revision) do
+    create(:revision, article_id: article.id,
+                      user_id: user.id,
+                      ithenticate_id: 10,
+                      report_url: 'https://example.com')
+  end
+
+  describe '.alert_content_expert' do
+    let(:mail) { described_class.alert_content_expert(revision) }
+    it 'delivers an email with to the content expert' do
+      allow(Features).to receive(:email?).and_return(true)
+      expect(mail.body.encoded).to match(course.title)
+      expect(mail.to).to eq([content_expert.email])
+    end
+  end
+end
