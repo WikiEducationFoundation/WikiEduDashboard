7dedd5ef93703eb008daf411e84d8dfaf7208d9b
hudakdidit <jonathan@wintr.us>
2016-04-22 15:33:16 -0700

    Style Updates to BarGraph Component

--- app/assets/javascripts/surveys/components/BarGraph.js
+++ app/assets/javascripts/surveys/components/BarGraph.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component, PropTypes } from 'react';
 import { answerTotals } from './utils';
 
@@ -6,14 +7,23 @@ export default class BarGraph extends Component {
     const answers = answerTotals(this.props);
     const totalAnswers = this.props.answers.length;
     return (
-      <div>
-        {Object.keys(answers).map((key) => {
+      <div className="results__bar-graph">
+        {_.keys(answers).map((key) => {
           const total = answers[key];
           const width = ((total / totalAnswers) * 100).toFixed(2);
-          const widthPercent = isNaN(width) ? '0%' : `${width}%`;
+          const nullWidth = isNaN(width);
+          const widthPercent = nullWidth ? '0%' : `${width}%`;
+          const valueOffset = nullWidth || width < 80 ? 2 : 20;
+          const barValuePosition = isNaN(width) ? '0%' : `${width - valueOffset}%`;
+          const greaterThanZero = !nullWidth && width > 0;
           return (<div key={key}>
-            <strong>{key}</strong>
-            <div style={{ height: 20, width: widthPercent, background: '#E5AB28' }}>{ `${widthPercent} (${total})` }</div>
+            <div className="results__bar-graph__row">
+              <div className="results__bar-graph__question"><strong>{key}</strong></div>
+              <div className="results__bar-graph__bar-row">
+                <div className={`results__bar-graph__bar ${(greaterThanZero ? 'active' : '')}`} style={{ height: 20, width: widthPercent, background: '#E5AB28', display: 'inline-block', verticalAlign: 'middle' }}></div>
+                {(greaterThanZero ? <span className="results__bar-graph__bar-value" style={{ position: 'absolute', display: 'inline-block', verticalAlign: 'middle', padding: '0 5px', left: barValuePosition }}>{widthPercent} <span className="total">({total})</span></span> : null)}
+              </div>
+            </div>
           </div>);
         })}
       </div>
