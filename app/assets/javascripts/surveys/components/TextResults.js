0a7401460dfbbfbd357dfb125f505c8f1343ad49
hudakdidit <jonathan@wintr.us>
2016-04-27 18:05:52 -0700

    TextResults component updates
    Follow-up questions are rendered in context with text results.
    Added handling for only rendering follow-up questions so this component
    can be re-used to do so for non-text questions.

--- app/assets/javascripts/surveys/components/TextResults.js
+++ app/assets/javascripts/surveys/components/TextResults.js
@@ -1,27 +1,53 @@
+import _ from 'lodash';
 import React, { Component, PropTypes } from 'react';
+const INITIAL_LIMIT = 3;
 
 export default class TextResults extends Component {
-  constructor() {
+  constructor(props) {
     super();
+    const { followUpOnly, answers_data } = props;
+    this.followUpAnswers = _.filter(props.follow_up_answers, (a) => {
+      return !_.isEmpty(a);
+    });
+    let limit = answers_data.length < INITIAL_LIMIT ? answers_data.length : INITIAL_LIMIT;
+    if (followUpOnly) {
+      limit = this.followUpAnswers.length < INITIAL_LIMIT ? this.followUpAnswers.length : INITIAL_LIMIT;
+    }
     this.state = {
-      sentiment: null
+      sentiment: null,
+      limit
     };
+    this.toggleShowMore = this._toggleShowMore.bind(this);
   }
   answersList() {
-    const answers = this.props.answers_data;
-    const list = answers.map(a => {
+    const { followUpOnly } = this.props;
+    const _answers = this.props.answers_data;
+    const answers = _answers.slice(0, this.state.limit);
+    const followUpAnswers = this.followUpAnswers;
+    const list = answers.map((a, i) => {
+      const { course } = a;
       const answer = a.data;
       const user = a.user;
       const sentiment = a.sentiment;
       let sentimentScore = null;
       if (sentiment.label !== undefined) {
-        sentimentScore = <div>sentiment:&nbsp;{sentiment.label} ({sentiment.score})</div>;
+        sentimentScore = <div className={`results__text__sentiment results__text__sentiment--${sentiment.label}`}>{sentiment.label} ({sentiment.score})</div>;
+      }
+      const noFollowUpAnswer = followUpAnswers[i] === undefined || followUpAnswers[i] === '';
+      const followUpAnswer = noFollowUpAnswer ? null : <em className="results__text__follow-up-answer">{followUpAnswers[i]}</em>;
+      const answerText = <div className="results__text__answer">{answer.answer_text}</div>;
+      if (followUpOnly && noFollowUpAnswer) {
+        return null;
       }
       return (
-        <div key={`answer_${answer.id}`} style={{ padding: 10, borderBottom: '1px solid black' }}>
-          <div>{user.username}</div>
+        <div key={`answer_${answer.id}`} className="results__text-answer">
+          <div className="results__text-answer__info">
+            <strong>{user.username}</strong>
+            <span>{course}</span>
+          </div>
           {sentimentScore}
-          <div>{answer.answer_text}</div>
+          {(followUpOnly === undefined ? answerText : null)}
+          {followUpAnswer}
         </div>
       );
     });
@@ -34,18 +60,47 @@ export default class TextResults extends Component {
       return null;
     }
     return (
-      <div>
-        <strong>Average Sentiment:</strong>&nbsp;
-        {sentiment.label} : {sentiment.average}
+      <div className={`results__text__sentiment--average results__text__sentiment--${sentiment.label}`}>
+        <strong>Average Sentiment</strong>:&nbsp;{sentiment.label} : {sentiment.average}
       </div>
     );
   }
 
+  _toggleShowMore() {
+    const { followUpOnly } = this.props;
+    const { limit } = this.state;
+    const totalAnswers = this.props.answers_data.length;
+    const followUpAnswers = this.followUpAnswers;
+    const total = followUpOnly ? followUpAnswers.length : totalAnswers;
+    const min = followUpAnswers < 3 ? followUpAnswers.length : 3;
+    this.setState({ limit: limit < total ? total : min });
+  }
+
   render() {
+    const { followUpOnly } = this.props;
+    const { limit } = this.state;
+    const followUpQuestionText = this.props.question.follow_up_question_text;
+    const followUpQuestion = followUpQuestionText === '' ? null : <em>{followUpQuestionText}</em>;
+    const answers = this.props.answers_data;
+    const followUpAnswers = this.followUpAnswers;
+    const showMore = (
+      <div className="results__text-answer__info">
+        <span className="contextual">
+          Displaying {limit} of
+          &nbsp;{(followUpOnly ? followUpAnswers.length : answers.length)}
+          &nbsp;{(followUpOnly ? 'follow-up answers' : 'answers')}.
+        </span>
+        <button type="button" onClick={this.toggleShowMore}>{(limit < answers.length ? 'Show More' : 'Show Less')}</button>
+      </div>
+    );
     return (
       <div>
-        {this.averageSentiment()}
-        {this.answersList()}
+        <div className="results__text">
+          {followUpQuestion}
+          {this.averageSentiment()}
+          {this.answersList()}
+        </div>
+        {(followUpAnswers.length > limit ? showMore : null)}
       </div>
     );
   }
@@ -53,5 +108,8 @@ export default class TextResults extends Component {
 
 TextResults.propTypes = {
   answers_data: PropTypes.array.isRequired,
-  sentiment: PropTypes.object
+  sentiment: PropTypes.object,
+  question: PropTypes.object,
+  follow_up_answers: PropTypes.array,
+  followUpOnly: PropTypes.bool
 };
