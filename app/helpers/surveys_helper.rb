47193c596f2f48e405d769c38ecb3e76e6399e1d
Sage Ross <ragesoss@gmail.com>
2016-05-02 16:24:42 -0700

    Refactor SurveysHelper

--- app/helpers/surveys_helper.rb
+++ app/helpers/surveys_helper.rb
@@ -11,11 +11,13 @@ module SurveysHelper
 
   def render_answer_form_helper(answer, form)
     partial = question_type(answer)
-    render partial: "rapidfire/answers/#{partial}", locals: { f: form, answer: answer, course: @course }
+    render partial: "rapidfire/answers/#{partial}",
+           locals: { f: form, answer: answer, course: @course }
   end
 
   def render_matrix_answer_labels(answer)
-    render partial: 'rapidfire/answers/matrix_answer_labels', locals: { answer: answer, course: @course }
+    render partial: 'rapidfire/answers/matrix_answer_labels',
+           locals: { answer: answer, course: @course }
   end
 
   def survey_preview_url(survey)
@@ -38,10 +40,11 @@ module SurveysHelper
     if options.empty?
       content_tag :div, 'remove me', data: { remove_me: true }
     else
-      select_tag(field_name, options_for_select(options), include_blank: (answer.question.multiple ? 'Select all that apply' : 'Select an option').to_s,
-                                                          required: is_required_question?(answer),
-                                                          multiple: answer.question.multiple,
-                                                          class: 'chosen-container')
+      select_tag(field_name, options_for_select(options),
+                 include_blank: answer.question.multiple ? 'Select all that apply' : 'Select an option',
+                 required: is_required_question?(answer),
+                 multiple: answer.question.multiple,
+                 class: 'chosen-container')
     end
   end
 
@@ -103,9 +106,7 @@ module SurveysHelper
     answers = options[:answers]
     index = options[:index]
     answer = answers[index]
-    answer_group_question = answer.question.validation_rules[:grouped_question]
     previous_answer = answers[index - 1]
-    previous_grouped_question = previous_answer.question.validation_rules[:grouped_question]
     if index == 0 && is_grouped_question(answer)
       return true
     elsif is_grouped_question(answer) && !is_grouped_question(previous_answer)
@@ -118,32 +119,25 @@ module SurveysHelper
   end
 
   def previous_question_in_same_group(current, previous)
-    current.question.validation_rules[:grouped_question] == previous.question.validation_rules[:grouped_question]
+    grouped_question(current) == grouped_question(previous)
   end
 
   def next_question_in_same_group(current, next_question)
     return false if next_question.nil?
-    current.question.validation_rules[:grouped_question] == next_question.question.validation_rules[:grouped_question]
+    grouped_question(current) == grouped_question(next_question)
   end
 
   def end_of_group(answer, answer_group_builder, index)
+    return false unless is_grouped_question(answer)
+
     total_questions = answer_group_builder.answers.length
-    grouped = is_grouped_question(answer)
-    is_last_question = grouped && index + 1 == total_questions
+    is_last_question = (index + 1 == total_questions)
+    return true if is_last_question
+
     next_question = answer_group_builder.answers[index + 1]
-    next_is_same_group = next_question_in_same_group(answer, next_question)
-    next_isnt_grouped = !next_question.nil? && next_question.question.validation_rules[:grouped].to_i != 1
-    if !grouped
-      return false
-    elsif is_last_question
-      return true
-    elsif !next_is_same_group
-      return true
-    elsif next_isnt_grouped
-      return true
-    else
-      false
-    end
+    return true unless next_question_in_same_group(answer, next_question)
+
+    return false
   end
 
   def question_group_locals(surveys_question_group, index, total, results = false)
@@ -195,15 +189,19 @@ module SurveysHelper
     @course = find_course_by_slug(params[:course_slug]) if has_course_slug
   end
 
-  def has_course_questions
-    !@question_group.questions.course_data_questions.empty?
+  def course_questions?(question_group)
+    !question_group.questions.course_data_questions.empty?
+  end
+
+  def survey_has_course_questions?
+    @surveys_question_groups.each do |sqg|
+      return true if course_questions?(sqg.question_group)
+    end
+    false
   end
 
   def set_course_if_survey_has_course_questions
-    course_questions = @surveys_question_groups.map do |sqg|
-      sqg.question_group.questions.course_data_questions
-    end
-    if !course_questions.empty? && !has_course_slug
+    if survey_has_course_questions? && !has_course_slug
       @courses = Course.all
       render 'course_select'
     else
@@ -212,7 +210,7 @@ module SurveysHelper
   end
 
   def set_course_if_course_questions
-    if has_course_questions && !has_course_slug
+    if course_questions?(@question_group) && !has_course_slug
       @courses = Course.all
       render 'course_select'
     else
