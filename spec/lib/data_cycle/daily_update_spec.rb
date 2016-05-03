0c92ed6e30d2d787d7f0188c4a104938022d884e
Sage Ross <ragesoss@gmail.com>
2016-05-03 16:45:59 -0700

    Move details from rake spec to constant_update_spec

--- spec/lib/data_cycle/daily_update_spec.rb
+++ spec/lib/data_cycle/daily_update_spec.rb
@@ -2,6 +2,23 @@ require 'rails_helper'
 require "#{Rails.root}/lib/data_cycle/daily_update"
 
 describe DailyUpdate do
+  describe 'on initialization' do
+    it 'calls lots of update routines' do
+      expect(AssignedArticleImporter).to receive(:import_articles_for_assignments)
+      expect(Cleaners).to receive(:rebuild_articles_courses)
+      expect(RatingImporter).to receive(:update_all_ratings)
+      expect(ArticleStatusManager).to receive(:update_article_status)
+      expect(ViewImporter).to receive(:update_all_views)
+      expect(UploadImporter).to receive(:find_deleted_files)
+      expect(UploadImporter).to receive(:import_all_uploads)
+      expect(UploadImporter).to receive(:update_usage_count)
+      expect(UploadImporter).to receive(:import_urls_in_batches)
+      expect(CacheUpdater).to receive(:update_all_caches)
+      expect(Raven).to receive(:capture_message)
+      DailyUpdate.new
+    end
+  end
+
   describe '#wait_until_constant_update_finishes' do
     it 'returns immediately if no constant update is running' do
       allow_any_instance_of(DailyUpdate).to receive(:create_pid_file)
--- spec/lib/tasks/batch_rake_spec.rb
+++ spec/lib/tasks/batch_rake_spec.rb
@@ -6,34 +6,14 @@ describe 'batch:update_constantly' do
 
   describe 'update_constantly' do
     it 'calls lots of update routines' do
-      expect(LegacyCourseImporter).to receive(:update_all_courses)
-      expect(UserImporter).to receive(:update_users)
-      expect(RevisionImporter).to receive(:update_all_revisions)
-      expect_any_instance_of(RevisionScoreImporter).to receive(:update_revision_scores)
-      expect(PlagiabotImporter).to receive(:find_recent_plagiarism)
-      expect(Article).to receive(:update_all_caches)
-      expect(ArticlesCourses).to receive(:update_all_caches)
-      expect(CoursesUsers).to receive(:update_all_caches)
-      expect(Course).to receive(:update_all_caches)
-      expect(StudentGreeter).to receive(:greet_all_ungreeted_students)
-      expect(Raven).to receive(:capture_message)
+      expect(ConstantUpdate).to receive(:new)
       subject.invoke
     end
   end
 
   describe 'update_daily' do
     it 'calls lots of update routines' do
-      expect(AssignedArticleImporter).to receive(:import_articles_for_assignments)
-      expect(Cleaners).to receive(:rebuild_articles_courses)
-      expect(RatingImporter).to receive(:update_all_ratings)
-      expect(ArticleStatusManager).to receive(:update_article_status)
-      expect(ViewImporter).to receive(:update_all_views)
-      expect(UploadImporter).to receive(:find_deleted_files)
-      expect(UploadImporter).to receive(:import_all_uploads)
-      expect(UploadImporter).to receive(:update_usage_count)
-      expect(UploadImporter).to receive(:import_urls_in_batches)
-      expect(CacheUpdater).to receive(:update_all_caches)
-      expect(Raven).to receive(:capture_message)
+      expect(DailyUpdate).to receive(:new)
       rake['batch:update_daily'].invoke
     end
   end
