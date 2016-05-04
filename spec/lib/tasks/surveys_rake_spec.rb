0075db342c953134c142959d69588917be690646
Sage Ross <ragesoss@gmail.com>
2016-05-04 10:40:25 -0700

    Clean up style violations in surveys_rake_spec

--- spec/lib/tasks/surveys_rake_spec.rb
+++ spec/lib/tasks/surveys_rake_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 # https://robots.thoughtbot.com/test-rake-tasks-like-a-boss
 
-
-describe "surveys:send_notifications" do
-  include_context "rake"
-  include_context "survey_assignment"
+describe 'surveys:send_notifications' do
+  include_context 'rake'
+  include_context 'survey_assignment'
   before do
     rake['surveys:create_notifications'].invoke
     subject.invoke
@@ -14,13 +13,13 @@ describe "surveys:send_notifications" do
     expect(ActionMailer::Base.deliveries.count).to eq(2)
   end
 
-  it "sends emails to the users email address" do
+  it 'sends emails to the users email address' do
     expect(ActionMailer::Base.deliveries.first.to.include?(@user.email)).to be(true)
     expect(ActionMailer::Base.deliveries.last.to.include?(@user2.email)).to be(true)
   end
 
-  it "sets SurveyNotification email_sent boolean attribute to true after sending" do
-    expect(SurveyNotification.where(:email_sent => false).length).to eq(0)
+  it 'sets SurveyNotification email_sent boolean attribute to true after sending' do
+    expect(SurveyNotification.where(email_sent: false).length).to eq(0)
   end
 
   it "only sends emails for notifications which haven't been dismissed" do
