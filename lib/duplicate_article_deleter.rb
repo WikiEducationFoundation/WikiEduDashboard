efe14777e71d43faff0388341feec24e6171e242
Sage Ross <ragesoss@gmail.com>
2016-05-11 16:56:56 -0700

    Refactor DuplicateArticleDeleter

--- lib/duplicate_article_deleter.rb
+++ lib/duplicate_article_deleter.rb
@@ -11,21 +11,16 @@ class DuplicateArticleDeleter
   # Entry point #
   ###############
   def resolve_duplicates(articles = nil)
-    articles ||= Article.where(deleted: false, wiki_id: @wiki.id)
-    titles = articles.map(&:title)
-    grouped = Article.where(title: titles, wiki_id: @wiki.id).group(%w(title namespace)).count
-    deleted_ids = []
-    grouped.each do |article|
-      next unless article[1] > 1
-      title = article[0][0]
-      namespace = article[0][1]
-      Rails.logger.debug "Resolving duplicates for '#{title}, ns #{namespace}'"
-      deleted_ids += delete_duplicates title, namespace
+    grouped = articles_grouped_by_title_and_namespace(articles)
+    pp grouped
+    @deleted_ids = []
+    grouped.each do |article_group|
+      delete_duplicates_in(article_group)
     end
 
     # At this stage check to see if the deleted articles' revisions still exist
     # if so, move them to their new article ID
-    limbo_revisions = Revision.where(article_id: deleted_ids)
+    limbo_revisions = Revision.where(article_id: @deleted_ids)
     RevisionImporter.new(@wiki).move_or_delete_revisions limbo_revisions
   end
 
@@ -33,6 +28,20 @@ class DuplicateArticleDeleter
   # Helper method #
   #################
   private
+
+  def articles_grouped_by_title_and_namespace(articles)
+    articles ||= Article.where(deleted: false, wiki_id: @wiki.id)
+    titles = articles.map(&:title)
+    Article.where(title: titles, wiki_id: @wiki.id).group(%w(title namespace)).count
+  end
+
+  def delete_duplicates_in(article_group)
+    return unless article_group[1] > 1
+    title = article_group[0][0]
+    namespace = article_group[0][1]
+    Rails.logger.debug "Resolving duplicates for '#{title}, ns #{namespace}'"
+    @deleted_ids += delete_duplicates(title, namespace)
+  end
 
   # Delete all articles with the given title
   # and namespace except for the most recently created
