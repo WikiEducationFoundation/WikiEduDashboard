91e2c817a2a534280db4161b0d419da42518f2cb
Sage Ross <ragesoss@gmail.com>
2016-05-13 09:13:04 -0700

    Fix case of no course for AfD alert

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -43,7 +43,7 @@ end
 # Alert for when an article has been nominated for deletion on English Wikipedia
 class ArticlesForDeletionAlert < Alert
   def main_subject
-    "#{article.title} — #{course.slug}"
+    "#{article.title} — #{course.try(:slug)}"
   end
 
   def url
