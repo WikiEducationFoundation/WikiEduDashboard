454bc93c83198ca14639ea3dccc2ea159ec595d7
Matt Fordham <matt@wintr.us>
2016-05-23 22:18:50 -0700

    Debugging AlertController specs on Travis

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -11,7 +11,10 @@ class AlertsController < ApplicationController
     @alert.user = current_user
 
     if @alert.save
+      puts "Saved alert: #{@alert}"
+      puts "Target user email: #{@alert.target_user.email}"
       @alert.email_target_user if @alert.target_user.email.present?
+      puts "Alert sent at: #{@alert.email_sent_at}"
       render json: { status: 200 }
     else
       render json: {
--- spec/controllers/alerts_controller_spec.rb
+++ spec/controllers/alerts_controller_spec.rb
@@ -10,22 +10,22 @@ describe AlertsController do
     let!(:user)             { create(:test_user) }
     let!(:target_user)      { create(:test_user) }
     let!(:courses_users)    { create(:courses_user, course_id: course.id, user_id: user.id) }
-    
+
     before do
       allow(controller).to receive(:current_user).and_return(user)
       allow(controller).to receive(:user_signed_in?).and_return(true)
       controller.instance_variable_set(:@course, course)
     end
 
-    it "should create Need Help alert and send email" do
+    it 'should create Need Help alert and send email' do
       expect_any_instance_of(AlertMailer).to receive(:alert).and_return(mock_mailer)
 
-      target_user.email = "email@email.com"
+      target_user.email = 'email@email.com'
       target_user.save
       alert_params = { message: 'hello?', target_user_id: target_user.id, course_id: course.id }
       post :create, alert_params, format: :json
 
-      # TODO - Figure out why Travis doesn't like this.
+      # TODO: Figure out why Travis doesn't like this.
       # expect(ActionMailer::Base.deliveries).not_to be_empty
       # expect(ActionMailer::Base.deliveries.last.to).to include(target_user.email)
       # expect(ActionMailer::Base.deliveries.last.subject).to include("#{user.username} / #{course.slug}")
