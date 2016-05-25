6828f256ff9333ff290fb71135acb38499e1c434
Matt Fordham <matt@wintr.us>
2016-05-25 15:02:46 -0700

    Fields for alternative project and language article assignments

--- app/assets/javascripts/utils/course_utils.js
+++ app/assets/javascripts/utils/course_utils.js
@@ -46,7 +46,7 @@ const CourseUtils = class {
     }
 
     const urlParts = /([a-z-]+)\.(wik[a-z]+)\.org\/wiki\/([^#]*)/.exec(articleTitle);
-    if (urlParts.length > 3) {
+    if (urlParts && urlParts.length > 3) {
       const title = decodeURIComponent(urlParts[3]).replace(/_/g, ' ');
       const project = urlParts[2];
       const language = urlParts[1];
@@ -58,7 +58,11 @@ const CourseUtils = class {
       };
     }
 
-    return null;
+    return {
+      title: articleTitleInput,
+      project: null,
+      language: null
+    };
   }
 
   articleFromAssignment(assignment) {
