2c979e840da289fc0b5d57b3908383cc4a145376
Sage Ross <ragesoss@gmail.com>
2016-06-08 12:30:47 -0700

    Log AskController search queries to Sentry

--- app/controllers/ask_controller.rb
+++ app/controllers/ask_controller.rb
@@ -5,6 +5,11 @@ class AskController < ApplicationController
   def search
     ask_root = 'http://ask.wikiedu.org/questions/scope:all/sort:activity-desc/'
 
+    # Logging to see how this feature gets used
+    Raven.capture_message 'ask.wikiedu.org query',
+                          level: 'info',
+                          extra: { query: params[:q], username: current_user.try(:username) }
+
     if params[:q].blank?
       # Default to the 'student' tag
       redirect_to "#{ask_root}tags:student/page:1/"
