290849c2bf054c05cb8aeed5fb5b0a6118c98e78
Sage Ross <ragesoss@gmail.com>
2016-06-13 16:47:45 -0700

    Log the source of ask queries
    We want to see which queries come from the GetHelp button vs. the nav bar.

--- app/controllers/ask_controller.rb
+++ app/controllers/ask_controller.rb
@@ -2,20 +2,27 @@ require 'uri'
 
 # Controller for ask.wikiedu.org search form
 class AskController < ApplicationController
-  def search
-    ask_root = 'http://ask.wikiedu.org/questions/scope:all/sort:activity-desc/'
+  ASK_ROOT = 'http://ask.wikiedu.org/questions/scope:all/sort:activity-desc/'.freeze
 
-    # Logging to see how this feature gets used
-    Raven.capture_message 'ask.wikiedu.org query',
-                          level: 'info',
-                          extra: { query: params[:q], username: current_user.try(:username) }
+  def search
+    log_to_sentry
 
     if params[:q].blank?
       # Default to the 'student' tag
-      redirect_to "#{ask_root}tags:student/page:1/"
+      redirect_to "#{ASK_ROOT}tags:student/page:1/"
     else
       query = URI.encode(params[:q])
-      redirect_to "#{ask_root}page:1/query:#{query}/"
+      redirect_to "#{ASK_ROOT}page:1/query:#{query}/"
     end
+  end
+
+  private
+
+  def log_to_sentry
+    # Logging to see how this feature gets used
+    Raven.capture_message 'ask.wikiedu.org query',
+                          level: 'info',
+                          tags: { 'source' => params[:source] },
+                          extra: { query: params[:q], username: current_user.try(:username) }
   end
 end
