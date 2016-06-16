06eb39455d89d9824d0500fc832feae2c8aada15
Sage Ross <ragesoss@gmail.com>
2016-06-16 11:31:22 -0700

    Add test for CourseTypeSelector timeline date logic

--- test/components/overview/course_type_selector.spec.js
+++ test/components/overview/course_type_selector.spec.js
@@ -36,4 +36,25 @@ describe('CourseTypeSelector', () => {
     Simulate.change(selector, { target: { value: 'VisitingScholarship' } });
     expect(spy.callCount).to.eq(1);
   });
+
+  it('sets timeline start and end when switching to ClassroomProgramCourse type', () => {
+    const basicCourse = {
+      type: 'BasicCourse',
+      start: '2016-01-01',
+      end: '2016-06-03'
+    };
+
+    const EditableCourseTypeSelector = ReactTestUtils.renderIntoDocument(
+      <CourseTypeSelector
+        course={basicCourse}
+        editable={true}
+      />
+    );
+    const selector = ReactTestUtils.findRenderedDOMComponentWithTag(EditableCourseTypeSelector, 'select');
+    expect(EditableCourseTypeSelector.props.course.timeline_start).to.be.undefined;
+    expect(EditableCourseTypeSelector.props.course.timeline_end).to.be.undefined;
+    Simulate.change(selector, { target: { value: 'ClassroomProgramCourse' } });
+    expect(EditableCourseTypeSelector.props.course.timeline_start).to.eq('2016-01-01');
+    expect(EditableCourseTypeSelector.props.course.timeline_end).to.eq('2016-06-03');
+  });
 });
