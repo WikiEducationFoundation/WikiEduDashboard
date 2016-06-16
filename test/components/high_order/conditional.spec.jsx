c41a6accd909c6a3707fd6ca8df9c94b21d6874c
Sage Ross <ragesoss@gmail.com>
2016-06-16 15:00:36 -0700

    Add test for conditional.jsx

---
+++ test/components/high_order/conditional.spec.jsx
@@ -0,0 +1,33 @@
+import '../../testHelper';
+// TextInput uses Conditional as a wrapper, so we'll test the intended behavior
+// through it, instead of trying to test Conditional directly or with mocks.
+import TextInput from '../../../app/assets/javascripts/components/common/text_input.cjsx';
+import React from 'react';
+import ReactTestUtils from 'react-addons-test-utils';
+
+describe('Conditional', () => {
+  it('renders the wrapped component when show is true', () => {
+    const renderer = ReactTestUtils.createRenderer();
+    renderer.render(
+      <TextInput
+        show={true}
+        value={'foo'}
+      />
+    );
+    const textInput = renderer.getRenderOutput();
+    expect(textInput.type.displayName).to.eq('TextInput');
+    expect(textInput.props.value).to.eq('foo');
+  });
+
+  it('renders nothing when show is false', () => {
+    const renderer = ReactTestUtils.createRenderer();
+    renderer.render(
+      <TextInput
+        show={false}
+        value={'foo'}
+      />
+    );
+    const textInput = renderer.getRenderOutput();
+    expect(textInput).to.be.null;
+  });
+});
