ccb070caf4ff6c1f46a2e936b2469199f65004b1
Matt Fordham <matt@wintr.us>
2016-06-21 11:23:43 -0700

    Added Caldendar and Rich Text Editor to styleguide

--- app/assets/javascripts/styleguide/styleguide.jsx
+++ app/assets/javascripts/styleguide/styleguide.jsx
@@ -2,7 +2,9 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TrixEditor from 'react-trix';
 import DatePicker from '../components/common/date_picker.jsx';
+import Calendar from '../components/common/calendar.cjsx';
 
 const StyleguideExamples = {
 
@@ -31,6 +33,25 @@ const StyleguideExamples = {
         placeholder="Please choose a date"
       />
     ), document.getElementById('date-picker-disabled'));
+  },
+
+  calendar() {
+    ReactDOM.render((
+      <Calendar
+        course={{ start: '2016-06-01', end: '2016-06-30' }}
+        editable={true}
+        calendarInstructions="Edit the dates of your course"
+      />
+    ), document.getElementById('calendar'));
+  },
+
+  richText() {
+    ReactDOM.render((
+      <TrixEditor
+        value="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        onChange={(e) => {console.log(e);}}
+      />
+    ), document.getElementById('rich-text'));
   }
 };
 
