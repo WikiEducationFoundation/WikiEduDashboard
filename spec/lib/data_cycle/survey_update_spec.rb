04e39572bea568410b1e7d0be127a52ecaa9ddf9
Sage Ross <ragesoss@gmail.com>
2016-07-05 12:18:30 -0700

    Move substantive tests of survey email behavior out of rake specs

--- spec/lib/data_cycle/survey_update_spec.rb
+++ spec/lib/data_cycle/survey_update_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 require "#{Rails.root}/lib/data_cycle/survey_update"
 
 describe SurveyUpdate do
-  describe 'on initialization' do
+  describe '#initialize' do
     it 'creates notifications, sends emails, and sends follow-ups' do
       expect(SurveyNotificationsManager).to receive(:create_notifications)
       update = SurveyUpdate.new
@@ -10,6 +10,38 @@ describe SurveyUpdate do
       expect(sentry_logs.grep(/SurveyNotifications created/).any?).to eq(true)
       expect(sentry_logs.grep(/survey invitations sent/).any?).to eq(true)
       expect(sentry_logs.grep(/survey reminders sent/).any?).to eq(true)
+    end
+  end
+
+  context 'when there are active surveys' do
+    include_context 'survey_assignment'
+    before do
+      SurveyUpdate.new
+    end
+
+    it "sends emails for all SurveyNotifications with emails that haven\'t been sent" do
+      expect(ActionMailer::Base.deliveries.count).to eq(2)
+    end
+
+    it 'sends emails to the users email address' do
+      expect(ActionMailer::Base.deliveries.first.to.include?(@user.email)).to be(true)
+      expect(ActionMailer::Base.deliveries.last.to.include?(@user2.email)).to be(true)
+    end
+
+    it 'sets SurveyNotification email_sent datetime attribute after sending' do
+      expect(SurveyNotification.where(email_sent_at: nil).length).to eq(0)
+    end
+
+    it "only sends emails for notifications which haven't been dismissed" do
+      SurveyUpdate.new
+      expect(ActionMailer::Base.deliveries.count).to eq(2)
+    end
+
+    private
+
+    def recipients(recipient_position)
+      deliveries = ActionMailer::Base.deliveries
+      recipient_position == :first ? deliveries.first.to : deliveries.last.to
     end
   end
 end
