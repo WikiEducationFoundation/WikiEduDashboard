28cd7f0afe53acb30565679e69a861281db22253
Sage Ross <ragesoss@gmail.com>
2016-07-18 16:37:05 -0700

    Refactor Survey#to_csv

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -15,44 +15,55 @@ class Survey < ActiveRecord::Base
     '--'
   end
 
+  CSV_HEADER = [
+    'Question Group',
+    'Grouped Question',
+    'Question Id',
+    'Question',
+    'Answer',
+    'Follow Up Question',
+    'Follow Up Answer',
+    'User',
+    'User Role',
+    'Course Slug',
+    'Course Cohorts',
+    'Course Tags'
+  ].freeze
+
   def to_csv
     CSV.generate do |csv|
-      csv << ['Question Group',
-              'Grouped Question',
-              'Question Id',
-              'Question',
-              'Answer',
-              'Follow Up Question',
-              'Follow Up Answer',
-              'User',
-              'User Role',
-              'Course Slug',
-              'Course Cohorts',
-              'Course Tags']
+      csv << CSV_HEADER
       rapidfire_question_groups.each do |question_group|
         question_group.questions.each do |question|
           question.answers.each do |answer|
-            course = answer.course(id)
-            course_slug = course.nil? ? nil : course.slug
-            cohorts = course.nil? ? nil : course.cohorts.collect(&:title).join(', ')
-            tags = course.nil? ? nil : course.tags.collect(&:tag).join(', ')
-            csv << [
-              question_group.name,
-              question.validation_rules[:grouped_question],
-              question.id,
-              question.question_text,
-              answer.answer_text,
-              question.follow_up_question_text,
-              answer.follow_up_answer_text,
-              answer.user.username,
-              answer.courses_user_role(id),
-              course_slug,
-              cohorts,
-              tags
-            ]
+            csv << csv_row(question_group, question, answer)
           end
         end
       end
     end
+  end
+
+  private
+
+  def csv_row(question_group, question, answer)
+    course = answer.course(id)
+    course_slug = course.nil? ? nil : course.slug
+    cohorts = course.nil? ? nil : course.cohorts.collect(&:title).join(', ')
+    tags = course.nil? ? nil : course.tags.collect(&:tag).join(', ')
+
+    [
+      question_group.name,
+      question.validation_rules[:grouped_question],
+      question.id,
+      question.question_text,
+      answer.answer_text,
+      question.follow_up_question_text,
+      answer.follow_up_answer_text,
+      answer.user.username,
+      answer.courses_user_role(id),
+      course_slug,
+      cohorts,
+      tags
+    ]
   end
 end
