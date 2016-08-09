542b44813a0b5c3f85ef5128e0c87ae4de4408d4
Sage Ross <ragesoss@gmail.com>
2016-08-09 15:52:09 -0700

    Add spec for RescueDevelopmentErrors

---
+++ spec/lib/errors/rescue_development_errors_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+require 'rails_helper'
+require "#{Rails.root}/lib/errors/rescue_development_errors"
+
+describe Errors::RescueDevelopmentErrors, type: :controller do
+  describe 'when ActionView::Template::Error is raised' do
+    controller(ApplicationController) do
+      include Errors::RescueDevelopmentErrors
+
+      def index
+        error_message = 'No such file or directory @ rb_sysopen - '\
+                        '/home/me/WikiEduDashboard/public/assets/stylesheets/rev-manifest.json'
+        raise ActionView::Template::Error.new(error_message, StandardError.new(error_message))
+      end
+    end
+
+    it 'renders an explanation with helpful advice' do
+      get :index
+      expect(response.body).to match(/gulp/)
+    end
+  end
+end
