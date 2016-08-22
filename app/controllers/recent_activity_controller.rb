044ae60577c5c9d9f3c1708fed837624d009a4df
Sage Ross <ragesoss@gmail.com>
2016-08-22 14:50:18 -0700

    Require signed in user to access plagiarism report

--- app/controllers/recent_activity_controller.rb
+++ app/controllers/recent_activity_controller.rb
@@ -6,6 +6,7 @@ class RecentActivityController < ApplicationController
   def index; end
 
   def plagiarism_report
+    require_signed_in
     ithenticate_id = params[:ithenticate_id]
     report_url = PlagiabotImporter.api_get_url(ithenticate_id: ithenticate_id)
     redirect_to report_url
