13695cc3b0b8eb03180dae60e3ea06151246c84c
Sage Ross <ragesoss@gmail.com>
2016-08-26 15:41:23 -0700

    Add uploads to course stat display
    Resolves #110

--- app/assets/javascripts/components/overview/overview_handler.jsx
+++ app/assets/javascripts/components/overview/overview_handler.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CourseStats from './course_stats.jsx';
 import AvailableActions from './available_actions.jsx';
 import Description from './description.jsx';
 import Milestones from './milestones.jsx';
@@ -10,7 +11,6 @@ import WeekStore from '../../stores/week_store.coffee';
 import ServerActions from '../../actions/server_actions.js';
 import Loading from '../common/loading.jsx';
 import CourseClonedModal from './course_cloned_modal.cjsx';
-import CourseUtils from '../../utils/course_utils.js';
 import SyllabusUpload from './syllabus-upload.jsx';
 import MyArticles from './my_articles.jsx';
 import Modal from '../common/modal.jsx';
@@ -115,36 +115,7 @@ const Overview = React.createClass({
     return (
       <section className="overview container">
         { syllabusUpload }
-        <div className="stat-display">
-          <div className="stat-display__stat" id="articles-created">
-            <div className="stat-display__value">{this.state.course.created_count}</div>
-            <small>{I18n.t('metrics.articles_created')}</small>
-          </div>
-          <div className="stat-display__stat" id="articles-edited">
-            <div className="stat-display__value">{this.state.course.edited_count}</div>
-            <small>{I18n.t('metrics.articles_edited')}</small>
-          </div>
-          <div className="stat-display__stat" id="total-edits">
-            <div className="stat-display__value">{this.state.course.edit_count}</div>
-            <small>{I18n.t('metrics.edit_count_description')}</small>
-          </div>
-          <div className="stat-display__stat tooltip-trigger" id="student-editors">
-            <div className="stat-display__value">{this.state.course.student_count}</div>
-            <small>{CourseUtils.i18n('student_editors', this.state.course.string_prefix)}</small>
-            <div className="tooltip dark" id="trained-count">
-              <h4 className="stat-display__value">{this.state.course.trained_count}</h4>
-              <p>{I18n.t('metrics.are_trained')}</p>
-            </div>
-          </div>
-          <div className="stat-display__stat" id="word-count">
-            <div className="stat-display__value">{this.state.course.word_count}</div>
-            <small>{I18n.t('metrics.word_count')}</small>
-          </div>
-          <div className="stat-display__stat" id="view-count">
-            <div className="stat-display__value">{this.state.course.view_count}</div>
-            <small>{I18n.t('metrics.view_count_description')}</small>
-          </div>
-        </div>
+        <CourseStats course={this.state.course} />
         <div className="primary">
           {primaryContent}
         </div>
