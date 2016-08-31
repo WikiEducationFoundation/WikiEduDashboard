f86cfd8b525e5466c0b51488d8cd5251ce0a9cb0
Sage Ross <ragesoss@gmail.com>
2016-08-30 22:03:40 -0700

    Turn on javascript errors in poltergeist specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -18,7 +18,7 @@ Capybara.configure do |config|
 end
 
 Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, js_errors: false)
+  Capybara::Poltergeist::Driver.new(app, js_errors: true)
 end
 
 Capybara::Webkit.configure do |config|
