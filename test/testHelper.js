9a875294a1da4980369913dad61275b6fc19f926
MusikAnimal <ziemba0123@gmail.com>
2016-10-11 00:34:50 -0400

    Add UTC time zone to JS test helper

--- test/testHelper.js
+++ test/testHelper.js
@@ -18,6 +18,7 @@ const momentRecur = require('moment-recur');
 const I18n = require('../public/assets/javascripts/i18n.js');
 const chai = require('chai');
 const sinonChai = require('sinon-chai');
+const TimeZones = { '(GMT+00:00) UTC': 'UTC' };
 
 global.$ = $;
 global._ = _;
@@ -33,6 +34,7 @@ global.chai = chai;
 global.expect = chai.expect;
 global.assert = chai.assert;
 global.Features = {};
+global.TimeZones = TimeZones;
 
 require('../public/assets/javascripts/i18n/en');
 
