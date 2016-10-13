b6c4ec61d768ba509803f874584a8441022018d7
Sage Ross <ragesoss@gmail.com>
2016-10-13 11:16:54 -0700

    Load email_template properly when editing SurveyAssignment

--- app/helpers/survey_assignments_helper.rb
+++ app/helpers/survey_assignments_helper.rb
@@ -30,9 +30,9 @@ module SurveyAssignmentsHelper
                                 selected: selected)
   end
 
-  def email_template_select(f)
+  def email_template_select(f, selected:)
     f.select :email_template,
-             options_for_select(SurveyMailer::TEMPLATES)
+             options_for_select(SurveyMailer::TEMPLATES, selected: selected)
   end
 
   def user_role(survey_assignment, total = 2)
