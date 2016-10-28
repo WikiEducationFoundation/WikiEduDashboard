2d959fa225fa7ec93584810240feadf9ce2e5ab7
Sage Ross <ragesoss@gmail.com>
2016-10-28 14:20:02 -0700

    WIP for loading diffs from wikipedia API

--- app/assets/javascripts/components/revisions/diff_viewer.jsx
+++ app/assets/javascripts/components/revisions/diff_viewer.jsx
@@ -7,9 +7,35 @@ const DiffViewer = React.createClass({
     revision: React.PropTypes.object
   },
 
+  getInitialState() {
+    return { showDiff: false };
+  },
+
+  showDiff() {
+    this.setState({ showDiff: true });
+    if (!this.state.fetched) {
+      this.fetchDiff();
+    }
+  },
+
+  hideDiff() {
+    this.setState({ showDiff: false });
+  },
+
+  fetchDiff() {
+    console.log('ohai')
+    $.ajax(
+      {
+        dataType: 'jsonp',
+        url: "https://en.wikipedia.org/w/api.php?action=compare&fromrev=139992&torev=139993&format=json",
+        success: (data) => { console.log(data); }
+      });
+  },
+
   render() {
     return (
       <div>
+        <button onClick={this.fetchDiff} className="button dark">ohai</button>
         <a className="inline" href={this.props.revision.url} target="_blank">{I18n.t('revisions.diff')}</a>
       </div>
     );
