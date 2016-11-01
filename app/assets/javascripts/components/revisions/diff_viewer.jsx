778b9527c778a9a371168cd2dc17bf545c70cdd2
Sage Ross <ragesoss@gmail.com>
2016-11-01 12:12:35 -0700

    Show diff comment, add handling for revisions with no changes

--- app/assets/javascripts/components/revisions/diff_viewer.jsx
+++ app/assets/javascripts/components/revisions/diff_viewer.jsx
@@ -33,8 +33,10 @@ const DiffViewer = React.createClass({
         dataType: 'jsonp',
         url: this.props.revision.api_url, // "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&revids=139993&rvdiffto=prev",
         success: (data) => {
+          const revisionData = data.query.pages[this.props.revision.mw_page_id].revisions[0];
           this.setState({
-            diff: data.query.pages[this.props.revision.mw_page_id].revisions[0].diff['*'],
+            diff: revisionData.diff['*'],
+            comment: revisionData.comment,
             fetched: true
           });
         }
@@ -46,21 +48,32 @@ const DiffViewer = React.createClass({
     let button;
     if (this.state.showDiff) {
       style = '';
-      button = <button onClick={this.hideDiff} className="button dark">Hide diff</button>;
+      button = <button onClick={this.hideDiff} className="button dark small">Hide</button>;
     } else {
       style = 'hidden';
-      button = <button onClick={this.showDiff} className="button dark">Show</button>;
+      button = <button onClick={this.showDiff} className="button dark small">Show</button>;
     }
     const className = `diff-viewer ${style}`;
+
+    let diff;
+    if (this.state.diff === '') {
+      diff = '<div>[no change]</div>';
+    } else {
+      diff = this.state.diff;
+    }
+
     return (
       <div>
         {button}
         <div className={className}>
           <p>
-            <a className="button dark" href={this.props.revision.url} target="_blank">View on wiki</a>
+            <a className="button dark small" href={this.props.revision.url} target="_blank">View on wiki</a>
             {button}
           </p>
-          <table><tbody dangerouslySetInnerHTML={{ __html: this.state.diff }} /></table>
+          <table>
+            <thead><p className="diff-comment">{this.state.comment}</p></thead>
+            <tbody dangerouslySetInnerHTML={{ __html: diff }} />
+          </table>
         </div>
       </div>
     );
