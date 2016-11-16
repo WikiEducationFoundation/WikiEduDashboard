fbf13bdb8a6a7436304c48881b0bc0bafde871a2
Sage Ross <ragesoss@gmail.com>
2016-11-16 15:15:18 -0800

    Further cleanup of QuestionGroupsHelper

--- app/helpers/question_groups_helper.rb
+++ app/helpers/question_groups_helper.rb
@@ -8,8 +8,8 @@ module QuestionGroupsHelper
     def initialize(question_group, course)
       @question_group = question_group
       @course = course
-      @question_group_campaigns = question_group.campaigns.pluck(:id)
-      @question_group_tags = question_group.tags.nil? ? [] : question_group.tags&.split(',')
+      @question_group_campaigns_ids = question_group.campaigns.pluck(:id)
+      @question_group_tags = question_group.tags.split(',')
     end
 
     def meets_conditions?
@@ -30,13 +30,13 @@ module QuestionGroupsHelper
     end
 
     def course_in_all_campaigns
-      return true if @question_group_campaigns.empty?
+      return true if @question_group_campaigns_ids.empty?
       return false if @course.nil?
 
-      matching = @question_group_campaigns.select do |campaign_id|
+      matching = @question_group_campaigns_ids.select do |campaign_id|
         CampaignsCourses.where(course_id: @course.id, campaign_id: campaign_id).any?
       end
-      matching.count == @question_group_campaigns.count
+      matching.count == @question_group_campaigns_ids.count
     end
   end
 end
