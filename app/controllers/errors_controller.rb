33fbe249c5ee4b8dd124878a7db86e1b21ae8842
Sage Ross <ragesoss@gmail.com>
2016-11-23 13:24:30 -0800

    Make sure login_error only renders if user isn't logged in
    Resolves #691

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -20,8 +20,12 @@ class ErrorsController < ApplicationController
   end
 
   def login_error
+    if user_signed_in?
+      redirect_to root_path
     # a status in the 500 range will automatically bypass this and
     # render internal_server_error
-    render status: 200
+    else
+      render status: 200
+    end
   end
 end
--- spec/controllers/errors_controller_spec.rb
+++ spec/controllers/errors_controller_spec.rb
@@ -8,23 +8,30 @@ describe ErrorsController do
   end
 
   describe 'file_not_found' do
-    it 'should have status 404' do
+    it 'returns status 404' do
       get :file_not_found
       expect(response.status).to eq(404)
     end
   end
 
   describe 'unprocessable' do
-    it 'should have status 422' do
+    it 'returns status 422' do
       get :unprocessable
       expect(response.status).to eq(422)
     end
   end
 
   describe 'internal_server_error' do
-    it 'should have status 500' do
+    it 'returns status 500' do
       get :internal_server_error
       expect(response.status).to eq(500)
+    end
+  end
+
+  describe 'login_error' do
+    it 'redirects to root path if user is logged in' do
+      get :login_error
+      expect(response).to redirect_to(root_path)
     end
   end
 end
