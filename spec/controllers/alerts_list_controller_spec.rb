4cea3f3329730255fd0847e4954aebe96ead1c29
Sage Ross <ragesoss@gmail.com>
2016-12-06 13:54:36 -0800

    Add test for AlertsList#show

--- spec/controllers/alerts_list_controller_spec.rb
+++ spec/controllers/alerts_list_controller_spec.rb
@@ -25,4 +25,27 @@ describe AlertsListController do
       end
     end
   end
+
+  describe '#show' do
+    let(:course) { create(:course) }
+    let(:user) { create(:user) }
+    let!(:alert) { create(:alert, type: 'NeedHelpAlert', course_id: course.id, user_id: user.id) }
+    context 'for admins' do
+      render_views
+      before { allow(controller).to receive(:current_user).and_return(admin) }
+      it 'renders the alert' do
+        get :show, params: { id: alert.id }
+        expect(response.status).to eq(200)
+        expect(response.body).to have_content(alert.type)
+      end
+    end
+
+    context 'for non-admins' do
+      before { allow(controller).to receive(:current_user).and_return(user) }
+      it 'redirects to the home page' do
+        get :show, params: { id: alert.id }
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end
 end
