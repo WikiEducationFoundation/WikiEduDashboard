9cca13fbd4ba54cdf57cbe9b5fe2d3ac5ae08fde
Sage Ross <ragesoss@gmail.com>
2016-12-09 07:03:25 -0800

    Remove Sentry noise from spec output

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -3,4 +3,6 @@ require 'raven'
 Raven.configure do |config|
   config.dsn = Figaro.env.sentry_dsn
   config.silence_ready = true
+  config.environments = %w[development staging production]
+  config.logger = Logger.new("/dev/null")
 end
