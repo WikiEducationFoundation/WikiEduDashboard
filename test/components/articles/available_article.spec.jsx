3499160bb8292e0664d42d3f0108d55d7c91e1fa
arreche <arreche@gmail.com>
2016-12-18 11:17:40 +0000

    Add a mocha test

---
+++ test/components/articles/available_article.spec.jsx
@@ -0,0 +1,41 @@
+import '../../testHelper';
+
+import React from 'react';
+
+import sinon from 'sinon';
+
+import AvailableArticle from '../../../app/assets/javascripts/components/articles/available_article.jsx';
+import NotificationActions from '../../../app/assets/javascripts/actions/notification_actions.js';
+
+describe('AvailableArticle', () => {
+  const props = {
+    course: { home_wiki: { language: 'en', project: 'wikipedia' } },
+    assignment: { article_title: 'two' },
+    current_user: {}
+  };
+
+  it('renders', () => {
+    const TestDom = ReactTestUtils.renderIntoDocument(
+      <div>
+        <AvailableArticle {...props} />
+      </div>
+    );
+    expect(TestDom.querySelector('.assignment')).to.exist;
+    expect(TestDom.textContent).to.contain('two');
+  });
+
+  it('notify when selec an article', () => {
+    const TestDom = ReactTestUtils.renderIntoDocument(
+      <div>
+        <AvailableArticle {...props} />
+      </div>
+    );
+
+    const spy = sinon.spy(NotificationActions, 'addNotification');
+
+    const select = TestDom.querySelector('button');
+    ReactTestUtils.Simulate.click(select);
+
+    expect(spy.callCount).to.eq(1);
+  });
+});
