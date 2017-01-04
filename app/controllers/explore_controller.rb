e59a6ddbc2b599b788168de74a53e77d8943566d
Sage Ross <ragesoss@gmail.com>
2017-01-04 13:46:42 -0800

    Fix error with admins viewing explore page

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -6,9 +6,11 @@ class ExploreController < ApplicationController
   def index
     # 'cohort' is the old name for campaign. We accept 'cohort' as an alternative
     # Redirect to new campaign overview page if a parameter is given, for backwards compatibility
-    campaign = params[:campaign] || params[:cohort]
-    redirect_to campaign_path(campaign) if campaign
+    campaign_param = params[:campaign] || params[:cohort]
+    redirect_to campaign_path(campaign_param) if campaign_param
 
-    @presenter = CoursesPresenter.new(current_user: current_user, campaign_param: ENV['default_campaign'])
+    @presenter = CoursesPresenter.new(current_user: current_user,
+                                      campaign_param: ENV['default_campaign'])
+    @campaign = @presenter.campaign
   end
 end
