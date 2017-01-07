027f4eab6ee17b6979c5fd83c466357c4f717706
Marks Polakovs <marks@markspolakovs.me>
2017-01-07 13:45:59 +0100

    Calm the linter down

--- app/assets/javascripts/components/enroll/enroll_card.jsx
+++ app/assets/javascripts/components/enroll/enroll_card.jsx
@@ -64,9 +64,9 @@ const EnrollCard = React.createClass({
     }
 
     const closeLink = (
-        <svg className="close" tabIndex="0" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" style={{ fill: 'currentcolor', verticalAlign: 'middle', width: '32px', height: '32px' }} >
-            <g><path d="M19 6.41l-1.41-1.41-5.59 5.59-5.59-5.59-1.41 1.41 5.59 5.59-5.59 5.59 1.41 1.41 5.59-5.59 5.59 5.59 1.41-1.41-5.59-5.59z"></path></g>
-        </svg>
+      <svg className="close" tabIndex="0" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" style={{ fill: 'currentcolor', verticalAlign: 'middle', width: '32px', height: '32px' }} >
+        <g><path d="M19 6.41l-1.41-1.41-5.59 5.59-5.59-5.59-1.41 1.41 5.59 5.59-5.59 5.59 1.41 1.41 5.59-5.59 5.59 5.59 1.41-1.41-5.59-5.59z"></path></g>
+      </svg>
     );
 
     return (
