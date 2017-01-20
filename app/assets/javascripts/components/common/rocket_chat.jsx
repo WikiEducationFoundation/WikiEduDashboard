55016a4d7f87fa2d5209a2e30c84434558b27616
Sage Ross <ragesoss@gmail.com>
2017-01-20 12:36:22 -0800

    Add chat login, sketch out next steps

--- app/assets/javascripts/components/common/rocket_chat.jsx
+++ app/assets/javascripts/components/common/rocket_chat.jsx
@@ -4,18 +4,36 @@ const RocketChat = React.createClass({
   displayName: 'RocketChat',
 
   getInitialState() {
-    return { loggedIn: true };
+    return { showChat: false };
+  },
+
+  login() {
+    this.setState({ showChat: true });
+
+    // TODO:
+    // If possible, start by checking if the user is already logged in to RocketChat.
+    // If not, proceed with login flow.
+    // First, request auth token from dashboard server
+
+    // On success, use that token to log in:
+    document.querySelector('iframe').contentWindow.postMessage({
+      externalCommand: 'login-with-token',
+      token: 'TOKEN-FROM-SERVER'
+    }, '*');
+    // Verify login success
+    this.setState({ loggedIn: true });
   },
 
   render() {
     let chatFrame;
-    if (this.state.loggedIn) {
-      chatFrame = <iframe style={{ display: 'block', width: '100%', height: '1000px' }} src="https://dashboardchat.wmflabs.org/" />;
+    // TODO: Hide chat until login succeeds.
+    if (this.state.showChat) {
+      chatFrame = <iframe id="chat" style={{ display: 'block', width: '100%', height: '1000px' }} src="https://dashboardchat.wmflabs.org/channel/general?layout=embedded" />;
     }
 
     return (
-
       <div className="modal">
+        <button className="button dark" onClick={this.login} >Chat!</button>
         {chatFrame}
       </div>
     );
