1e01a52022cfb96bb39d2e3b65ad01bde41c50a1
Sage Ross <ragesoss@gmail.com>
2017-02-03 16:52:39 -0800

    Fix broken spec, add spec for new join behavior

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -218,8 +218,8 @@ describe Course, type: :model do
       let(:role2) { CoursesUsers::Roles::STUDENT_ROLE }
       let(:user3) { user1.id }
       let(:role3) { CoursesUsers::Roles::INSTRUCTOR_ROLE }
-      it 'returns 1' do
-        expect(subject).to eq(1)
+      it 'returns 2' do
+        expect(subject).to eq(2)
       end
     end
   end
