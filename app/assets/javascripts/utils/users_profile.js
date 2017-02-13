724b3b1905eb980f4c99a5ba53af669d0304800b
Sejal <sejal.khatri5@gmail.com>
2017-02-13 22:03:19 +0530

    Add Page sliding animation to user Profile Pages and improve Styling

--- app/assets/javascripts/utils/users_profile.js
+++ app/assets/javascripts/utils/users_profile.js
@@ -1,8 +1,9 @@
-// jQuery for making the navbar stick when scrolled
+// jQuery for user profile pages
 $(() => {
   // Check the initial Poistion of the Sticky Header
-  const stickyHeaderTop = $('#userprofile_navbar').offset().top;
-
+  const stickyHeaderTop = $('#profile').offset().top;
+  const navbarHeight = $('#userprofile_navbar').height();
+  // making the navbar stick when scrolled
   $(window).scroll(() => {
     if ($(window).scrollTop() >= stickyHeaderTop) {
       $('#userprofile_navbar').css({ position: 'fixed', top: '50px', 'z-index': 1, width: '100%', right: 0 });
@@ -12,5 +13,12 @@ $(() => {
       $('#userprofile_navbar').css({ position: 'static', top: 'stickyHeaderTop' });
       $('#highlight').css({ display: 'none' });
     }
+  });
+
+  $('.profile_container #userprofile_navbar li a').on("click", function () {
+    const id = this.hash;
+    $('html, body').animate({
+      scrollTop: $(id).offset().top - navbarHeight
+    }, 500);
   });
 });
