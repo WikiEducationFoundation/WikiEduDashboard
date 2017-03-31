08998714740abe16a1ca582e9978f23746725b77
Sage Ross <ragesoss@gmail.com>
2017-03-31 15:50:18 -0700

    Upgrade capistrano locked version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.6.1'
+lock '3.8.0'
 
 set :application, 'wiki_edu_dashboard'
 set :repo_url, 'git@github.com:WikiEducationFoundation/WikiEduDashboard.git'
