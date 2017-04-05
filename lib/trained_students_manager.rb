d98749be552223e9d0908c6f92ba7ac58cefac00
Sage Ross <ragesoss@gmail.com>
2017-04-04 17:57:39 -0700

    Remove unused method, smoke test untested method

--- lib/trained_students_manager.rb
+++ lib/trained_students_manager.rb
@@ -9,10 +9,6 @@ class TrainedStudentsManager
   end
 
   def students_with_overdue_training
-    # NOTE: Here we can use .students instead of .students_without_nonstudents,
-    # because by the time the new training system was introduced, users were
-    # no longer allowed to have both student and nonstudent roles in the same
-    # course.
     return [] unless @course.training_modules.any?
     @course.students.select do |student|
       @course.training_modules.any? do |training_module|
