03b428e37ba6befc8d629c475827cfaca6018992
Sage Ross <ragesoss@gmail.com>
2017-04-11 08:05:30 -0700

    Reduce external dependencies in AssignedArticleImporter test
    This spec started failing when someone deleted the article '100' today.
    We can do without importing 100 articles here.

--- spec/lib/importers/assigned_article_importer_spec.rb
+++ spec/lib/importers/assigned_article_importer_spec.rb
@@ -11,9 +11,6 @@ describe AssignedArticleImporter do
            article_title: 'This_article_does_not_exist',
            article_id: nil,
            course_id: 1)
-    (1..100).each do |i|
-      create(:assignment, id: i, article_title: i.to_s, article_id: nil, course_id: 1)
-    end
     VCR.use_cassette 'assignments_importer' do
       AssignedArticleImporter.import_articles_for_assignments
     end
@@ -21,7 +18,7 @@ describe AssignedArticleImporter do
     expect(article.mw_page_id).to eq(38956275)
     expect(Assignment.find(101).article_id).to eq(article.id)
     expect(Assignment.find(102).article_id).to be_nil
-    expect(Assignment.all.count).to eq(102)
+    expect(Assignment.all.count).to eq(2)
     expect(Assignment.where(article_id: nil).count).to eq(1)
   end
 end
