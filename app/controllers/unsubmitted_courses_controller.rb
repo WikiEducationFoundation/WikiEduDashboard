3561e2104198737de9ebd1003cb6eded3b4c90ff
Sage Ross <ragesoss@gmail.com>
2017-04-11 16:47:27 -0700

    Avoid N+1 queries on unsubmitted_courses list

--- app/controllers/unsubmitted_courses_controller.rb
+++ app/controllers/unsubmitted_courses_controller.rb
@@ -3,7 +3,7 @@ class UnsubmittedCoursesController < ApplicationController
   respond_to :html
 
   def index
-    @courses_list = Course.unsubmitted.order(created_at: :desc)
+    @courses_list = Course.unsubmitted.order(created_at: :desc).includes(:tags, :instructors)
     @presenter = CoursesPresenter.new(current_user: current_user, courses_list: @courses_list)
   end
 end
