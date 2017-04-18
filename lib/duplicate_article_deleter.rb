3d62c6983dd4117e9653a9dc3ac1d4b05db4240b
Sage Ross <ragesoss@gmail.com>
2017-04-18 15:56:51 -0700

    Keep from mistakenly marking some articles as deleted
    Articles with case-variants of the same title would end up being deleted as duplicates when they should not have been.

--- lib/duplicate_article_deleter.rb
+++ lib/duplicate_article_deleter.rb
@@ -47,8 +47,11 @@ class DuplicateArticleDeleter
   # and namespace except for the most recently created
   def delete_duplicates(title, ns)
     articles = Article.where(title: title, namespace: ns, wiki_id: @wiki.id).order(:created_at)
-    deleted = articles.where.not(id: articles.last.id)
-    deleted.update_all(deleted: true)
+    # Here we must verify that the titles match, since searching by title is case-insensitive.
+    deleted = articles.where.not(id: articles.last.id).select { |article| article.title == title }
+    deleted.each do |article|
+      article.update(deleted: true)
+    end
     deleted.map(&:id)
   end
 end
