567e74b23c6982ea71938ae1c7e5fe105b2657d1
Sage Ross <ragesoss@gmail.com>
2017-04-24 13:28:17 -0700

    Remove unused code from RocketChat, test API errors

--- spec/lib/chat/rocket_chat_spec.rb
+++ spec/lib/chat/rocket_chat_spec.rb
@@ -77,5 +77,13 @@ describe RocketChat do
         expect(course.chatroom_id).to be_nil
       end
     end
+
+    context 'when the Rocket.Chat API returns an error' do
+      let(:course) { create(:course, flags: { enable_chat: true }) }
+      before { stub_chat_error }
+      it 'raises RocketChatAPIError' do
+        expect { subject.create_channel_for_course }.to raise_error(RocketChat::RocketChatAPIError)
+      end
+    end
   end
 end
