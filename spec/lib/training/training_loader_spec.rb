979674901207612928c089a26b84d90ba3c02d34
Sage Ross <ragesoss@gmail.com>
2017-04-24 13:12:42 -0700

    Extend TrainingLoader spec to test all training content classes

--- spec/lib/training/training_loader_spec.rb
+++ spec/lib/training/training_loader_spec.rb
@@ -1,24 +1,55 @@
 # frozen_string_literal: true
+
 require 'rails_helper'
 require "#{Rails.root}/lib/training/training_loader"
 require "#{Rails.root}/lib/training_slide"
 
 describe TrainingLoader do
   describe '#load_content' do
-    let(:content_class) { TrainingSlide }
+    before do
+      allow(Features).to receive(:wiki_trainings?).and_return(true)
+    end
+
     let(:subject) do
       TrainingLoader.new(content_class: content_class,
                          path_to_yaml: "#{Rails.root}/training_content/none/*.yml",
                          trim_id_from_filename: false,
-                         wiki_base_page: 'Training modules/dashboard/slides-test')
+                         wiki_base_page: wiki_base_page)
     end
-    before do
-      allow(Features).to receive(:wiki_trainings?).and_return(true)
+
+    describe 'for slides' do
+      let(:content_class) { TrainingSlide }
+      let(:wiki_base_page) { 'Training modules/dashboard/slides-test' }
+
+      it 'returns an array of training content' do
+        VCR.use_cassette 'training/load_from_wiki' do
+          slides = subject.load_content
+          expect(slides.first.content).not_to be_empty
+        end
+      end
     end
-    it 'returns an array of training content' do
-      VCR.use_cassette 'training/load_from_wiki' do
-        slides = subject.load_content
-        expect(slides.first.content).not_to be_empty
+
+    describe 'for modules' do
+      let(:content_class) { TrainingModule }
+      let(:wiki_base_page) { 'Training modules/dashboard/modules-test' }
+
+      it 'returns an array of training content' do
+        VCR.use_cassette 'training/load_from_wiki' do
+          modules = subject.load_content
+          expect(modules.first.slug).not_to be_empty
+        end
+      end
+    end
+
+    describe 'for libraries' do
+      let(:content_class) { TrainingLibrary }
+      let(:wiki_base_page) { 'Training modules/dashboard/libraries-test' }
+
+      it 'returns an array of training content' do
+        VCR.use_cassette 'training/load_from_wiki' do
+          libraries = subject.load_content
+          expect(libraries.first.slug).not_to be_empty
+        end
       end
     end
   end
