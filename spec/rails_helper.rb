06d9cd1a19bf3d2a1d095d7ec98e9b76feec7c47
Sage Ross <ragesoss@gmail.com>
2017-04-25 17:43:58 -0700

    Better null values for Sentry client

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,7 +12,7 @@ require 'capybara/rspec'
 require 'capybara-screenshot/rspec'
 require 'capybara/poltergeist'
 
-url_blacklist = ['https://wikiedu.org', 'https://fonts.googleapis.com']
+url_blacklist = ['https://wikiedu.org', 'https://fonts.googleapis.com', 'http://sentry.example.com']
 Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(app, js_errors: true, url_blacklist: url_blacklist)
 end
