1a0c58fb8b06cbb2cabbf9c0e67071dbcc6c91e9
Sage Ross <ragesoss@gmail.com>
2017-05-18 11:17:24 -0700

    Test for regression of broken CourseNavbar links
    This is one way to access and test the props being passed to child components. Because of quirks with how react-router's Link element is handled, the href's aren't available in the test environment unless we add some scaffolding for a Router to wrap it, but with shallow rendering and accessing the child nodes, we can verify that the right prop is passed to Link (without relying the behavior of the Link component itself).
    
    This covers the bug fixed in 6d2c7687b98cf306e95ded6316694842e7c57cf7

--- test/components/common/course_navbar.spec.jsx
+++ test/components/common/course_navbar.spec.jsx
@@ -1,24 +1,31 @@
 import 'testHelper';
 import React from 'react';
-import { render } from 'enzyme';
+import { shallow } from 'enzyme';
 import CourseNavbar from 'components/common/course_navbar';
 
 describe('Timeline link', () => {
   Features = { enableGetHelpButton: true };
   const currentUser = { role: 0 };
+  const slug = '/courses/foo/bar_(baz)';
+
   it('renders for a ClassroomProgramCourse', () => {
     const course = {
       type: 'ClassroomProgramCourse',
-      flags: { enable_chat: true } // adds Chat link
+      flags: { enable_chat: true }, // adds Chat link
+      title: 'bar'
     };
     const component = (
       <CourseNavbar
         course={course}
-        location={{ pathname: 'foo/bar_(baz)' }}
+        location={{ pathname: slug }}
         currentUser={currentUser}
+        courseLink={slug}
       />
     );
-    expect(render(component).find('#timeline-link').length).to.eq(1);
+    // Timeline link is rendered
+    expect(shallow(component).find('#timeline-link').length).to.eq(1);
+    // Correct Home link is rendered
+    expect(shallow(component).find('Link').nodes[0].props.to).to.eq(`${slug}/home`);
   });
 
   it('does not render for a BasicCourse', () => {
@@ -28,10 +35,13 @@ describe('Timeline link', () => {
     const component = (
       <CourseNavbar
         course={course}
-        location={{ pathname: 'foo/bar_(baz)' }}
+        location={{ pathname: slug }}
         currentUser={currentUser}
+        courseLink={slug}
       />
     );
-    expect(render(component).find('#timeline-link').length).to.eq(0);
+    expect(shallow(component).find('#timeline-link').length).to.eq(0);
+    // Correct Home link is rendered
+    expect(shallow(component).find('Link').nodes[0].props.to).to.eq(`${slug}/home`);
   });
 });
