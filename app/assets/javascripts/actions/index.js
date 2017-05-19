2c9da01004cb5e2e33438dd98b46106bfc07912c
Sage Ross <ragesoss@gmail.com>
2017-05-19 11:53:49 -0700

    Remove duplicate code

--- app/assets/javascripts/actions/index.js
+++ app/assets/javascripts/actions/index.js
@@ -1,23 +1,3 @@
 import * as types from '../constants/action_types.js';
-import ApiFailAction from './api_fail_action.js';
-import API from '../utils/api.js';
 
 export const toggleUI = key => ({ type: types.TOGGLE_UI, key });
-
-// This action uses the Thunk middleware pattern: instead of returning a plain
-// action object, it returns a function that takes the store dispatch fucntion —
-// which Thunk automatically provides — and can then dispatch a series of plain
-// actions to be handled by the store.
-// This is how actions with side effects — such as API calls — are handled in
-// Redux.
-export function submitNeedHelpAlert(data) {
-  return function (dispatch) {
-    dispatch({ type: types.NEED_HELP_ALERT_SUBMITTED });
-    API.createNeedHelpAlert(data)
-      .then(() => (dispatch({ type: types.NEED_HELP_ALERT_CREATED })))
-      // TODO: The Flux stores still handle API failures, so we delegate to a
-      // Flux action. Once all API_FAIL actions can be handled by Redux, we can
-      // replace this with a regular action dispatch.
-      .catch(response => (ApiFailAction.fail(response)));
-  };
-}
