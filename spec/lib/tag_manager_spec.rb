5dd76bdebf1ea3c58bfe404c8cd448e8804d116c
Sage Ross <ragesoss@gmail.com>
2017-06-01 15:14:13 -0700

    Handle first_time vs returning tags correctly on P&E Dashboard
    The logic of user#returning_instructor? assumes that courses must be approved before they are part of a campaign. This allows us to mark multiple newly submitted courses from the same new instructor as 'first_time_instructor', since they are all created before the first one is approved. But we must handle the tagging differently when open_course_creation is enabled, because new courses already have a campaign at the time they get the initial tags added... meaning every course was being tagged 'returning_instructor'
    
    Resolves #1294
    
    I also deleted all 'returning_instructor' tags on P&E Dashboard, since they were mostly incorrect.

--- spec/lib/tag_manager_spec.rb
+++ spec/lib/tag_manager_spec.rb
@@ -6,19 +6,42 @@ require "#{Rails.root}/lib/tag_manager"
 describe TagManager do
   describe '#initial_tags' do
     let(:course) { create(:course) }
+    let(:course2) { create(:course, slug: 'second_course') }
     let(:creator) { create(:user) }
     let(:subject) { course.tags.first.tag }
 
-    it 'adds a returning_instructor tag if the creator is a returning instructor' do
-      expect(creator).to receive(:returning_instructor?).and_return(true)
-      TagManager.new(course).initial_tags(creator: creator)
-      expect(subject).to eq('returning_instructor')
+    context 'when courses must be approved' do
+      it 'adds a returning_instructor tag if the creator is a returning instructor' do
+        expect(creator).to receive(:returning_instructor?).and_return(true)
+        TagManager.new(course).initial_tags(creator: creator)
+        expect(subject).to eq('returning_instructor')
+      end
+
+      it 'adds a first_time_instructor tag if the creator is not a returning instructor' do
+        expect(creator).to receive(:returning_instructor?).and_return(false)
+        TagManager.new(course).initial_tags(creator: creator)
+        expect(subject).to eq('first_time_instructor')
+      end
     end
 
-    it 'adds a first_time_instructor tag if the creator is not a returning instructor' do
-      expect(creator).to receive(:returning_instructor?).and_return(false)
-      TagManager.new(course).initial_tags(creator: creator)
-      expect(subject).to eq('first_time_instructor')
+    context 'when open course creation is enabled' do
+      before do
+        allow(Features).to receive(:open_course_creation?).and_return(true)
+        create(:courses_user, user: creator, course: course,
+                              role: CoursesUsers::Roles::INSTRUCTOR_ROLE)
+      end
+
+      it 'adds a returning_instructor tag if the creator has more than one course' do
+        create(:courses_user, user: creator, course: course2,
+                              role: CoursesUsers::Roles::INSTRUCTOR_ROLE)
+        TagManager.new(course).initial_tags(creator: creator)
+        expect(subject).to eq('returning_instructor')
+      end
+
+      it 'adds a first_time_instructor tag if the creator has only one course' do
+        TagManager.new(course).initial_tags(creator: creator)
+        expect(subject).to eq('first_time_instructor')
+      end
     end
   end
 end
