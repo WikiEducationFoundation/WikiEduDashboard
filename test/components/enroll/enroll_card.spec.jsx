cd1bf755c17c07f1a2868d57956befeac3f6f7cb
Sage Ross <ragesoss@gmail.com>
2017-06-06 12:05:12 -0700

    Fix EnrollCard component test

--- test/components/enroll/enroll_card.spec.jsx
+++ test/components/enroll/enroll_card.spec.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import ReactTestUtils from 'react-addons-test-utils';
+import { mount } from 'enzyme';
 import '../../testHelper';
 import EnrollCard from '../../../app/assets/javascripts/components/enroll/enroll_card.jsx';
 
 describe('EnrollCard', () => {
   it('Warns about ended course', () => {
-    const TestEnrollCard = ReactTestUtils.renderIntoDocument(
+    const TestEnrollCard = mount(
       <EnrollCard
         course={{ ended: true }}
       />
     );
-    const h1 = ReactTestUtils.findRenderedDOMComponentWithTag(TestEnrollCard, 'h1');
-    expect(h1.textContent).to.eq('The course has ended.');
+    const h1 = TestEnrollCard.find('h1');
+    expect(h1.first().text()).to.eq('The course has ended.');
   });
 });
