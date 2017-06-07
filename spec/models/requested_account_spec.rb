890bf5df3fe698fbcd1fc2bf748a12db32f7db8c
Sage Ross <ragesoss@gmail.com>
2017-06-07 14:28:50 -0700

    Annotate new model

--- spec/models/requested_account_spec.rb
+++ spec/models/requested_account_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: requested_accounts
+#
+#  id         :integer          not null, primary key
+#  course_id  :integer
+#  username   :string(255)
+#  email      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe RequestedAccount, type: :model do
