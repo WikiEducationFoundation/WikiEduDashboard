52a81cda30fbd1b97e36d40cb062cf408f51b387
Sage Ross <ragesoss@gmail.com>
2017-06-23 08:08:38 -0700

    Update Replica spec
    A revision probably got deleted.

--- spec/lib/replica_spec.rb
+++ spec/lib/replica_spec.rb
@@ -112,7 +112,7 @@ describe Replica do
 
         es_wiki = Wiki.new(language: 'es', project: 'wikipedia')
         response = Replica.new(es_wiki).get_revisions(all_users, rev_start, rev_end)
-        expect(response.count).to eq(24)
+        expect(response.count).to eq(23)
       end
     end
 
