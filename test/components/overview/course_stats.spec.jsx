ec2ad6f05df578afbaecf65da35610f7a906bcc6
Sejal <sejal.khatri5@gmail.com>
2017-06-27 18:38:36 +0530

    Update the enzyme test spec and upgrade the onClickOutside wrapper for react components

--- test/components/overview/course_stats.spec.jsx
+++ test/components/overview/course_stats.spec.jsx
@@ -1,7 +1,6 @@
 import '../../testHelper';
 import React from 'react';
-import { shallow } from 'enzyme'; // Shallow rendering is useful to constrain yourself to testing
- // a component as a unit, and to ensure that your tests aren't indirectly asserting on behavior of child components.
+import { shallow } from 'enzyme';
 import CourseStats from '../../../app/assets/javascripts/components/overview/course_stats.jsx';
 
 describe('for view count zero', () => {
@@ -9,31 +8,31 @@ describe('for view count zero', () => {
     view_count: '0',
     upload_usages_count: 0
   };
-  const stats = (
+  const testStats = (
     <CourseStats
       course={course}
     />
   );
   const msgString = I18n.t('metrics.view_data_unavailable');
   it('renders view data unavailable message', () => {
-    const wrapper = shallow(stats);
-    console.log(wrapper);
-    // expect(wrapper.contains(<div className="stat-display__data"></div>)).to.be.true;
+    const wrapper = shallow(testStats);
     expect(wrapper.contains(msgString)).to.be.true;
   });
 });
 
 describe('for view count greater than zero', () => {
   const course = {
-    view_count: 3
+    view_count: '3',
+    upload_usages_count: 0
   };
-  const component = (
+  const testStats = (
     <CourseStats
       course={course}
     />
   );
+  const msgString = I18n.t('metrics.view_data_unavailable');
   it('doesn\'t render view data unavailable message', () => {
-    const wrapper = shallow(component);
-    expect(wrapper.contains(<div className="stat-display__data"></div>)).to.be.false;
+    const wrapper = shallow(testStats);
+    expect(wrapper.contains(msgString)).to.be.false;
   });
 });
