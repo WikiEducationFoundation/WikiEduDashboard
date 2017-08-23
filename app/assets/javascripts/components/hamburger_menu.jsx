9817b257bba1acca41ca16b22341b50ddb83525d
Sejal <sejal.khatri5@gmail.com>
2017-08-23 08:15:54 +0530

    Try to avoid burger menu and navbar css clashes

--- app/assets/javascripts/components/hamburger_menu.jsx
+++ app/assets/javascripts/components/hamburger_menu.jsx
@@ -131,25 +131,27 @@ const HamburgerMenu = React.createClass({
     }
     return (
       <div>
-        <div className="container">
-          <div className="top-nav__site-logo">
-            <a className="logo__link" href= {this.props.rootUrl}>
-              <img src ={this.props.logoPath} alt = "wiki logo" />
-            </a>
+        <nav className="ham-nav">
+          <div className="container">
+            <div className="ham-nav__site-logo">
+              <a className="logo__link" href= {this.props.rootUrl}>
+                <img src ={this.props.logoPath} alt = "wiki logo" />
+              </a>
+            </div>
+            <Menu right>
+              <CustomLink to={this.props.exploreurl} name={this.props.explorename} clickedElement="explore" />
+              {myDashboard}
+              {forAdmin}
+              {disableTraining}
+              {Sandbox}
+              {help}
+              {wikiEd}
+              {helpEnabled}
+              {languageSwitcherEnabled}
+              {loggingLinks}
+            </Menu>
           </div>
-          <Menu right>
-            <CustomLink to={this.props.exploreurl} name={this.props.explorename} clickedElement="explore" />
-            {myDashboard}
-            {forAdmin}
-            {disableTraining}
-            {Sandbox}
-            {help}
-            {wikiEd}
-            {helpEnabled}
-            {languageSwitcherEnabled}
-            {loggingLinks}
-          </Menu>
-        </div>
+        </nav>
       </div>
     );
   }
