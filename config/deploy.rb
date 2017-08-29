ae0dfc32b5341e0614423e89bc5784e57dff9c18
Sage Ross <ragesoss@gmail.com>
2017-08-28 18:09:40 -0700

    bundle update

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.8.2'
+lock '3.9.0'
 
 set :application, 'wiki_edu_dashboard'
 set :repo_url, 'git@github.com:WikiEducationFoundation/WikiEduDashboard.git'
