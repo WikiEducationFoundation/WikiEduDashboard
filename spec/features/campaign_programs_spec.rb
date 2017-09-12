4c60784dcbc9f3f269019173975e10aac57ba45c
Alexander Hedges <alehed@users.noreply.github.com>
2017-09-12 21:29:43 +0200

    add test for regression and test sorting functionality for real

--- spec/features/campaign_programs_spec.rb
+++ spec/features/campaign_programs_spec.rb
@@ -6,9 +6,14 @@ describe 'campaign programs page', type: :feature, js: true do
   let(:user)  { create(:user) }
   let(:campaign) { create(:campaign) }
   let(:course) { create(:course) }
+  let(:course2) { create(:visiting_scholarship, school: 'ZZZ', slug: 'ZZZ/Basket-weaving_scholarship_(spring_2015)') }
   let!(:campaigns_course) do
     create(:campaigns_course, campaign_id: campaign.id,
                               course_id: course.id)
+  end
+  let!(:campaigns_course2) do
+    create(:campaigns_course, campaign_id: campaign.id,
+                              course_id: course2.id)
   end
 
   # tests for whether Remove button should be shown live in CampaignsControllerSpec
@@ -21,7 +26,7 @@ describe 'campaign programs page', type: :feature, js: true do
       alert_message = I18n.t('campaign.confirm_course_removal', title: course.title,
                                                                 campaign_title: campaign.title)
       page.accept_alert alert_message do
-        find('.remove-course').click
+        all('.remove-course')[1].click
       end
       expect(page).to have_content('has been removed')
       expect(CampaignsCourses.find_by_id(campaigns_course.id)).to be_nil
@@ -54,14 +59,36 @@ describe 'campaign programs page', type: :feature, js: true do
 
       # Sortable by title
       expect(page).to have_selector('#courses [data-sort="title"].sort.asc')
-      find('#courses [data-sort="title"].sort').trigger('click')
+      find('#courses [data-sort="title"].sort').click
       expect(page).to have_selector('#courses [data-sort="title"].sort.desc')
 
       # Sortable by institution
-      find('#courses [data-sort="school"].sort').trigger('click')
+      find('#courses [data-sort="school"].sort').click
       expect(page).to have_selector('#courses [data-sort="school"].sort.asc')
-      find('#courses [data-sort="school"].sort').trigger('click')
+      find('#courses [data-sort="school"].sort').click
       expect(page).to have_selector('#courses [data-sort="school"].sort.desc')
+    end
+
+    def expect_underwater_before_regular_basketweaving
+      expect(page.find(:xpath, '//table/tbody[2]/tr[1]/td[1]')).to have_content 'Basket-weaving scholarship'
+      expect(page.find(:xpath, '//table/tbody[2]/tr[2]/td[1]')).to have_content 'Underwater basket-weaving'
+    end
+    def expect_regular_before_underwater_basketweaving
+      expect(page.find(:xpath, '//table/tbody[2]/tr[1]/td[1]')).to have_content 'Underwater basket-weaving'
+      expect(page.find(:xpath, '//table/tbody[2]/tr[2]/td[1]')).to have_content 'Basket-weaving scholarship'
+    end
+
+    it 'should sort the contained courses' do
+      visit "/campaigns/#{campaign.slug}/programs"
+
+      expect_underwater_before_regular_basketweaving
+      find('#courses [data-sort="title"].sort').click
+      expect_regular_before_underwater_basketweaving
+
+      find('#courses [data-sort="school"].sort').click
+      expect_regular_before_underwater_basketweaving
+      find('#courses [data-sort="school"].sort').click
+      expect_underwater_before_regular_basketweaving
     end
   end
 end
