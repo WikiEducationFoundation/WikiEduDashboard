2bc62a1319ffdca36996303febe6d21b973af378
KarmaHater <andra.lally@gmail.com>
2017-10-06 22:14:17 +0200

    refactor dyk reducer

---
+++ app/assets/javascripts/constants/ui.js
@@ -0,0 +1,1 @@
+export const TOGGLE_UI = 'TOGGLE_UI';
---
+++ test/reducers/did_you_know.js
@@ -0,0 +1,35 @@
+import "../testHelper";
+import reducer from "../../app/assets/javascripts/reducers/did_you_know.js";
+import { RECEIVE_DYK } from "../../app/assets/javascripts/constants";
+
+describe("slider menu reducer", () => {
+  it("should return the initial state", () => {
+    const initialState = {
+      articles: [],
+      loading: true
+    };
+    expect(reducer(undefined, {})).toEqual(initialState);
+  });
+
+  it("should update array with new did you know articles", () => {
+    const newArticles = [{ title: "new-1" }, { title: "new-2" }];
+    const oldArticles = [{ title: "old-1" }, { title: "old-2" }];
+
+    const initialState = new Map({
+      articles: oldArticles
+    });
+
+    expect(
+      reducer(initialState, {
+        type: RECEIVE_DYK,
+        payload: {
+          articles: newArticles
+        }
+      })
+    ).toEqual(
+      fromJS({
+        articles: newArticles
+      })
+    );
+  });
+});
