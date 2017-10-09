57e5ce6300877d9e40c78553c356f3d840d10a03
Jacob Stoebel <jstoebel@gmail.com>
2017-10-09 15:12:37 -0400

    remove findDOMNode
    also refactor several tests to use enzyme

--- test/components/overview/course_cloned_modal.spec.jsx
+++ test/components/overview/course_cloned_modal.spec.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import ReactTestUtils from 'react-dom/test-utils';
-import ReactDOM from 'react-dom';
 import '../../testHelper';
 import CourseClonedModal from '../../../app/assets/javascripts/components/overview/course_cloned_modal.jsx';
-
+import { mount } from 'enzyme';
 describe('CourseClonedModal', () => {
   const course = {
     slug: 'foo/bar_(baz)',
@@ -12,31 +10,24 @@ describe('CourseClonedModal', () => {
     title: 'bar',
     expected_students: 0
   };
-
-  it('renders a Modal', () => {
-    const TestModal = ReactTestUtils.renderIntoDocument(
-      <CourseClonedModal
-        course={course}
-      />
+  const TestModal = mount(
+    <CourseClonedModal
+      course={course}
+    />
     );
 
-    const renderedModal = ReactTestUtils.findRenderedDOMComponentWithClass(TestModal, 'cloned-course');
-    expect(renderedModal).not.to.be.empty;
+  it('renders a Modal', () => {
+    const renderedModal = TestModal.find('.cloned-course');
+    expect(renderedModal).to.have.length(1);
     TestModal.setState({ error_message: null });
-    const warnings = ReactTestUtils.scryRenderedDOMComponentsWithClass(TestModal, 'warning');
-    expect(warnings).to.be.empty;
+    const warnings = TestModal.find('.warning');
+    expect(warnings).to.have.length(0);
   });
 
   it('renders an error message if state includes one', () => {
-    const TestModal = ReactTestUtils.renderIntoDocument(
-      <CourseClonedModal
-        course={course}
-      />
-    );
     TestModal.setState({ error_message: 'test error message' });
-
-    const warnings = ReactTestUtils.scryRenderedDOMComponentsWithClass(TestModal, 'warning');
+    const warnings = TestModal.find('.warning');
     expect(warnings).not.to.be.empty;
-    expect(ReactDOM.findDOMNode(warnings[0]).textContent).to.eq('test error message');
+    expect(warnings.first().text()).to.eq('test error message');
   });
 });
