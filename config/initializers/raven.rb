a785d7683ed6abeefdae6b4662af1e4ea2000b23
Sage Ross <ragesoss@gmail.com>
2017-11-07 09:20:10 -0800

    Configure dummy transport for Sentry in test env
    This will exercise everything except actually sending Sentry events to a server, so that we know when our usage of Raven is broken.

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,11 +1,11 @@
 require 'raven'
-
+require 'raven/transports/dummy' if Rails.env.test?
 is_on_windows = (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
 
 Raven.configure do |config|
-  config.dsn = Figaro.env.sentry_dsn
+  config.dsn = ENV['sentry_dsn']
   config.silence_ready = true
-  config.environments = %w[development staging production]
+  config.environments = %w[development staging production test]
   config.logger = Logger.new("/dev/null") unless is_on_windows
   config.logger = Logger.new("nul") if is_on_windows
 end
