f30cd9da761261d25b5e0b1a2828d8b06d4c3591
mauditecandela <candela.jimenez@gmail.com>
2017-11-08 15:04:46 +0100

    - Cleans up UpdateLog - Creates setter method to avoid the check up

--- app/models/update_log.rb
+++ app/models/update_log.rb
@@ -1,12 +1,17 @@
 class UpdateLog
 
+  def self.setting_record
+    Setting.find_or_create_by(key: 'metrics_update')
+  end
+
   def self.log_update(time)
-    setting = Setting.find_or_create_by(key: 'metrics_update')
+    setting = self.setting_record
     setting.value["last_update"] = time
     setting.save
   end
 
   def self.last_update
-    Setting.find_by(key: 'metrics_update') ? Setting.find_by(key: 'metrics_update').value['last_update'].strftime("%H:%M:%S - %d/%m/%Y") : 0
+    self.setting_record
+    Setting.find_by(key: 'metrics_update').value['last_update']
   end
 end
