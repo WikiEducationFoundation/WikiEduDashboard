f0f20b21a5ac30bca30d80ac1b3bba39504e7f8c
Sage Ross <ragesoss@gmail.com>
2017-11-22 14:59:15 -0800

    Escape apostrope so we can enable the lint rule

--- app/assets/javascripts/components/articles/course_ores_plot.jsx
+++ app/assets/javascripts/components/articles/course_ores_plot.jsx
@@ -60,7 +60,7 @@ const CourseOresPlot = createReactClass({
               This graph visualizes, in aggregate, how much articles developed from
               when students first edited them until now. The <em>Structural Completeness </em>
               rating is based on a machine learning project (<a href="https://www.mediawiki.org/wiki/ORES/FAQ" target="_blank">ORES</a>)
-              that estimates an article's quality rating based on the amount of
+              that estimates an article&apos;s quality rating based on the amount of
               prose, the number of wikilinks, images and section headers, and other features.
             </p>
           </div>
