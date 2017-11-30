f14e3a274e77b7abfbd6775a65743c85e1f0bbe8
Arun Kumar <arunkumarm1995@gmail.com>
2017-11-30 08:02:33 -0500

    Convert Conditional component to a stateless functional component

--- app/assets/javascripts/components/high_order/conditional.jsx
+++ app/assets/javascripts/components/high_order/conditional.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import createReactClass from 'create-react-class';
 import PropTypes from 'prop-types';
 
 // Enables DRY and simple conditional components
 // Renders items when 'show' prop is undefined
 
-const Conditional = function (Component) {
-  return createReactClass({
-    propTypes: {
-      show: PropTypes.bool
-    },
-
-    render() {
-      if (this.props.show === undefined || this.props.show) {
-        return (<Component {...this.props} />);
-      }
-      return false;
+const Conditional = (Component) => {
+  const HighOrderComponent = (props) => {
+    if (props.show) {
+      return <Component {...props} />;
     }
-  });
+    return null;
+  };
+
+  HighOrderComponent.propTypes = {
+    show: PropTypes.bool.isRequired
+  };
+
+  HighOrderComponent.defaultProps = {
+    show: true
+  };
+
+  return HighOrderComponent;
 };
 
 export default Conditional;
