101f3c0ee679e607b260ba271cb2110ffedee810
Arun Kumar <arunkumarm1995@gmail.com>
2017-12-01 11:44:28 -0500

    Convert class components to stateless functional components (#1525)
    * Convert Root to a stateless functional component
    
    * Convert InlineUsers to a stateless functional component
    
    * Convert RevisionList to a stateless functional component
    
    * Convert OrderableBlock to a stateless functional component
    
    * Convert BarGraph to a stateless functional component
    
    * Convert TrainingApp to a stateless functional component
    
    * Replace class component with stateless functional component in specs

--- app/assets/javascripts/components/timeline/orderable_block.jsx
+++ app/assets/javascripts/components/timeline/orderable_block.jsx
@@ -1,40 +1,53 @@
 import React from 'react';
-import createReactClass from 'create-react-class';
 import PropTypes from 'prop-types';
 import Reorderable from '../high_order/reorderable.jsx';
 
-const OrderableBlock = createReactClass({
-  displayName: 'OrderableBlock',
+const OrderableBlock = ({
+  title,
+  kind,
+  isDragging,
+  disableUp,
+  disableDown,
+  onMoveUp,
+  onMoveDown
+}) => {
+  const opacity = isDragging ? 0.5 : 1;
 
-  propTypes: {
-    title: PropTypes.string,
-    kind: PropTypes.string.isRequired,
-    disableUp: PropTypes.bool.isRequired,
-    disableDown: PropTypes.bool.isRequired,
-    canDrag: PropTypes.bool.isRequired,
-    onDrag: PropTypes.func.isRequired,
-    onMoveUp: PropTypes.func.isRequired,
-    onMoveDown: PropTypes.func.isRequired,
-    isDragging: PropTypes.bool.isRequired
-  },
+  return (
+    <div className="block block--orderable" style={{ opacity }}>
+      <h4 className="block-title">{title}</h4>
+      <p>{kind}</p>
+      <button
+        onClick={onMoveDown}
+        className="button border"
+        aria-label={I18n.t('timeline.move_block_up')}
+        disabled={disableDown}
+      >
+        <i className="icon icon-arrow-down" />
+      </button>
+      <button
+        onClick={onMoveUp}
+        className="button border"
+        aria-label={I18n.t('timeline.move_block_down')}
+        disabled={disableUp}
+      >
+        <i className="icon icon-arrow-up" />
+      </button>
+    </div>);
+};
 
-  render() {
-    const opacity = this.props.isDragging ? 0.5 : 1;
+OrderableBlock.propTypes = {
+  title: PropTypes.string,
+  kind: PropTypes.string.isRequired,
+  disableUp: PropTypes.bool.isRequired,
+  disableDown: PropTypes.bool.isRequired,
+  onMoveUp: PropTypes.func.isRequired,
+  onMoveDown: PropTypes.func.isRequired,
+  isDragging: PropTypes.bool.isRequired
+};
 
-    return (
-      <div className="block block--orderable" style={{ opacity }}>
-        <h4 className="block-title">{this.props.title}</h4>
-        <p>{this.props.kind}</p>
-        <button onClick={this.props.onMoveDown} className="button border" aria-label={I18n.t('timeline.move_block_up')} disabled={this.props.disableDown}>
-          <i className="icon icon-arrow-down" />
-        </button>
-        <button onClick={this.props.onMoveUp} className="button border" aria-label={I18n.t('timeline.move_block_down')} disabled={this.props.disableUp}>
-          <i className="icon icon-arrow-up" />
-        </button>
-      </div>);
-  }
-}
-
+export default Reorderable(
+  OrderableBlock,
+  'block',
+  'onDrag'
 );
-
-export default Reorderable(OrderableBlock, 'block', 'onDrag');
