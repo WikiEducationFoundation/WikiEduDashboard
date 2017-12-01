2c741a9573096c89464128d8a73f7d50a2e4bc73
Ben Blanchard <benjamin.blanchard@gmail.com>
2017-11-30 23:36:10 -0500

    Fixed formatting.

--- app/assets/javascripts/utils/course_utils.js
+++ app/assets/javascripts/utils/course_utils.js
@@ -90,8 +90,8 @@ const CourseUtils = class {
   }
 
   articleFromAssignment(assignment, defaultWiki) {
-    const language = assignment.language  || defaultWiki.language || 'en';
-    const project = assignment.project  || defaultWiki.project || 'wikipedia';
+    const language = assignment.language || defaultWiki.language || 'en';
+    const project = assignment.project || defaultWiki.project || 'wikipedia';
     const articleUrl = assignment.article_url || this.urlFromTitleAndWiki(assignment.article_title, language, project);
     const article = {
       rating: assignment.article_rating,
