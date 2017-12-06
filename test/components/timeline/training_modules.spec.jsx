18394a7a3adac7ee29292751b39541598c10c587
takidelfin <takidelfin@gmail.com>
2017-12-06 16:33:59 +0100

    Fix style erros thrown by Travis

--- test/components/timeline/training_modules.spec.jsx
+++ test/components/timeline/training_modules.spec.jsx
@@ -1,12 +1,12 @@
 import '../../testHelper';
 
 import React from 'react';
-import {shallow, mount, render} from 'enzyme';
+import { mount } from 'enzyme';
 import TrainingModules from '../../../app/assets/javascripts/components/timeline/training_modules.jsx';
 
 describe('TrainingModules', () => {
   describe('render', () => {
-    const TrainingModulesM = mount(<TrainingModules block_modules={["test1", "test2", "test3"]} all_modules={["module1","module2","module3 "]}/>);
+    const TrainingModulesM = mount(<TrainingModules block_modules={["test1", "test2", "test3"]} all_modules={["module1", "module2", "module3 " ]}/>);
 
     describe('Initial State', () => {
       it('should map array block_modules with their ids', () => {
@@ -30,13 +30,12 @@ describe('TrainingModules', () => {
     });
     describe('trainingSelector', () => {
       it('should return object', () => {
-        expect(TrainingModulesM.instance().trainingSelector()).to.be.an('object');
+        expect(TrainingModulesM.instance().trainingSelector()).to.be_an('object');
       });
       // I can't get it work. `yarn test` throwed that `find` is invalid chai property...
       // it('should map `options` properly', () => {
       //   expect(TrainingModulesM.instance().trainingSelector()).find('[options]').to.be.an('array');
       // });
     });
-
   });
 });
