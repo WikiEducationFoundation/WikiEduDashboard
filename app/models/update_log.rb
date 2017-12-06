d787e3745aadb37f11bcdcc5bcd58268885182aa
mauditecandela <candela.jimenez@gmail.com>
2017-12-06 14:46:30 +0100

    - Adds new method to calculate Average Delay of updates - Modifies old last_update method and instead of requesting the 'last_update' key, looks for the last update that took place - Deletes old log_updates method and instead creates a new method that updates the last 10 updates - Adds the expected delay time to the Overview Handler - Adds translations to ES, CA and EN - Includes a test for the new methods for the update_log.rb

--- app/models/update_log.rb
+++ app/models/update_log.rb
@@ -4,14 +4,31 @@ class UpdateLog
     Setting.find_or_create_by(key: 'metrics_update')
   end
 
-  def self.log_update(time)
+  def self.last_update
     setting = self.setting_record
-    setting.value["last_update"] = time
+    setting.value.values.last
+
+  end
+
+  def self.log_updates(time)
+    setting = self.setting_record
+    last_update = setting.value.keys.last
+    if last_update === nil
+      new_update = 0
+    elsif last_update >= 10
+      setting.value.delete(last_update - 10)
+      new_update = last_update + 1
+    else 
+      new_update = last_update + 1
+    end
+    setting.value[new_update] = time
     setting.save
   end
 
-  def self.last_update
-    self.setting_record
-    Setting.find_by(key: 'metrics_update').value['last_update']
+  def self.average_delay
+    setting = self.setting_record
+    seconds = setting.value.map {|key, value| (Time.parse(setting.value[key].to_s) - Time.parse(setting.value[key-1].to_s) if key != setting.value.keys[0]).to_i}
+    average_delay = seconds.inject(:+)/(seconds.length-1) if seconds.length > 1 
   end
+
 end
