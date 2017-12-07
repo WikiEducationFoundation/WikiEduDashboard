115f122b1e44af556fd53ea2c81b0a616f497645
mauditecandela <candela.jimenez@gmail.com>
2017-12-07 19:12:20 +0100

    Simplifies log_updates method by: - Use of constant to define maximum amount of updates - Better wording (index instead of last_update/new_update) - Deletion of unneeded code

--- app/models/update_log.rb
+++ app/models/update_log.rb
@@ -1,4 +1,8 @@
 class UpdateLog
+  ####################
+  # CONSTANTS        #
+  ####################
+  MAX_UPDATES_TO_KEEP = 10
 
   def self.setting_record
     Setting.find_or_create_by(key: 'metrics_update')
@@ -6,7 +10,7 @@ class UpdateLog
 
   def self.log_delay
     setting = self.setting_record
-    constant_update =  setting.value['constant_update']
+    constant_update = setting.value['constant_update']
     if constant_update 
       seconds = constant_update.keys.length > 1 ? constant_update.map {|key, value| (Time.parse(constant_update[key]["end_time"].to_s) - Time.parse(constant_update[key-1]["end_time"].to_s) if key != constant_update.keys[0]).to_i} : [0]
       setting.value['average_delay'] = seconds.inject(:+)/(seconds.length-1) if seconds.length > 1
@@ -16,19 +20,18 @@ class UpdateLog
 
   def self.log_updates(starttime, endtime)
     setting = self.setting_record
-    update_record = setting.value['constant_update'] ? setting.value['constant_update'] : setting.value['constant_update'] = Hash.new
-    last_update = update_record.keys.last
-    max_updates = 10
-    if last_update === nil
-      new_update = 0
-      update_record[new_update] = Hash.new
-    elsif last_update >= max_updates
-      update_record.delete(last_update - max_updates)
-      new_update = last_update + 1
+    update_record = setting.value['constant_update']
+    if setting.value['constant_update'] === nil
+      setting.value['constant_update'] = Hash.new
+      index = 0 
+    elsif last_update >= MAX_UPDATES_TO_KEEP 
+      index = update_record.keys.last
+      update_record.delete(index - MAX_UPDATES_TO_KEEP)
+      index = index + 1
     else 
-      new_update = last_update + 1
+      index = update_record.keys.last + 1
     end
-    update_record[new_update] = {'start_time' => starttime, 'end_time' => endtime}
+    setting.value['constant_update'][index] = {'start_time' => starttime, 'end_time' => endtime}
     setting.save
     self.log_delay
   end
