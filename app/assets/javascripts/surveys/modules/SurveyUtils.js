a7ca7961fafd65dcd4318d7a9654e276cf6e6bdf
Adam Mescher <admescher@gmail.com>
2017-12-06 22:59:54 -0700

    Add danling commas to SurveyUtils.js

--- app/assets/javascripts/surveys/modules/SurveyUtils.js
+++ app/assets/javascripts/surveys/modules/SurveyUtils.js
@@ -5,13 +5,13 @@ const Utils = {
       question_id: params[0],
       operator: params[1],
       value: params[2].trim().split(' ').join('_'),
-      multi: params[3] === 'multi'
+      multi: params[3] === 'multi',
     });
   },
 
   toTitleCase(str) {
     return str.replace(/\w\S*/g, txt => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
-  }
+  },
 };
 
 export default Utils;
