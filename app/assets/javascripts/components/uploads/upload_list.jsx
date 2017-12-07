6787ffc87e115cf60a3b876986b874eae35d79a6
Adam Mescher <admescher@gmail.com>
2017-12-07 09:04:21 -0700

    Add dangling commas to upload_list.jsx

--- app/assets/javascripts/components/uploads/upload_list.jsx
+++ app/assets/javascripts/components/uploads/upload_list.jsx
@@ -18,25 +18,25 @@ const UploadList = ({ uploads, course }) => {
   const keys = {
     image: {
       label: I18n.t('uploads.image'),
-      desktop_only: false
+      desktop_only: false,
     },
     file_name: {
       label: I18n.t('uploads.file_name'),
-      desktop_only: true
+      desktop_only: true,
     },
     uploaded_by: {
       label: I18n.t('uploads.uploaded_by'),
-      desktop_only: true
+      desktop_only: true,
     },
     usage_count: {
       label: I18n.t('uploads.usage_count'),
-      desktop_only: true
+      desktop_only: true,
     },
     date: {
       label: I18n.t('uploads.datetime'),
       desktop_only: true,
-      info_key: 'uploads.time_doc'
-    }
+      info_key: 'uploads.time_doc',
+    },
   };
 
   return (
@@ -52,7 +52,7 @@ const UploadList = ({ uploads, course }) => {
 
 UploadList.propTypes = {
   uploads: PropTypes.array,
-  course: PropTypes.object
+  course: PropTypes.object,
 };
 
 export default Editable(UploadList, [UploadStore], ServerActions.saveUploads, getState);
