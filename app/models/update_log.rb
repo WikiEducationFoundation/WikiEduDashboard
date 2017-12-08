4468887b30171a8aec6fe2590566c39a634a12fa
mauditecandela <candela.jimenez@gmail.com>
2017-12-08 11:51:12 +0100

    - Creates new private methods reused at log_updates Note: private method for saving the record was not working - needs research

--- app/models/update_log.rb
+++ app/models/update_log.rb
@@ -8,31 +8,9 @@ class UpdateLog
     Setting.find_or_create_by(key: 'metrics_update')
   end
 
-  def self.log_delay
-    setting = self.setting_record
-    constant_update = setting.value['constant_update']
-    if constant_update 
-      seconds = constant_update.keys.length > 1 ? constant_update.map {|key, value| (Time.parse(constant_update[key]["end_time"].to_s) - Time.parse(constant_update[key-1]["end_time"].to_s) if key != constant_update.keys[0]).to_i} : [0]
-      setting.value['average_delay'] = seconds.inject(:+)/(seconds.length-1) if seconds.length > 1
-    end
-    setting.save
-  end
-
   def self.log_updates(starttime, endtime)
-    setting = self.setting_record
-    update_record = setting.value['constant_update']
-    if setting.value['constant_update'] === nil
-      setting.value['constant_update'] = Hash.new
-      index = 0 
-    elsif last_update >= MAX_UPDATES_TO_KEEP 
-      index = update_record.keys.last
-      update_record.delete(index - MAX_UPDATES_TO_KEEP)
-      index = index + 1
-    else 
-      index = update_record.keys.last + 1
-    end
-    setting.value['constant_update'][index] = {'start_time' => starttime, 'end_time' => endtime}
-    setting.save
+    self.add_new_log
+    self.update_data(starttime, endtime)
     self.log_delay
   end
 
@@ -45,4 +23,40 @@ class UpdateLog
     setting = self.setting_record
     setting.value['constant_update'] ? setting.value['constant_update'].values.last['end_time'] : nil
   end
+
+  private 
+
+    def self.add_new_log
+      setting = self.setting_record
+      if setting.value['constant_update'] === nil
+        setting.value['constant_update'] = Hash.new
+        setting.save
+      end
+    end
+
+    def self.update_data(starttime, endtime)
+      setting = self.setting_record
+      last_update = setting.value['constant_update'].keys.last
+      if last_update === nil
+        index = 0 
+      elsif last_update >= MAX_UPDATES_TO_KEEP 
+        setting.value['constant_update'].delete(last_update - MAX_UPDATES_TO_KEEP)
+        index = last_update + 1
+      else 
+        index = last_update + 1
+      end
+      setting.value['constant_update'][index] = {'start_time' => starttime, 'end_time' => endtime}
+      setting.save
+    end
+
+    def self.log_delay
+      setting = self.setting_record
+      constant_update = setting.value['constant_update']
+      if constant_update 
+        seconds = constant_update.keys.length > 1 ? constant_update.map {|key, value| (Time.parse(constant_update[key]["end_time"].to_s) - Time.parse(constant_update[key-1]["end_time"].to_s) if key != constant_update.keys[0]).to_i} : [0]
+        setting.value['average_delay'] = seconds.inject(:+)/(seconds.length-1) if seconds.length > 1
+        setting.save
+      end
+    end
+
 end
