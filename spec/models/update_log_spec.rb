ceec65d5a9223d9c842a940e7c7001215123e025
Sage Ross <ragesoss@gmail.com>
2017-12-13 11:15:32 -0800

    Reformatting of UpdateLog spec

--- spec/models/update_log_spec.rb
+++ spec/models/update_log_spec.rb
@@ -7,7 +7,8 @@ describe UpdateLog do
     it 'adds the time of the metrics last update as a value to the settings table' do
       UpdateLog.log_updates(Time.now, Time.now)
       setting = UpdateLog.setting_record
-      expect(setting.value['constant_update'].values.last['end_time']).to be_within(2.seconds).of(Time.now)
+      expect(setting.value['constant_update'].values.last['end_time'])
+        .to be_within(2.seconds).of(Time.now)
     end
 
     it 'adds a maximum of 10 records' do
@@ -42,15 +43,18 @@ describe UpdateLog do
 
   describe '.log_delay' do
     it 'adds the average delay time to the settings table' do
-      UpdateLog.log_updates(Time.parse("2017-12-06 11:11:33 +0100"), Time.parse("2017-12-06 13:11:33 +0100"))
-      UpdateLog.log_updates(Time.parse("2017-12-06 13:13:33 +0100"), Time.parse("2017-12-06 15:13:33 +0100"))
-      UpdateLog.log_updates(Time.parse("2017-12-06 15:30:00 +0100"), Time.parse("2017-12-06 21:30:00 +0100"))
-      UpdateLog.log_updates(Time.parse("2017-12-06 21:45:00 +0100"), Time.parse("2017-12-06 22:45:00 +0100"))
+      UpdateLog.log_updates(Time.parse("2017-12-06 11:11:33 +0100"),
+                            Time.parse("2017-12-06 13:11:33 +0100"))
+      UpdateLog.log_updates(Time.parse("2017-12-06 13:13:33 +0100"),
+                            Time.parse("2017-12-06 15:13:33 +0100"))
+      UpdateLog.log_updates(Time.parse("2017-12-06 15:30:00 +0100"),
+                            Time.parse("2017-12-06 21:30:00 +0100"))
+      UpdateLog.log_updates(Time.parse("2017-12-06 21:45:00 +0100"),
+                            Time.parse("2017-12-06 22:45:00 +0100"))
       delay = UpdateLog.setting_record.value["average_delay"]
       expect(delay).to eq(11469)
     end
   end
-
 
   describe '.average_delay' do
     it 'returns the average delay time for updates' do
@@ -66,10 +70,10 @@ describe UpdateLog do
     end
 
     it 'returns nil if the there was only one update' do
-      UpdateLog.log_updates(Time.parse("2017-12-06 11:11:33 +0100"), Time.parse("2017-12-06 13:11:33 +0100"))
+      UpdateLog.log_updates(Time.parse("2017-12-06 11:11:33 +0100"),
+                            Time.parse("2017-12-06 13:11:33 +0100"))
       delay = UpdateLog.average_delay
       expect(delay).to be(nil)
     end
-
   end
 end
