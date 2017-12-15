48af2c67a54fc5ab249586e39ad117ff54df38e7
mauditecandela <candela.jimenez@gmail.com>
2017-12-15 12:36:28 +0100

    adds references to reducers

--- app/assets/javascripts/reducers/index.js
+++ app/assets/javascripts/reducers/index.js
@@ -1,5 +1,14 @@
 import { combineReducers } from 'redux';
-
+import articleDetails from './article_details';
+import categories from './categories';
+import confirm from './confirm';
+import didYouKnow from './did_you_know';
+import feedback from './feedback';
+import needHelpAlert from './need_help_alert';
+import notifications from './notifications';
+import revisions from './revisions';
+import ui from './ui';
+import userCourses from './user_courses';
 
 const reducer = combineReducers({
   articleDetails,
