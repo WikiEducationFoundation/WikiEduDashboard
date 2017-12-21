9738cdbfc066feef07cef075facd42b396e183bb
mauditecandela <candela.jimenez@gmail.com>
2017-12-21 20:47:03 +0100

    - Adds a migration and a new default_course_type field to the campaigns table - Adds a validation of this field to the campaign model - Sends the default course through the course creator component - Adds the default_campaign_type through the campaigns/show.json.jbuilder - Adds input to Campaigns Edit Form in order to add a default course type - Adds a type state to the course reducer

--- app/assets/javascripts/reducers/course_creator.js
+++ app/assets/javascripts/reducers/course_creator.js
@@ -6,7 +6,7 @@ const initialState = {
 
 export default function course(state = initialState, action) {
   switch (action.type) {
-  default:
-    return state;
+    default:
+      return state;
   }
 }
