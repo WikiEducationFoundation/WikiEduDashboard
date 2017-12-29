0fbcc4163f39eb80ecf7c3d3846f5dfd13444b78
Sage Ross <ragesoss@gmail.com>
2017-12-28 20:17:09 -0800

    Enable errors on missing i18n messages, fix missing ones

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
 
   # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+  config.action_view.raise_on_missing_translations = true
 
   # i18n-js
   # Provides support for localization/translations on the front end utilizing Rails localization.
