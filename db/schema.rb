dde16e0f18c09ecd64dfac3a95d3d9ad11816411
mauditecandela <candela.jimenez@gmail.com>
2018-01-02 16:20:16 +0100

    changes migrations id due to changes in the local files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171221184052) do
+ActiveRecord::Schema.define(version: 20171223152043) do
 
   create_table "alerts", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "course_id"
