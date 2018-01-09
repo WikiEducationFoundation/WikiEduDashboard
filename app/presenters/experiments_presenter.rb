056055b758b8637d55210f95eec4d88de9038c00
Sage Ross <ragesoss@gmail.com>
2018-01-09 12:58:30 -0800

    Update opt-in message and link

--- app/presenters/experiments_presenter.rb
+++ app/presenters/experiments_presenter.rb
@@ -15,9 +15,9 @@ class ExperimentsPresenter
 
   def notification
     {
-      message: 'Would you like to opt in to our research collaboration with Carnegie Mellon University?',
+      message: 'Would you like to add our latest peer discussion service to help your students with their assignment?',
       read_more: 'Read more here.',
-      read_more_link: 'http://kraut.hciresearch.org/sites/kraut.hciresearch.org/files/open/WP-Instructor-ConsentForm-v3.pdf',
+      read_more_link: 'https://wikiedu.org/intertwine-research-collaboration/',
       opt_in_link: opt_in_link,
       opt_out_link: opt_out_link
     }
