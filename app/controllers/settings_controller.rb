344625eebc6b0d4c38b4bbb26bf80641f947b280
Jacob Stoebel <jstoebel@gmail.com>
2018-01-14 14:24:58 -0500

    move settings functionality to its own controller

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -13,15 +13,71 @@ class SettingsController < ApplicationController
   def index
   end
 
-  def new
+  def all_admins
+    respond_to do |format|
+      format.json do
+        # display all admins and super admins
+        admins = User.where(permissions: 1)
+                     .or(User.where(permissions: 3))
+
+        render json: {admins: admins}
+      end
+    end
   end
 
-  def create
+
+  def upgrade_admin
+    update_admin do
+      attempt_admin_upgrade { |resp| render resp and return }
+    end
   end
 
-  def delete
+  def downgrade_admin
+    update_admin do
+      attempt_admin_downgrade { |resp| render resp and return }
+    end
   end
 
-  def destroy
+  private
+
+  ##
+  # handles shared functionality for upgrading and demoting admin status
+  def update_admin
+    respond_to do |format|
+      format.json do
+        @user = User.find_by_username username_param
+        ensure_user_exists { return }
+        yield
+      end
+    end
   end
+
+  ##
+  # attempt to upgrade `user` to admin unless they already are one
+  def attempt_admin_upgrade
+    # if user was already an admin or super admin
+    if @user.admin?
+      message = I18n.t('settings.admin_users.new.already_admin', username: @user.username)
+      yield json: { message: message }, status: 422
+    end
+    # happy path!
+    @user.update_attributes permissions: User::Permissions::ADMIN
+    message = I18n.t('settings.admin_users.new.elevate_success', username: @user.username)
+    yield json: { message: message }, status: 200
+  end
+
+  ##
+  # attempt to upgrade `user` to instructor unless they already are one
+  def attempt_admin_downgrade
+    # already an instructor
+    if @user.instructor_permissions?
+      message = I18n.t('settings.admin_users.remove.already_instructor', username: @user.username)
+      yield json: { message: message }, status: 422
+    end
+    # happy path
+    @user.update_attributes permissions: User::Permissions::INSTRUCTOR
+    message = I18n.t('settings.admin_users.remove.demote_success', username: @user.username)
+    yield json: { message: message }, status: 200
+  end
+
 end
