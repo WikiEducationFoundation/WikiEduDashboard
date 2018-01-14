b2b04fae23a9011e4d9094005ead36a19ebf5638
takidelfin <takidelfin@gmail.com>
2018-01-14 19:05:23 +0100

    Fix `button` aria role

--- app/assets/javascripts/components/students/student.jsx
+++ app/assets/javascripts/components/students/student.jsx
@@ -75,7 +75,7 @@ const Student = createReactClass({
       assignButton = (
         <AssignCell
           {...this.props}
-          role={button}
+          role="button"
           editable={this.props.editable}
           assignments={this.props.assigned}
         />
@@ -84,7 +84,7 @@ const Student = createReactClass({
       reviewButton = (
         <AssignCell
           {...this.props}
-          role={button}
+          role="button"
           editable={this.props.editable}
           assignments={this.props.reviewing}
         />
