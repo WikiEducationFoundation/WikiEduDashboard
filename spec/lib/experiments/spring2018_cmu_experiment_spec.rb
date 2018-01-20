258a95d4489c5e998d804d229cd9066fe2757e96
Sage Ross <ragesoss@gmail.com>
2018-01-19 16:59:29 -0800

    Add automatic insertion of Intertwine training modules upon opt-in

--- spec/lib/experiments/spring2018_cmu_experiment_spec.rb
+++ spec/lib/experiments/spring2018_cmu_experiment_spec.rb
@@ -46,11 +46,14 @@ describe Spring2018CmuExperiment do
   end
 
   let(:course) { create(:course, flags: { Spring2018CmuExperiment::STATUS_KEY => 'email_sent' }) }
-
+  let(:week) { create(:week, course: course) }
+  let(:block) { create(:block, week: week, title: 'Get started on Wikipedia', training_module_ids: [1, 2]) }
   describe '#opt_in and #opt_out' do
-    it 'updates the experiment status of a course to opted_in' do
+    it 'updates the experiment status of a course to opted_in and adds trainings to timeline' do
+      expect(block.training_module_ids).not_to include(18)
       Spring2018CmuExperiment.new(course).opt_in
       expect(course.flags[Spring2018CmuExperiment::STATUS_KEY]).to eq('opted_in')
+      expect(block.reload.training_module_ids).to include(18)
     end
   end
 
