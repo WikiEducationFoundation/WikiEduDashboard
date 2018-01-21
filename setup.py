1fd52ba134d1a382f353bfb34734354f7ac54ccc
psinghal20 <psinghal20@gmail.com>
2018-01-22 03:52:19 +0530

    feat: add calls for OSX script and windows batch file

--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ import platform,subprocess
 
 def deb_setup():
   print ("Your system is found to be debian-based.")
-  subprocess.run("sudo chmod 777 setup/deb-setup.sh && setup/deb-setup.sh",shell=True,check=True)
+  subprocess.run("sudo chmod 775 setup/deb-setup.sh && setup/deb-setup.sh",shell=True,check=True)
 
 def win_setup():
   print ("Your system is found to be Windows")
+  subprocess.run("runas /user:Administrator \"setup\win-setup.bat\"",shell=True,check=True)
 
 def osx_setup():
   print ("Your system is found to be OSX")
+  subprocess.run("sudo chmod 775 setup/deb-setup.sh && setup/deb-setup.sh",shell=True,check=True)
 
 if platform.platform().lower().find('ubuntu') != -1 or platform.platform.lower().find('debian') != -1:
   deb_setup()
