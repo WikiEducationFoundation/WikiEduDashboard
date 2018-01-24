fb791b941ce5d886814f8a352d93775309fa4586
mauditecandela <candela.jimenez@gmail.com>
2018-01-24 11:48:04 +0100

    - Adds specs for Create Requested Account

--- spec/services/create_requested_account_spec.rb
+++ spec/services/create_requested_account_spec.rb
@@ -6,13 +6,15 @@ describe CreateRequestedAccount, focus: true do
   let(:creator) { create(:admin) }
   let(:course) { create(:course) }
   let(:user) { create(:user) }
-  let(:requested_account) { create(:requested_account, course_id: course.id, username: user.username, email: user.email) }
+  let(:requested_account) { create(:requested_account, course_id: course.id, username: user.username, email: "Pepe") }
   let(:subject) do
     described_class.new(requested_account, creator)
   end
 
   it 'creates the requested accounts' do
     stub_account_creation
-    expect(subject.result[:failure]).to_not be_nil
+    allow(UserImporter).to receive(:new_from_username).and_return(user)
+    expect(subject.result[:success]).not_to be_nil
+    expect(user.username).to eq("Ragesock")
   end
 end
