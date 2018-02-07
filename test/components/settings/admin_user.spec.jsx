6d6e422a1b1576e05159fbbd939d11df93fc84f5
Jacob Stoebel <jstoebel@gmail.com>
2018-02-07 10:23:32 -0500

    complete spec for AdminUser

--- test/components/settings/admin_user.spec.jsx
+++ test/components/settings/admin_user.spec.jsx
@@ -1,6 +1,6 @@
 import '../../testHelper';
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import AdminUser from '../../../app/assets/javascripts/components/settings/admin_user.jsx';
 
 import configureMockStore from 'redux-mock-store';
@@ -10,37 +10,89 @@ const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
 describe('AdminUser', () => {
-  it('renders user info', () => {
-    const expectedUser = { id: 1, username: 'testUser', real_name: 'real name', permissions: 3 };
-    const store = mockStore({
-      settings: {
-        adminUsers: [expectedUser],
-        revokingAdmin: {
-          status: false,
-          username: null,
-        }
-      },
-      notifications: [],
+
+  describe('not revoking', () => {
+    let expectedUser;
+    let wrapper;
+    beforeEach(() => {
+      expectedUser = { id: 1, username: 'testUser', real_name: 'real name', permissions: 3 };
+      const store = mockStore({
+        settings: {
+          adminUsers: [expectedUser],
+          revokingAdmin: {
+            status: false,
+            username: null,
+          }
+        },
+        notifications: [],
+      });
+
+      wrapper = mount(<Provider store={store} >
+        <table>
+          <tbody>
+            <AdminUser user={expectedUser} key={1} />
+          </tbody> 
+        </table>
+      </Provider>);
+    })
+
+    it('renders user info', () => {
+      const cells = wrapper.find('td');
+
+      const expectedCellValues = [
+        expectedUser.username,
+        expectedUser.real_name,
+        expectedUser.permissions === 3 ?
+          'Super Admin' :
+          'Admin',
+      ];
+
+      expectedCellValues.forEach((expectedValue, idx) => {
+        expect(
+          cells.at(idx).find('p').first().text()
+        ).to.equal(expectedValue);
+      })
     });
 
-    const wrapper = shallow(<Provider store={store} >
-      <AdminUser />
-    </Provider>);
+    it('renders the revoking button', () => {
+      const button = wrapper.find('td p button').first();
+      expect(button.text())
+        .to.equal(I18n.t('settings.admin_users.remove.revoke_button'))
+      expect(button.hasClass('dark')).to.equal(true)
+    })
+  }) // not revoking
 
-    const cells = wrapper.find('td');
+  describe('revoking', () => {
+    let expectedUser;
+    let wrapper;
+    beforeEach(() => {
+      expectedUser = { id: 1, username: 'testUser', real_name: 'real name', permissions: 3 };
+      const store = mockStore({
+        settings: {
+          adminUsers: [expectedUser],
+          revokingAdmin: {
+            status: true,
+            username: expectedUser.username,
+          }
+        },
+        notifications: [],
+      });
 
-    const expectedCellValues = [
-      expectedUser.username,
-      expectedUser.real_name,
-      expectedUser.permissions === 3 ?
-        'Super Admin' :
-        'Admin',
-    ];
+      wrapper = mount(<Provider store={store} >
+        <table>
+          <tbody>
+            <AdminUser user={expectedUser} key={1} />
+          </tbody> 
+        </table>
+      </Provider>);
+    })
 
-    cells.forEach((cell, idx) => {
-      expect(
-        cell.find('p').first().text()
-      ).to.equal(expectedCellValues[idx]);
-    });
-  });
+    it('renders the revoking button', () => {
+      const button = wrapper.find('td p button').first();
+      expect(button.text())
+        .to.equal(I18n.t('settings.admin_users.remove.revoking_button_working'))
+      expect(button.hasClass('border')).to.equal(true)
+    })
+  })
+
 });
