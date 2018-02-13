011cd81338be3008e8dd1a375c72fea27faeca21
psinghal20 <psinghal20@gmail.com>
2018-02-14 00:52:48 +0530

    fix: change exit status of script when ruby is not found

--- setup/deb-setup.sh
+++ setup/deb-setup.sh
@@ -108,7 +108,7 @@ else
   echo "gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB"
   echo "curl -sSL https://get.rvm.io | bash -s stable"
   echo "rvm install ruby-2.5.0"
-  exit 1;
+  exit 0;
 fi
 
 printf '[*] Installing bundler... \n'
