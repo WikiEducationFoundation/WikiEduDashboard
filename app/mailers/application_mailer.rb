e7f6d1703617a7e167b3b2cd552c93c7351ffbe8
Sage Ross <ragesoss@gmail.com>
2018-02-20 13:57:33 -0800

    Set a default reply-to address

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: "dashboard@#{ENV['dashboard_url']}"
+  default from: "dashboard@#{ENV['dashboard_url']}",
+          reply_to: ENV['default_reply_email']
   layout 'mailer'
   add_template_helper(SurveysUrlHelper)
 end
