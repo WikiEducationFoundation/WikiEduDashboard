eb5d4e4e7109691077963f141688f98c14798d83
mauditecandela <candela.jimenez@gmail.com>
2018-03-01 08:51:33 +0100

    Deletes unneeded require

--- spec/lib/data_cycle/short_update_spec.rb
+++ spec/lib/data_cycle/short_update_spec.rb
@@ -2,7 +2,6 @@
 
 require 'rails_helper'
 require "#{Rails.root}/lib/data_cycle/short_update"
-require "#{Rails.root}/app/services/update_course_revisions"
 
 describe ShortUpdate do
   describe 'on initialization' do
