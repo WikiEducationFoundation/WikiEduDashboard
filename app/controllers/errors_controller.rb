a0bb3015737318feb005d0ca559344b4297864d3
Amit Joki <amitjoki@hotmail.com>
2018-03-11 08:24:21 +0530

    Fat controller thin views

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -5,6 +5,7 @@ class ErrorsController < ApplicationController
   respond_to :html, :json
 
   def file_not_found
+    @message = getMessage
     render status: 404
   end
 
@@ -18,6 +19,16 @@ class ErrorsController < ApplicationController
 
   def incorrect_passcode
     render status: 401
+  end
+
+  private
+
+  def getMessage
+    if params[:endpoint] == 'overview'
+      return "No such course #{params[:school]}/#{params[:titleterm]} exists."
+    else
+      return I18n.t "error_404.header"
+    end
   end
 
   def login_error
