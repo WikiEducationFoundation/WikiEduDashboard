a7277f5337575fc57f20c8ced10560fda2fbd806
vzhz <vehanus@gmail.com>
2018-03-11 17:44:53 -0400

    Remove logic that is now duplicating the extracted YesNoSelector
    Addressing this comment https://github.com/WikiEducationFoundation/WikiEduDashboard/pull/1748/files/3b27c7f7375a5e317238aee402e74203b0289c40#r172085000

--- app/assets/javascripts/components/overview/course_edit_toggle.jsx
+++ app/assets/javascripts/components/overview/course_edit_toggle.jsx
@@ -1,61 +1,15 @@
 import React from 'react';
-import uuid from 'uuid';
-import CourseActions from '../../actions/course_actions.js';
+import YesNoSelector from '../../actions/course_actions.js';
 
-const CourseEditToggle = React.createClass({
-  propTypes: {
-    course: React.PropTypes.object,
-    editable: React.PropTypes.bool
-  },
-
-  componentWillMount() {
-    this.setState({
-      id: uuid.v4()
-    });
-  },
-
-  _handleChange(e) {
-    const course = this.props.course;
-    const courseEditEnabled = e.target.value;
-    if (courseEditEnabled === 'yes') {
-      course.course_edit_enabled = true;
-    } else if (courseEditEnabled === 'no') {
-      course.course_edit_enabled = false;
-    }
-    CourseActions.updateCourse(course);
-  },
-
-  render() {
-    const currentValue = this.props.course.course_edit_enabled;
-    let selector = (
-      <span>
-        <strong>{I18n.t("courses.course_edit_enabled")}:</strong> {currentValue ? 'yes' : 'no'}
-      </span>
+const CourseEditToggle = ({ course, editable }) => {
+  return (
+    <YesNoSelector
+      courseProperty="course_edit_enabled"
+      label={I18n.t("courses.course_edit_enabled")}
+      course={course}
+      editable={editable}
+    />
     );
-    if (this.props.editable) {
-      selector = (
-        <div className="form-group">
-          <span htmlFor={this.state.id}>
-            <strong>{I18n.t("courses.course_edit_enabled")}:</strong>
-          </span>
-          <select
-            id={this.state.id}
-            name="course_edit_enabled"
-            value={currentValue ? 'yes' : 'no'}
-            onChange={this._handleChange}
-          >
-            <option value="yes">yes</option>
-            <option value="no">no</option>
-          </select>
-        </div>
-      );
-    }
-    return (
-      <div className="course_edit_toggle">
-        {selector}
-      </div>
-    );
-  }
-});
+};
 
 export default CourseEditToggle;
