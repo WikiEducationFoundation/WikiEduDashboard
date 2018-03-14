58d5dcd16789b80f7927090b901a4f164f2683c0
Sage Ross <ragesoss@gmail.com>
2018-03-14 10:20:40 -0700

    Fix rubocop violation

--- lib/analytics/course_statistics.rb
+++ lib/analytics/course_statistics.rb
@@ -19,6 +19,7 @@ class CourseStatistics
 
   # For a set of course ids, generate a human-readable summary of what the users
   # in those courses contributed.
+  # rubocop:disable Metrics/MethodLength
   def report_statistics
     report = {
       course_count: @course_ids.uniq.count,
@@ -39,6 +40,7 @@ class CourseStatistics
     report = { @opts[:campaign].to_sym => report } if @opts[:campaign]
     report
   end
+  # rubocop:enable Metrics/MethodLength
 
   def articles_edited
     Article.where(namespace: 0, id: @page_ids)
