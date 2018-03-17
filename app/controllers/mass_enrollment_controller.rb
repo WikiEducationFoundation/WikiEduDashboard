c2103aeafdb2421639a42e7200c25d754d5ab65b
srish <srishakatux@gmail.com>
2018-03-16 19:31:33 -0700

    Move mass enrollment functions to a service
    Fixes #1796

--- app/controllers/mass_enrollment_controller.rb
+++ app/controllers/mass_enrollment_controller.rb
@@ -13,29 +13,9 @@ class MassEnrollmentController < ApplicationController
 
   def add_users
     @course = Course.find_by(slug: params[:course_id])
-
     usernames_list = params[:usernames].lines.map(&:strip)
-    @results = {}
-    usernames_list.each do |username|
-      @results[username] = add_user(username)
-    end
-
+    @results = AddUsers.new(course: @course, usernames_list: usernames_list).add_all_at_once
     UpdateCourseWorker.schedule_edits(course: @course, editing_user: current_user)
-
     render :index
-  end
-
-  private
-
-  def add_user(username)
-    user = find_or_import_user(username)
-    return { failure: 'Not an existing user.' } unless user
-    JoinCourse.new(course: @course, user: user, role: CoursesUsers::Roles::STUDENT_ROLE).result
-  end
-
-  def find_or_import_user(username)
-    user = User.find_by(username: username)
-    user = UserImporter.new_from_username(username, @course.home_wiki) if user.nil?
-    user
   end
 end
