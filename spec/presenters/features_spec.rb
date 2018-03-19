742feffec40f659358bf48fea04cf9233f29c88e
Amit Joki <amitjoki@hotmail.com>
2018-03-19 23:19:49 +0530

    Updated features spec

--- spec/presenters/features_spec.rb
+++ spec/presenters/features_spec.rb
@@ -5,21 +5,12 @@ require_relative '../../app/presenters/features'
 
 describe Features do
   describe '.wiki_ed?' do
-    context 'when the url is dashboard-testing.wikiedu.org' do
+    context 'when wiki_Education is true' do
       before do
         allow(ENV).to receive(:[]).with('wiki_education').and_return('true')
       end
       it 'returns true' do
         expect(Features.wiki_ed?).to eq(true)
-      end
-    end
-
-    context 'when the url is outreachdashboard.wmflabs.org' do
-      before do
-        allow(ENV).to receive(:[]).with('wiki_education').and_return('false')
-      end
-      it 'returns false' do
-        expect(Features.wiki_ed?).to eq(false)
       end
     end
   end
