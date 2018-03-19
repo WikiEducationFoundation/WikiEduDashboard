7997fee277acda9c1cd903e23bf69cf160895bb5
psinghal20 <psinghal20@gmail.com>
2018-03-19 06:05:41 +0530

    feat: add constants and actions for uploads reducer

---
+++ app/assets/javascripts/actions/uploads_actions.js
@@ -0,0 +1,34 @@
+import { RECEIVE_UPLOADS, SORT_UPLOADS, API_FAIL } from '../constants';
+import logErrorMessage from '../utils/log_error_message';
+
+const fetchUploads = (courseId) => {
+  return new Promise((res, rej) => {
+    return $.ajax({
+      type: 'GET',
+      url: `/courses/${courseId}/uploads.json`,
+      success(data) {
+        return res(data);
+      }
+    })
+    .fail((obj) => {
+      logErrorMessage(obj);
+      return rej(obj);
+    });
+  });
+};
+
+export const receiveUploads = (courseId) => dispatch => {
+  return (
+    fetchUploads(courseId)
+      .then(resp => dispatch({
+        type: RECEIVE_UPLOADS,
+        data: resp,
+      }))
+      .catch(resp => dispatch({
+        type: API_FAIL,
+        data: resp
+      }))
+  );
+};
+
+export const sortUploads = key => ({ type: SORT_UPLOADS, key: key });
