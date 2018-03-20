04a67dacd615fcad4f4679a81ce0bfea1193265a
Sage Ross <ragesoss@gmail.com>
2018-03-20 16:10:07 -0700

    Set unique_expiration timeout for course update workers
    By default, unique job locks expire after 30 minutes, so duplicate jobs have been accumulating on P&E Dashboard, where updating all current courses takes longer than that.

--- app/workers/course_data_update_worker.rb
+++ app/workers/course_data_update_worker.rb
@@ -2,8 +2,9 @@
 require_dependency "#{Rails.root}/app/services/update_course_stats"
 
 class CourseDataUpdateWorker
+  THIRTY_DAYS = 60 * 60 * 24 * 30
   include Sidekiq::Worker
-  sidekiq_options unique: :until_executed
+  sidekiq_options unique: :until_executed, unique_expiration: THIRTY_DAYS
 
   def self.update_course(course_id:, queue:)
     CourseDataUpdateWorker.set(queue: queue).perform_async(course_id)
