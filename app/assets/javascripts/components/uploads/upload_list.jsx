a4caef22e3049b1a47abf205a76a2a81935f2654
Yunju <yunju@yunjus-air.attlocal.net>
2018-03-23 17:35:52 -0700

    Update none text

--- app/assets/javascripts/components/uploads/upload_list.jsx
+++ app/assets/javascripts/components/uploads/upload_list.jsx
@@ -1,23 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Editable from '../high_order/editable.jsx';
 import Upload from './upload.jsx';
-import UploadStore from '../../stores/upload_store.js';
-import ServerActions from '../../actions/server_actions.js';
-//import CourseUtils from '../../utils/course_utils.js';
+import CourseUtils from '../../utils/course_utils.js';
 
-const getState = () => ({ uploads: UploadStore.getModels() });
-
-let elements;
-//const none_message={CourseUtils.i18n('uploads_none', course.string_prefix)}
-const UploadList = ({ uploads }) => {
+const UploadList = ({ uploads, course }) => {
+  let elements;
   if (uploads.length > 1) {
       elements = uploads.map(upload => {
         return <Upload upload={upload} key={upload.id} />;
     });
   } else {
-      elements = (<div className="none"><p>There is no image
-      or other media file contributed to Wikimedia Commons.</p></div>);
+      elements = (<div className="none"><p>{CourseUtils.i18n('uploads_none', course.string_prefix)}</p></div>);
 }
 
   return (
@@ -33,4 +26,4 @@ UploadList.propTypes = {
   course: PropTypes.object
 };
 
-export default Editable(UploadList, [UploadStore], ServerActions.saveUploads, getState);
+export default UploadList;
