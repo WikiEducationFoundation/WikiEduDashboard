d2310f8eeffd181fe2f48b7f3c24836984d53e18
Sage Ross <ragesoss@gmail.com>
2018-03-26 09:29:34 -0700

    Don't retry when a course data update fails.
    Failure is typically because a course was deleted, so there's no point retrying. Any still-current course will get added back into the queue if an update fails for some other reason.

--- app/workers/course_data_update_worker.rb
+++ app/workers/course_data_update_worker.rb
@@ -4,7 +4,9 @@ require_dependency "#{Rails.root}/app/services/update_course_stats"
 class CourseDataUpdateWorker
   THIRTY_DAYS = 60 * 60 * 24 * 30
   include Sidekiq::Worker
-  sidekiq_options unique: :until_executed, unique_expiration: THIRTY_DAYS
+  sidekiq_options unique: :until_executed,
+                  unique_expiration: THIRTY_DAYS,
+                  retry: 0 # Move job to the 'dead' queue if it fails
 
   def self.update_course(course_id:, queue:)
     CourseDataUpdateWorker.set(queue: queue).perform_async(course_id)
