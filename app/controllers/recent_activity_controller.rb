6350308bc0ce3df23f8569036e56bf2ecce7ea26
Sage Ross <ragesoss@gmail.com>
2018-03-28 11:42:39 -0700

    Automatic login redirection for ithenticate URLs

--- app/controllers/recent_activity_controller.rb
+++ app/controllers/recent_activity_controller.rb
@@ -7,7 +7,13 @@ class RecentActivityController < ApplicationController
   def index; end
 
   def plagiarism_report
-    require_signed_in
+    unless current_user
+      # If user is not logged in, send user to login and redirect back to the
+      # report upon successful login.
+      redirect_to "/users/auth/mediawiki?origin=#{CGI.escape(request.fullpath)}"
+      return
+    end
+
     ithenticate_id = params[:ithenticate_id]
     report_url = PlagiabotImporter.api_get_url(ithenticate_id: ithenticate_id)
     redirect_to report_url
