924f84855cb61e67d5602360ded9f2951429b4ec
Sage Ross <ragesoss@gmail.com>
2018-03-30 15:18:03 -0700

    Don't include staff on first enrollment alert emails.

--- app/mailers/first_enrolled_student_alert_mailer.rb
+++ app/mailers/first_enrolled_student_alert_mailer.rb
@@ -6,7 +6,6 @@ class FirstEnrolledStudentAlertMailer < ApplicationMailer
     @alert = alert
     set_course_and_users
     params = { to: @instructors.pluck(:email),
-               cc: @admins.pluck(:email),
                bcc: @alert.bcc_to_salesforce_email,
                subject: @alert.main_subject }
     params[:reply_to] = @alert.reply_to unless @alert.reply_to.nil?
@@ -17,7 +16,6 @@ class FirstEnrolledStudentAlertMailer < ApplicationMailer
 
   def set_course_and_users
     @course = @alert.course
-    @admins = @course.nonstudents.where(permissions: 1)
     @instructors = @course.instructors
     @greeted_users = @instructors.pluck(:username).to_sentence # eg, "User, User2, and User3"
     @course_link = "https://#{ENV['dashboard_url']}/courses/#{@course.slug}"
