82d9981d649972b353cdaee4c6f2dc0e0cebeba8
Mariam Ahhttouche <maryam.ahhtouch@gmail.com>
2018-04-04 18:04:13 +0000

    Add jest tests for ContributionStats component

---
+++ test/components/user_profiles/contribution_stats.spec.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import sinon from 'sinon';
+import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import ContributionStats from '../../../app/assets/javascripts/components/user_profiles/contribution_stats.jsx';
+import InstructorStats from '../../../app/assets/javascripts/components/user_profiles/contribution_stats.jsx';
+import StudentStats from '../../../app/assets/javascripts/components/user_profiles/contribution_stats.jsx';
+import '../../testHelper';
+
+describe('ContributionStats', () => {
+    const props = {
+            params: {username: "nol"},
+    };
+    const state = {
+        isStudent: {student: false}
+    }
+    it('calls componentDidMount', () => {
+        sinon.spy(ContributionStats.prototype, 'componentDidMount');
+        const wrapper = mount(<ContributionStats {...props} store={reduxStore} />)
+        expect(ContributionStats.prototype.componentDidMount).to.have.property('callCount', 1);
+        ContributionStats.prototype.componentDidMount.restore();
+    });
+    
+    it('allows us to set props', () => {
+        const wrapper = mount(<ContributionStats {...props} store={reduxStore} />)
+        expect(wrapper.props().params.username).to.equal('nol');
+        wrapper.setProps({params:{ username: 'mei' }});
+        expect(wrapper.props().params.username).to.equal('mei');
+    });
+    
+    it('displays loading while loading', () => {
+        const wrapper = mount(<ContributionStats {...props} store={reduxStore} />)
+        wrapper.setProps({isLoading: true});
+        expect(wrapper.text()).to.contain('Loading');
+    });
+    
+    it('renders content for instructor if user is instructor', () => {
+        let wrapper = mount(<ContributionStats {...props} store={reduxStore} />);
+        wrapper.setState({isInstructor: {instructor: true}});
+        wrapper.setState({isStudent: {student: false}});
+        expect(wrapper.find(InstructorStats)).to.have.length(1);
+        expect(wrapper.find(StudentStats)).to.have.length(0);
+    });
+    
+    it('renders content for student if user is student', () => {
+        let wrapper = mount(<ContributionStats {...props} store={reduxStore} />);
+        wrapper.setState({isStudent: {student: true}});
+        wrapper.setState({isInstructor: {instructor: false}});
+        expect(wrapper.find(StudentStats)).to.have.length(1);
+        expect(wrapper.find(InstructorStats)).to.have.length(0);
+    });
+});
