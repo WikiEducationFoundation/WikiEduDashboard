50dfddbe0f4a264a0d3bbf38842f038dfc70531f
Wendy Yuchen Sun <wendyyuchensun@gmail.com>
2018-04-24 21:50:30 +0800

    Always show type column in alert table

--- app/assets/javascripts/components/alerts/alert.jsx
+++ app/assets/javascripts/components/alerts/alert.jsx
@@ -6,7 +6,7 @@ const Alert = ({ alert }) => {
   return (
     <tr className="alert">
       <td className="desktop-only-tc date">{moment(alert.created_at).format('YYYY-MM-DD   h:mm A')}</td>
-      <td className="desktop-only-tc">{alert.type}</td>
+      <td className="alert-type">{alert.type}</td>
       <td className="desktop-only-tc"><a target="_blank" href={`/courses/${alert.course_slug}`}>{alert.course}</a></td>
       <td className="desktop-only-tc"><a target="_blank" href={`/users/${alert.user}`}>{alert.user}</a></td>
       <td><a target="_blank" href={alert.article_url}>{alert.article}</a></td>
