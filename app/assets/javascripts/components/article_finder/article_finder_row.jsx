83061a871af5d6a3bc4c6bd45a79b0277688ca0e
psinghal20 <psinghal20@gmail.com>
2018-05-24 23:27:20 +0530

    refactor: replace magic numbers with constants

--- app/assets/javascripts/components/article_finder/article_finder_row.jsx
+++ app/assets/javascripts/components/article_finder/article_finder_row.jsx
@@ -1,38 +1,41 @@
 import React from 'react';
 import createReactClass from 'create-react-class';
 
+import { TITLE_RECEIVED, PAGEASSESSMENT_RECEIVED,
+ REVISION_RECEIVED, REVISIONSCORE_RECEIVED, PAGEVIEWS_RECEICED } from "../../constants";
+
 const ArticleFinderRow = createReactClass({
   render() {
     let pageviews;
-    if (this.props.article.fetchState === 4) {
+    if (this.props.article.fetchState === REVISIONSCORE_RECEIVED) {
      pageviews = (<div className="results-loading"> &nbsp; &nbsp; </div>);
     }
     if (this.props.article.pageviews) {
       pageviews = this.props.article.pageviews;
     }
-    else if (this.props.article.fetchState === 5) {
+    else if (this.props.article.fetchState === PAGEVIEWS_RECEICED) {
       pageviews = (<div>Page Views not found!</div>);
     }
 
     let revScore;
-    if (this.props.article.fetchState === 2 || this.props.article.fetchState === 3) {
+    if (this.props.article.fetchState === PAGEASSESSMENT_RECEIVED || this.props.article.fetchState === REVISION_RECEIVED) {
      revScore = (<div className="results-loading"> &nbsp; &nbsp; </div>);
     }
     if (this.props.article.revScore) {
       revScore = this.props.article.revScore;
     }
-    else if (this.props.article.fetchState >= 4) {
+    else if (this.props.article.fetchState >= REVISIONSCORE_RECEIVED) {
       revScore = (<div>Estimation Score not found!</div>);
     }
 
     let grade;
-    if (this.props.article.fetchState === 1) {
+    if (this.props.article.fetchState === TITLE_RECEIVED) {
       grade = (<div className="results-loading"> &nbsp; &nbsp; </div>);
     }
     if (this.props.article.grade) {
       grade = this.props.article.grade;
     }
-    else if (this.props.article.fetchState >= 2) {
+    else if (this.props.article.fetchState >= PAGEASSESSMENT_RECEIVED) {
       grade = (<div>Not rated</div>);
     }
     return (
--- app/assets/javascripts/reducers/article_finder.js
+++ app/assets/javascripts/reducers/article_finder.js
@@ -1,5 +1,8 @@
 import _ from 'lodash';
-import { RECEIVE_CATEGORY_RESULTS, CLEAR_FINDER_STATE, RECEIVE_ARTICLE_PAGEVIEWS, RECEIVE_ARTICLE_PAGEASSESSMENT, RECEIVE_ARTICLE_REVISION, RECEIVE_ARTICLE_REVISIONSCORE } from "../constants";
+import { RECEIVE_CATEGORY_RESULTS, CLEAR_FINDER_STATE, RECEIVE_ARTICLE_PAGEVIEWS,
+ RECEIVE_ARTICLE_PAGEASSESSMENT, RECEIVE_ARTICLE_REVISION,
+ RECEIVE_ARTICLE_REVISIONSCORE, TITLE_RECEIVED, PAGEASSESSMENT_RECEIVED,
+ REVISION_RECEIVED, REVISIONSCORE_RECEIVED, PAGEVIEWS_RECEICED } from "../constants";
 
 const initialState = {
   articles: {},
@@ -20,7 +23,7 @@ export default function articleFinder(state = initialState, action) {
         newStateArticles[data.title] = {
           pageid: data.pageid,
           ns: data.ns,
-          fetchState: 1
+          fetchState: TITLE_RECEIVED
         };
       });
       return {
@@ -32,7 +35,7 @@ export default function articleFinder(state = initialState, action) {
       const newStateArticles = _.cloneDeep(state.articles);
       const title = action.data.title.replace(/_/g, ' ');
       newStateArticles[title].pageviews = action.data.pageviews;
-      newStateArticles[title].fetchState = 5;
+      newStateArticles[title].fetchState = PAGEVIEWS_RECEICED;
       return {
         articles: newStateArticles,
         loading: false
@@ -42,7 +45,7 @@ export default function articleFinder(state = initialState, action) {
       const newStateArticles = _.cloneDeep(state.articles);
       action.data.forEach((data) => {
         newStateArticles[data.title].grade = data.classGrade;
-        newStateArticles[data.title].fetchState = 2;
+        newStateArticles[data.title].fetchState = PAGEASSESSMENT_RECEIVED;
       });
       return {
         articles: newStateArticles,
@@ -53,7 +56,7 @@ export default function articleFinder(state = initialState, action) {
       const newStateArticles = _.cloneDeep(state.articles);
       action.data.forEach((data) => {
         newStateArticles[data.title].revid = data.revid;
-        newStateArticles[data.title].fetchState = 3;
+        newStateArticles[data.title].fetchState = REVISION_RECEIVED;
       });
       return {
         articles: newStateArticles,
@@ -65,7 +68,7 @@ export default function articleFinder(state = initialState, action) {
       action.data.forEach((data) => {
         const article = _.find(newStateArticles, { revid: parseInt(data.revid) });
         article.revScore = data.revScore;
-        article.fetchState = 4;
+        article.fetchState = REVISIONSCORE_RECEIVED;
       });
       return {
         articles: newStateArticles,
