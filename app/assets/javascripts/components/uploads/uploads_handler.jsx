cb37edf42be7905fe35c8bfc9b81bb37767b4ab7
ihsavru <vurvashi11@gmail.com>
2018-05-27 00:47:27 +0530

    code improvement + add yarn.lock

--- app/assets/javascripts/components/uploads/uploads_handler.jsx
+++ app/assets/javascripts/components/uploads/uploads_handler.jsx
@@ -28,8 +28,7 @@ const UploadsHandler = createReactClass({
 
   componentWillReceiveProps(nextProps) {
     const data = nextProps.uploads.slice(this.state.offset, this.state.offset + this.state.perPage);
-    this.setState({ data: data });
-    this.setState({ pageCount: nextProps.uploads.length / this.state.perPage });
+    this.setState({ data: data, pageCount: nextProps.uploads.length / this.state.perPage });
   },
 
   setUploadData() {
