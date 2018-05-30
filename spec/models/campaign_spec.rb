622e82792133d047554d7ca64fd0b83ddb9be078
Sage Ross <ragesoss@gmail.com>
2018-05-30 08:44:35 -0700

    Add spec for Campaign#course_string_prefix

--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -132,4 +132,20 @@ describe Campaign do
       expect(campaign.end).to eq(Time.new(2016, 2, 10, 23, 59, 59, '+00:00'))
     end
   end
+
+  describe '#course_string_prefix' do
+    it 'is the dashboard default when default_course_type is not set' do
+      result = Campaign.new(title: 'no default').course_string_prefix
+      expect(result).to eq('courses')
+    end
+
+    it 'is based on default_course_type when there is one' do
+      result = Campaign.new(title: 'generic default',
+                            default_course_type: 'BasicCourse').course_string_prefix
+      expect(result).to eq('courses_generic')
+      result = Campaign.new(title: 'classroom default',
+                            default_course_type: 'ClassroomProgramCourse').course_string_prefix
+      expect(result).to eq('courses')
+    end
+  end
 end
