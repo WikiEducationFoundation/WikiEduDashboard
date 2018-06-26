e630c8a87422b0c7b65305f143a607f93d5f09b6
Sage Ross <ragesoss@gmail.com>
2018-06-26 12:27:57 -0700

    Training modules should still be updatable if updates are paused

--- spec/lib/data_cycle/training_update_spec.rb
+++ spec/lib/data_cycle/training_update_spec.rb
@@ -6,15 +6,6 @@ require "#{Rails.root}/lib/data_cycle/training_update"
 describe TrainingUpdate do
   let(:subject) { described_class.new(module_slug: 'all').result }
 
-  context 'when updates are paused' do
-    before do
-      allow_any_instance_of(described_class).to receive(:updates_paused?).and_return(true)
-    end
-    it 'returns with a relevant message' do
-      expect(subject).to match(/Updates are currently paused/)
-    end
-  end
-
   context 'when another training update is running' do
     before do
       allow_any_instance_of(described_class).to receive(:update_running?).and_return(true)
