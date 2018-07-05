c940732d1cec73b5822f94e95535e86b5e640b62
psinghal20 <psinghal20@gmail.com>
2018-07-06 00:01:25 +0530

    fix: remove search for assignments when no course is associated with article finder

--- app/assets/javascripts/components/article_finder/article_finder.jsx
+++ app/assets/javascripts/components/article_finder/article_finder.jsx
@@ -225,11 +225,13 @@ const ArticleFinder = createReactClass({
     if (this.state.isSubmitted && !this.props.loading) {
       const elements = _.map(this.props.articles, (article, title) => {
         let assignment;
-        if (this.props.current_user.isNonstudent) {
-          assignment = _.find(this.props.assignments, { article_title: title, user_id: null });
-        }
-        else if (this.props.current_user.role === STUDENT_ROLE) {
-          assignment = _.find(this.props.assignments, { article_title: title, user_id: this.props.current_user.id });
+        if (this.props.course_id) {
+          if (this.props.current_user.isNonstudent) {
+            assignment = _.find(this.props.assignments, { article_title: title, user_id: null });
+          }
+          else if (this.props.current_user.role === STUDENT_ROLE) {
+            assignment = _.find(this.props.assignments, { article_title: title, user_id: this.props.current_user.id });
+          }
         }
         return (
           <ArticleFinderRow
