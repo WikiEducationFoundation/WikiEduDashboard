f1c0bf587000c22d51002098d4cd1697a9fe2a2d
Sage Ross <ragesoss@gmail.com>
2018-07-23 11:43:49 -0700

    Fix specs
    Recently on English Wikipedia, all Bplus-class articles were merged in B-class, which broke the WikiApi spec.

--- app/services/update_course_stats.rb
+++ app/services/update_course_stats.rb
@@ -52,7 +52,9 @@ class UpdateCourseStats
     return unless debug?
     @sentry_logs ||= {}
     @sentry_logs[step] = Time.zone.now
-    Raven.capture_message("#{@course.title} update: #{step}", level: 'warn', extra: { logs: @sentry_logs })
+    Raven.capture_message "#{@course.title} update: #{step}",
+                          level: 'warn',
+                          extra: { logs: @sentry_logs }
   end
 
   def debug?
