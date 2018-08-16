91b7311456ba0b94433490dfba4f50591b8ef467
Sage Ross <ragesoss@gmail.com>
2018-08-16 12:48:31 -0700

    Extend block cleanup script

--- docs/cleanup_scripts/relative_training_urls.rb
+++ docs/cleanup_scripts/relative_training_urls.rb
@@ -8,3 +8,13 @@ bad_block_ids.each do |block_id|
   block.content.gsub! '"../../../training', '"https://dashboard.wikiedu.org/training'
   block.save
 end
+
+# Fixes training links that lack the domain, so that TinyMCE won't turn them
+# into relative links upon editing the block.
+more_bad_block_ids = Block.all.select { |b| b.content[/"\/training/] }.map(&:id)
+
+more_bad_block_ids.each do |block_id|
+  block = Block.find block_id
+  block.content.gsub! '"/training', '"https://dashboard.wikiedu.org/training'
+  block.save
+end
