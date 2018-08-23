2a7e80f99856152928f261613abee568abcc890a
Tory Dannenberg <rfdberg@gmail.com>
2018-08-23 15:41:42 -0600

    Fix user uploads tests

--- test/components/user_profiles/user_uploads.spec.jsx
+++ test/components/user_profiles/user_uploads.spec.jsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import ReactTestUtils from 'react-dom/test-utils';
-import { shallow, mount } from 'enzyme';
-
+import { shallow } from 'enzyme';
 import '../../testHelper';
-
 import UserUploads from '../../../app/assets/javascripts/components/user_profiles/user_uploads.jsx';
 
 describe('UserUploads', () => {
-  it('should match the snapshot with no uploads', () => {
-    const message = 'This user has not contributed any images or other media files to Wikimedia Commons.';
+  it('should not render the upload list if there are no user uploads', () => {
     const mockUploads = [];
-    const wrapper = shallow(<UserUploads courses={courses} uploads={mockUploads} />);
+    const wrapper = shallow(<UserUploads uploads={mockUploads} />);
     expect(wrapper.find('UploadList').length).to.equal(0);
   });
 
-  it('should match the snapshot with uploads', () => {
-
+  it('should render the upload list component if there are user uploads', () => {
     const mockUploads = [{ id: 1 }, { id: 2 }];
     const wrapper = shallow(<UserUploads uploads={mockUploads} />);
-
     expect(wrapper.find('UploadList').length).to.equal(1);
   });
 });
