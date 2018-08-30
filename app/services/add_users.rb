bdd24d4b816b76f13a4038065264b6b1db49c3b1
Sage Ross <ragesoss@gmail.com>
2018-08-30 09:22:20 -0700

    Fix mass addition of users for non-normalized usernames
    MassEnrollmentWorker was hitting errors for users that are added with a non-normalized form of the username, because the original input was used as the key in the results hash rather than the normalized version of the username that we store in the User record and find the user by.
    
    This refactor of AddUsers puts the normalized username in place of the submitted text, so that the usernames of successfully-added users alway match the User records.

--- app/services/add_users.rb
+++ app/services/add_users.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require_dependency "#{Rails.root}/lib/importers/user_importer"
 
 class AddUsers
   def initialize(course:, usernames_list:)
@@ -7,24 +8,28 @@ class AddUsers
   end
 
   def add_all_at_once
-    results = {}
+    @results = {}
     @usernames_list.each do |username|
-      results[username] = add_one_by_one(username)
+      add_one_by_one(username)
     end
-    return results
+    return @results
   end
 
   private
 
   def add_one_by_one(username)
-    user = find_or_import_user(username)
-    return { failure: 'Not an existing user.' } unless user
-    JoinCourse.new(course: @course, user: user, role: CoursesUsers::Roles::STUDENT_ROLE).result
+    user = find_or_import_user(username) { return }
+    @results[user.username] = JoinCourse.new(course: @course,
+                                             user: user,
+                                             role: CoursesUsers::Roles::STUDENT_ROLE).result
   end
 
   def find_or_import_user(username)
     user = User.find_by(username: username)
-    user = UserImporter.new_from_username(username, @course.home_wiki) if user.nil?
-    user
+    user ||= UserImporter.new_from_username(username, @course.home_wiki)
+    return user if user.present?
+
+    @results[username] = { failure: 'Not an existing user.' }
+    yield
   end
 end
