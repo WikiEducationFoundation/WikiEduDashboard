d3f48763332c5e1d15ecafa021d1f787db108cfa
Sage Ross <ragesoss@gmail.com>
2018-09-06 16:23:49 -0700

    Change 'Enrollment' button to 'Add/Remove Students'
    This was a fairly frequent point of confusion for instructors, and I think for P&E Dashboard users as well.

--- spec/controllers/self_enrollment_controller_spec.rb
+++ spec/controllers/self_enrollment_controller_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require "#{Rails.root}/lib/wiki_course_edits"
+require "#{Rails.root}/lib/wiki_preferences_manager"
 
 describe SelfEnrollmentController do
   describe '#enroll_self' do
