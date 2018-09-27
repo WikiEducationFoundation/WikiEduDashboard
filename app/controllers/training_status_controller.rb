96ea4f6c7e7c003278ddf18e2bff6469c2c62cca
Amit Joki <amitjoki@gmail.com>
2018-09-27 22:22:18 +0530

    Remove N+1 Queries (#2016)
    Avoid N+1 query for TrainingModulesUsers in the user profile training status endpoint

--- app/controllers/training_status_controller.rb
+++ app/controllers/training_status_controller.rb
@@ -10,7 +10,5 @@ class TrainingStatusController < ApplicationController
 
   def user
     @user = User.find_by(username: params[:username])
-    training_module_ids = @user.training_modules_users.pluck(:training_module_id)
-    @training_modules = TrainingModule.all.select { |tm| training_module_ids.include?(tm.id) }
   end
 end
