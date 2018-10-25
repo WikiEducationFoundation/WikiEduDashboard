e2a922afa944d41aecb2613009e7f79ad54c27de
Sage Ross <sage@ragesoss.com>
2018-10-25 11:08:33 -0700

    Move alert subtypes to their own directory

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module WikiEduDashboard
   class Application < Rails::Application
+    config.autoload_paths += Dir[Rails.root.join("app", "models", "{*/}")]
+
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: true,
