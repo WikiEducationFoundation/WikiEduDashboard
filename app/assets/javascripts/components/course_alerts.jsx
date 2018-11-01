17fbc47ad341d3e3c7e82b166345a1e49323c435
takidelfin <takidelfin@gmail.com>
2018-11-01 12:18:53 +0100

    New 'CourseAlert' component for displaying alerts in 'Course' view

---
+++ app/assets/javascripts/components/course_alerts.jsx
@@ -0,0 +1,122 @@
+import moment from 'moment';
+import React from 'react';
+import PropTypes from 'prop-types';
+import createReactClass from 'create-react-class';
+
+import CourseUtils from '../utils/course_utils';
+import CourseAlert from './course_alert';
+import OptInNotification from './common/opt_in_notification';
+
+const CourseAlerts = createReactClass({
+  displayName: 'CourseAlerts',
+
+  propTypes: {
+    userRoles: PropTypes.object.isRequired,
+    course: PropTypes.object.isRequired,
+    weeks: PropTypes.any.isRequired,
+    courseLinkParams: PropTypes.string.isRequired,
+    usersLoaded: PropTypes.bool.isRequired,
+    studentCount: PropTypes.number.isRequired,
+    updateCourse: PropTypes.func.isRequired,
+    persistCourse: PropTypes.func.isRequired,
+    dismissNotification: PropTypes.func.isRequired
+  },
+
+  submit(e) {
+    e.preventDefault();
+    if (!confirm(I18n.t('courses.warn_mirrored'))) { return; }
+    this.props.updateCourse({ submitted: true });
+    return this.props.persistCourse(this.props.course.slug);
+  },
+
+  dismissSurvey(surveyNotificationId) {
+    if (confirm(I18n.t('courses.dismiss_survey_confirm'))) {
+      return this.props.dismissNotification(surveyNotificationId);
+    }
+  },
+
+  render() {
+    const course = this.props.course;
+    const userRoles = this.props.userRoles;
+
+    const alerts = [];
+
+    // //////////////////////////////////
+    // Admin / Instructor notifications /
+    // //////////////////////////////////
+    // For unpublished courses, when viewed by an instructor or admin
+    if (userRoles.isNonstudent && !course.legacy && !course.published) {
+      // If it's an unsubmitted ClassroomProgramCourse
+      const isUnsubmittedClassroomProgramCourse = !course.submitted && course.type === 'ClassroomProgramCourse';
+      if (isUnsubmittedClassroomProgramCourse) {
+        // Show submit button if there is a timeline with trainings, or user is admin.
+        if (CourseUtils.hasTrainings(this.props.weeks) || userRoles.isAdmin) {
+          alerts.push(<CourseAlert key="submit" actionMessage="application.submit" buttonLink="#" onClick={this.submit}>courses.review_timeline</CourseAlert>);
+          // Show 'add trainings' message if there is a timeline with no trainings
+        } else if (this.props.weeks.length) {
+          alerts.push(<CourseAlert key="submit" actionMessage="courses.training_nav" buttonLink={`${this.props.courseLinkParams}/timeline`}>courses.add_trainings</CourseAlert>);
+          // Show 'create a timeline' message if there is no timeline.
+        } else {
+          alerts.push(<CourseAlert key="submit" actionMessage="courses.launch_wizard" buttonLink={`${this.props.courseLinkParams}/timeline/wizard`}>courses.create_timeline</CourseAlert>);
+        }
+      }
+      if (!(course.type === 'ClassroomProgramCourse')) {
+        alerts.push(<CourseAlert key="noCampaign">courses.no_campaign</CourseAlert>);
+      }
+      // When the course has been submitted
+      if (course.submitted) {
+        // Show instructors the 'submitted' notice.
+        if (!userRoles.isAdmin) {
+          alerts.push(<CourseAlert key="submit">courses.submitted_note</CourseAlert>);
+          // Instruct admins to approve the course by adding a campaign.
+        } else {
+          const homeLink = `${this.props.courseLinkParams}/home`;
+          alerts.push(<CourseAlert key="publish" courseLink={homeLink} actionMessage="courses.overview">courses.submitted_admin</CourseAlert>);
+        }
+      }
+    }
+
+    // For published courses with no students, highlight the enroll link
+    const hasNoStudents = this.props.usersLoaded && this.props.studentCount === 0;
+    if (userRoles.isNonstudent && course.published && hasNoStudents && !course.legacy) {
+      const enrollEquals = '?enroll=';
+      const url = window.location.origin + this.props.courseLinkParams + enrollEquals + course.passcode;
+      alerts.push(<CourseAlert key="enroll" buttonLink={url} actionMessage={url}>{course.string_prefix}published</CourseAlert>);
+    }
+
+    // ////////////////////////
+    // Training notifications /
+    // ////////////////////////
+    if (course.incomplete_assigned_modules && course.incomplete_assigned_modules.length) {
+      // `table` key is because it comes back as an openstruct
+      const module = course.incomplete_assigned_modules[0].table;
+      const messageKey = moment().isAfter(module.due_date, 'day') ? 'courses.training_overdue' : 'courses.training_due';
+
+      alerts.push(<CourseAlert key="upcoming_module" buttonLink={module.link} actionClassName="pull-right" actionMessage="courses.training_nav">{I18n.t(messageKey, { title: module.title, date: module.due_date })}</CourseAlert>);
+    }
+
+    // //////////////////////
+    // Survey notifications /
+    // //////////////////////
+    if (course.survey_notifications && course.survey_notifications.length) {
+      course.survey_notifications.forEach((notification) => {
+        const dismissOnClick = () => this.dismissSurvey(notification.id);
+        return alerts.push(<CourseAlert key={`survey_notification_${notification.id}`} className="notification--survey" actionLink={notification.survey_url} actionClassName="pull-right" actionMessage="courses.survey.link" components={<button className="button small pull-right border inverse-border" onClick={dismissOnClick}>{I18n.t('courses.dismiss_survey')}</button>}>{notification.message || 'courses.survey.notification_message'}</CourseAlert>);
+      });
+    }
+
+    // //////////////////////////
+    // Experiment notifications /
+    // //////////////////////////
+    if (course.experiment_notification) {
+      alerts.push(<OptInNotification notification={course.experiment_notification} key="opt_in" />);
+    }
+    return (
+      <div className="course-alerts">
+        {alerts}
+      </div>
+    );
+  }
+});
+
+export default CourseAlerts;
