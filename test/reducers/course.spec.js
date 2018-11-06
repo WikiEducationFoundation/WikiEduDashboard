538beed9c29ec831857ba0be382b579bc204b27c
AZReed <azreed@gmail.com>
2018-11-06 00:32:26 -0300

    course reducer test

--- test/reducers/course.spec.js
+++ test/reducers/course.spec.js
@@ -1,9 +1,40 @@
 import deepFreeze from 'deep-freeze';
 import course from '../../app/assets/javascripts/reducers/course';
-import { UPDATE_COURSE } from '../../app/assets/javascripts/constants';
+import { UPDATE_COURSE, RECEIVE_COURSE, RECEIVE_COURSE_UPDATE, PERSISTED_COURSE } from '../../app/assets/javascripts/constants';
 import '../testHelper';
 
 describe('course reducer', () => {
+
+  it('receives a course via RECEIVE_COURSE', () => {
+    const initialState = {};
+    const action = { type: RECEIVE_COURSE, data: { course: { title: 'title' } }  };
+    deepFreeze(initialState);
+
+    const newState = course(initialState, action);
+    expect(newState).to.deep.eq(action.data.course);
+  });
+
+  it('receives a course update via RECEIVE_COURSE_UPDATE with newStats', () => {
+    const initialState = {};
+    const action = { type: RECEIVE_COURSE_UPDATE, data: { course: { title: 'title' } }  };
+    deepFreeze(initialState);
+
+    const newStats = [ "createdCount", "editedCount", "editCount", "studentCount", "wordCount", "viewCount", "uploadCount"];
+
+    const newState = course(initialState, action);
+    expect(newState.title).to.deep.eq(action.data.course.title);
+    expect(newState.newStats).to.have.all.keys(newStats);
+  });
+
+  it('persisted course via PERSISTED_COURSE', () => {
+    const initialState = {description: 'initial description'};
+    const action = { type: PERSISTED_COURSE, data: { course: { title: 'new title' } }  };
+    deepFreeze(initialState);
+
+    const newState = course(initialState, action);
+    expect(newState).to.deep.eq({description: 'initial description', title: 'new title'})
+  });
+
   it('updates an attribute via UPDATE_COURSE', () => {
     const initialState = { title: 'old title', term: 'old term' };
     const action = { type: UPDATE_COURSE, course: { title: 'new title' } };
