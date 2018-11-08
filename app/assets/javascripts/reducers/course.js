ecddc7f6139269aabf9c2b2db71da52eabf0121c
AZReed <azreed@gmail.com>
2018-11-08 15:48:40 -0300

    minor refactorings

--- app/assets/javascripts/reducers/course.js
+++ app/assets/javascripts/reducers/course.js
@@ -49,7 +49,7 @@ export default function course(state = initialState, action) {
       return { ...action.data.course };
     case RECEIVE_INITIAL_CAMPAIGN: {
       const campaign = action.data.campaign;
-      const newState = {
+      return {
         ...state,
         initial_campaign_id: campaign.id,
         initial_campaign_title: campaign.title,
@@ -57,7 +57,6 @@ export default function course(state = initialState, action) {
         type: campaign.default_course_type,
         passcode: campaign.default_passcode
       };
-      return newState;
     }
     case ADD_CAMPAIGN:
     case DELETE_CAMPAIGN:
@@ -65,9 +64,10 @@ export default function course(state = initialState, action) {
     case RECEIVE_COURSE_CLONE:
       return { ...action.data.course };
     case DISMISS_SURVEY_NOTIFICATION: {
-      const newState = { ...state };
-      newState.survey_notifications = _.reject(state.survey_notifications, { id: action.id });
-      return newState;
+      return {
+        ...state,
+        survey_notifications: _.reject(state.survey_notifications, { id: action.id })
+      };
     }
     case TOGGLE_EDITING_SYLLABUS:
       return { ...state, editingSyllabus: !state.editingSyllabus };
