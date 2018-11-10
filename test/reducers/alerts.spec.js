9993c42ea5f018232e781d3b551a9b9a6f998fda
Dmitry Maksyoma <ledestin@gmail.com>
2018-11-10 21:53:39 +1300

    Move deepFreeze() calls right after variable definition

--- test/reducers/alerts.spec.js
+++ test/reducers/alerts.spec.js
@@ -10,11 +10,11 @@ import '../testHelper';
 describe('course reducer', () => {
   it('assigns alerts to passed alerts and sorts them via RECEIVE_ALERTS', () => {
     const initialState = { alerts: [] };
+    deepFreeze(initialState);
     const action = { type: RECEIVE_ALERTS,
       data: { alerts: [{ created_at: 1 }, { created_at: 2 }] }
     };
     const expectedSortedAlerts = [{ created_at: 2 }, { created_at: 1 }];
-    deepFreeze(initialState);
 
     const newState = alerts(initialState, action);
     expect(newState.alerts).to.deep.eq(expectedSortedAlerts);
@@ -22,9 +22,9 @@ describe('course reducer', () => {
 
   it('sorts existing alerts via SORT_ALERTS', () => {
     const initialState = { alerts: [{ custom_key: 2 }, { custom_key: 1 }] };
+    deepFreeze(initialState);
     const action = { type: SORT_ALERTS, key: 'custom_key' };
     const expectedSortedAlerts = [{ custom_key: 1 }, { custom_key: 2 }];
-    deepFreeze(initialState);
 
     const newState = alerts(initialState, action);
     expect(newState.alerts).to.deep.eq(expectedSortedAlerts);
@@ -33,8 +33,8 @@ describe('course reducer', () => {
 
   it('assigns selectedFilters via FILTER_ALERTS', () => {
     const initialState = { alerts: [] };
-    const action = { type: FILTER_ALERTS, selectedFilters: 'foo' };
     deepFreeze(initialState);
+    const action = { type: FILTER_ALERTS, selectedFilters: 'foo' };
 
     const newState = alerts(initialState, action);
     expect(newState.selectedFilters).to.eq('foo');
