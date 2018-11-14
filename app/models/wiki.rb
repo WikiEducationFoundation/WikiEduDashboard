839cc583f56457d4aa20d9605d307d111b332a37
Sage Ross <ragesoss@gmail.com>
2018-11-14 09:42:17 -0800

    Add more wiki languages
    I went through the lists of 1000+ article Wikipedias and lower, and added all the missing languages. This includes Tulu (tcy), which users are waiting for: https://phabricator.wikimedia.org/T209235
    
    Hopefully this is all the new Wikipedia languages since the last time this was updated.

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -37,19 +37,19 @@ class Wiki < ApplicationRecord
   validates_inclusion_of :project, in: PROJECTS
 
   LANGUAGES = %w[
-    aa ab ace af ak als am an ang ar arc arz as ast av ay az azb
+    aa ab ace ady af ak als am an ang ar arc arz as ast atj av ay az azb
     ba bar bat-smg bcl be be-tarask be-x-old bg bh bi bjn bm bn bo bpy br bs
     bug bxr ca cbk-zam cdo ce ceb ch cho chr chy ckb cmn co commons cr crh cs csb cu
-    cv cy cz da de diq dk dsb dv dz ee egl el eml en eo epo es et eu ext fa
-    ff fi fiu-vro fj fo fr frp frr fur fy ga gag gan gd gl glk gn gom got gsw
+    cv cy cz da de din diq dk dsb dty dv dz ee egl el eml en eo epo es et eu ext fa
+    ff fi fiu-vro fj fo fr frp frr fur fy ga gag gan gd gl glk gn gom gor got gsw
     gu gv ha hak haw he hi hif ho hr hsb ht hu hy hz ia id ie ig ii ik ilo
-    incubator io is it iu ja jbo jp jv ka kaa kab kbd kg ki kj kk kl km kn ko
-    koi kr krc ks ksh ku kv kw ky la lad lb lbe lez lg li lij lmo ln lo lrc lt
+    incubator inh io is it iu ja jam jbo jp jv ka kaa kab kbd kbp kg ki kj kk kl km kn ko
+    koi kr krc ks ksh ku kv kw ky la lad lb lbe lez lfn lg li lij lmo ln lo lrc lt
     ltg lv lzh mai map-bms mdf mg mh mhr mi min minnan mk ml mn mo mr mrj ms mt
     mus mwl my myv mzn na nah nan nap nb nds nds-nl ne new ng nl nn no nov nrm
-    nso nv ny oc om or os pa pag pam pap pcd pdc pfl pi pih pl pms pnb pnt ps
-    pt qu rm rmy rn ro roa-rup roa-tara ru rue rup rw sa sah sc scn sco sd se
-    sg sgs sh si simple sk sl sm sn so sq sr srn ss st stq su sv sw szl ta te
+    nso nv ny oc olo om or os pa pag pam pap pcd pdc pfl pi pih pl pms pnb pnt ps
+    pt qu rm rmy rn ro roa-rup roa-tara ru rue rup rw sa sah sat sc scn sco sd se
+    sg sgs sh si simple sk sl sm sn so sq sr srn ss st stq su sv sw szl ta tcy te
     tet tg th ti tk tl tn to tpi tr ts tt tum tw ty tyv udm ug uk ur uz ve
     vec vep vi vls vo vro w wa war wikipedia wo wuu xal xh xmf yi yo yue za
     zea zh zh-cfr zh-classical zh-cn zh-min-nan zh-tw zh-yue zu
