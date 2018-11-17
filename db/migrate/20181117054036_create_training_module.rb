688dcfc7d642fedc030f28838e8fad45bcab9905
Amit Joki <amitjoki@hotmail.com>
2018-11-17 11:37:32 +0530

    Create migrations

---
+++ db/migrate/20181117054036_create_training_module.rb
@@ -0,0 +1,13 @@
+class CreateTrainingModule < ActiveRecord::Migration[5.2]
+  def change
+    create_table :training_modules do |t|
+      t.string :name
+      t.string :estimated_ttc
+      t.string :wiki_page
+      t.string :slug, index: { unique: true }
+      t.text :slide_slugs, limit: 16_000
+      t.text :description, limit: 16_000
+      t.timestamps null: false
+    end
+  end
+end
