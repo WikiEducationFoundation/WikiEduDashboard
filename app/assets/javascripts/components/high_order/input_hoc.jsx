4e8e4d49c2a4ff6ddae14f63f7e1a65c60473c95
Sage Ross <ragesoss@gmail.com>
2018-11-19 16:49:26 -0800

    Remove ValidationStore from InputHOC

--- app/assets/javascripts/components/high_order/input_hoc.jsx
+++ app/assets/javascripts/components/high_order/input_hoc.jsx
@@ -3,18 +3,25 @@ import { connect } from 'react-redux';
 import createReactClass from 'create-react-class';
 import uuid from 'uuid';
 import _ from 'lodash';
-import shallowCompare from 'react-addons-shallow-compare';
-import ValidationStore from '../../stores/validation_store.js';
 import { addValidation, setValid, setInvalid } from '../../actions/validation_actions';
 
 // This needs to be implemented as a mixin for state reasons.
 // If there's a good way for high-order components to set state on
 // children like this then let's use it.
 
+
+const getValidation = (key, validations) => {
+  if (validations[key] && validations[key].changed) {
+    return validations[key].valid;
+  }
+  return true;
+};
+
 const mapStateToProps = state => ({
   validations: state.validations.validations,
   errorQueue: state.validations.errorQueue
 });
+
 const mapDispatchToProps = {
   addValidation,
   setValid,
@@ -25,19 +32,20 @@ const InputHOC = (Component) => {
   const validatingComponent = createReactClass({
     displayName: `Input${Component.displayName}`,
 
-    mixins: [ValidationStore.mixin],
     // value passed is HOC's state value
     getInitialState() {
       return { value: this.props.value };
     },
 
     componentWillReceiveProps(props) {
+      const valid = getValidation(props.value_key, props.validations);
+
       return this.setState(
         {
           value: props.value,
+          invalid: !valid,
           id: props.id || this.state.id || uuid.v4() // create a UUID if no id prop
         }, function () {
-          const valid = ValidationStore.getValidation(this.props.value_key);
           if (valid && this.props.required && (!props.value || props.value === null || props.value.length === 0)) {
             return this.props.addValidation(this.props.value_key, I18n.t('application.field_required'));
           }
@@ -46,7 +54,13 @@ const InputHOC = (Component) => {
     },
 
     shouldComponentUpdate(nextProps, nextState) {
-      return shallowCompare(this, nextProps, nextState);
+      if (this.state.value === nextState.value
+            && this.state.id === nextState.id
+            && this.state.invalid === nextState.invalid
+            && this.props.editable === nextProps.editable) {
+        return false;
+      }
+      return true;
     },
     // onChange will now be handled by the HOC component
     onChange(e) {
@@ -64,10 +78,6 @@ const InputHOC = (Component) => {
           return this.validate();
         });
       }
-    },
-
-    storeDidChange() {
-      return this.setState({ invalid: !ValidationStore.getValidation(this.props.value_key) });
     },
 
     validate() {
