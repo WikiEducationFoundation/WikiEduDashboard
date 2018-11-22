3cc92a59945f0b0f92de7cea45501dc083c1e06e
Amit Joki <amitjoki@hotmail.com>
2018-11-22 18:49:35 +0530

    React changes

---
+++ app/assets/javascripts/components/settings/special_users_list.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import List from '../common/list.jsx';
+import SpecialUser from './views/special_user.jsx';
+ const SpecialUserList = ({ specialUsers, sortBy }) => {
+  const elements = Object.keys(specialUsers).map((position, index) => {
+    const username = specialUsers[position] && specialUsers[position].username;
+    return <SpecialUser username={username} position={position} key={index} />;
+  });
+   const keys = {
+    position: {
+      label: 'Position',
+      desktop_only: true
+    },
+    username: {
+      label: 'User Name',
+      desktop_only: false,
+    }
+  };
+   return (
+    <div>
+      <List
+        elements={elements}
+        keys={keys}
+        table_key="special-users"
+        none_message={'No Special Users Defined!'}
+        sortBy={sortBy}
+      />
+     </div>
+  );
+};
+ SpecialUserList.propTypes = {
+  specialUsers: PropTypes.object
+};
+ export default SpecialUserList;
