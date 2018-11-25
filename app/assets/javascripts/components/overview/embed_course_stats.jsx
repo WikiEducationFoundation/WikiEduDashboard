62612b2aaf1f05cfd858bf7a4335381ace87d09c
Amit Joki <amitjoki@hotmail.com>
2018-11-25 13:12:21 +0530

    Embed button and related CSS changes

---
+++ app/assets/javascripts/components/overview/embed_course_stats.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import createReactClass from 'create-react-class';
+import PropTypes from 'prop-types';
+import Popover from '../common/popover.jsx';
+import PopoverExpandable from '../high_order/popover_expandable.jsx';
+
+const EmbedCourseStats = createReactClass({
+  displayName: 'EmbedCourseStats',
+
+  propTypes: {
+    is_open: PropTypes.bool
+  },
+
+  getInitialState() {
+    return { status: '' };
+  },
+
+  getKey() {
+    return 'embed_button';
+  },
+
+  copyToClipboard(e) {
+    const el = e.target;
+    el.select();
+    document.execCommand('copy');
+    this.setState({ status: 'Copied!' });
+  },
+
+  render() {
+    const url = `${location.href}/stats`;
+    const steps = (
+      <tr>
+        <td>
+          <h3>To embed the Course Stats:</h3>
+          <p>Copy the following code into the <code>&lt;body&gt;</code> section.</p>
+          <textarea
+            id="embed"
+            readOnly
+            value={
+`<iframe src="${url}" style="width:100%;border:0px none transparent;"></iframe>`}
+            onClick={this.copyToClipboard}
+          />
+          <small>{this.state.status}</small>
+        </td>
+      </tr>
+    );
+
+    return (
+      <div className="pop__container embed_stats">
+        <button onClick={this.props.open} className="button small">
+          Embed&nbsp;&lt;/&gt;
+        </button>
+        <Popover
+          is_open={this.props.is_open}
+          rows={steps}
+        />
+      </div>
+    );
+  }
+});
+
+export default PopoverExpandable(EmbedCourseStats);
