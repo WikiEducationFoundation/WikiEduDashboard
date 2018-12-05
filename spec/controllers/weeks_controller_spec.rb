dcee561669ca4caf90ea4f445f6f949b5b8c6085
Kamil <kam.mikulski@gmail.com>
2018-12-05 19:23:36 +0100

    convert weeks controller test's to request type

--- spec/controllers/weeks_controller_spec.rb
+++ spec/controllers/weeks_controller_spec.rb
@@ -2,18 +2,18 @@
 
 require 'rails_helper'
 
-describe WeeksController do
+describe WeeksController, type: :request do
   describe '#destroy' do
     let!(:week) { create(:week) }
     let(:admin) { create(:admin, id: 2) }
 
     before do
-      allow(controller).to receive(:current_user).and_return(admin)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
     end
 
     it 'destroys the week' do
       expect(Week.count).to eq(1)
-      delete :destroy, params: { id: week.id }, format: :json
+      delete "/weeks/#{week.id}", params: { id: week.id, format: :json }
       expect(Week.count).to eq(0)
     end
   end
