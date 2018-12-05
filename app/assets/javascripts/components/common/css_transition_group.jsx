7f730f5527e081791f3787beae6dbec1467a7990
Amit Joki <amitjoki@hotmail.com>
2018-12-05 12:55:01 +0530

    Wrap each child with CSSTransition

--- app/assets/javascripts/components/common/css_transition_group.jsx
+++ app/assets/javascripts/components/common/css_transition_group.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import TransitionGroup from 'react-transition-group/TransitionGroup';
 import CSSTransition from 'react-transition-group/CSSTransition';
 
-const CSSTransitionGroup = props => (
-  <TransitionGroup>
-    <CSSTransition {...props} />
-  </TransitionGroup>
-);
+const CSSTransitionGroup = (props) => {
+  const children = React.Children.map(props.children, child =>
+    <CSSTransition {...props}>{child}</CSSTransition>
+  );
+  return (
+    <TransitionGroup>
+      {children}
+    </TransitionGroup>
+  );
+};
 
 export default CSSTransitionGroup;
