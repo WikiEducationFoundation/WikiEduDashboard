b6b3e01d43323eadacd83aa5de862debd2807fea
Kamil <kam.mikulski@gmail.com>
2018-12-10 13:13:01 +0100

    convert training controller test to request type

--- spec/controllers/training_controller_spec.rb
+++ spec/controllers/training_controller_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-describe TrainingController do
+describe TrainingController, type: :request do
   let(:user) { create(:user) }
   let(:library_id) { 'students' }
   let(:module_id)  { TrainingModule.all.first.slug }
@@ -10,9 +10,11 @@ describe TrainingController do
   before { TrainingModule.load_all }
 
   describe 'show' do
-    subject { get :show, params: request_params }
+    subject { get "/training/#{library_id}", params: request_params }
 
-    before  { allow(controller).to receive(:current_user).and_return(user) }
+    before  do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    end
 
     let(:request_params) { { library_id: library_id } }
 
@@ -33,7 +35,7 @@ describe TrainingController do
   end
 
   describe '#training_module' do
-    subject { get :training_module, params: request_params }
+    subject { get "/training/#{library_id}/#{module_id}", params: request_params }
 
     let(:request_params) do
       {
@@ -42,7 +44,9 @@ describe TrainingController do
       }
     end
 
-    before { allow(controller).to receive(:current_user).and_return(user) }
+    before do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    end
 
     context 'module is legit' do
       it 'sets the presenter' do
@@ -62,18 +66,18 @@ describe TrainingController do
 
   describe '#reload' do
     context 'for all modules' do
-      let(:subject) { get :reload, params: { module: 'all' } }
+      let(:subject) { get '/reload_trainings', params: { module: 'all' } }
 
       it 'returns the result upon success' do
         subject
-        expect(response.body).to have_content 'Success!'
+        expect(response.body).to include('Success!')
       end
 
       it 'displays an error message upon failure' do
         allow(TrainingModule).to receive(:load_all)
           .and_raise(TrainingBase::DuplicateIdError, 'oh noes!')
         subject
-        expect(response.body).to have_content 'oh noes!'
+        expect(response.body).to include('oh noes!')
       end
     end
 
@@ -83,19 +87,19 @@ describe TrainingController do
         TrainingSlide.delete_all
       end
 
-      let(:subject) { get :reload, params: { module: 'plagiarism' } }
+      let(:subject) { get '/reload_trainings', params: { module: 'plagiarism' } }
 
       it 'returns the result upon success' do
         allow(Features).to receive(:wiki_trainings?).and_return(true)
         VCR.use_cassette 'wiki_trainings' do
           subject
         end
-        expect(response.body).to have_content 'Success!'
+        expect(response.body).to include('Success!')
       end
 
       it 'displays an error message if the module does not exist' do
-        get :reload, params: { module: 'image-and-medium' }
-        expect(response.body).to have_content 'No module'
+        get '/reload_trainings', params: { module: 'image-and-medium' }
+        expect(response.body).to include('No module')
       end
     end
   end
