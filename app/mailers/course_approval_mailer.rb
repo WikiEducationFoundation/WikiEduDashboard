192c27f3c625d18555ae020a6b9d14d2783860fe
Sage Ross <ragesoss@gmail.com>
2018-12-19 11:21:43 -0800

    Update course approval emails
    Now they'll be different between new and returning instructors.

--- app/mailers/course_approval_mailer.rb
+++ app/mailers/course_approval_mailer.rb
@@ -2,7 +2,7 @@
 
 class CourseApprovalMailer < ApplicationMailer
   def self.send_approval_notification(course, instructor)
-    return unless Features.email?
+    return unless Features.email? && Features.wiki_ed?
     return if instructor.email.nil?
     email(course, instructor).deliver_now
   end
@@ -11,8 +11,12 @@ class CourseApprovalMailer < ApplicationMailer
     @course = course
     @instructor = instructor
     @course_link = "https://#{ENV['dashboard_url']}/courses/#{@course.slug}"
+    @is_returning_instructor = @course.tag? 'returning_instructor'
     @enroll_link = "#{@course_link}?enroll=#{@course.passcode}"
+    @signed = SpecialUsers.classroom_program_manager&.real_name || 'The Wiki Education team'
+    # rubocop:disable Metrics/LineLength
     mail(to: @instructor.email,
-         subject: "Your Wiki Ed course page for #{@course.title} has been approved")
+         subject: "Your Wiki Education course page for #{@course.title} / #{@course.term} at #{@course.school} is now live")
+    # rubocop:enable Metrics/LineLength
   end
 end
--- spec/mailers/previews/course_approval_mailer_preview.rb
+++ spec/mailers/previews/course_approval_mailer_preview.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class CourseApprovalMailerPreview < ActionMailer::Preview
-  def approval
+  def returning_instructor_approval
+    CourseApprovalMailer.email(example_returning_course, example_user)
+  end
+
+  def new_instructor_approval
     CourseApprovalMailer.email(example_course, example_user)
   end
 
@@ -9,12 +13,23 @@ class CourseApprovalMailerPreview < ActionMailer::Preview
 
   def example_course
     Course.new(
-      slug: 'Example_University/Example_Course_(term)',
+      school: 'Example University',
+      title: 'Example Course',
+      term: 'Fall 2019',
+      slug: 'Example_University/Example_Course_(Fall_2019)',
       passcode: 'abcdefg'
     )
   end
 
+  def example_returning_course
+    course = example_course
+    course.define_singleton_method(:tag?) do |tag|
+      tag == 'returning_instructor'
+    end
+    course
+  end
+
   def example_user
-    User.new(email: 'sage@example.com', username: 'Ragesoss')
+    User.new(email: 'sage@example.com', username: 'Ragesoss', real_name: 'Sage Ross')
   end
 end
