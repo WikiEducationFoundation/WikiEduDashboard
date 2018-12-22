0f3c4e75fd6e2bef9499fdb669bf2497b8f2bd07
Kamil <kam.mikulski@gmail.com>
2018-12-22 22:36:14 +0100

    convert greeting controller test to request type

--- spec/controllers/greeting_controller_spec.rb
+++ spec/controllers/greeting_controller_spec.rb
@@ -2,13 +2,15 @@
 
 require 'rails_helper'
 
-describe GreetingController do
-  subject { put :greet_course_students, params: { course_id: course.id } }
+describe GreetingController, type: :request do
+  subject { put '/greeting', params: { course_id: course.id } }
 
   let(:admin) { create(:admin) }
   let(:course) { create(:course) }
 
-  before { allow(controller).to receive(:current_user).and_return(admin) }
+  before do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+  end
 
   it 'greets ungreeted students' do
     expect(GreetUngreetedStudents).to receive(:new).and_call_original
