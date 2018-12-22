97fee37ed596370b1e8b5afa8f66f91f46282d2c
Kamil <kam.mikulski@gmail.com>
2018-12-22 23:03:57 +0100

    convert question controller test to request type

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,13 +2,16 @@
 
 require 'rails_helper'
 
-describe QuestionsController do
+describe QuestionsController, type: :request do
   describe '#question' do
     context 'when the question exists' do
       let!(:question) { create(:q_checkbox) }
       let(:question_id) { question.id }
 
-      before { get :question, params: { id: question_id }, format: :json }
+      before do
+        params = { id: question_id, format: :json }
+        get "/surveys/question_group_question/#{question_id}", params: params
+      end
 
       it 'renders the question in json' do
         expect(Oj.load(response.body)['question']['id']).to eq(question.id)
@@ -21,7 +24,10 @@ describe QuestionsController do
       let!(:question) { create(:q_checkbox, position: 1) }
       let(:question_id) { question.id }
 
-      before { get :update_position, params: { id: question_id, position: 50 } }
+      before do
+        params = { id: question_id, position: 50 }
+        put '/surveys/question_position', params: params
+      end
 
       it 'updates the postion' do
         expect(Rapidfire::Question.last.position).to eq(50)
