7487f958c3547aae5129118f41c6f81cad60abac
Kamil <kam.mikulski@gmail.com>
2018-12-23 21:29:18 +0100

    convert course clone controller test to request type

--- spec/controllers/course_clone_controller_spec.rb
+++ spec/controllers/course_clone_controller_spec.rb
@@ -2,13 +2,13 @@
 
 require 'rails_helper'
 
-describe CourseCloneController do
+describe CourseCloneController, type: :request do
   describe '#clone' do
     let(:user) { create(:user) }
     let(:course) { create(:course) }
 
     before do
-      allow(controller).to receive(:current_user).and_return(user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     end
 
     context 'when the user is the original instructor' do
@@ -19,14 +19,14 @@ describe CourseCloneController do
 
       it 'clones the course' do
         expect_any_instance_of(CourseCloneManager).to receive(:clone!)
-        post :clone, params: { id: course.id }
+        post "/clone_course/#{course.id}", params: { id: course.id }
       end
     end
 
     context 'when the user is not the original instructor' do
       it 'returns a 401 error' do
         expect(CourseCloneManager).not_to receive(:new)
-        post :clone, params: { id: course.id }
+        post "/clone_course/#{course.id}", params: { id: course.id }
         expect(response.status).to eq(401)
       end
     end
