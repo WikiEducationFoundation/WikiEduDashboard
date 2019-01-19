e390915f75a5fe3aa5b22615a4361903ada180a2
Wes Reid <bwreid@gmail.com>
2019-01-18 16:26:29 -0800

    Store onboarding alert information in serialized hash

--- lib/alerts/onboarding_alert_manager.rb
+++ lib/alerts/onboarding_alert_manager.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class OnboardingAlertManager
-  def create_alert(user_name, msg)
+  def create_alert(user_name, msg, details)
     user = User.find_by(username: user_name)
     alert = Alert.create(type: 'OnboardingAlert',
                          user: user,
                          message: msg,
+                         details: details,
                          target_user: SpecialUsers.outreach_manager)
     alert.email_target_user
   end
