6860894d589436a6e9ee6e9f13631394bf8818ab
shivanshbindal9 <shivanshbindal603@gmail.com>
2019-01-29 13:56:27 +0530

    add react-select

--- app/assets/javascripts/components/timeline/block_type_select.jsx
+++ app/assets/javascripts/components/timeline/block_type_select.jsx
@@ -3,6 +3,8 @@ import createReactClass from 'create-react-class';
 import PropTypes from 'prop-types';
 import Conditional from '../high_order/conditional.jsx';
 import InputHOC from '../high_order/input_hoc.jsx';
+import Select from 'react-select';
+import selectStyles from '../../styles/single_select';
 
 const BlockTypeSelect = createReactClass({
   displayName: 'BlockTypeSelect',
@@ -10,7 +12,18 @@ const BlockTypeSelect = createReactClass({
   propTypes: {
     value: PropTypes.any,
     options: PropTypes.array,
-    editable: PropTypes.bool,
+    editable: PropTypes.bool
+  },
+
+  getInitialState() {
+    const initialState = this.props.options[this.props.value];
+    return { selectedOption: { value: this.props.value, label: initialState } };
+  },
+
+  handleClick(selectedOption) {
+    this.setState({ selectedOption });
+    const e = { target: selectedOption };
+    this.props.onChange(e);
   },
 
   render() {
@@ -20,28 +33,31 @@ const BlockTypeSelect = createReactClass({
       <div className="tooltip dark">
         <p>{I18n.t('timeline.block_type')}</p>
       </div>
-          );
+    );
 
     const options = this.props.options.map((option, i) => {
-      return <option value={i} key={i}>{option}</option>;
+      return { value: i, label: option };
     });
 
     if (this.props.editable) {
       return (
         <div className="form-group">
-          <label htmlFor={this.props.id} className={labelClass}>{label}{tooltip}</label>
-          <select
+          <label htmlFor={this.props.id} className={labelClass}>
+            {label}
+            {tooltip}
+          </label>
+          <Select
             id={this.props.id}
-            value={this.props.value}
-            onChange={this.props.onChange}
-          >
-            {options}
-          </select>
-        </div>);
+            value={options.find(option => option.value === this.state.selectedOption.value)}
+            onChange={this.handleClick}
+            options={options}
+            styles={selectStyles}
+          />
+        </div>
+      );
     }
     return <span>{this.props.options[this.props.value]}</span>;
   }
-}
-);
+});
 
 export default Conditional(InputHOC(BlockTypeSelect));
