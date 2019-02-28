aa2f78fbf915cead2c859febb2b941e3a03b9e47
Wes Reid <bwreid@gmail.com>
2019-02-28 09:45:38 -0800

    Create Requested Accounts page for admin
    - Add page to show all requested accounts for the dashboard
    - Add notifications bell on the navigation
    - Update and create relevant tests

--- spec/controllers/requested_accounts_controller_spec.rb
+++ spec/controllers/requested_accounts_controller_spec.rb
@@ -12,6 +12,41 @@ describe RequestedAccountsController, type: :request do
     let(:username) { 'username' }
     let(:email) { 'valid@example.com' }
 
+    describe '#index' do
+      let(:requested_account) do
+        create(
+          :requested_account,
+          course: course,
+          username: username,
+          email: email
+        )
+      end
+
+      it 'should raise an error if the user is not signed in' do
+        get '/requested_accounts'
+        expect(response.status).to eq(401)
+        expect(response.body).to include('authorized')
+      end
+
+      it 'should raise an error if the user is not an admin' do
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+        get '/requested_accounts'
+        expect(response.status).to eq(401)
+        expect(response.body).to include('authorized')
+      end
+
+      it 'should load the page with requested accounts if the user is an admin' do
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+        requested_account
+        get '/requested_accounts'
+        expect(response.status).to eq(200)
+        expect(response.body).to include('Requested accounts')
+        expect(response.body).to include(email)
+      end
+    end
+
     describe '#request_account' do
       let(:requested_account) do
         create(
@@ -94,6 +129,29 @@ describe RequestedAccountsController, type: :request do
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
         allow(UserImporter).to receive(:new_from_username).and_return(user)
         post route, params: { course_slug: course.slug }
+        expect(response.status).to eq(200)
+        expect(RequestedAccount.count).to eq(0)
+      end
+    end
+
+    describe '#create_all_accounts' do
+      before do
+        RequestedAccount.create(course_id: course.id, username: username, email: email)
+      end
+
+      it 'does not create the accounts if user is not authorized' do
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+        post '/requested_accounts'
+        expect(response.status).to eq(401)
+        expect(course.flags[:register_accounts]).to be(nil)
+        expect(RequestedAccount.count).to eq(1)
+      end
+
+      it 'creates the accounts if user is authorized' do
+        stub_account_creation
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+        allow(UserImporter).to receive(:new_from_username).and_return(user)
+        post '/requested_accounts'
         expect(response.status).to eq(200)
         expect(RequestedAccount.count).to eq(0)
       end
