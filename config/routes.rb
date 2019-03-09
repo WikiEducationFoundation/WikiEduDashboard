33ffd1ea35af2eb0b4c6f139b181a7caad606471
hardy1334 <hardychugh34@gmail.com>
2019-03-09 18:52:58 +0530

    refactoring-controller

--- config/routes.rb
+++ config/routes.rb
@@ -98,7 +98,7 @@ Rails.application.routes.draw do
           constraints: { id: /.*/ }, via: [:post, :delete]
     match 'courses/*id/tag' => 'courses#tag',
           constraints: { id: /.*/ }, via: [:post, :delete]
-    match 'courses/*id/user' => 'users#enroll',
+    match 'courses/*id/user' => 'enroll_courses#enroll',
           constraints: { id: /.*/ }, via: [:post, :delete]
 
     # show-type actions: first all the specific json endpoints,
