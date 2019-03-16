7003b7093c2d0f9ef3afe04938c024e629cb4098
Mariah <mariahajmal@yahoo.com>
2019-03-16 14:49:35 +0500

    Label remove button for students #2531

--- app/assets/javascripts/components/overview/campaign_editable.jsx
+++ app/assets/javascripts/components/overview/campaign_editable.jsx
@@ -69,7 +69,7 @@ const CampaignEditable = createReactClass({
 
     const campaignList = this.props.campaigns.map((campaign) => {
       const removeButton = (
-        <button className="button border plus" onClick={this.removeCampaign.bind(this, campaign.title)}>-</button>
+        <button className="button border plus" onClick={this.removeCampaign.bind(this, campaign.title)}>Remove</button>
       );
       return (
         <tr key={`${campaign.id}_campaign`}>
