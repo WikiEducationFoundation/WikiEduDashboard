72987d171d40d8f99d11dc081372589f75b3466b
Sage Ross <ragesoss@gmail.com>
2019-04-02 16:24:27 -0700

    Ring NotificationBell for open tickets as well as requested accounts
    Now the bell goes to /admin, but we want to still go to the /requested_accounts page directly for Programs & Events Dashboard, where tickets aren't yet going to be used.

--- app/assets/javascripts/components/nav/notifications_bell.jsx
+++ app/assets/javascripts/components/nav/notifications_bell.jsx
@@ -4,10 +4,14 @@ import fetch from 'cross-fetch';
 export default class NotificationsBell extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = { open_tickets: false, requested_accounts: false };
   }
 
   componentDidMount() {
+    fetch('/td/open_tickets')
+      .then(res => res.json())
+      .then(({ open_tickets }) => this.setState({ open_tickets }))
+      .catch(err => err);
     fetch('/requested_accounts.json')
       .then(res => res.json())
       .then(({ requested_accounts }) => this.setState({ requested_accounts }))
@@ -17,9 +21,9 @@ export default class NotificationsBell extends React.Component {
   render() {
     return (
       <li aria-describedby="notification-message" className="notifications">
-        <a href="/requested_accounts" className="icon icon-notifications_bell"/>
+        <a href="/admin" className="icon icon-notifications_bell"/>
         {
-          this.state.requested_accounts
+          (this.state.requested_accounts || this.state.open_tickets)
           ? (
             <span className="bubble red">
               <span id="notification-message" className="screen-reader">You have new notifications.</span>
