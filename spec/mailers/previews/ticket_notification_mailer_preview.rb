4b5261ce4bcbcef43d71e3ad34c1f0bc5d35c925
Wes Reid <bwreid@gmail.com>
2019-04-03 10:46:39 -0700

    Minor style updates to email

--- spec/mailers/previews/ticket_notification_mailer_preview.rb
+++ spec/mailers/previews/ticket_notification_mailer_preview.rb
@@ -2,11 +2,19 @@
 
 class TicketNotificationMailerPreview < ActionMailer::Preview
   def message_to_student
+    sender = User.new(username: 'admin',
+                      real_name: 'Delano (Wiki Edu)',
+                      permissions: User::Permissions::ADMIN)
+    recipient = User.new(username: 'flanagan.hyder')
     TicketNotificationMailer.notify(course, message, recipient, sender)
   end
 
   def message_to_instructor
-    TicketNotificationMailer.notify(course, message, recipient(:admin), sender)
+    sender = User.new(username: 'flanagan.hyder')
+    recipient = User.new(username: 'admin',
+                         real_name: 'Delano (Wiki Edu)',
+                         permissions: User::Permissions::ADMIN)
+    TicketNotificationMailer.notify(course, message, recipient, sender)
   end
 
   private
@@ -15,20 +23,14 @@ class TicketNotificationMailerPreview < ActionMailer::Preview
     Course.new(slug: 'course/title', title: 'My Course')
   end
 
-  def recipient(admin=false)
-    User.new(username: 'janesmith',
-             real_name: 'Jane Smith',
-             permissions: admin ? User::Permissions::ADMIN : User::Permissions::NONE)
-  end
-
-  def sender
-    User.new(username: 'senderjoed')
-  end
-
   def message
     content = %(
-      <p>Hello there,</p>
-      <p>I have a question about using the dashboard...</p>
+      <p>Hi there,</p>
+      <p>Tabella et aspernatur verecundia comburo et averto addo abutor
+      caelestis sed adduco. Similique argentum deduco crustulum solium utrum
+      undique denique. Vesco surgo ex pauci aveho aperiam. Arx volutabrum
+      canonicus quo addo theatrum arcesso cognatus cohors. Tepidus auris qui
+      convoco ulterius bibo confugo.</p>
     )
     ticket = TicketDispenser::Ticket.first || TicketDispenser::Dispenser.call(
       content: content,
