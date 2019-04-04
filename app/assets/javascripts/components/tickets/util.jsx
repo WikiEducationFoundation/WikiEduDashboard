236c2387b00a4c8358aeddf6d11031e8860530da
Wes Reid <bwreid@gmail.com>
2019-04-04 09:56:54 -0700

    Add note to status definitions for tickets

--- app/assets/javascripts/components/tickets/util.jsx
+++ app/assets/javascripts/components/tickets/util.jsx
@@ -1,3 +1,6 @@
+// These statuses are connected to the statuses that are set
+// in TicketDispenser::Ticket. If you change them here, you
+// should change them there.
 export const STATUSES = [
   'Open',
   'Waiting Response',
