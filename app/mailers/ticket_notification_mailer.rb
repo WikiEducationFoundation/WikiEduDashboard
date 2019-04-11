95a63eba69d669313020e66f8c5b1ba07bda2d03
Sage Ross <ragesoss@gmail.com>
2019-04-11 13:31:27 -0700

    Add spec for TicketsController
    It also hits TicketNotificationMailer, which has been tweaked to not break if the course is missing.

--- app/mailers/ticket_notification_mailer.rb
+++ app/mailers/ticket_notification_mailer.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class TicketNotificationMailer < ApplicationMailer
-  def self.notify_of_message(course, recipient, sender, message)
+  def self.notify_of_message(course, message, recipient, sender)
     return unless Features.email?
-    notify(course, recipient, sender, message).deliver_now
+    notify(course, message, recipient, sender).deliver_now
   end
 
   def notify(course, message, recipient, sender)
@@ -13,12 +13,13 @@ class TicketNotificationMailer < ApplicationMailer
     @recipient = recipient
     @sender = sender
     @sender_name = @sender.real_name || @sender.username
+    subject_prefix = @course ? "#{course.title}: " : ''
 
-    @course_link = "https://#{ENV['dashboard_url']}/courses/#{@course.slug}"
+    @course_link = "https://#{ENV['dashboard_url']}/courses/#{@course.slug}" if @course
     @ticket_dashboard_link = "https://#{ENV['dashboard_url']}/tickets/dashboard"
     mail(to: @recipient.email,
          from: @sender.email,
-         subject: "#{course.title}: Response to your help request",
+         subject: subject_prefix + 'Response to your help request',
          reply_to: @sender.email)
   end
 end
---
+++ spec/controllers/tickets_controller_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe TicketsController, type: :request do
+  let(:admin) { create(:admin, email: 'admin@wikiedu.org') }
+  let(:user) { create(:user, email: 'student@hello.edu') }
+
+  let(:ticket) do
+    TicketDispenser::Dispenser.call(
+      content: 'hello',
+      owner_id: admin.id,
+      sender_id: user.id
+    )
+  end
+  let(:message) { ticket.messages.first }
+
+  before do
+    login_as admin
+  end
+
+  describe '#dashboard' do
+    it 'renders the main tickets dashboard URL' do
+      get '/tickets/dashboard'
+      expect(response.status).to eq(200)
+      expect(response.body).to include("id='react_root'")
+    end
+
+    it 'renders a ticket show path' do
+      get '/tickets/dashboard/1'
+      expect(response.status).to eq(200)
+      expect(response.body).to include("id='react_root'")
+    end
+  end
+
+  describe '#notify' do
+    it 'triggers an email reply' do
+      expect(TicketNotificationMailer).to receive(:notify_of_message).and_call_original
+      post '/tickets/notify', params: { message_id: message.id, sender_id: admin.id }
+      expect(response.status).to eq(200)
+    end
+  end
+end
