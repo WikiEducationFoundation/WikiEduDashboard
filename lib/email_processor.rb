6e1b28dd09b7eb004e8f457d72d3d7b1a29f58bc
Wes Reid <bwreid@gmail.com>
2019-04-16 14:52:51 -0700

    Allow for forwarded emails to attempt to pull correct sender
    - If an email is sent and includes the specified forwarding
      email as a CC, attempt to search through the message to find
      the sender

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -3,24 +3,43 @@
 class EmailProcessor
   def initialize(email)
     @email = email
+    @content, @course, @from_address, @owner, @reference_id, @sender = nil
   end
 
   def process
-    recipient_emails = @email.to.pluck(:email)
-    @owner = User.find_by(greeter: true, email: recipient_emails)
-
-    @from_address = @email.from[:email]
-    @sender = User.find_by(email: @from_address) if @from_address
-    @course = @sender.courses.last if @sender
-
-    body, @reference_id = parse_body_and_reference_id
-    @content = body
-    @content += " #{from_signature(@email.from)}" if @sender.blank?
-
+    define_owner
+    define_sender
+    define_content_and_reference_id
     dispense_or_thread_ticket
   end
 
+  def email_addresses_from_email
+    raw_body = @email.raw_body
+    expression = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{0,63}\b/i
+    raw_body.scan(expression)
+  end
+
   private
+
+  def define_owner
+    recipient_emails = @email.to.pluck(:email)
+    @owner = User.find_by(greeter: true, email: recipient_emails)
+    @from_address = @email.from[:email]
+  end
+
+  def define_sender
+    recipient_emails = @email.cc ? @email.cc.pluck(:email) : []
+    if recipient_emails.include?(ENV['TICKET_FORWARDING_EMAIL_ADDRESS'])
+      addresses = email_addresses_from_email
+      @sender = User.find_by(greeter: false, email: addresses)
+    elsif @from_address
+      @sender = User.find_by(email: @from_address)
+    end
+  end
+
+  def define_course
+    @course = @sender.courses.last if @sender
+  end
 
   def from_signature(from)
     ''"
@@ -29,11 +48,10 @@ class EmailProcessor
     "''
   end
 
-  def parse_body_and_reference_id
+  def define_content_and_reference_id
+    @content = @email.body
     reference = @email.raw_body.match('ref_(.*)_ref')
-    reference_id = reference[1] if reference
-
-    return [@email.body, reference_id]
+    @reference_id = reference[1] if reference
   end
 
   def dispense_or_thread_ticket
