ff7f0c0f468799222b1d88e6bc1d059d5bc12e7b
Wes Reid <bwreid@gmail.com>
2019-04-16 14:52:51 -0700

    Allow for forwarded emails to attempt to pull correct sender
    - If an email is sent and includes the specified forwarding
      email as a CC, attempt to search through the message to find
      the sender

--- spec/lib/email_processor_spec.rb
+++ spec/lib/email_processor_spec.rb
@@ -50,7 +50,6 @@ describe EmailProcessor do
 
       message = ticket.messages.first
       expect(message.sender).to eq(nil)
-      expect(message.content).to include('From other@email.com')
     end
 
     it 'does not set an owner if it is not addressed to a Wikipedia Expert' do
@@ -89,6 +88,44 @@ describe EmailProcessor do
       processor.process
 
       expect(ticket.messages.length).to eq(2)
+    end
+
+    it 'should assign forwarded emails to the original sender' do
+      student
+      body = <<~EXAMPLE
+        Example email test\r\n\r\n---------- Forwarded message ---------\r\nFrom:
+        Student <student@email.com>\r\nDate: Mon, Apr 8, 2019 at 3:42 PM\r\n
+        Subject: Help!\r\nTo: <staff@email.com>\r\n\r\n\r\nHelp message\r\n
+      EXAMPLE
+      email = create(:email,
+                     to: [{ email: expert.email }],
+                     cc: [{ email: ENV['TICKET_FORWARDING_EMAIL_ADDRESS'] }],
+                     from: { email: 'other@email.com' },
+                     raw_body: body)
+      processor = described_class.new(email)
+      processor.process
+
+      expect(TicketDispenser::Ticket.all.count).to eq(1)
+      expect(TicketDispenser::Message.all.count).to eq(1)
+
+      ticket = TicketDispenser::Ticket.first
+      message = TicketDispenser::Message.first
+      expect(ticket.owner).to eq(expert)
+      expect(message.sender).to eq(student)
+    end
+  end
+
+  describe '#email_addresses_from_email' do
+    it 'should return a list of all emails that appear in the email body' do
+      body = <<~EXAMPLE
+        Example email test\r\n\r\n---------- Forwarded message ---------\r\nFrom:
+        Person A <aaa@email.com>\r\nDate: Mon, Apr 8, 2019 at 3:42 PM\r\n
+        Subject: Help!\r\nTo: <bbb@email.com>\r\n\r\n\r\nHelp message\r\n
+      EXAMPLE
+      email = build(:email, raw_body: body)
+      expected_result = ['aaa@email.com', 'bbb@email.com']
+
+      expect(described_class.new(email).email_addresses_from_email).to match_array(expected_result)
     end
   end
 end
