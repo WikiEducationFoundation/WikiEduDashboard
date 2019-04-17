c2c3db39bcbe9eacb1f1747964f75fa563c432f3
Wes Reid <bwreid@gmail.com>
2019-04-17 10:57:07 -0700

    Update test config

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,6 +27,7 @@ ENV['chat_server'] = 'https://dashboardchat.wmflabs.org'
 ENV['SF_SERVER'] = 'https://cs54.salesforce.com/'
 ENV['edit_en.wikipedia.org'] = 'true'
 ENV['dashboard_url'] = 'dashboard.wikiedu.org'
+ENV['TICKET_FORWARDING_DOMAIN'] = 'wikiedu.org'
 
 Rails.application.configure do
   # Settings specified here will take
