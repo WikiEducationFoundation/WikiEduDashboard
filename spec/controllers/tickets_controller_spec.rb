1d459813b62a8a707c998b7bd740c6aa979e5073
Sage Ross <ragesoss@gmail.com>
2019-04-25 15:00:09 -0700

    Improve specs for Ticket replies and notifications

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -13,10 +13,14 @@ describe TicketsController, type: :request do
       sender_id: user.id
     )
   end
-  let(:message) { ticket.messages.first }
+  let(:message) { ticket.messages.last }
 
   before do
     login_as admin
+    TicketDispenser::Message.create(ticket: ticket, kind: TicketDispenser::Message::Kinds::NOTE,
+      content: 'this is a note')
+    TicketDispenser::Message.create(ticket: ticket, kind: TicketDispenser::Message::Kinds::REPLY,
+      content: 'this is a reply')
   end
 
   describe '#dashboard' do
@@ -40,6 +44,15 @@ describe TicketsController, type: :request do
       expect(response.status).to eq(200)
     end
 
+    it 'includes replies and excludes notes' do
+      expect(TicketNotificationMailer).to receive(:notify_of_message).and_call_original
+      post '/tickets/reply', params: { message_id: message.id, sender_id: admin.id }
+
+      delivery = ActionMailer::Base.deliveries.first
+      expect(delivery.text_part.body).not_to include('this is a note')
+      expect(delivery.text_part.body).to include('this is a reply')
+    end
+
     it 'triggers an email reply even if there is no sender' do
       message.update(sender: nil, details: { sender_email: user.email })
       expect(TicketNotificationMailer).to receive(:notify_of_message).and_call_original
@@ -49,17 +62,25 @@ describe TicketsController, type: :request do
   end
 
   describe '#notify_owner' do
-    it 'triggers an email reply' do
-      new_owner = create(:admin, username: 'otheradmin', email: 'otheradmin@email.com')
-      ticket.update(owner: new_owner)
+    let(:recipient) { create(:admin, username: 'otheradmin', email: 'otheradmin@email.com') }
+    let(:message) do
+      TicketDispenser::Message.create(ticket: ticket, kind: TicketDispenser::Message::Kinds::NOTE,
+                                      content: 'this is a note')
+    end
+
+    it 'includes notes and replies' do
+      ticket.update(owner: recipient)
+
       expect(TicketNotificationMailer).to receive(:notify_of_message).and_call_original
       params = { message_id: message.id, sender_id: admin.id }
       post '/tickets/notify_owner', params: params
       expect(response.status).to eq(200)
 
       delivery = ActionMailer::Base.deliveries.first
-      expect(delivery.to).to include(new_owner.email)
+      expect(delivery.to).to include(recipient.email)
       expect(delivery.from).to include(admin.email)
+      expect(delivery.html_part.body).to include('this is a note')
+      expect(delivery.text_part.body).to include('this is a reply')
     end
   end
 end
