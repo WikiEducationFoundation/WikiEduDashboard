73fa8239167525f1a073563811fbc989c4b56949
Wes Reid <bwreid@gmail.com>
2019-04-25 09:31:59 -0700

    Skip over notes and unsuccessful replies when responding to ticket

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -5,4 +5,16 @@ module TicketsHelper
   def sender_for_message(message)
     message.sender&.real_name || message.sender&.username || message.details[:sender_email]
   end
+
+  def successful_replies_in_reverse(_ticket, recipient)
+    successful_messages = @ticket.messages.reject do |message|
+      message.details[:delivery_failed]
+    end
+
+    reversed_messages = successful_messages.reverse[1..-1]
+
+    reversed_messages.select do |message|
+      recipient.admin? || message.kind == TicketDispenser::Message::Kinds::REPLY
+    end
+  end
 end
