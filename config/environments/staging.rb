34b5667181235b7c20561f908b7f0a5d5ca376c1
Sage Ross <ragesoss@gmail.com>
2019-04-30 14:56:08 -0700

    Add mailer previews path

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -72,19 +74,20 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => ENV["SMTP_HOST"] }
+  config.action_mailer.default_url_options = { host: ENV['SMTP_HOST'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default charset: 'utf-8'
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: 'smtp.gmail.com',
     port: 587,
-    domain: ENV["SMTP_HOST"],
-    authentication: "plain",
+    domain: ENV['SMTP_HOST'],
+    authentication: 'plain',
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD']
   }
   config.action_mailer.show_previews = true
+  config.action_mailer.preview_path = "#{Rails.root}/spec/mailers/previews"
 end
