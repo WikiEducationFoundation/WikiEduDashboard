4f281e4dcb5459ce7ce11a2608817bfdedf964c9
Sage Ross <ragesoss@gmail.com>
2019-05-14 15:01:51 -0700

    Limit active editors to mainspace

--- docs/analytics_scripts/monthly_active_editors.rb
+++ docs/analytics_scripts/monthly_active_editors.rb
@@ -1,8 +1,8 @@
 # Quick version of https://stats.wikimedia.org/v2/#/en.wikipedia.org/contributing/editors/normal|table|2018-04-01~2019-04-01|activity_level~1..4-edits*5..24-edits*25..99-edits*100..-edits
 active_editors_by_month = []
-36.times do |i|
+12.times do |i|
   month = i.months.ago.month
   year = i.months.ago.year
-  active_editor_count = Revision.where('extract(month from date) = ?', month).where('extract(year from date) = ?', year).group('user_id').having('count(*) > 4').count.count
+  active_editor_count = Revision.joins(:article).where(articles: { namespace: '0' }).where('extract(month from date) = ?', month).where('extract(year from date) = ?', year).group('user_id').having('count(*) > 4').count.count
   active_editors_by_month << ["#{year}-#{month}-01", active_editor_count]
 end
