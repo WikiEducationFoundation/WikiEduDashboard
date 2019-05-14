29c43b15cc7cd2bb63ab2573838b287a640c9f5a
Sage Ross <ragesoss@gmail.com>
2019-05-14 11:39:23 -0700

    Use TrainingModules component to show assigned modules

--- app/assets/javascripts/components/resources/resources.jsx
+++ app/assets/javascripts/components/resources/resources.jsx
@@ -5,27 +5,18 @@ import _ from 'lodash';
 
 import CourseLink from '../common/course_link.jsx';
 import { getWeeksArray } from '../../selectors';
-
+import TrainingModules from '../timeline/training_modules';
 
 const Resources = ({ weeks }) => {
   const blocks = _.flatten(weeks.map(week => week.blocks));
-  const modules = _.flatten(blocks.map(block => block.training_modules));
-  const trainings = modules.map((module) => {
-    if (!module) { return; }
-    return (
-      <li key={`training-link-${module.id}`}><CourseLink to={`/training/students/${module.slug}`}>{module.name}</CourseLink></li>
-    );
-  });
+  const modules = _.compact(_.flatten(blocks.map(block => block.training_modules)));
   return (
     <div id="resources">
       <div className="section-header">
         <h3>{I18n.t('resources.header')}</h3>
         <div id="training-modules">
-          <h4>Assigned training modules</h4>
-          <ul>
-            {trainings}
-          </ul>
-          <CourseLink to={'/training'} className="button dark">All student training modules</CourseLink>
+          <TrainingModules block_modules={modules} trainingLibrarySlug="students" />
+          <CourseLink to={'/training/students'} className="button dark pull-right">All student training modules</CourseLink>
         </div>
         <hr />
         <div id="handouts">
