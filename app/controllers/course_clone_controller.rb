c67b7b06417b800df7fab2bdb6b7ed4d7c172ea5
Wes Reid <bwreid@gmail.com>
2019-05-18 10:35:36 +0200

    Add clone button to campaigns list for campaign organizer

--- app/controllers/course_clone_controller.rb
+++ app/controllers/course_clone_controller.rb
@@ -9,7 +9,11 @@ class CourseCloneController < ApplicationController
     @course = Course.find(params[:id])
     check_permission
     new_course = CourseCloneManager.new(@course, current_user).clone!
-    render json: { course: new_course.as_json }
+
+    respond_to do |format|
+      format.json { render json: { course: new_course.as_json } }
+      format.html { redirect_to "/courses/#{new_course.slug}" }
+    end
   end
 
   private
