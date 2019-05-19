dbc642de390a4cd8176f230f31b2d281b302023f
Sage Ross <sage@ragesoss.com>
2019-05-19 01:36:45 -0700

    Add stats download modal to Campaign overview
    This adds the CSV downloads for a campaign, previously only available on the campaigns index, to the overview page of each campaign. The new feature is implemented with React, so it adds the start of a CampaignOverviewHandler component for gradually porting the rest of the view to React.
    
    Also, it's implemented with React Hooks, the first use of Hooks in the codebase!

---
+++ app/assets/javascripts/components/campaign/campaign_overview_handler.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import CampaignStatsDownloadModal from './campaign_stats_download_modal';
+
+const CampaignOverviewHandler = (props) => {
+  return <CampaignStatsDownloadModal {...props} />;
+};
+
+export default CampaignOverviewHandler;
