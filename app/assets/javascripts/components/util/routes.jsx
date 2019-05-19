c51de3ec62a3aeef5f8eab559a17409f0ccf7d71
Sage Ross <sage@ragesoss.com>
2019-05-19 01:36:45 -0700

    Add stats download modal to Campaign overview
    This adds the CSV downloads for a campaign, previously only available on the campaigns index, to the overview page of each campaign. The new feature is implemented with React, so it adds the start of a CampaignOverviewHandler component for gradually porting the rest of the view to React.
    
    Also, it's implemented with React Hooks, the first use of Hooks in the codebase!

--- app/assets/javascripts/components/util/routes.jsx
+++ app/assets/javascripts/components/util/routes.jsx
@@ -6,6 +6,7 @@ import Onboarding from '../onboarding/index.jsx';
 import { ConnectedCourseCreator } from '../course_creator/course_creator.jsx';
 import ArticleFinder from '../article_finder/article_finder.jsx';
 import AlertsHandler from '../alerts/alerts_handler.jsx';
+import CampaignOverviewHandler from '../campaign/campaign_overview_handler.jsx';
 import CampaignOresPlot from '../campaign/campaign_ores_plot.jsx';
 import RecentActivityHandler from '../activity/recent_activity_handler.jsx';
 import TrainingApp from '../../training/components/training_app.jsx';
@@ -21,6 +22,7 @@ const routes = (
     <Route path="/courses/:course_school/:course_title" component={Course} />
     <Route path="/course_creator" component={ConnectedCourseCreator} />
     <Route path="/users/:username" component={UserProfile} />
+    <Route path="/campaigns/:campaign_slug/overview" component={CampaignOverviewHandler} />
     <Route path="/campaigns/:campaign_slug/alerts" component={AlertsHandler} />
     <Route path="/campaigns/:campaign_slug/ores_plot" component={CampaignOresPlot} />
     <Route path="/settings" component={SettingsHandler} />
