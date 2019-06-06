6176aad712892923b2772ee053afd0ab9073e03e
Khyati <khyatisoneji5@gmail.com>
2019-06-06 19:12:18 +0530

    remove changes related to courses_users

--- db/migrate/20190605072846_add_previous_features_to_revision.rb
+++
@@ -1,5 +0,0 @@
-class AddPreviousFeaturesToRevision < ActiveRecord::Migration[5.2]
-  def change
-    add_column :revisions, :features_previous, :text
-  end
-end
--- db/migrate/20190605103334_add_refs_sum_to_courses_users.rb
+++
@@ -1,5 +0,0 @@
-class AddRefsSumToCoursesUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :courses_users, :refs_sum_ms, :integer, default: 0
-  end
-end
