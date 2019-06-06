e03f0e94a2e298be8866f1e6e3998528b9fd8d5e
Khyati <khyatisoneji5@gmail.com>
2019-06-07 00:18:28 +0530

    process all revisions of article

--- docs/analytics_scripts/ores_changes.rb
+++ docs/analytics_scripts/ores_changes.rb
@@ -2,19 +2,19 @@
 
 term = Campaign.find_by(slug: 'spring_2017')
 
-first_revs = []
+all_revs = []
 term.courses.each do |course|
   course.articles_courses.each do |ac|
-    first_revs << ac.all_revisions.order('date ASC').first
+    all_revs << ac.all_revisions
   end
 end
 
 importer = RevisionScoreImporter.new
 
-first_revs.each do |rev|
+all_revs.each do |rev|
   next if rev.nil?
   next if rev.wp10_previous
   next if rev.wiki_id != 1
   puts rev.id
   importer.send(:update_wp10_previous, rev)
-end; first_revs.count
+end; all_revs.count
