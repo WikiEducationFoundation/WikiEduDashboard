bd40abf878ff3c94bb142ee282d03f9d6d71c140
Wes Reid <bwreid@gmail.com>
2019-06-12 16:21:04 -0700

    Refactor AssignCell

--- app/assets/javascripts/components/students/assign_cell.jsx
+++ app/assets/javascripts/components/students/assign_cell.jsx
@@ -1,71 +1,71 @@
 import React from 'react';
-import createReactClass from 'create-react-class';
 import PropTypes from 'prop-types';
 import AssignButton from './assign_button.jsx';
 import { trunc } from '../../utils/strings';
 import CourseUtils from '../../utils/course_utils.js';
 
-const AssignCell = createReactClass({
-  displayName: 'AssignCell',
-
-  propTypes: {
-    assignments: PropTypes.array,
-    prefix: PropTypes.string,
-    current_user: PropTypes.object,
-    student: PropTypes.object,
-    editable: PropTypes.bool,
-    role: PropTypes.number,
-    tooltip_message: PropTypes.string,
-    course: PropTypes.object.isRequired,
-    wikidataLabels: PropTypes.object
-  },
-
-  stop(e) {
-    return e.stopPropagation();
-  },
-
-  render() {
-    let link;
-    if (this.props.assignments.length > 0) {
-      const article = CourseUtils.articleFromAssignment(this.props.assignments[0], this.props.course.home_wiki);
-      if (this.props.assignments.length > 1) {
-        const articleCount = I18n.t('users.number_of_articles', { count: this.props.assignments.length });
-        link = (
-          <span>
-            {this.props.prefix}
-            {articleCount}
-          </span>
-        );
-      } else {
-        const titleText = trunc(article.formatted_title, 30);
-        if (article.url) {
-          link = (
-            <span>
-              {this.props.prefix}
-              <a onClick={this.stop} href={article.url} target="_blank">{titleText}</a>
-            </span>
-          );
-        } else {
-          link = <span>{this.props.prefix}{titleText}</span>;
-        }
-      }
-    } else if (!this.props.current_user) {
-      link = <span>{I18n.t('users.no_articles')}</span>;
-    }
-
-    let isCurrentUser;
-    if (this.props.student) { isCurrentUser = this.props.current_user.id === this.props.student.id; }
-    const instructorOrAdmin = this.props.current_user.isInstructor || this.props.current_user.admin;
-    const permitted = isCurrentUser || (instructorOrAdmin && this.props.editable);
-
-    return (
-      <div className="inline-button-peer">
-        {link}
-        <AssignButton {...this.props} role={this.props.role} permitted={permitted} />
-      </div>
+// Helper Components
+const ArticleLink = ({ content, href, prefix }) => {
+  if (href) {
+    content = (
+      <a
+        href={href}
+        onClick={e => e.stopPropagation()}
+        target="_blank"
+      >
+        { content }
+      </a>
     );
   }
-}
-);
+
+  return (
+    <span>
+      { prefix }{ content }
+    </span>
+  );
+};
+
+// Main Component
+export const AssignCell = (props) => {
+  const { assignments, course, current_user, editable, prefix, student } = props;
+
+  let link;
+  if (assignments.length) {
+    const article = CourseUtils.articleFromAssignment(assignments[0], course.home_wiki);
+    if (assignments.length > 1) {
+      const count = I18n.t('users.number_of_articles', { count: assignments.length });
+      link = <ArticleLink prefix={prefix} content={count} />;
+    } else {
+      const title = trunc(article.formatted_title, 30);
+      link = <ArticleLink prefix={prefix} href={article.url} content={title} />;
+    }
+  } else if (!current_user) {
+    link = <ArticleLink content={I18n.t('users.no_articles')} />;
+  }
+
+  let isCurrentUser;
+  if (student) { isCurrentUser = current_user.id === student.id; }
+  const instructorOrAdmin = current_user.isInstructor || current_user.admin;
+  const permitted = isCurrentUser || (instructorOrAdmin && editable);
+
+  return (
+    <div className="inline-button-peer">
+      {link}
+      <AssignButton {...props} permitted={permitted} />
+    </div>
+  );
+};
+
+AssignCell.propTypes = {
+  assignments: PropTypes.array,
+  prefix: PropTypes.string,
+  current_user: PropTypes.object,
+  student: PropTypes.object,
+  editable: PropTypes.bool,
+  role: PropTypes.number,
+  tooltip_message: PropTypes.string,
+  course: PropTypes.object.isRequired,
+  wikidataLabels: PropTypes.object
+};
 
 export default AssignCell;
