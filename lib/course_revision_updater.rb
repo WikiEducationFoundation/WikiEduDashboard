8e73dc5faab9c717ad40eab8a27f5919b8d8756a
Sage Ross <ragesoss@gmail.com>
2019-07-01 10:48:21 -0700

    Simplify CourseRevisionUpdate's use of course wikis

--- lib/course_revision_updater.rb
+++ lib/course_revision_updater.rb
@@ -17,19 +17,9 @@ class CourseRevisionUpdater
     @course = course
   end
 
-  def default_wiki_ids
-    wiki_ids = []
-    # For Programs & Events Dashboard, pull in Wikidata edits by default for all
-    # courses.
-    wiki_ids << Wiki.get_or_create(language: nil, project: 'wikidata').id unless Features.wiki_ed?
-    wiki_ids
-  end
-
   def update_revisions_for_relevant_wikis(all_time)
-    wiki_ids = @course.wikis.pluck(:id) + default_wiki_ids
-    wiki_ids.uniq.each do |wiki_id|
-      RevisionImporter.new(Wiki.find(wiki_id), @course)
-                      .import_revisions_for_course(all_time: all_time)
+    @course.wikis.each do |wiki|
+      RevisionImporter.new(wiki, @course).import_revisions_for_course(all_time: all_time)
     end
   end
 end
