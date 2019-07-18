b873ca06f3fda10c8edfbfc701db4466b6a0e063
Wes Reid <bwreid@gmail.com>
2019-07-18 15:24:37 -0700

    Update assignments constants

--- app/assets/javascripts/constants/assignments.js
+++ app/assets/javascripts/constants/assignments.js
@@ -2,9 +2,11 @@ export const RECEIVE_ASSIGNMENTS = 'RECEIVE_ASSIGNMENTS';
 export const ADD_ASSIGNMENT = 'ADD_ASSIGNMENT';
 export const DELETE_ASSIGNMENT = 'DELETE_ASSIGNMENT';
 export const UPDATE_ASSIGNMENT = 'UPDATE_ASSIGNMENT';
+
+// Not for Redux
 export const ASSIGNED_ROLE = 0;
 export const REVIEWING_ROLE = 1;
 
-export const IMPROVING_ARTICLE = 0;
-export const NEW_ARTICLE = 1;
-export const REVIEWING_ARTICLE = 2;
+export const IMPROVING_ARTICLE = 'improving_article';
+export const NEW_ARTICLE = 'new_article';
+export const REVIEWING_ARTICLE = 'reviewing_article';
