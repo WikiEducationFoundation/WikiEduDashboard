95daad1432991f20d21d220e9b401c58847338ca
Wes Reid <bwreid@gmail.com>
2019-07-25 15:38:16 -0700

    Update MyAssignment.spec
    - Use mount() function from Enzyme
    - Assignments now come with article_urls even if they are new, so
      make modifications to account for that

--- test/components/overview/my_assignment.spec.jsx
+++ test/components/overview/my_assignment.spec.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
 
-import { MyAssignment } from '../../../app/assets/javascripts/components/overview/my_assignment.jsx';
 import '../../testHelper';
+import configureMockStore from 'redux-mock-store';
+import { MyAssignment } from '../../../app/assets/javascripts/components/overview/my_assignment.jsx';
 import Feedback from '../../../app/assets/javascripts/components/common/feedback.jsx';
 
+const mockStore = configureMockStore([]);
 describe('<MyAssignment />', () => {
-  const assignment = { id: 1, role: 0, article_title: '1' };
+  const store = mockStore({ feedback: {} });
+  const assignment = { id: 1, role: 0, article_title: '1', article_url: 'https://en.wikipedia.org/wiki/1' };
   const course = { home_wiki: { language: 'en', project: 'wikipedia' } };
 
   it('feedback button is displayed on sandbox and improving articles', () => {
     const props = { course: course, assignment: assignment, last: false, wikidataLabels: {} };
-    // Sandbox articles
-    let wrapper = shallow(<MyAssignment {...props} />);
-    expect(wrapper.find(Feedback)).to.have.length(1);
-
-    // Improving Articles
-    props.assignment = { id: 1, role: 0, article_title: 'One', article_url: 'https://en.wikipedia.org/wiki/1' };
-    wrapper = shallow(<MyAssignment {...props} />);
+    const wrapper = mount(
+      <Provider store={store}>
+        <MyAssignment {...props} />
+      </Provider>
+    );
     expect(wrapper.find(Feedback)).to.have.length(1);
   });
 
@@ -25,11 +27,11 @@ describe('<MyAssignment />', () => {
     const props = { course: course, assignment: assignment, last: false, wikidataLabels: {} };
     props.course = { home_wiki: { language: 'en', project: 'wikivoyage' } };
 
-    let wrapper = shallow(<MyAssignment {...props} />);
-    expect(wrapper.find(Feedback)).to.have.length(0);
-
-    props.assignment = { id: 1, role: 0, article_title: 'One', article_url: 'https://en.wikipedia.org/wiki/1' };
-    wrapper = shallow(<MyAssignment {...props} />);
+    const wrapper = mount(
+      <Provider store={store}>
+        <MyAssignment {...props} />
+      </Provider>
+    );
     expect(wrapper.find(Feedback)).to.have.length(0);
   });
 });
