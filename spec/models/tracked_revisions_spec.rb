bb65c450b133a4a3fa2f99e21c6de730138c317c
Amit Joki <amitjoki@gmail.com>
2019-08-02 08:31:46 +0530

    Test for data in Course, ArticlesCourse and CoursesUsers

--- spec/models/tracked_revisions_spec.rb
+++ spec/models/tracked_revisions_spec.rb
@@ -12,15 +12,31 @@ describe 'Course#tracked_revisions' do
   end
 
   it 'fetches the course stats for only tracked articles' do
+    # Fetch the original data
     VCR.use_cassette 'course_upload_importer/Hbultra' do
       UpdateCourseStats.new(course)
     end
+
+    # Check if the data is cached
+    old_character_sum = course.character_sum
+    expect(course.character_sum).to eq(old_character_sum)
+    expect(CoursesUsers.first.character_sum_ms).to eq(old_character_sum)
+    expect(course.articles_courses.tracked.sum(:character_sum)).to eq(old_character_sum)
     expect(course.revisions.count).to eq(course.tracked_revisions.count)
+
+    # Exclude an article from being tracked
     untracked_id = Article.find_by(title: 'Afterload').id
     course.articles_courses.where(article_id: untracked_id).update(tracked: false)
+
+    # Update the data to reflect the new data
     VCR.use_cassette 'course_upload_importer/Hbultra' do
       UpdateCourseStats.new(course)
     end
+
+    # Check if the updated cached data is lesser than previous data due to excluded articles
+    expect(course.character_sum).to be < old_character_sum
+    expect(course.articles_courses.tracked.sum(:character_sum)).to be < old_character_sum
+    expect(CoursesUsers.first.character_sum_ms).to be < old_character_sum
     expect(course.tracked_revisions.count).to be < course.revisions.count
   end
 end
