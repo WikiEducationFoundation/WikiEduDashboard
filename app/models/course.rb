5b38b07ead49b14176aadcf6c2403b2d883a9210
Sage Ross <ragesoss@gmail.com>
2019-10-01 10:26:31 -0700

    Remove unused method

--- app/models/course.rb
+++ app/models/course.rb
@@ -340,10 +340,6 @@ class Course < ApplicationRecord
     flags['academic_system']
   end
 
-  def academic_system_present?
-    flags.key?('academic_system')
-  end
-
   def edit_settings_present?
     flags.key?('edit_settings')
   end
