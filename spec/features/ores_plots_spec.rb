cd06ce04b6b86179a2ff79edf297b0a3e7775f61
Sage Ross <ragesoss@gmail.com>
2019-10-01 10:12:22 -0700

    Improve spec coverage of OresPlotController

--- spec/features/ores_plots_spec.rb
+++ spec/features/ores_plots_spec.rb
@@ -5,11 +5,18 @@ require "#{Rails.root}/lib/analytics/histogram_plotter"
 
 describe 'ORES plots', type: :feature do
   describe 'for a single course' do
-    let(:course) { create(:course) }
+    let(:course) { create(:course, article_count: 1) }
 
     it 'returns a json array' do
       visit "/courses/#{escaped_slug course.slug}/ores_plot.json"
       expect(JSON.parse(page.body)).to eq('ores_plot' => [])
+    end
+
+    it 'rerenders via the refresh link', js: true do
+      visit "/courses/#{escaped_slug course.slug}/articles/edited"
+      click_button 'Change in Structural Completeness'
+      click_link 'Refresh Cached Data'
+      expect(page).to have_text('This graph visualizes')
     end
   end
 
