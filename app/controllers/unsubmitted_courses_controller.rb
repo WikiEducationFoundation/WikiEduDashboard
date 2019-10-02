367b6f823d3cfe2478983b025480543b53396727
Sage Ross <ragesoss@gmail.com>
2019-10-02 10:52:13 -0700

    Fix unsubmitted courses list

--- app/controllers/unsubmitted_courses_controller.rb
+++ app/controllers/unsubmitted_courses_controller.rb
@@ -4,7 +4,6 @@ class UnsubmittedCoursesController < ApplicationController
   respond_to :html
 
   def index
-    @courses_list = Course.unsubmitted.order(created_at: :desc).includes(:tags, :instructors)
-    @presenter = CoursesPresenter.new(current_user: current_user, courses_list: @courses_list)
+    @unsubmitted_courses = Course.unsubmitted.order(created_at: :desc).includes(:tags, :instructors)
   end
 end
