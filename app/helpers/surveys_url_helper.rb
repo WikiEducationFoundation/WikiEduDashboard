2b730a5dce161ad74a71ec27f9105e3c41940848
Sage Ross <ragesoss@gmail.com>
2019-11-13 15:06:37 -0800

    Replace ampersand in course slug for survey links
    We disallow "&" in a course title on the frontend, but that validation wasn't preventing submission for several months. We now have 11 courses, most of them recent (and a few much older) that have this character. It's fine except in cases where the slug is passed as a URL param, in which case it gets treated as a separated instead of being automatically encoded in by the browser. This should fix survey links that are going out to Fall 2019 courses.

--- app/helpers/surveys_url_helper.rb
+++ app/helpers/surveys_url_helper.rb
@@ -2,6 +2,6 @@
 
 module SurveysUrlHelper
   def course_survey_url(notification)
-    "#{survey_url(notification.survey)}?course_slug=#{notification.course.slug}"
+    "#{survey_url(notification.survey)}?course_slug=#{notification.course.slug.gsub('&', '%26')}"
   end
 end
