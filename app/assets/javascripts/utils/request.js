c10d7f6c76992b395eaff49a75274096f764b976
Wes Reid <bwreid@gmail.com>
2019-11-19 10:52:22 -0800

    Do not send CSRF Token to external URLs from request helper

--- app/assets/javascripts/utils/request.js
+++ app/assets/javascripts/utils/request.js
@@ -1,14 +1,17 @@
 import Rails from 'rails-ujs';
 import fetch from 'cross-fetch';
 
+const isRelativePath = path => !path.match(/http(s?)/g);
+
 export default (path, { method = 'GET', body = null } = {}) => {
   const options = {
     headers: {
-      'Content-Type': 'application/json',
-      'X-CSRF-Token': Rails.csrfToken()
+      'Content-Type': 'application/json'
     },
     method
   };
 
+  // If the path is an internal request, include the CSRF Token
+  if (isRelativePath(path)) options.headers['X-CSRF-Token'] = Rails.csrfToken();
   return fetch(path, body ? { body, ...options } : options);
 };
