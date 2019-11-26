f61110afd9c6a473e91688650542fc5c3b8ae78a
xiaobai007 <jiang.kat@gmail.com>
2019-11-25 22:49:41 -0800

    Update front end to point to new rails route for user enrollment

--- app/assets/javascripts/actions/user_actions.js
+++ app/assets/javascripts/actions/user_actions.js
@@ -8,13 +8,13 @@ export const fetchUsers = courseSlug => (dispatch) => {
 };
 
 export const addUser = (courseSlug, user) => (dispatch) => {
-  return API.modify('user', courseSlug, user, true)
+  return API.modify('user_enroll', courseSlug, user, true)
     .then(data => dispatch({ type: ADD_USER, data }))
     .catch(data => dispatch({ type: API_FAIL, data }));
 };
 
 export const removeUser = (courseSlug, user) => (dispatch) => {
-  return API.modify('user', courseSlug, user, false)
+  return API.modify('user_enroll', courseSlug, user, false)
     .then(data => dispatch({ type: REMOVE_USER, data }))
     .catch(data => dispatch({ type: API_FAIL, data }));
 };
