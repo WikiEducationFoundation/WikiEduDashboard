e52b2f258f076d957e739c8a00d9af1192a7392f
Kartik Prajapati <kariktprajapati987@gmail.com>
2019-12-13 04:16:15 +0530

    Update RemoveButton.jsx

--- app/assets/javascripts/components/overview/my_articles/components/Categories/List/Assignment/Header/Actions/RemoveButton.jsx
+++ app/assets/javascripts/components/overview/my_articles/components/Categories/List/Assignment/Header/Actions/RemoveButton.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const RemoveButton = ({ assignment, unassign }) => (
-  <div>
-    <button
-      onClick={() => unassign(assignment)}
-      className="button danger small"
-    >
-      Remove
-    </button>
-  </div>
-);
+export const RemoveButton = ({ assignment, unassign, editable }) => {
+  let removeButton;
+  if (editable) {
+    removeButton = <button onClick={() => unassign(assignment)} className="button danger small">Remove</button>;
+  } else {
+    removeButton = <button onClick={() => unassign(assignment)} className="button danger small" disabled>Remove</button>;
+  }
+
+  return (
+    <div>
+      {removeButton}
+    </div>
+  );
+};
 
 RemoveButton.propTypes = {
   // props
   assignment: PropTypes.object.isRequired,
+  editable: PropTypes.bool,
 
   // actions
   unassign: PropTypes.func.isRequired,
