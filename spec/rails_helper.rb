76251d0fc4029e3068b5a00b522b31698dcfb919
Wes Reid <bwreid@gmail.com>
2019-12-30 10:38:01 -0800

    Update code for new rubocop version

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -42,7 +42,7 @@ Capybara.server = :puma, { Silent: true }
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
