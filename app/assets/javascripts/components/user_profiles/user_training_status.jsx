6aa761f80321811b1be1fc6f0920df5c60984272
Sage Ross <ragesoss@gmail.com>
2020-02-19 10:08:04 -0800

    Fix rendering of user profile pages
    Now the TrainingStatus component requires an `exercises` object and the `kind` property for each module.
    
    Also, add a feature spec that would have caught this error.

--- app/assets/javascripts/components/user_profiles/user_training_status.jsx
+++ app/assets/javascripts/components/user_profiles/user_training_status.jsx
@@ -10,7 +10,7 @@ const UserTrainingStatus = createReactClass({
   render() {
     let status;
     if (this.props.trainingModules.length > 0) {
-      status = <TrainingStatus trainingModules={this.props.trainingModules} />;
+      status = <TrainingStatus exercises={{}} trainingModules={this.props.trainingModules} />;
     } else {
       status = <span>{I18n.t('users.user_no_training_status')}</span>;
     }
