898de0d7331c2e1229179082d27cece534cb0dbc
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2020-03-19 03:56:15 +0530

    Deleting all open requested accounts on course deletion.

--- app/models/course.rb
+++ app/models/course.rb
@@ -72,7 +72,10 @@ class Course < ApplicationRecord
   has_many :staff, -> { where('courses_users.role = 4') },
            through: :courses_users, source: :user
   has_many :survey_notifications, dependent: :destroy
-  has_many :requested_accounts
+  has_many :requested_accounts,
+           class_name: 'RequestedAccount',
+           foreign_key: 'course_id',
+           dependent: :destroy
   has_many :tickets,
            class_name: 'TicketDispenser::Ticket',
            foreign_key: 'project_id',
