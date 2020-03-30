4fb4632eafb79540434d4b1c28f321ae4f270866
Sage Ross <ragesoss@gmail.com>
2020-03-30 15:04:50 -0700

    Tweak command

--- db/migrate/20200327172134_convert_training_modules_to_utf8_mb4.rb
+++ db/migrate/20200327172134_convert_training_modules_to_utf8_mb4.rb
@@ -1,10 +1,10 @@
 class ConvertTrainingModulesToUtf8Mb4 < ActiveRecord::Migration[6.0]
   def change
-    remove_index :training_slides, :slug, unique: true;
+    remove_index :training_slides, :slug
     execute "ALTER TABLE training_slides ROW_FORMAT=DYNAMIC"
     execute "ALTER TABLE training_slides CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
     execute "ALTER TABLE training_slides MODIFY content TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
     execute "ALTER TABLE training_slides MODIFY translations TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
-    add_index :training_slides, :slug, unique: true;
+    add_index :training_slides, :slug, unique: true
   end
 end
