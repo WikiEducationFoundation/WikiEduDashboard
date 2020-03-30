74767663ca39f807efe198bc6760fe0469475c28
Sage Ross <ragesoss@gmail.com>
2020-03-30 15:13:25 -0700

    Specify length of utf8mb4 index

--- db/migrate/20200327172134_convert_training_modules_to_utf8_mb4.rb
+++ db/migrate/20200327172134_convert_training_modules_to_utf8_mb4.rb
@@ -5,6 +5,6 @@ class ConvertTrainingModulesToUtf8Mb4 < ActiveRecord::Migration[6.0]
     execute "ALTER TABLE training_slides CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
     execute "ALTER TABLE training_slides MODIFY content TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
     execute "ALTER TABLE training_slides MODIFY translations TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
-    add_index :training_slides, :slug, unique: true
+    add_index :training_slides, :slug, unique: true, length: 191
   end
 end
