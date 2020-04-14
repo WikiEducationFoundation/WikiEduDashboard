1aa1bb5b38028349467a8053b95f17d8f1518f4b
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2020-04-15 02:27:30 +0530

    Single quotes.

--- spec/config/deploy_spec.rb
+++ spec/config/deploy_spec.rb
@@ -4,8 +4,8 @@ require 'rails_helper'
 
 describe 'Deployment configuration' do
   it 'verifies that the Capistrano gem version being locked is correct' do
-    deploy_config_text = File.read("config/deploy.rb")
-    capistrano_version = deploy_config_text.scan(/lock '.*'/)[0].gsub("lock ", "").gsub(/[']/, "")
+    deploy_config_text = File.read('config/deploy.rb')
+    capistrano_version = deploy_config_text.scan(/lock '.*'/)[0].gsub('lock ', '').gsub(/[']/, '')
     expect(Gem.loaded_specs['capistrano'].version.to_s).to eq(capistrano_version)
   end
 end
