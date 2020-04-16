cec0aa395cfe7de20b134c9318e33f57d58c2e20
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2020-04-16 18:19:35 +0530

    Fixed rubocop errors.

--- spec/services/add_sandbox_template_spec.rb
+++ spec/services/add_sandbox_template_spec.rb
@@ -6,7 +6,7 @@ describe AddSandboxTemplate do
   let(:course) { create(:course, home_wiki_id: 1) }
   let(:user) { create(:user) }
   let(:enrolling_user) { create(:user, username: 'Belajane41') }
-  let(:no_default_sandbox_template) { 'No default or sandbox template present.' }
+  let(:no_template) { 'No default or sandbox template present.' }
   let(:sandbox) { "User:#{enrolling_user.username}/sandbox" }
 
   before do
@@ -31,11 +31,10 @@ describe AddSandboxTemplate do
   end
 
   it 'adds sandbox template' do
-    allow_any_instance_of(WikiApi).to receive(:get_page_content).and_return(no_default_sandbox_template)
+    allow_any_instance_of(WikiApi).to receive(:get_page_content).and_return(no_template)
     expect_any_instance_of(WikiEdits).to receive(:add_to_page_top).once
     expect_any_instance_of(WikiEdits).not_to receive(:post_whole_page)
     described_class.new(home_wiki: course.home_wiki, sandbox: sandbox,
                         sandbox_template: @sandbox_template, current_user: user)
   end
-
 end
