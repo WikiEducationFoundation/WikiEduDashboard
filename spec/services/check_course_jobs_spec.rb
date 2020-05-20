3af821daf6b813c5975a936c04542117541df915
Sage Ross <ragesoss@gmail.com>
2020-05-20 09:05:45 -0700

    Slightly better testing of CheckCourseJobs

--- spec/services/check_course_jobs_spec.rb
+++ spec/services/check_course_jobs_spec.rb
@@ -22,8 +22,23 @@ describe CheckCourseJobs do
   end
 
   describe '#find_job' do
-    it 'runs without error' do
-      subject.find_job
+    context 'when no update is scheduled' do
+      it 'returns nil' do
+        expect(subject.find_job).to be_nil
+      end
+    end
+
+    context 'when there is an update scheduled' do
+      before do
+        Sidekiq::Testing.disable!
+        CourseDataUpdateWorker.set(queue: 'test').perform_async(course.id)
+      end
+
+      after { Sidekiq::Testing.inline! }
+
+      it 'returns the update job' do
+        expect(subject.find_job).to be_a(Sidekiq::Job)
+      end
     end
   end
 
