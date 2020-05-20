cb68c7a01a38dd5b6491b82cb32aa4d17d292673
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2020-05-21 00:01:43 +0530

    Used new constant instead.

--- lib/ores_api.rb
+++ lib/ores_api.rb
@@ -13,6 +13,7 @@ class OresApi
   # Use this if we need to make parallel threaded requests.
   # CONCURRENCY = 4
 
+  ORES_SERVER_URL = 'https://ores.wikimedia.org'
   REVS_PER_REQUEST = 50
 
   # All the wikis with an articlequality model as of 2018-09-18
@@ -32,14 +33,12 @@ class OresApi
   end
 
   def get_revision_data(rev_ids)
-    server_conn = ores_server
-    url_prefix = server_conn.url_prefix
     url_query = query_url(rev_ids)
-    response = server_conn.get(url_query)
+    response = ores_server.get(url_query)
     ores_data = Oj.load(response.body)
     ores_data
   rescue StandardError => e
-    save_course_error_record(@course, e.class, (url_prefix + url_query).to_s)
+    save_course_error_record(@course, e.class, ORES_SERVER_URL + url_query)
     raise e unless TYPICAL_ERRORS.include?(e.class)
     return {}
   end
@@ -66,7 +65,7 @@ class OresApi
   end
 
   def ores_server
-    conn = Faraday.new(url: 'https://ores.wikimedia.org')
+    conn = Faraday.new(url: ORES_SERVER_URL)
     conn.headers['User-Agent'] = ENV['dashboard_url'] + ' ' + Rails.env
     conn
   end
