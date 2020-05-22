9f4ad192783954b7a8164be782445ccc63a069eb
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2020-05-23 02:15:04 +0530

    Added paramter sentry_tag_uuid and removed condition, to be handled in other places.

--- lib/errors/course_update_error_handling.rb
+++ lib/errors/course_update_error_handling.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 module CourseUpdateErrorHandling
-  def save_course_error_record(course, error, url: nil, miscellaneous: nil)
-    return unless course.present?
+  def save_course_error_record(course, error, sentry_tag_uuid, url: nil, miscellaneous: nil)
     CourseErrorRecord.create(course: course,
                              type_of_error: error.class,
                              api_call_url: url,
+                             sentry_tag_uuid: sentry_tag_uuid,
                              miscellaneous: miscellaneous)
   end
 end
