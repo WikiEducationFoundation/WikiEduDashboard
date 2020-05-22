ee0e5178f8f1da43526c539a0df52c67a5a3f471
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2020-05-23 00:41:19 +0530

    New sentry_tag_uuid field for CourseErrorRecord.

--- app/models/course_error_record.rb
+++ app/models/course_error_record.rb
@@ -8,6 +8,7 @@
 #  updated_at          :datetime
 #  course_id           :integer
 #  type_of_error       :string(255)
+#  sentry_tag_uuid     :string(255)
 #  api_call_url        :text(65535)
 #  miscellaneous       :text(65535)
 #
