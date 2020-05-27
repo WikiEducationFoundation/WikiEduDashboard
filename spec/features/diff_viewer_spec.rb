2e606034bbf35b3b84d50036fcba5f3977aa8762
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2020-05-27 13:00:13 +0530

    Changed user and updated course in before block.

--- spec/features/diff_viewer_spec.rb
+++ spec/features/diff_viewer_spec.rb
@@ -3,30 +3,33 @@
 require 'rails_helper'
 
 describe 'difference viewer', type: :feature, js: true do
-  let(:course) { create(:course, start: '2013-10-01', end: '2014-10-01') }
-  let(:user) { create(:user, username: '66.45.13.253') }
+  let(:course) { create(:course, start: '2013-02-07', end: '2014-01-01') }
+  let(:user) { create(:user, username: 'Greatgavini') }
 
-  it 'checks if renders details properly' do
+  before do
     create(:courses_user, course_id: course.id, user_id: user.id)
 
     # Updating course
-    login_as user
     VCR.use_cassette 'diff_viewer/course_update' do
-      visit "/courses/#{course.slug}/manual_update"
+      UpdateCourseStats.new(course, full: true)
     end
+  end
 
-    # User has 6 contributions since 24th Oct 2013
-    # https://en.wikipedia.org/wiki/Special:Contributions/66.45.13.253
+  it 'checks if renders details properly' do
     visit "/courses/#{course.slug}/activity"
-    expect(page).to have_css('button.icon-diff-viewer', count: 6)
 
-    # Checking length of difference, difference of characters (which in this case
-    # is ' lilah', should appear only once) and a small portion of unchanged text,
-    # which should appear two times (i.e., in both the columns of diff viewer)
-    # https://en.wikipedia.org/w/index.php?title=Heaven%27s_Gate_(religious_group)&diff=prev&oldid=578607606
+    # User has 3 contributions since 7th Feb 2013
+    # https://en.wikipedia.org/wiki/Special:Contributions/Greatgavini
+    expect(page).to have_css('button.icon-diff-viewer', count: 3)
+
+    # Checking length of difference, a small portion of changed text (which in this case
+    # is 'They see ash run into', should appear only once) and a small portion of unchanged text
+    # (which in this case is 'Although Misty is the leader'), which should appear two
+    # times (i.e., in both the columns of diff viewer)
+    # https://en.wikipedia.org/w/index.php?diff=prev&oldid=554568302
     all('button.icon-diff-viewer').last.click
-    expect(page).to have_content('6 Chars Added')
-    expect(page).to have_content(' lilah').once
-    expect(page).to have_content('Jacques Vallée').twice
+    expect(page).to have_content('-1299 Chars Added')
+    expect(page).to have_content('They see ash run into').once
+    expect(page).to have_content('Although Misty is the leader').twice
   end
 end
