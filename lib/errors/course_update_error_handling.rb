0f7a54cd0d99bf5130dc22fc40eadfba75e82826
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2020-05-28 03:23:48 +0530

    Changed arguments.

--- lib/errors/course_update_error_handling.rb
+++ lib/errors/course_update_error_handling.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 module CourseUpdateErrorHandling
-  def save_course_error_record(course, error, sentry_tag_uuid, url: nil, miscellaneous: nil)
+  def save_course_error_record(course, error, sentry_tag_uuid, optional_params)
     CourseErrorRecord.create(course: course,
                              type_of_error: error.class,
-                             api_call_url: url,
                              sentry_tag_uuid: sentry_tag_uuid,
-                             miscellaneous: miscellaneous)
+                             api_call_url: optional_params[:url],
+                             miscellaneous: optional_params[:miscellaneous])
   end
 end
