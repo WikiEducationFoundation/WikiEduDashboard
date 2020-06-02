7691aa5d60f8527df7ce891b1f510b553f974f3e
Amit Joki <amitjoki@gmail.com>
2020-06-02 10:25:21 +0530

    fix eslint issues

--- prepare.js
+++ prepare.js
@@ -1,5 +1,7 @@
+/* eslint no-console: ["error", { allow: ["log"] }] */
+
 function elapsed(hrtime) {
-  var seconds = (hrtime[0] + (hrtime[1] / 1e9)).toFixed(3);
+  const seconds = (hrtime[0] + (hrtime[1] / 1e9)).toFixed(3);
   start = process.hrtime(); // reset the time
   return seconds;
 }
@@ -11,12 +13,12 @@ const config = require('./config');
 // cleans the stale assets
 console.log('Started cleaning stale assets');
 execSync(`rm -rf ${config.outputPath}`);
-console.log('\x1b[33m%s\x1b[0m', 'Finished cleaning stale assets after ' + elapsed(process.hrtime(start)) + ' seconds');
+console.log('\x1b[33m%s\x1b[0m', `Finished cleaning stale assets after ${elapsed(process.hrtime(start))} seconds`);
 
 // export i18n files
 console.log('Started emitting i18n translations');
 execSync('bundle exec rails i18n:js:export');
-console.log('\x1b[33m%s\x1b[0m', 'Finished emitting i18n translations after ' + elapsed(process.hrtime(start)) + ' seconds');
+console.log('\x1b[33m%s\x1b[0m', `Finished emitting i18n translations after ${elapsed(process.hrtime(start))} seconds`);
 
 // copies static assets from the source to assets folder
 const copyPaths = [{
@@ -32,9 +34,8 @@ const copyPaths = [{
   from: './node_modules/tinymce/skins',
   to: `${config.outputPath}/${config.jsDirectory}/skins`,
 }];
-let idx = 0;
-copyPaths.forEach(entry => {
-  switch(idx) {
+copyPaths.forEach((entry, idx) => {
+  switch (idx) {
     case 0: console.log('Started copying jQuery'); break;
     case 1: console.log('Started copying images'); break;
     case 2: console.log('Started copying fonts'); break;
@@ -42,13 +43,12 @@ copyPaths.forEach(entry => {
     default: console.log('Started copying static assets'); break;
   }
   execSync(`cp -r ${entry.from} ${entry.to}`);
-  switch(idx) {
-    case 0: console.log('\x1b[33m%s\x1b[0m', 'Finished copying jQuery ' + elapsed(process.hrtime(start)) + ' seconds'); break;
-    case 1: console.log('\x1b[33m%s\x1b[0m', 'Finished copying images ' + elapsed(process.hrtime(start)) + ' seconds'); break;
-    case 2: console.log('\x1b[33m%s\x1b[0m', 'Finished copying fonts ' + elapsed(process.hrtime(start)) + ' seconds'); break;
-    case 3: console.log('\x1b[33m%s\x1b[0m', 'Finished copying tinymce skins ' + elapsed(process.hrtime(start)) + ' seconds'); break;
-    default: console.log('\x1b[33m%s\x1b[0m', 'Finished copying static assets ' + elapsed(process.hrtime(start)) + ' seconds'); break;
+  switch (idx) {
+    case 0: console.log('\x1b[33m%s\x1b[0m', `Finished copying jQuery after ${elapsed(process.hrtime(start))} seconds`); break;
+    case 1: console.log('\x1b[33m%s\x1b[0m', `Finished copying images after ${elapsed(process.hrtime(start))} seconds`); break;
+    case 2: console.log('\x1b[33m%s\x1b[0m', `Finished copying fonts after ${elapsed(process.hrtime(start))} seconds`); break;
+    case 3: console.log('\x1b[33m%s\x1b[0m', `Finished copying tinymce skins after ${elapsed(process.hrtime(start))} seconds`); break;
+    default: console.log('\x1b[33m%s\x1b[0m', `Finished copying static assets after ${elapsed(process.hrtime(start))} seconds`); break;
   }
-  idx++;
 });
 
