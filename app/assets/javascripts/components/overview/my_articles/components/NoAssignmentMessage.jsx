50cf5fe0c4ee79eba3d453ddcd98b0f4591a0a5f
Sage Ross <ragesoss@gmail.com>
2020-06-04 08:55:24 -0700

    Fix link to training module

--- app/assets/javascripts/components/overview/my_articles/components/NoAssignmentMessage.jsx
+++ app/assets/javascripts/components/overview/my_articles/components/NoAssignmentMessage.jsx
@@ -4,7 +4,7 @@ export default () => (
   <div>
     <p>You have not chosen an article to work on. When you have found an article to work on, use the button above to assign it.</p>
     <aside>
-      <a href="/training/students/using-the-dashboard" target="_blank" className="button ghost-button">
+      <a href="/training/students/keeping-track-of-your-work" target="_blank" className="button ghost-button">
         How to use the Dashboard
       </a>
       <a href="/training/students/finding-your-article" target="_blank" className="button ghost-button">
