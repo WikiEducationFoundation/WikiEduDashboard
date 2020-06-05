e9793df9073b391fbcd935bb8d2c9c20cdbb8b64
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2020-06-06 01:54:35 +0530

    Moved course update related things here from ErrorHandling module.

--- lib/errors/course_update_error_handling.rb
+++ lib/errors/course_update_error_handling.rb
@@ -1,11 +1,23 @@
 # frozen_string_literal: true
 
 module CourseUpdateErrorHandling
+  def perform_course_error_handling(error, level, extra, course, optional_params)
+    sentry_tag_uuid = SecureRandom.uuid
+    save_course_error_record(course, error, sentry_tag_uuid, optional_params)
+    report_course_exception_sentry(error, level, extra, sentry_tag_uuid)
+  end
+
   def save_course_error_record(course, error, sentry_tag_uuid, optional_params)
     CourseErrorRecord.create(course: course,
                              type_of_error: error.class,
                              sentry_tag_uuid: sentry_tag_uuid,
                              api_call_url: optional_params[:url],
                              miscellaneous: optional_params[:miscellaneous])
+  end
+
+  def report_course_exception_sentry(error, level, extra, sentry_tag_uuid)
+    Raven.tags_context(uuid: sentry_tag_uuid) do
+      Raven.capture_exception error, level: level, extra: extra
+    end
   end
 end
