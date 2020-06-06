0d074d599f516c441425d797bbdda67cdf18cda9
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2020-06-06 21:58:48 +0530

    Used keyword arguments and moved build_optional_params into relevant module CourseUpdateErrorHandling.

--- lib/errors/error_handling.rb
+++ lib/errors/error_handling.rb
@@ -5,11 +5,12 @@ require_dependency "#{Rails.root}/lib/errors/course_update_error_handling"
 module ErrorHandling
   include CourseUpdateErrorHandling
 
-  def perform_error_handling(error, extra, course, optional_params)
+  def perform_error_handling(error:, extra:, course:, optional_params:)
     Rails.logger.info "Caught #{error}"
     level = error_level(error)
     if course.present?
-      perform_course_error_handling(error, level, extra, course, optional_params)
+      perform_course_error_handling(error: error, level: level, extra: extra,
+                                    course: course, optional_params: optional_params)
     else
       report_exception_sentry(error, level, extra)
     end
@@ -26,20 +27,5 @@ module ErrorHandling
 
   def report_exception_sentry(error, level, extra)
     Raven.capture_exception error, level: level, extra: extra
-  end
-
-  # Building optional params for Replica and OresApi which require
-  # response body for Oj::ParseError to be saved in CourseErrorRecord
-  # WikiApi does not have Oj::ParseError as a typical error, which is
-  # why it does not use this method
-  def build_optional_params(course, error, url, response_body)
-    return {} unless course.present?
-    optional_params = { url: url }
-    if error.class == Oj::ParseError
-      # First 500 characters of response_body sufficient to analyze the error.
-      # In most cases of Oj::ParseError, the response is not a JSON object
-      optional_params[:miscellaneous] = { response_body: response_body[0..499] }
-    end
-    optional_params
   end
 end
