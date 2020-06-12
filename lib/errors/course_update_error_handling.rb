3683d325111c7354203a5551d4d65a7e709f58a0
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2020-06-13 02:25:38 +0530

    Added support for number of errors occuring.

--- lib/errors/course_update_error_handling.rb
+++ lib/errors/course_update_error_handling.rb
@@ -3,6 +3,7 @@
 module CourseUpdateErrorHandling
   def perform_error_handling(error_record)
     report_course_exception_sentry(error_record)
+    update_course_flags
   end
 
   def report_course_exception_sentry(error_record)
@@ -13,5 +14,11 @@ module CourseUpdateErrorHandling
                               course_update_id: sentry_tag_uuid,
                               course: @course.slug
                             })
+  end
+
+  def update_course_flags
+    @course.flags[:errors] ||= {}
+    @course.flags[:errors][sentry_tag_uuid] ||= 0
+    @course.flags[:errors][sentry_tag_uuid] += 1
   end
 end
