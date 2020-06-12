a3bc663bcccf53f6676034ed663ef239508c2864
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2020-06-13 02:09:01 +0530

    Made error handling more general.

--- lib/errors/error_handling.rb
+++ lib/errors/error_handling.rb
@@ -1,15 +1,11 @@
 # frozen_string_literal: true
 
-require_dependency "#{Rails.root}/lib/errors/course_update_error_handling"
-
 module ErrorHandling
-  include CourseUpdateErrorHandling
-
   def perform_error_handling(error_record)
     Rails.logger.info "Caught #{error_record.error}"
     error_record.level = error_level(error_record.error)
-    if error_record.course_present?
-      report_course_exception_sentry(error_record)
+    if error_record.specific_error_tasks?
+      error_record.perform_error_handling
     else
       report_exception_sentry(error_record)
     end
