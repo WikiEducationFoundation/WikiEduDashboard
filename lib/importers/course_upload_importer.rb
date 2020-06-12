e497fab5a5fa4d0f60d836c02bdd6d27f3bd3f1a
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2020-06-13 03:32:38 +0530

    Removed method.

--- lib/importers/course_upload_importer.rb
+++ lib/importers/course_upload_importer.rb
@@ -4,12 +4,6 @@ require_dependency "#{Rails.root}/lib/importers/upload_importer"
 
 #= Imports uploads by students during a course
 class CourseUploadImporter
-  def self.update_courses(courses)
-    courses.each do |course|
-      new(course).run
-    end
-  end
-
   def initialize(course)
     @course = course
     @start = course.start
