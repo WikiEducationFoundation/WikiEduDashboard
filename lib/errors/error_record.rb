be6751a3bd06abe01adffe2377d70c477068e114
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2020-06-13 02:10:00 +0530

    Changed ErrorRecord to be more general.

--- lib/errors/error_record.rb
+++ lib/errors/error_record.rb
@@ -4,26 +4,18 @@ class ErrorRecord
   attr_reader :error, :sentry_extra, :level
   attr_writer :level
 
-  def initialize(error, sentry_extra, update_course_stats)
+  def initialize(error, sentry_extra, update_obj: nil)
     @error = error
     @sentry_extra = sentry_extra
-    @update_course_stats = update_course_stats
+    @update_obj = update_obj
     @level = nil
   end
 
-  def course
-    @update_course_stats.course if @update_course_stats.present?
+  def specific_error_tasks?
+    @update_obj.present?
   end
 
-  def course_present?
-    course.present?
-  end
-
-  def course_slug
-    course.slug if course_present?
-  end
-
-  def sentry_tag_uuid
-    @update_course_stats.sentry_tag_uuid if course_present?
+  def perform_error_handling
+    @update_obj.perform_error_handling(self)
   end
 end
