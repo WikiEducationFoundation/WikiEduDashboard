9d761ef31bc68451ad622948d9cf1763fb522804
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2020-06-18 06:52:12 +0530

    Changed variable names.

--- lib/data_cycle/update_logger.rb
+++ lib/data_cycle/update_logger.rb
@@ -4,11 +4,11 @@ class UpdateLogger
   ################
   # Entry points #
   ################
-  def self.update_course(course, hash_info)
+  def self.update_course(course, new_logs)
     logs = course.flags['update_logs']
-    new_logs = new(logs).update(hash_info)
-    course.flags['update_logs'] = new_logs
-    course.flags['average_update_delay'] = average_delay(new_logs)
+    updated_logs = new(logs).update(new_logs)
+    course.flags['update_logs'] = updated_logs
+    course.flags['average_update_delay'] = average_delay(updated_logs)
     course.save
   end
 
@@ -40,9 +40,9 @@ class UpdateLogger
     @last_update_number = @log_hash.keys.max || 0
   end
 
-  def update(hash_info)
+  def update(new_logs)
     @update_number = @last_update_number + 1
-    @log_hash[@update_number] = hash_info
+    @log_hash[@update_number] = new_logs
     @log_hash.delete(@log_hash.keys.min) until @log_hash.count <= MAX_UPDATES
     @log_hash
   end
