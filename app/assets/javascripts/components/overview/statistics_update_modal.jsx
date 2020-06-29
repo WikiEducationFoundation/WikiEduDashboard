89a3e54dd1e2e28ab1e6f037c03bc3e7ddfafd4b
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2020-06-30 01:57:34 +0530

    Added bullet points for missing data section and removed Modal.
    The css requirements were getting different, so I removed Modal and defined a css class in stylesheets for the same.

--- app/assets/javascripts/components/overview/statistics_update_modal.jsx
+++ app/assets/javascripts/components/overview/statistics_update_modal.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Modal from '../common/modal';
 
 const StatisticsUpdateModal = (props) => {
     const helpMessage = Features.wikiEd ? I18n.t('metrics.wiki_ed_help') : I18n.t('metrics.outreach_help');
@@ -12,32 +11,28 @@ const StatisticsUpdateModal = (props) => {
     const updateNumbers = Object.keys(props.course.flags.update_logs);
     const totalUpdatesMessage = `${I18n.t('metrics.total_updates')}: ${updateNumbers[updateNumbers.length - 1]}`;
 
-    let missingDataMessage;
-    if (props.course.type === 'ArticleScopedProgram') {
-      missingDataMessage = `${I18n.t('metrics.article_scoped_program_info')} ${I18n.t('metrics.replag_info')}`;
-    } else {
-      missingDataMessage = `${I18n.t('metrics.replag_info')}`;
-    }
-
     return (
-      <Modal modalClass="statistics-update-modal">
-        <b>{I18n.t('metrics.update_status_heading')}</b>
-        <br/>
-        { errorMessage }
-        <ul>
-          <li>{ totalUpdatesMessage }</li>
-          <li>{ props.nextUpdateMessage }</li>
-        </ul>
-        <b>{I18n.t('metrics.missing_data_heading')}</b>
-        <br/>
-        { missingDataMessage }
-        <br/>
-        <a href="https://replag.toolforge.org/" target="_blank">{I18n.t('metrics.replag_link')}</a>
-        <br/>
-        <small className="mt1">{ helpMessage }</small>
-        <br/>
-        <button className="button dark" onClick={props.toggleModal}>{I18n.t('metrics.close_modal')}</button>
-      </Modal>
+      <div className="statistics-update-modal-container">
+        <div className="statistics-update-modal">
+          <b>{I18n.t('metrics.update_status_heading')}</b>
+          <br/>
+          { errorMessage }
+          <ul>
+            <li>{ totalUpdatesMessage }</li>
+            <li>{ props.nextUpdateMessage }</li>
+          </ul>
+          <b>{I18n.t('metrics.missing_data_heading')}</b>
+          <br/>
+          { I18n.t('metrics.missing_data_info') }:
+          <ul>
+            <li>{ I18n.t('metrics.replag_info') }<a href="https://replag.toolforge.org/" target="_blank">{I18n.t('metrics.replag_link')}</a></li>
+            { props.course.type === 'ArticleScopedProgram' && <li>{ I18n.t('metrics.article_scoped_program_info') }</li> }
+          </ul>
+          <small>{ helpMessage }</small>
+          <br/>
+          <button className="button dark" onClick={props.toggleModal}>{I18n.t('metrics.close_modal')}</button>
+        </div>
+      </div>
     );
 };
 
