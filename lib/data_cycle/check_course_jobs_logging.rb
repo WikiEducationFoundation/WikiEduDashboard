b1d3b7b45dbcab4f15ad500c12e49b3ce4dbb0e4
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2020-07-04 04:21:10 +0530

    New module for logging.
    Orphan locks are reported to Sentry and also counted as failed previous updates, i.e., a previous update which was supposed to happen, but didn't due to orphan lock blocking the job from executing.

---
+++ lib/data_cycle/check_course_jobs_logging.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_dependency "#{Rails.root}/lib/data_cycle/update_logger"
+
+module CheckCourseJobsLogging
+  def log_orphan_record
+    Raven.capture_message('Orphan lock removed',
+                          level: 'warn',
+                          extra: { courses: sentry_extra })
+    return nil
+  end
+
+  def log_previous_failed_update
+    UpdateLogger.update_course(@course, 'orphan_lock_failure' => true)
+  end
+
+  def sentry_extra
+    { course: @course.slug, queue: @queue }
+  end
+end
