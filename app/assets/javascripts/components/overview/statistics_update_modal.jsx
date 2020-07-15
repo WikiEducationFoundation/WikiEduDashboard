29dabd692994451dab9e8446351a9718dc2e92ee
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2020-07-16 01:54:39 +0530

    Added more course update information.
    In the last 10 logs displayed count of failure and errored updates. Also, a summary for latest update: failure-orphan lock, success and error.

--- app/assets/javascripts/components/overview/statistics_update_modal.jsx
+++ app/assets/javascripts/components/overview/statistics_update_modal.jsx
@@ -3,13 +3,21 @@ import React from 'react';
 const StatisticsUpdateModal = (props) => {
     const helpMessage = Features.wikiEd ? I18n.t('metrics.wiki_ed_help') : I18n.t('metrics.outreach_help');
 
-    let errorMessage = '';
+    let lastUpdateSummary = '';
     const errorCount = props.course.updates.last_update.error_count;
-    if (errorCount && errorCount > 0) {
-      errorMessage = `${I18n.t('metrics.error_count_message', { error_count: errorCount })}`;
+
+    if (errorCount === 0) {
+      lastUpdateSummary = `${I18n.t('metrics.last_update_success')}`;
+    } else if (errorCount > 0) {
+      lastUpdateSummary = `${I18n.t('metrics.error_count_message', { error_count: errorCount })}`;
     } else if (props.course.updates.last_update.orphan_lock_failure) {
-      errorMessage = `${I18n.t('metrics.last_update_failed')}`;
+      lastUpdateSummary = `${I18n.t('metrics.last_update_failed')}`;
     }
+
+    const updateLogs = Object.values(props.course.flags.update_logs);
+    const failureUpdatesCount = updateLogs.filter(log => log.orphan_lock_failure !== undefined).length;
+    const erroredUpdatesCount = updateLogs.filter(log => log.error_count !== undefined && log.error_count > 0).length;
+    const recentUpdatesSummary = I18n.t('metrics.recent_updates_summary', { total: updateLogs.length, failure_count: failureUpdatesCount, error_count: erroredUpdatesCount });
 
     // Update numbers (ids) are stored incrementally as counts in update_logs, so the
     // last update number is the total number of updates till now
@@ -23,8 +31,9 @@ const StatisticsUpdateModal = (props) => {
         <div className="statistics-update-modal">
           <b>{I18n.t('metrics.update_status_heading')}</b>
           <br/>
-          { errorMessage }
+          { lastUpdateSummary }
           <ul>
+            <li>{ recentUpdatesSummary }</li>
             <li>{ totalUpdatesMessage }</li>
             { updateTimesInformation !== null && <li>{updateTimesInformation[1]}</li> }
           </ul>
