f9c0f2162d2674b986426b8b566f3ed713f7a061
Sage Ross <ragesoss@gmail.com>
2020-08-06 08:53:48 -0700

    Add 'all questions' view for FAQs

--- spec/features/faq_spec.rb
+++ spec/features/faq_spec.rb
@@ -6,13 +6,20 @@ describe 'FAQs', type: :feature, js: true do
   let!(:faq) { create(:faq, title: 'How does this work?', content: 'It works.') }
   let(:admin) { create(:admin) }
 
-  before { login_as admin }
+  before do
+    FaqTopic.update(slug: 'top', name: 'Top questions', faqs: [])
+    login_as admin
+  end
 
   describe 'INDEX page' do
     it 'has search working search' do
       visit '/faq'
       fill_in 'faq_search', with: 'how does'
       click_button 'submit_search'
+      expect(page).to have_content 'How does this work?'
+      click_link 'Top questions'
+      expect(page).not_to have_content 'How does this work?'
+      click_link 'All questions'
       expect(page).to have_content 'How does this work?'
     end
   end
