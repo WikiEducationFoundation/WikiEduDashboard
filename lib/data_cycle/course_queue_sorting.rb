ef5002a3fd1ef97d1607071fca19cc6969a376a9
Sage Ross <ragesoss@gmail.com>
2020-11-04 12:20:01 -0800

    Sort courses based on update time, not length
    This retains length-based queue sorting for the initial update, but after the first update, it will go based on how much time previous updates took. Currently on P&E Dashboard, there would be about 300 courses with short updates, 72 with medium updates, and 173 with long updates.

--- lib/data_cycle/course_queue_sorting.rb
+++ lib/data_cycle/course_queue_sorting.rb
@@ -2,6 +2,28 @@
 
 module CourseQueueSorting
   def queue_for(course)
+    case average_update_time(course)
+    when nil
+      initial_queue(course)
+    when 0..30 # up to 30 seconds
+      'short_update'
+    when 31..600 # up to 10 minutes
+      'medium_update'
+    when (601..Float::INFINITY) # more than 10 minutes
+      'long_update'
+    end
+  end
+
+  def average_update_time(course)
+    logs = course.flags['update_logs']
+    return unless logs.present?
+    total_time = logs.keys.sum do |update_number|
+      logs[update_number]['end_time'].to_f - logs[update_number]['start_time'].to_f
+    end
+    (total_time / logs.keys.count).to_i
+  end
+
+  def initial_queue(course)
     course_length = course.end - course.start
     not_ended = Time.zone.now < course.end
     if course_length < 3.days && not_ended
--- spec/lib/data_cycle/schedule_course_updates_spec.rb
+++ spec/lib/data_cycle/schedule_course_updates_spec.rb
@@ -4,6 +4,24 @@ require 'rails_helper'
 require "#{Rails.root}/lib/data_cycle/schedule_course_updates"
 
 describe ScheduleCourseUpdates do
+  let(:fast_update_logs) do
+    {
+      'update_logs' => { 1 => { 'start_time' => 2.seconds.ago, 'end_time' => 1.second.ago } }
+    }
+  end
+
+  let(:medium_update_logs) do
+    {
+      'update_logs' => { 1 => { 'start_time' => 2.minutes.ago, 'end_time' => 1.second.ago } }
+    }
+  end
+
+  let(:slow_update_logs) do
+    {
+      'update_logs' => { 1 => { 'start_time' => 2.hours.ago, 'end_time' => 1.second.ago } }
+    }
+  end
+
   describe 'on initialization' do
     before do
       create(:editathon, start: 1.day.ago, end: 2.hours.from_now,
@@ -12,10 +30,13 @@ describe ScheduleCourseUpdates do
                       slug: 'Medium/Course', needs_update: true)
       create(:course, start: 1.day.ago, end: 1.year.from_now,
                       slug: 'Long/Program')
+      create(:course, slug: 'Fast/Updates', flags: fast_update_logs)
+      create(:course, slug: 'Medium/Updates', flags: medium_update_logs)
+      create(:course, slug: 'Slow/Updates', flags: slow_update_logs)
     end
 
     it 'calls the revisions and articles updates on courses currently taking place' do
-      expect(UpdateCourseStats).to receive(:new).thrice
+      expect(UpdateCourseStats).to receive(:new).exactly(6).times
       expect(Raven).to receive(:capture_message).and_call_original
       update = described_class.new
       sentry_logs = update.instance_variable_get(:@sentry_logs)
@@ -39,17 +60,20 @@ describe ScheduleCourseUpdates do
 
   describe 'on calling update workers' do
     let(:queue) { 'medium_update' }
+    let(:short_queue) { 'short_update' }
+    let(:flags) { nil }
 
     context 'a course has no job enqueued' do
       before do
         Sidekiq::Testing.disable!
         create(:course, start: 1.day.ago, end: 2.months.from_now,
-                        slug: 'Medium/Course', needs_update: true)
+                        slug: 'Medium/Course', needs_update: true, flags: flags)
       end
 
       after do
         # Clearing the queue after the test
         Sidekiq::Queue.new(queue).clear
+        Sidekiq::Queue.new(short_queue).clear
         Sidekiq::Testing.inline!
       end
 
@@ -74,10 +98,14 @@ describe ScheduleCourseUpdates do
 
         # No job before
         expect(Sidekiq::Queue.new(queue).size).to eq 0
+        expect(Sidekiq::Queue.new(short_queue).size).to eq 0
         described_class.new
 
         # 1 job enqueued by ScheduleCourseUpdates
-        expect(Sidekiq::Queue.new(queue).size).to eq 1
+        # It goes in the 'short' queue, because once it has an update log, queue
+        # sorting happens based on average time of recent updates, which
+        # is considered 0 for a failed update.
+        expect(Sidekiq::Queue.new(short_queue).size).to eq 1
         # Orphan lock  failure is logged
         expect(Course.first.flags['update_logs'][1]['orphan_lock_failure'].present?).to eq true
       end
