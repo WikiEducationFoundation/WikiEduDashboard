a7e647f178a95f8ad0005055b3e902e60e3eaf69
Sage Ross <ragesoss@gmail.com>
2020-11-30 16:23:08 -0800

    Account for travis-ci issues

--- spec/features/request_account_spec.rb
+++ spec/features/request_account_spec.rb
@@ -6,6 +6,8 @@ describe 'Account requests', type: :feature, js: true do
   let(:course) { create(:course, end: 1.week.from_now, flags: { register_accounts: true }) }
 
   it 'lets an unregistered user request an account' do
+    pending 'This may fail on travis-ci if the IP range is blocked from account creation.'
+
     visit "/courses/#{course.slug}?enroll=#{course.passcode}"
     click_button 'Request an account'
     fill_in 'new_account_username', with: 'My Example Username 123'
@@ -13,5 +15,7 @@ describe 'Account requests', type: :feature, js: true do
     click_button 'Check username availability'
     click_button 'Request account'
     expect(page).to have_content 'Your request for an account has been submitted'
+
+    pass_pending_spec
   end
 end
