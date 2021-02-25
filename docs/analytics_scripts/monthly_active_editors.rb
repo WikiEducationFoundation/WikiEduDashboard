63bb2eac293b162f7d3e0e4c6b339f6cbdd82fa2
Sage Ross <ragesoss@gmail.com>
2021-02-25 11:01:02 -0800

    Add a variation to monthly active editors calculation script

--- docs/analytics_scripts/monthly_active_editors.rb
+++ docs/analytics_scripts/monthly_active_editors.rb
@@ -6,3 +6,14 @@ active_editors_by_month = []
   active_editor_count = Revision.joins(:article).where(articles: { namespace: '0' }).where('extract(month from date) = ?', month).where('extract(year from date) = ?', year).group('user_id').having('count(*) > 4').count.count
   active_editors_by_month << ["#{year}-#{month}-01", active_editor_count]
 end
+
+# Calendar year version
+active_editors_by_month = []
+12.times do |i|
+  month = i + 1
+  year = 2020
+  pp month
+  active_editor_count = Revision.joins(:article).where(articles: { namespace: '0' }).where('extract(month from date) = ?', month).where('extract(year from date) = ?', year).group('user_id').having('count(*) > 4').count.count
+  active_editors_by_month << ["#{year}-#{month}", active_editor_count]
+end
+puts active_editors_by_month
