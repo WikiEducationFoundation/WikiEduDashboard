551df142649b11c7bcda7d928101fa816c3d4c54
Nate Berkopec <nate.berkopec@gmail.com>
2021-03-16 10:30:29 -0600

    Divider for the index hash

--- db/migrate/20210316163633_add_unique_article_index.rb
+++ db/migrate/20210316163633_add_unique_article_index.rb
@@ -1,7 +1,6 @@
 class AddUniqueArticleIndex < ActiveRecord::Migration[6.0]
   def change
     change_table :articles do |t|
-      t.virtual :index_hash, type: :string, as: "IF(deleted, NULL, CONCAT(mw_page_id, wiki_id))", stored: true
       t.index :index_hash, unique: true
     end
   end
