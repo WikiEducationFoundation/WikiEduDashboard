d4deaa557d90bd275a9e9c21453bc7ff3f71e5ff
Nate Berkopec <nate.berkopec@gmail.com>
2021-03-18 08:47:29 -0600

    Send course ID to newrelic for CDUW

--- config/initializers/nr_sidekiq_queue_latency.rb
+++ config/initializers/nr_sidekiq_queue_latency.rb
@@ -1,7 +1,14 @@
 class NewRelicQueueLatencyLogger
   def call(worker, job, queue)
     queue_latency = Time.now.utc - Time.at(job["enqueued_at"], in: "+00:00")
-    ::NewRelic::Agent.add_custom_attributes({ queue_latency: queue_latency, queue: queue })
+    info = {
+      queue_latency: queue_latency,
+      queue: queue,
+    }
+    if worker.class.to_s == "CourseDataUpdateWorker"
+      info[:course_id] = job["args"][0]
+    end
+    ::NewRelic::Agent.add_custom_attributes(info)
     yield
   end
 end
