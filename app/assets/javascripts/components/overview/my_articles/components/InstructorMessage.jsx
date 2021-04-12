eeee2a408cba51483ae2c1065d49e97443cb1983
Sage Ross <ragesoss@gmail.com>
2021-04-12 16:54:03 -0700

    Missing word in instructor message

--- app/assets/javascripts/components/overview/my_articles/components/InstructorMessage.jsx
+++ app/assets/javascripts/components/overview/my_articles/components/InstructorMessage.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default () => (
   <div className="example-interface-notice">
-    <p>This is the same My Articles interface that students. You can use it to walk through the same steps students will go through. However, only student contributions will be tracked.</p>
+    <p>This is the same My Articles interface that students have. You can use it to walk through the same steps students will go through. However, only student contributions will be tracked.</p>
   </div>
 );
