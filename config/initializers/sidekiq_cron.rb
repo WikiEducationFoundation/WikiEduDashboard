7f5ddc104ef0358c58b5dc69f3171616a9c19d0a
Sage Ross <ragesoss@gmail.com>
2021-04-28 14:28:10 -0700

    Handle unavailability of Redis in sidekiq-cron initializer
    This is necessary to avoid breaking the build of Docker images when the Ruby builder attempts to run asset pipeline.

--- config/initializers/sidekiq_cron.rb
+++ config/initializers/sidekiq_cron.rb
@@ -1,2 +1,6 @@
 schedule_file = "config/schedule.yml"
-Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file)
+begin
+  Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file)
+rescue
+  puts 'Could not enqueue sidekiq-cron schedule.'
+end
