dbfa3543478fcd481d85fc9d74d4e0ed2ab66283
Sage Ross <ragesoss@gmail.com>
2021-04-30 10:03:03 -0700

    Update name of `factory_bot`

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module WikiEduDashboard
         routing_specs: false,
         controller_specs: false,
         request_specs: false
-      g.fixture_replacement :factory_girl, dir: "spec/factories"
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
 
     # Settings in config/environments/* take precedence over those specified here.
