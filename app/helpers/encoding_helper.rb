2669eb625694288eba47402c211c46e2cbe61f05
Sage Ross <ragesoss@gmail.com>
2021-05-20 07:55:57 -0700

    Handle nil input for sanitizing 4-byte characters in strings

--- app/helpers/encoding_helper.rb
+++ app/helpers/encoding_helper.rb
@@ -5,7 +5,7 @@ module EncodingHelper
   # Updating the production databases to use utf8mb4 for all tables
   # would remove the need for it.
   def sanitize_4_byte_string(string)
-    if string.chars.any? { |c| c.bytes.count >= 4 }
+    if string&.chars&.any? { |c| c.bytes.count >= 4 }
       CGI.escape(string)
     else
       string
