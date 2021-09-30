299c9506f144892b9b84ee3e79fabe9402be4b3e
Sage Ross <ragesoss@gmail.com>
2021-09-30 09:14:07 -0700

    Add cleanup script for cloneable sandboxed courses
    The 'stay_in_sandboxes' option was an experiment that we've decided isn't worth continuing, so we want to make sure the old courses that were set up with timeline content built around staying in sandboxes don't get cloned.
    
    I used this script to remove the 'cloneable' tag from all the sandboxed courses in Wiki Education Dashboard production.

---
+++ docs/cleanup_scripts/uncloneable_sandboxed_courses.rb
@@ -0,0 +1,7 @@
+# Remove the 'clonable' tag from all courses that are 'stay_in_sandbox'
+
+sandboxed = Course.all.select { |c| c.stay_in_sandbox? }
+sandoxed_cloneable = sandboxed.select { |c| c.tag?('cloneable') }
+course_ids = sandboxed_cloneable.map(&:id)
+tags = Tag.where(course_id: course_ids, tag: 'cloneable')
+tags.destroy_all
