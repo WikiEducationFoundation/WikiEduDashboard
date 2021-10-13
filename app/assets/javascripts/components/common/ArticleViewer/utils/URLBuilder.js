50c1943b060db6a460c4781cc99fdf052ecf8cb1
1v4n4 <ivananl@yahoo.com>
2021-10-13 20:49:39 +0200

    Reverting to initial state to keep shema and yml files unchanged

--- app/assets/javascripts/components/common/ArticleViewer/utils/URLBuilder.js
+++ app/assets/javascripts/components/common/ArticleViewer/utils/URLBuilder.js
@@ -16,8 +16,7 @@ export class URLBuilder {
   }
 
   wikiURL() {
-    const language = this.article.language || 'www';
-    const project = this.article.project;
+    const { language = 'www', project } = this.article;
 
     if (!project) throw new TypeError('Article project is missing!');
 
