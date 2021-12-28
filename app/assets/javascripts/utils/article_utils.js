4fb64793265813f33e47d9b201ff2c095d419793
Ivana NL <ivananl@yahoo.com>
2021-12-28 23:13:41 +0100

    Remove ratings for items on Recent Activity tab and add item terminology (#4707)

--- app/assets/javascripts/utils/article_utils.js
+++ app/assets/javascripts/utils/article_utils.js
@@ -1,19 +1,23 @@
 export default class ArticleUtils {
-// Returns article or item, based on home Wiki project.
+  // Returns article or item, based on home Wiki project.
   static articlesOrItems(project) {
     return project === 'wikidata' ? 'items' : 'articles';
   }
 
-// Returns article or item message, based on articles or items keyword.
+  // Returns article or item message, based on articles or items keyword.
   static articlesOrItemsMsg(messageKey, articlesOrItems, defaultarticlesOrItems = 'articles') {
     return I18n.t(`${articlesOrItems}.${messageKey}`, {
       defaults: [{ scope: `${defaultarticlesOrItems}.${messageKey}` }]
     });
   }
 
-// Returns article or item message, based on home Wiki project.
-static I18n(messageKey, project) {
-  const articlesOrItems = ArticleUtils.articlesOrItems(project);
-  return ArticleUtils.articlesOrItemsMsg(messageKey, articlesOrItems);
-}
+  // Returns article or item message, based on home Wiki project.
+  static I18n(messageKey, project) {
+    const articlesOrItems = ArticleUtils.articlesOrItems(project);
+    return ArticleUtils.articlesOrItemsMsg(messageKey, articlesOrItems);
+  }
+
+  static projectSuffix(project, messageKey) {
+    return project === 'wikidata' ? `${messageKey}_wikidata` : messageKey;
+  }
 }
