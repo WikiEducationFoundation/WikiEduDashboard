d0e7a9cfc309463651c40556a7a79b6b9dbc16ea
Sage Ross <ragesoss@gmail.com>
2022-01-05 14:02:48 -0800

    Remove unused instance variables in term recap mailer
    Since I unset the recruiting deadline in production, these mailers have all been failing.

--- app/mailers/term_recap_mailer.rb
+++ app/mailers/term_recap_mailer.rb
@@ -32,7 +32,5 @@ class TermRecapMailer < ApplicationMailer
     @presenter = CoursesPresenter.new(current_user: nil, campaign_param: campaign.slug)
 
     @greeted_users = @instructors.map { |user| user.real_name || user.username }.to_sentence
-    @upcoming_term = Campaign.find_by(slug: Deadlines.recruiting_term)
-    @deadline = Deadlines.deadline.strftime('%B %-d, %Y')
   end
 end
