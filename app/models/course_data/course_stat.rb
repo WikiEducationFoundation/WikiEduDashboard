ab15b53bb760ad5d08a6997b63ba4ed2f146940e
1v4n4 <ivananl@yahoo.com>
2022-01-19 17:00:13 +0100

    Add UpdateWikidataStats service

--- app/models/course_data/course_stat.rb
+++ app/models/course_data/course_stat.rb
@@ -1,3 +1,14 @@
+# frozen_string_literal: true
+# == Schema Information
+#
+# Table name: course_stat
+#
+#  id          :integer
+#  course_id   :integer
+#  stats_hash  :text
+#  created_at  :datetime
+#  updated_at  :datetime
+
 class CourseStat < ApplicationRecord
   belongs_to :course
   serialize :stats_hash, Hash
---
+++ app/services/update_wikidata_stats.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require_dependency "#{Rails.root}/lib/wikidata_summary_parser"
+
+class UpdateWikidataStats
+  def initialize(course)
+    @course = course
+    update_summaries
+    update_wikidata_stats
+  end
+
+  private
+
+  def update_summaries
+    return if wikidata_revisions.empty?
+    wikidata_revisions.find_in_batches do |rev_batch|
+      rev_batch.each do |rev|
+        summary = WikidataSummaryParser.fetch_summary(rev)
+        next if summary.nil?
+        begin
+          rev.update!(summary: summary)
+        rescue ActiveRecord::StatementInvalid => e
+          Sentry.capture_exception e
+          rev.update(summary: CGI.escape(summary))
+        end
+      end
+    end
+  end
+
+  # Get Wikidata stats based on revision's summaries
+
+  def update_wikidata_stats
+    return if course_revisions.empty?
+    stats = WikidataSummaryParser.analyze_revisions(course_revisions)
+    course_stat = CourseStat.find_by(course_id: @course.id)
+                  || CourseStat.create(course_id: @course.id)
+    course_stat.stats_hash = stats
+    course_stat.save
+  end
+
+  def wikidata_revisions
+    @course.revisions.where(wiki: wikidata, summary: nil, deleted: false)
+  end
+
+  def course_revisions
+    @course.revisions.where(wiki: wikidata, deleted: false)
+  end
+
+  def wikidata
+    Wiki.get_or_create(language: nil, project: 'wikidata')
+  end
+end
