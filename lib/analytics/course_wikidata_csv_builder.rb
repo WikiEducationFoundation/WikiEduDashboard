3c9b6ecd8a7ac86d334a2d668fd69f5dfcfe0def
1v4n4 <ivananl@yahoo.com>
2022-02-16 08:12:46 +0100

    Add CSV for no Wikidata stats case and fix the test

--- lib/analytics/course_wikidata_csv_builder.rb
+++ lib/analytics/course_wikidata_csv_builder.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'csv'
-require_dependency "#{Rails.root}/lib/wikidata_summary_parser"
 
 class CourseWikidataCsvBuilder
   def initialize(course)
@@ -10,7 +9,12 @@ class CourseWikidataCsvBuilder
 
   def generate_csv
     csv_data = [CSV_HEADERS]
-    @course.course_stat.stats_hash['www.wikidata.org'].each do |revision_type, count|
+    stats =  if @course.course_stat
+               @course.course_stat.stats_hash['www.wikidata.org']
+             else
+               { 'total revisions' => 0 }
+             end
+    stats.each do |revision_type, count|
       csv_data << [revision_type, count]
     end
     CSV.generate { |csv| csv_data.each { |line| csv << line } }
