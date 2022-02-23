3751ba053335e0a30c13e417b552f88eb508ad73
Vaidehi <vaidehiatpadkar1@gmail.com>
2022-02-24 01:12:18 +0530

    Define 'allWeeks' in milestone test component

--- test/components/overview/milestones.spec.jsx
+++ test/components/overview/milestones.spec.jsx
@@ -6,14 +6,18 @@ import Milestones from '../../../app/assets/javascripts/components/overview/mile
 
 describe('Milestones', () => {
   const block = { id: 1, kind: 2, content: 'Tacos are great' };
-  const week = { order: 1, blocks: [block] };
-  const week2 = { order: 2, blocks: [] };
+  const weeks_1 = { order: 1, blocks: [block] };
+  const weeks_2 = { order: 2, blocks: [] };
+  const allWeeks_1 = { empty: false, weekNumber: 1, order: 1, blocks: [block] };
+  const allWeeks_2 = { empty: true, weekNumber: 2 };
+  const allWeeks_3 = { empty: false, weekNumber: 3, order: 2, blocks: [] }; 
   const course = { start: '2016-01-01', timeline_start: '2016-01-01' };
 
   const TestMilestonesWithWeeks = ReactTestUtils.renderIntoDocument(
     <Milestones
       timelineStart={'2018-07-20T23:59:59.000Z'}
-      weeks={[week, week2]}
+      weeks={[weeks_1, weeks_2]}
+      allWeeks = {allWeeks_1, allWeeks_2, allWeeks_3}
       course={course}
     />
   );
