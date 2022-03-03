473ee9371a3daa87c5b9cc54b66a07d04e60f32f
1v4n4 <ivananl@yahoo.com>
2022-03-03 21:49:16 +0100

    Add translations to a navbar

--- app/assets/javascripts/components/common/campaign_navbar.jsx
+++ app/assets/javascripts/components/common/campaign_navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
+import CourseUtils from '../../utils/course_utils';
 
 const CampaignNavbar = ({ campaign }) => {
   return (
@@ -8,33 +9,33 @@ const CampaignNavbar = ({ campaign }) => {
       <div className="campaign_navigation">
         <div className="container">
           <a className="nav__item">
-            <h2 className="title">Campaign: {campaign.title}</h2>
+            <h2 className="title">{I18n.t('campaign.campaign')}: {campaign.title}</h2>
           </a>
           <nav>
             <div className="nav__item" id="overview-link">
-              <p><a href={`/campaigns/${campaign.slug}/overview`} >Home</a>
+              <p><a href={`/campaigns/${campaign.slug}/overview`} >{I18n.t('courses.overview')}</a>
               </p>
             </div>
             <div className="nav__item">
               <p>
-                <a href={`/campaigns/${campaign.slug}/programs`}>Programs</a>
+                <a href={`/campaigns/${campaign.slug}/programs`}>{CourseUtils.i18n('courses', campaign.course_string_prefix)}</a>
               </p>
             </div>
             <div className="nav__item" id="articles-link">
               <p>
-                <a href={`/campaigns/${campaign.slug}/articles`}>Articles</a>
+                <a href={`/campaigns/${campaign.slug}/articles`}>{I18n.t('courses.articles')}</a>
               </p>
             </div>
             <div className="nav__item">
               <p>
-                <a href={`/campaigns/${campaign.slug}/users`}>Editors</a>
+                <a href={`/campaigns/${campaign.slug}/users`}>{CourseUtils.i18n('students', campaign.course_string_prefix)}</a>
               </p>
             </div>
             <div className="nav__item">
-              <p><NavLink to={`/campaigns/${campaign.slug}/ores_plot`}>ORES</NavLink></p>
+              <p><NavLink to={`/campaigns/${campaign.slug}/ores_plot`}>{I18n.t('courses.ores_plot')}</NavLink></p>
             </div>
             <div className="nav__item">
-              <p><NavLink to={`/campaigns/${campaign.slug}/alerts`}>Alerts</NavLink></p>
+              <p><NavLink to={`/campaigns/${campaign.slug}/alerts`}>{I18n.t('courses.alerts')}</NavLink></p>
             </div>
             <div className="campaign-nav__search" >
               <form action={`/campaigns/${campaign.slug}/programs`} acceptCharset="UTF-8" method="get">
@@ -42,7 +43,7 @@ const CampaignNavbar = ({ campaign }) => {
                   type="text"
                   name="courses_query"
                   id="coureses_query"
-                  placeholder={`Search ${campaign.title}`}
+                  placeholder={`${I18n.t('campaign.search')} ${campaign.title}`}
                 />
                 <button type="submit">
                   <i className="icon icon-search" />
