28405052bde372533e65d22f46a5b79b529bfb5d
Sage Ross <ragesoss@gmail.com>
2022-03-09 09:20:42 -0800

    Enable SidekiqUniqueJobs lock_info

--- config/initializers/sidekiq_unique_jobs.rb
+++ config/initializers/sidekiq_unique_jobs.rb
@@ -1,6 +1,7 @@
 SidekiqUniqueJobs.configure do |config|
   config.enabled = !Rails.env.test? # turn it off for tests
   config.lock_ttl = nil # Do not expire locks, by default
+  config.lock_info = true
 end
 
 Sidekiq.configure_server do |config|
