6bbddb9ffa0725928603594ab2f0d4c4871359a7
Shashwat <10794178+TheTrio@users.noreply.github.com>
2022-03-12 13:36:05 +0530

    Fixed formatting and used wiki directly

--- spec/features/articles_edited_spec.rb
+++ spec/features/articles_edited_spec.rb
@@ -8,20 +8,23 @@ describe 'Articles Edited view', type: :feature, js: true do
   let(:home_wiki) { Wiki.get_or_create language: 'en', project: 'wikipedia' }
   let(:es_wiktionary) { create(:wiki, language: 'es', project: 'wiktionary') }
   let(:fr_wikidata) { create(:wiki, language: 'fr', project: 'wikidata') }
-  
+
   before do
     stub_wiki_validation
-    course = create(:course,
+
+    course = create(
+      :course,
       slug: slug,
       start: '2017-01-01',
       end: '2021-01-01',
-      home_wiki_id: home_wiki.id
+      home_wiki: home_wiki
     )
-    article1 = create(:article, title: 'Article 1', wiki_id: home_wiki.id)
-    article2 =  create(:article, title: 'Article 2', wiki_id: es_wiktionary.id)
-    article3 =  create(:article, title: 'Article 3', wiki_id: fr_wikidata.id)
-    article4 =  create(:article, title: 'Article 4', wiki_id: fr_wikidata.id)
-    article5 =  create(:article, title: 'Article 5', wiki_id: fr_wikidata.id)
+
+    article1 = create(:article, title: 'Article 1', wiki: home_wiki)
+    article2 =  create(:article, title: 'Article 2', wiki: es_wiktionary)
+    article3 =  create(:article, title: 'Article 3', wiki: fr_wikidata)
+    article4 =  create(:article, title: 'Article 4', wiki: fr_wikidata)
+    article5 =  create(:article, title: 'Article 5', wiki: fr_wikidata)
 
     course.campaigns << Campaign.first
     create(:courses_user, course: course, user: user)
@@ -31,11 +34,40 @@ describe 'Articles Edited view', type: :feature, js: true do
     create(:revision, article: article4, user: user, date: '2020-03-01')
     create(:revision, article: article5, user: user, date: '2020-05-01')
 
-    create(:articles_course, course: course, article: article1, user_ids: [user.id])
-    create(:articles_course, course: course, article: article2, user_ids: [user.id], new_article: true)
-    create(:articles_course, course: course, article: article3, user_ids: [user.id])
-    create(:articles_course, course: course, article: article4, user_ids: [user.id], new_article: true)
-    create(:articles_course, course: course, article: article5, user_ids: [user.id], tracked: false)
+    create(
+      :articles_course,
+      course: course,
+      article: article1,
+      user_ids: [user.id]
+    )
+    create(
+      :articles_course,
+      course: course,
+      article: article2,
+      user_ids: [user.id],
+      new_article: true
+    )
+    create(
+      :articles_course,
+      course: course,
+      article: article3,
+      user_ids: [user.id]
+    )
+    create(
+      :articles_course,
+      course: course,
+      article: article4,
+      user_ids: [user.id],
+      new_article: true
+    )
+
+    create(
+      :articles_course,
+      course: course,
+      article: article5,
+      user_ids: [user.id],
+      tracked: false
+    )
   end
 
   it 'article development graphs fetch and render edit data' do
@@ -49,84 +81,84 @@ describe 'Articles Edited view', type: :feature, js: true do
     visit "/courses/#{slug}/articles"
     select 'wikidata'
 
-    expect(page).to have_content("Article 3")
-    expect(page).to have_content("Article 4")
+    expect(page).to have_content('Article 3')
+    expect(page).to have_content('Article 4')
 
-    expect(page).to have_no_content("Article 1")
-    expect(page).to have_no_content("Article 2")
-    expect(page).to have_no_content("Article 5")
+    expect(page).to have_no_content('Article 1')
+    expect(page).to have_no_content('Article 2')
+    expect(page).to have_no_content('Article 5')
 
-    expect(page.current_url).to include("wiki=wikidata")
+    expect(page.current_url).to include('wiki=wikidata')
   end
 
-  it 'newness filter works and updates the URL' do 
+  it 'newness filter works and updates the URL' do
     visit "/courses/#{slug}/articles"
     select 'New'
 
-    expect(page).to have_content("Article 2")
-    expect(page).to have_content("Article 4")
+    expect(page).to have_content('Article 2')
+    expect(page).to have_content('Article 4')
 
-    expect(page).to have_no_content("Article 1")
-    expect(page).to have_no_content("Article 3")
-    expect(page).to have_no_content("Article 5")
+    expect(page).to have_no_content('Article 1')
+    expect(page).to have_no_content('Article 3')
+    expect(page).to have_no_content('Article 5')
 
-    expect(page.current_url).to include("newness=new")
+    expect(page.current_url).to include('newness=new')
   end
 
-  it 'tracked filter works and updates the URL' do 
+  it 'tracked filter works and updates the URL' do
     visit "/courses/#{slug}/articles"
     select 'Untracked'
 
-    expect(page).to have_content("Article 5")
-    
-    expect(page).to have_no_content("Article 1")
-    expect(page).to have_no_content("Article 2")
-    expect(page).to have_no_content("Article 3")
-    expect(page).to have_no_content("Article 4")
+    expect(page).to have_content('Article 5')
 
-    expect(page.current_url).to include("tracked=untracked")
+    expect(page).to have_no_content('Article 1')
+    expect(page).to have_no_content('Article 2')
+    expect(page).to have_no_content('Article 3')
+    expect(page).to have_no_content('Article 4')
+
+    expect(page.current_url).to include('tracked=untracked')
   end
 
-  it 'filters are set from the URL' do 
+  it 'filters are set from the URL' do
     visit "/courses/#{slug}/articles?newness=new&wiki=wikidata"
 
-    expect(page).to have_content("Article 4")
-    
-    expect(page).to have_no_content("Article 1")
-    expect(page).to have_no_content("Article 2")
-    expect(page).to have_no_content("Article 3")
-    expect(page).to have_no_content("Article 5")
+    expect(page).to have_content('Article 4')
+
+    expect(page).to have_no_content('Article 1')
+    expect(page).to have_no_content('Article 2')
+    expect(page).to have_no_content('Article 3')
+    expect(page).to have_no_content('Article 5')
   end
 
-  it 'switching to a different tab and returning maintains filters' do 
+  it 'switching to a different tab and returning maintains filters' do
     visit "/courses/#{slug}/articles"
 
     select 'Existing'
     select 'wikidata'
 
-    expect(page).to have_content("Article 3")
+    expect(page).to have_content('Article 3')
 
-    expect(page).to have_no_content("Article 1")
-    expect(page).to have_no_content("Article 2")
-    expect(page).to have_no_content("Article 4")
-    expect(page).to have_no_content("Article 5")
+    expect(page).to have_no_content('Article 1')
+    expect(page).to have_no_content('Article 2')
+    expect(page).to have_no_content('Article 4')
+    expect(page).to have_no_content('Article 5')
 
-    expect(page.current_url).to include("wiki=wikidata")
-    expect(page.current_url).to include("newness=existing")
-    
+    expect(page.current_url).to include('wiki=wikidata')
+    expect(page.current_url).to include('newness=existing')
+
     # # go to another tab
     click_link 'Uploads'
     # # return back
     click_link 'Articles'
 
-    expect(page).to have_content("Article 3")
+    expect(page).to have_content('Article 3')
 
-    expect(page).to have_no_content("Article 1")
-    expect(page).to have_no_content("Article 2")
-    expect(page).to have_no_content("Article 4")
-    expect(page).to have_no_content("Article 5")
+    expect(page).to have_no_content('Article 1')
+    expect(page).to have_no_content('Article 2')
+    expect(page).to have_no_content('Article 4')
+    expect(page).to have_no_content('Article 5')
 
-    expect(page.current_url).to include("wiki=wikidata")
-    expect(page.current_url).to include("newness=existing")
+    expect(page.current_url).to include('wiki=wikidata')
+    expect(page.current_url).to include('newness=existing')
   end
 end
