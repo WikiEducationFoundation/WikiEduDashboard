41d77b46360e0afc607d18ffd1d0d187d2348530
Shashwat <10794178+TheTrio@users.noreply.github.com>
2022-04-01 00:53:10 +0530

    changed to use constants when dispatching action

--- app/assets/javascripts/actions/media_wiki_revisions_actions.js
+++ app/assets/javascripts/actions/media_wiki_revisions_actions.js
@@ -1,6 +1,7 @@
 import { fetchClassFromRevisions } from '../utils/media_wiki_classes';
 import { getWikiMap } from '../utils/revision_utils';
 import { fetchReferencesAdded } from '../utils/media_wiki_references_utils';
+import { RECEIVE_ASSESSMENTS, RECEIVE_REFERENCES } from '../constants/revisions';
 
 export const fetchRevisionsAndReferences = async (revisions, dispatch) => {
   const wikiMap = getWikiMap(revisions);
@@ -8,7 +9,7 @@ export const fetchRevisionsAndReferences = async (revisions, dispatch) => {
     // eslint-disable-next-line no-console
     console.log('Successfully fetched all page assessment information');
     return dispatch({
-      type: 'RECEIVE_ASSESSMENTS',
+      type: RECEIVE_ASSESSMENTS,
       data: { assessments }
     });
   });
@@ -17,7 +18,7 @@ export const fetchRevisionsAndReferences = async (revisions, dispatch) => {
     // eslint-disable-next-line no-console
     console.log('Successfully fetched all references information');
     dispatch({
-      type: 'RECEIVE_REFERENCES',
+      type: RECEIVE_REFERENCES,
       data: { referencesAdded }
     });
   });
