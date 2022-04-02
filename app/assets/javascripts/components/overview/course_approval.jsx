db970188d55a04e12cc2849bd67dffc976beaaeb
Vaidehi <vaidehiatpadkar1@gmail.com>
2022-04-02 20:13:22 +0530

    Fetch special users and update component with real data

--- app/assets/javascripts/components/overview/course_approval.jsx
+++ app/assets/javascripts/components/overview/course_approval.jsx
@@ -1,59 +1,93 @@
 import React from 'react';
+
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import createReactClass from 'create-react-class';
 import Select from 'react-select';
 import selectStyles from '../../styles/single_select';
+import { fetchSpecialUsers } from '../../actions/settings_actions';
+
 
 const CourseApproval = createReactClass({
     displayName: 'CourseApproval',
 
+    propTypes: {
+      fetchSpecialUsers: PropTypes.func,
+      specialUsers: PropTypes.object,
+    },
+
     getInitialState() {
       return {
-        selectedProgramManager: { value: 'user2', label: 'User2' },
-        selectedWikiExpert: { value: 'user2', label: 'User2' },
-        selectedCampaign: { value: 'campaign1', label: 'Campaign1' }
+        selectedProgramManager: {},
+        selectedWikiExpert: {},
+        selectedCampaign: {},
+        programManagerOptions: [],
+        wikiExpertOptions: [],
+        campaignOptions: []
       };
     },
 
+    componentDidMount() {
+      this.props.fetchSpecialUsers();
+    },
+
+    componentDidUpdate(prevProps) {
+      if (this.props.specialUsers !== prevProps.specialUsers ) {
+        if (this.props.specialUsers.classroom_program_manager) {
+          this.setState({
+            selectedProgramManager: {
+              value: this.props.specialUsers.classroom_program_manager[0].username,
+              label: this.props.specialUsers.classroom_program_manager[0].username,
+            },
+            programManagerOptions: this.props.specialUsers.classroom_program_manager.map((user) => {
+              return { value: user.username, label: user.username };
+            })
+          });
+        }
+
+        if (this.props.specialUsers.wikipedia_experts) {
+          this.setState({
+            selectedWikiExpert: {
+              value: this.props.specialUsers.wikipedia_experts[0].username,
+              label: this.props.specialUsers.wikipedia_experts[0].username,
+            },
+            wikiExpertOptions: this.props.specialUsers.wikipedia_experts.map((user) => {
+              return { value: user.username, label: user.username };
+            })
+          });
+        }
+      }
+    },
+
     _handleProgramManagerChange(selectedOption) {
-      return this.setState({ selectedProgramManager : selectedOption });
+      return this.setState({ selectedProgramManager: selectedOption });
     },
 
     _handleWikiExpertChange(selectedOption) {
-      return this.setState({ selectedWikiExpert : selectedOption });
+      return this.setState({ selectedWikiExpert: selectedOption });
     },
 
     _handleCampaignChange(selectedOption) {
-      return this.setState({ selectedCampaign : selectedOption });
+      return this.setState({ selectedCampaign: selectedOption });
     },
 
     render() {
-        const programManagerOptions = [
-          { value: 'user1', label: 'User1' },
-          { value: 'user2', label: 'User2' },
-          { value: 'user3', label: 'User3' },
-        ];
-
-        const wikiExpertOptions = [
-          { value: 'user1', label: 'User1' },
-          { value: 'user2', label: 'User2' },
-          { value: 'user3', label: 'User3' },
-        ];
-
-        const campaignOptions = [
-          { value: 'campaign1', label: 'Campaign1' },
-          { value: 'campaign2', label: 'Campaign2' },
-          { value: 'campaign3', label: 'Campaign3' },
-        ];
+        const { programManagerOptions, wikiExpertOptions, campaignOptions, 
+        selectedProgramManager, selectedWikiExpert, selectedCampaign } = this.state;
+        
+        const programManagerValue = programManagerOptions.empty ? null : programManagerOptions.find(option => option.value === selectedProgramManager.value);
+        const wikiExpertValue = wikiExpertOptions.empty ? null : wikiExpertOptions.find(option => option.value === selectedWikiExpert.value);
+        const campaignValue = campaignOptions.empty ? null : campaignOptions.find(option => option.value === selectedCampaign.value);
 
         const programManagerSelector = (
           <div className="course-approval-field form-group">
-            <div className='group-left'>
+            <div className="group-left">
               <label htmlFor="program_manager">Add Program Manager:</label>
             </div>
-            <div className='group-right'> 
+            <div className="group-right">
               <Select
                 id={'program_manager'}
-                value={programManagerOptions.find(option => option.value === this.state.selectedProgramManager.value)}
+                value={programManagerValue}
                 onChange={this._handleProgramManagerChange}
                 options={programManagerOptions}
                 simpleValue
@@ -65,13 +99,13 @@ const CourseApproval = createReactClass({
 
         const wikiExpertSelector = (
           <div className="course-approval-field form-group">
-            <div className='group-left'>
+            <div className="group-left">
               <label htmlFor="wiki_expert">Add Wiki Expert:</label>
             </div>
-            <div className='group-right'> 
+            <div className="group-right">
               <Select
                 id={'wiki_expert'}
-                value={wikiExpertOptions.find(option => option.value === this.state.selectedWikiExpert.value)}
+                value={wikiExpertValue}
                 onChange={this._handleWikiExpertChange}
                 options={wikiExpertOptions}
                 simpleValue
@@ -83,13 +117,13 @@ const CourseApproval = createReactClass({
 
         const campaignSelector = (
           <div className="course-approval-field form-group">
-            <div className='group-left'>
+            <div className="group-left">
               <label htmlFor="campaign">Add Campaign:</label>
             </div>
-            <div className='group-right'> 
+            <div className="group-right">
               <Select
                 id={'campaign'}
-                value={campaignOptions.find(option => option.value === this.state.selectedCampaign.value)}
+                value={campaignValue}
                 onChange={this._handleCampaignChange}
                 options={campaignOptions}
                 simpleValue
@@ -104,7 +138,7 @@ const CourseApproval = createReactClass({
             <div className="section-header">
               <h3>Course Approval Form</h3>
             </div>
-            <div className='course-approval-fields'>
+            <div className="course-approval-fields">
               {programManagerSelector}
               {wikiExpertSelector}
               {campaignSelector}
@@ -114,4 +148,12 @@ const CourseApproval = createReactClass({
     }
 });
 
-export default CourseApproval;
+const mapStateToProps = state => ({
+  specialUsers: state.settings.specialUsers,
+});
+
+const mapDispatchToProps = {
+  fetchSpecialUsers,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CourseApproval);
