02040166f3a90147ebba39cba51b3c85711d1a5a
Selene Flores <xochitl.selene.fp@gmail.com>
2022-04-07 00:20:41 -0500

    add copy course from production controller

---
+++ app/controllers/copy_course_from_production_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+#= Controller for copying course from production to test
+class CopyCourseFromProductionController < ApplicationController
+    respond_to :html
+    before_action :require_permissions
+
+    def index
+        @created_course = ""
+    end
+
+    def create
+        copied_course = CopyCourseFromProduction.new(params[:url])
+        pp 'Course created!'
+        pp "http://localhost:3000/courses/#{copied_course.course_slug}"
+        redirect_to copy_course_from_production_path, 
+            locals: { created_course: copied_course.course_slug },
+            notice: "Course #{copied_course.course_slug} was created."
+    end
+end
---
+++ app/services/copy_course_from_production.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require 'net/http'
+
+class CopyCourseFromProduction
+  attr_reader :course, :course_data, :course_slug
+
+  def initialize(url)
+    @course_slug = make_copy_of(url)
+  end
+
+  private
+
+  def make_copy_of(url)
+    @copied_data = get_main_course_data(url)
+
+    add_course(copied_data)
+    add_user_to_course(url)
+
+    course.slug
+  end
+
+  def get_main_course_data(url)
+    # Get the main course data
+    course_data = JSON.parse(Net::HTTP.get(URI(url + '/course.json')))['course']
+    # Extract the attributes we want to copy
+    params_to_copy = %w[school title term description start end subject slug timeline_start
+                        timeline_end type flags]
+    copied_data = {}
+    params_to_copy.each { |p| copied_data[p] = course_data[p] }
+    home_wiki = Wiki.get_or_create(language: course_data['home_wiki']['language'],
+                                   project: course_data['home_wiki']['project'])
+    copied_data['home_wiki_id'] = home_wiki.id
+    copied_data['passcode'] = 'passcode' # set an arbitrary passcode
+    copied_data
+  end
+
+  def add_course(copied_data)
+    # Create the course
+    @course = Course.create!(
+      copied_data
+    )
+    # Add the tracked wikis
+    @course_data['wikis'].each do |wiki_hash|
+      wiki = Wiki.get_or_create(language: wiki_hash['language'],
+                                project: wiki_hash['project'])
+      next if wiki.id == home_wiki.id # home wiki was automatically added already
+      course.wikis << wiki
+    end
+  end
+
+  def add_user_to_course(url)
+    # Get the user list
+    user_data = JSON.parse(Net::HTTP.get(URI(url + '/users.json')))['course']['users']
+    # Add the users to the course
+    user_data.each do |user_hash|
+      user = User.find_or_create_by!(username: user_hash['username'])
+      CoursesUsers.create!(user_id: user.id, role: user_hash['role'], course_id: @course.id)
+    end
+  end
+end
