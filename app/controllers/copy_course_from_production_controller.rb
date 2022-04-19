ecd6486288eafd4dff4518664b57ded7d7360153
Selene Flores <xochitl.selene.fp@gmail.com>
2022-04-18 23:17:35 -0500

    remove pp output

--- app/controllers/copy_course_from_production_controller.rb
+++ app/controllers/copy_course_from_production_controller.rb
@@ -11,7 +11,6 @@ class CopyCourseFromProductionController < ApplicationController
 
   def copy
     copied_course = CopyCourseFromProduction.new(params[:url])
-    pp "http://localhost:3000/courses/#{copied_course.course_slug}"
     redirect_to copy_course_from_production_path,
                 locals: { copied_course: copied_course.course_slug },
                 notice: "Course #{copied_course.title} was created. http://localhost:3000/courses/#{copied_course.course_slug}"
