d49f46200acd3cf91310105c9ff01dd8c7c77940
Selene Flores <xochitl.selene.fp@gmail.com>
2022-04-19 00:30:50 -0500

    Moving cases to hit server only from CopyCourse

--- spec/controllers/copy_course_from_production_controller_spec.rb
+++ spec/controllers/copy_course_from_production_controller_spec.rb
@@ -6,24 +6,15 @@ describe CopyCourseFromProductionController, type: :request do
   describe '#copy' do
     let(:user) { create(:user) }
     let(:admin) { create(:admin) }
-    let(:url_base) { 'https://dashboard.wikiedu.org/courses/' }
-    let(:existent_prod_course_slug) do
-      'University_of_South_Carolina/Invertebrate_Zoology_(Spring_2022)'
-    end
-
-    let(:subject) do
-      copy route, params: { url: url_base + existent_prod_course_slug }
-    end
 
     context 'when the user is an admin' do
       before do
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
       end
 
-      it 'copies the course' do
-        subject
+      it 'shows the feature to copy the course' do
+        get '/course_copy_prod'
         expect(response.status).to eq(200)
-        expect(Course.exists?(slug: existent_prod_course_slug)).to eq(true)
       end
     end
 
@@ -33,7 +24,6 @@ describe CopyCourseFromProductionController, type: :request do
       end
 
       it 'returns a 401 error' do
-        expect(CopyCourseFromProduction).not_to receive(:new)
         get '/course_copy_prod'
         expect(response.status).to eq(401)
       end
--- spec/services/copy_course_from_production_spec.rb
+++ spec/services/copy_course_from_production_spec.rb
@@ -3,19 +3,25 @@
 require 'rails_helper'
 
 describe CopyCourseFromProduction do
-  let(:existent_prod_course) do
-    'https://dashboard.wikiedu.org/courses/University_of_California_Merced/Feminism,_Handmaids_and_Wild_Seeds_(Spring_2021)'
+  let(:url_base) { 'https://dashboard.wikiedu.org/courses/' }
+  let(:existent_prod_course_slug) do
+    'University_of_South_Carolina/Invertebrate_Zoology_(Spring_2022)'
   end
 
   context 'with courses in production' do
     let(:subject) do
-      described_class.new(existent_prod_course)
+      described_class.new(url_base + existent_prod_course_slug)
+    end
+
+    it 'copies the course' do
+      subject
+      expect(Course.exists?(slug: existent_prod_course_slug)).to eq(true)
     end
 
     it 'copy course to dev env' do
       result = subject.result
-      expect(result['failure']).to be_nil
-      expect(result['success']).not_to be_nil
+      expect(result['course']).not_to be_nil
     end
+
   end
 end
