93e10de9972ec7262dcab62273a180668de8fb4c
Vaidehi A <vaidehiatpadkar1@gmail.com>
2022-05-05 01:19:07 +0530

    Added some comments.

--- spec/features/course_approval_spec.rb
+++ spec/features/course_approval_spec.rb
@@ -8,6 +8,8 @@ describe 'Course Approval', type: :feature, js: true do
 
   before do
     page.current_window.resize_to(1920, 1080)
+
+    # Create a course starting in January 2022 (Spring 2022)
     create(:course,
            id: 10001,
            title: 'My Course',
@@ -18,6 +20,7 @@ describe 'Course Approval', type: :feature, js: true do
            start: '2022-01-01'.to_date,
            end: '2022-06-30'.to_date)
 
+    # Create special users
     SpecialUsers.set_user('classroom_program_manager', program_manager.username)
     SpecialUsers.set_user('wikipedia_experts', wiki_expert.username)
   end
@@ -50,7 +53,10 @@ describe 'Course Approval', type: :feature, js: true do
       expect(page).to have_content 'Course Approval Form'
     end
 
+    # If suitable campaign is not inferred and not selected by default, submit button is disabled 
     describe 'with campaign not inferred' do
+
+      # Create a campaign belonging to Fall 2022 season
       before do
         create(:campaign, title: 'Fall 2022', slug: 'fall_2022')
       end
@@ -63,7 +69,10 @@ describe 'Course Approval', type: :feature, js: true do
       end
     end
 
+    # Submit button is clickable when campaign is inferred by default (or manually selected)
     describe 'with campaign inferred' do
+
+      # Create a campaign belonging to Spring 2022 season
       before do
         create(:campaign, title: 'Spring 2022', slug: 'spring_2022')
       end
