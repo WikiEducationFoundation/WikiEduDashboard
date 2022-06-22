7e0442498bb19e761e3c424bc8edbffe53854c5e
Shashwat <10794178+TheTrio@users.noreply.github.com>
2022-06-22 23:16:21 +0530

    add comments

--- lib/development/dev_server_proxy.rb
+++ lib/development/dev_server_proxy.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
+# This proxy forwards requests to /assets/*.* to the Webpack Dev Server(localhost:8080 by default)
+# It is used when running `yarn hot`
+# You can find a similar example here https://github.com/ncr/rack-proxy#rails-middleware-example
+# This proxy is added as a middleware in config/initializers/dev_server.rb and only gets called 
+# during testing/development.
+
 class WebpackDevServerProxy < Rack::Proxy
   def initialize(app = nil, opts = {})
     super
