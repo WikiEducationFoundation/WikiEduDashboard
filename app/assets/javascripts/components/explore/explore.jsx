14c6bc4172eafe7067818f682782eb416e3841db
Shashwat <10794178+TheTrio@users.noreply.github.com>
2022-07-02 17:45:58 +0530

    show create course button if Features.open_course_creation is true

--- app/assets/javascripts/components/explore/explore.jsx
+++ app/assets/javascripts/components/explore/explore.jsx
@@ -6,11 +6,12 @@ import DetailedCampaignList from '../campaign/detailed_campaign_list';
 
 const Explore = () => {
   const user = getCurrentUser(useSelector(state => state));
+  const showCreateCampaignButton = user.admin || Features.open_course_creation;
   return (
     <div id="campaigns">
       <DetailedCampaignList headerText={I18n.t('campaign.newest_campaigns')} newest/>
       <div className="campaigns-actions" >
-        {user.admin && <a className="button dark" href="campaigns/new?create=true">{I18n.t('campaign.create_campaign')}</a>}
+        {showCreateCampaignButton && <a className="button dark" href="campaigns/new?create=true">{I18n.t('campaign.create_campaign')}</a>}
         <a href="/campaigns" className="button">
           {I18n.t('campaign.all_campaigns')} <span className="icon icon-rt_arrow" />
         </a>
