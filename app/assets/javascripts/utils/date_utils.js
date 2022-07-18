e7f3b51a943178c13edb889730f438b8832c6778
Shashwat <10794178+TheTrio@users.noreply.github.com>
2022-07-18 23:55:33 +0530

    add comments

--- app/assets/javascripts/utils/date_utils.js
+++ app/assets/javascripts/utils/date_utils.js
@@ -1,5 +1,9 @@
 import { parseISO } from 'date-fns';
 
+// this exists because unlike moment-js, date-fns doesn't have a generalized toDate function
+/**
+ * @param  {Date|string} date
+*/
 export const toDate = (date) => {
   if (date instanceof Date) {
     return date;
