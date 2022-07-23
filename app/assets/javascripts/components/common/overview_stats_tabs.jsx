918faf07ebb6d66924d90e9d77d0405f78781b57
Vaidehi <vaidehiatpadkar1@gmail.com>
2022-07-23 10:13:51 +0530

    Move stats title function to article utils.

--- app/assets/javascripts/components/common/overview_stats_tabs.jsx
+++ app/assets/javascripts/components/common/overview_stats_tabs.jsx
@@ -11,51 +11,17 @@ import ArticleUtils from '../../utils/article_utils';
 const OverviewStatsTabs = ({ statistics }) => {
   const [currentTabId, setCurrentTabId] = useState(0);
 
-  const getTabTitle = (ns_id, wiki) => {
-    const project = wiki.split('.')[1];
-    let ns_title = ArticleUtils.NamespaceIdMapping[ns_id];
-    if (typeof (ns_title) !== 'string') ns_title = ns_title[project];
-    return `${I18n.t(`namespace.${ns_title}`)} (${wiki})`;
-  };
-
-  const getContentTitle = (ns_id, wiki) => {
-    const project = wiki.split('.')[1];
-    let ns_title = ArticleUtils.NamespaceIdMapping[ns_id];
-    if (typeof (ns_title) !== 'string') ns_title = ns_title[project];
-    return `Stats for ${I18n.t(`namespace.${ns_title}`)} (${wiki})`;
-  };
-
   const onTabChange = (e) => {
     return setCurrentTabId(Number(e.currentTarget.id));
   };
 
-  const hasWikidataStats = () => {
-    if (statistics['www.wikidata.org']) return true;
-    return false;
-  };
-
   const statsDataList = [];
-  // if there are wikidata overview stats, they should be displayed on first tab
-  if (hasWikidataStats) {
-    statsDataList.push({
-      id: 0,
-      tabTitle: 'Wikidata',
-      contentTitle: null, // title for wikidata stats already exists in WikidataOverviewStats component
-      data: statistics['www.wikidata.org']
-    });
-  }
-
-  let index = hasWikidataStats ? 1 : 0;
-  Object.keys(statistics).forEach((wiki_ns) => {
-    if (!wiki_ns.includes('namespace')) return; // return if it doesn't contain namespace stats
-
-    const ns_id = Number(wiki_ns.split('-')[2]); // namespace id
-    const wiki = wiki_ns.split('-')[0];
+  let index = 0;
+  Object.keys(statistics).forEach((wiki_ns_key) => {
     const id = index;
-    const tabTitle = getTabTitle(ns_id, wiki);
-    const contentTitle = getContentTitle(ns_id, wiki);
-    const data = statistics[wiki_ns];
-    statsDataList.push({ id, tabTitle, contentTitle, data });
+    const statsTitle = ArticleUtils.overviewStatsTitle(wiki_ns_key);
+    const data = statistics[wiki_ns_key];
+    statsDataList.push({ id, statsTitle, data });
     index += 1;
   });
 
@@ -65,15 +31,15 @@ const OverviewStatsTabs = ({ statistics }) => {
         key={obj.id}
         id={obj.id}
         onClick={onTabChange}
-        title={obj.tabTitle}
+        title={obj.statsTitle}
         active={currentTabId === obj.id}
       />
     );
   });
 
-  const contentTitle = statsDataList[currentTabId].contentTitle;
+  const title = statsDataList[currentTabId].statsTitle;
   const data = statsDataList[currentTabId].data;
-  const content = (hasWikidataStats && currentTabId === 0)
+  const content = (title === 'www.wikidata.org')
     ? <WikidataOverviewStats statistics={data} isCourseOverview={true}/>
     : <NamespaceOverviewStats statistics={data} />;
 
@@ -83,7 +49,7 @@ const OverviewStatsTabs = ({ statistics }) => {
         {tabsList}
       </div>
       <div className="content-container">
-        <h2 className="title">{contentTitle}</h2>
+        <h2 className="title">{title}</h2>
         <div className="stats-data">
           {content}
         </div>
