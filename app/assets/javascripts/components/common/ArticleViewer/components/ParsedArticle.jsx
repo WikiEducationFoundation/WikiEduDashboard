0b1d73a954a0e231e5697e5ab46826b838d3dd27
Sage Ross <ragesoss@gmail.com>
2022-07-25 15:20:48 -0700

    Add target="_blanks" to typical links in ArticleViewer
    This cleanly handles standard wikilinks in Wikipedia articles, so that clicking one will open the Wikipedia article in a new tab instead of navigating the user away from the Dashboard.
    
    Wikidata links, however, are formatted differently, with a `title` attribute before the `href` and relative links instead of absolute ones... so this change doesn't affect Wikidata items, which have broken links in ArticleViewer anyway.

--- app/assets/javascripts/components/common/ArticleViewer/components/ParsedArticle.jsx
+++ app/assets/javascripts/components/common/ArticleViewer/components/ParsedArticle.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+
+const wikilinkMatcher = new RegExp('<a href=', 'g');
+const blankTargetLink = '<a target="_blank" href=';
+
 export const ParsedArticle = ({ highlightedHtml, whocolorHtml, parsedArticle }) => {
-  const articleHTML = highlightedHtml || whocolorHtml || parsedArticle;
+  let articleHTML = highlightedHtml || whocolorHtml || parsedArticle;
+
+  // This sets `target="_blank"` for all of the link in the article HTML,
+  // so that clicking one will open it in a new tab.
+  articleHTML = articleHTML?.replace(wikilinkMatcher, blankTargetLink);
+
   return (
     <div className="parsed-article" dangerouslySetInnerHTML={{ __html: articleHTML }} />
   );
