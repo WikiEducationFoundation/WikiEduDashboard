c0b47bcfddb9c272322af26e2948d01e1b9485b5
Vaidehi <vaidehiatpadkar1@gmail.com>
2022-07-27 01:10:18 +0530

    New stats content component.

--- app/assets/javascripts/components/common/overview_stats_tabs.jsx
+++ app/assets/javascripts/components/common/overview_stats_tabs.jsx
@@ -2,8 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import OverviewStatsTab from './OverviewStats/overview_stats_tab';
-import NamespaceOverviewStats from './OverviewStats/namespace_overview_stats';
-import WikidataOverviewStats from './wikidata_overview_stats';
+import OverviewStatsContent from './OverviewStats/overview_stats_content';
 
 import { overviewStatsLabel } from '../../utils/wiki_utils';
 
@@ -36,23 +35,14 @@ const OverviewStatsTabs = ({ statistics }) => {
     index += 1;
   });
 
-  const title = statsList[currentTabId].statsTitle;
-  const data = statsList[currentTabId].statsData;
-  const content = (title === 'www.wikidata.org')
-    ? <WikidataOverviewStats statistics={data} isCourseOverview={true}/>
-    : <NamespaceOverviewStats statistics={data} />;
+  const content = <OverviewStatsContent content={statsList[currentTabId]} />;
 
   return (
     <div className="overview-stats-tabs-container">
       <div className="tabs-container">
         {tabsList}
       </div>
-      <div className="content-container">
-        <h2 className="title">{title}</h2>
-        <div className="stats-data">
-          {content}
-        </div>
-      </div>
+      {content}
     </div>
   );
 };
