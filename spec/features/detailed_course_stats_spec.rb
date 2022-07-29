1639e9f806721613465d047c2f2864c1e0798b54
Vaidehi <vaidehiatpadkar1@gmail.com>
2022-07-30 01:03:08 +0530

    Update feature test

--- spec/features/detailed_course_stats_spec.rb
+++ spec/features/detailed_course_stats_spec.rb
@@ -25,21 +25,28 @@ describe 'Detailed course overview stats', type: :feature, js: true do
     visit "/courses/#{course.slug}"
   end
 
-  it 'shows tabs for all the course stats objects' do
+  it 'shows all the course_stat objects in a tabbed UI layout' do
+    # Tabs corresponding to all the stats are rendered
     expect(page.find('#tab-0')).to have_content('www.wikidata.org')
     expect(page.find('#tab-1')).to have_content('en.wikibooks.org - Cookbook')
     expect(page.find('#tab-2')).to have_content('en.wikipedia.org - Mainspace')
-  end
 
-  it 'shows cookbook stats on clicking the \'en.wikibooks.org - Cookbook\' tab' do
-    page.find('#tab-1').click
-    expect(page).to have_content('en.wikibooks.org - Cookbook')
-    expect(page).to have_content('Articles Edited')
-  end
+    # Default tab's content matches first course_stat object's stats
+    expect(page.find('.tab.active')).to have_content('www.wikidata.org')
+    expect(page.find('.content-container .title')).to have_content('www.wikidata.org')
+    expect(page.find('.content-container')).to have_content("Claims\n35")
 
-  it 'shows wikidata overview stats on clicking the \'www.wikidata.org\' tab' do
-    page.find('#tab-0').click
-    expect(page).to have_content('www.wikidata.org')
-    expect(page).to have_content('Claims')
+    # Clicking other tabs renders the respective stats data
+    page.find('.tab', text: 'en.wikibooks.org - Cookbook').click
+    expect(page.find('.tab.active')).to have_content('en.wikibooks.org - Cookbook')
+    expect(page.find('.content-container .title')).not_to have_content('www.wikidata.org')
+    expect(page.find('.content-container .title')).to have_content('en.wikibooks.org - Cookbook')
+    expect(page.find('.content-container')).to have_content("72\nArticles Edited")
+
+    page.find('.tab', text: 'en.wikipedia.org - Mainspace').click
+    expect(page.find('.tab.active')).to have_content('en.wikipedia.org - Mainspace')
+    expect(page.find('.content-container .title')).not_to have_content('en.wikibooks.org - Cookbook')
+    expect(page.find('.content-container .title')).to have_content('en.wikipedia.org - Mainspace')
+    expect(page.find('.content-container')).to have_content("16\nArticles Created")
   end
 end
