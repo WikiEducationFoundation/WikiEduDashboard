566b6a79062e6c878d2596cb5d304e5fa89104d2
Vaidehi A <vaidehiatpadkar1@gmail.com>
2022-08-01 20:54:40 +0530

    Fix ruby lint errors.

--- app/services/update_wiki_namespace_stats.rb
+++ app/services/update_wiki_namespace_stats.rb
@@ -11,13 +11,13 @@ class UpdateWikiNamespaceStats
   def update_stats
     stat_key = "#{@wiki.domain}-namespace-#{@namespace}"
     stats = {
-      'edited_count': edited_articles_count,
-      'new_count': new_articles_count,
-      'revision_count': revision_count,
-      'user_count': user_count,
-      'word_count': word_count,
-      'reference_count': reference_count,
-      'view_count': view_count
+      edited_count: edited_articles_count,
+      new_count: new_articles_count,
+      revision_count: revision_count,
+      user_count: user_count,
+      word_count: word_count,
+      reference_count: reference_count,
+      view_count: view_count
     }
     course_stats = CourseStat.find_or_create_by(course_id: @course.id)
     course_stats.stats_hash[stat_key] = stats
@@ -27,20 +27,20 @@ class UpdateWikiNamespaceStats
   # live, tracked course revisions filtered by wiki and namespace
   def revisions_filtered_by_wiki_namespace
     @course.revisions.where.not(article_id: @course.articles_courses.not_tracked.pluck(:article_id))
-                     .where(wiki_id: @wiki.id)
-                     .namespace(@namespace).live
+           .where(wiki_id: @wiki.id)
+           .namespace(@namespace).live
   end
 
   # live, tracked articles filtered by wiki and namespace
   def articles_filtered_by_wiki_namespace
     @course.articles_courses.joins(:article).where(articles: { wiki: @wiki, namespace: @namespace })
-                                            .tracked.live
+           .tracked.live
   end
 
   def edited_articles_count
     articles = articles_filtered_by_wiki_namespace
     articles.count
-	end
+  end
 
   def new_articles_count
     articles = articles_filtered_by_wiki_namespace
