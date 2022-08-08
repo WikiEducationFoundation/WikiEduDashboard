08cf347067a3274fa58f24a90e113019b8c137f1
Sage Ross <ragesoss@gmail.com>
2022-08-08 09:32:33 -0700

    Remove deprecated Sentry async
    This feature will be removed in a future version of Sentry, and it might be a source of some Redis memory problems (which have been and maybe still are a problem for P&E Dashboard). See https://github.com/getsentry/sentry-ruby/issues/1522

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,4 @@
 Sentry.init do |config|
   config.dsn = ENV['sentry_dsn']
   config.enabled_environments = %w[development staging production test]
-  config.async = lambda { |event| SentryWorker.perform_async(event.to_hash) }
 end
