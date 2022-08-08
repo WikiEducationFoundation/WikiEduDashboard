090c7035e7d6cd3e56a0f2f62fca91450d62d0f0
Shashwat <10794178+TheTrio@users.noreply.github.com>
2022-08-09 02:27:54 +0530

    include jQuery explicitly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,11 +24,7 @@ module ApplicationHelper
   end
 
   def hot_javascript_tag(filename)
-    js_path = hot_javascript_path(filename)
-    if (filename == 'campaigns') || (filename.include? 'survey')
-      return javascript_include_tag '/assets/javascripts/jquery.min.js', js_path
-    end
-    javascript_include_tag js_path
+    javascript_include_tag hot_javascript_path(filename)
   end
 
   def hot_javascript_path(filename)
