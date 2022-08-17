233be9bfa983a8c2be18ca451049ba9d23782cbf
Vaidehi A <vaidehiatpadkar1@gmail.com>
2022-08-17 23:58:47 +0530

    Add annotations.

--- app/models/course_wiki_namespaces.rb
+++ app/models/course_wiki_namespaces.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: course_wiki_namespaces
+#
+#  id               :bigint           not null, primary key
+#  namespace        :integer
+#  courses_wikis_id :bigint
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 class CourseWikiNamespaces < ApplicationRecord
   belongs_to :courses_wikis
 end
--- spec/factories/course_wiki_namespaces.rb
+++ spec/factories/course_wiki_namespaces.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: course_wiki_namespaces
+#
+#  id               :bigint           not null, primary key
+#  namespace        :integer
+#  courses_wikis_id :bigint
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 FactoryBot.define do
   factory :course_wiki_namespaces do
     namespace { 1 }
