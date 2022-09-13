20839b50e1665768c2b726b649e7c376c24319b0
Sage Ross <ragesoss@gmail.com>
2022-09-13 16:25:14 -0700

    Even more staging cleanup

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -18,10 +18,6 @@ set :deploy_to, '/var/www/dashboard'
 
 set :default_env, { 'PASSENGER_INSTANCE_REGISTRY_DIR' => '/var/www/dashboard/shared/tmp/pids' }
 
-# This is normally set from the deploying machine's newrelic.yml.
-# The ENV variable is for deployment via travis-ci.
-set :newrelic_license_key, ENV['NEWRELIC_LICENSE_KEY']
-set :newrelic_appname, 'Wiki Ed Dashboard (staging)'
 namespace :deploy do
   after "deploy:updated", "newrelic:notice_deployment"
 end
