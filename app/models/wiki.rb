85745897a23a989b1af4c04c69c3216e76d242b8
Sage Ross <ragesoss@gmail.com>
2022-10-10 12:16:40 -0700

    Add additional language codes for missing Wikipedias
    I added all the languages codes missing here based on the latest version of the "Details table" on the list of Wikipedias: https://en.wikipedia.org/w/index.php?title=List_of_Wikipedias&oldid=1114445997#Details_table
    
    This includes Tyap (kcg), which a user contacted me to request: https://meta.wikimedia.org/w/index.php?title=User_talk:Sage_(Wiki_Ed)&oldid=23914518#Please_add_Tyap_Wikipedia

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -39,19 +39,19 @@ class Wiki < ApplicationRecord
   validates_inclusion_of :project, in: PROJECTS
 
   LANGUAGES = %w[
-    aa ab ace ady af ak als am ami an ang ar arc arz as ast atj av ay az azb
-    ba ban bar bat-smg bcl be be-tarask be-x-old bg bh bi bjn bm bn bo bpy br bs
+    aa ab ace ady af ak als alt am ami an ang ar arc ary arz as ast atj av avk ay awa az azb
+    ba ban bar bat-smg bcl be be-tarask be-x-old bg bh bi bjn blk bm bn bo bpy br bs
     bug bxr ca cbk-zam cdo ce ceb ch cho chr chy ckb cmn co commons cr crh cs csb cu
     cv cy da dag de din diq dk dsb dty dv dz ee egl el eml en eo epo es et eu ext fa
-    ff fi fiu-vro fj fo fr frp frr fur fy ga gag gan gd gl glk gn gom gor got gsw
-    gu gv ha hak haw he hi hif ho hr hsb ht hu hy hz ia id ie ig ii ik ilo
-    incubator inh io is it iu ja jam jbo jp jv ka kaa kab kbd kbp kg ki kj kk kl km kn ko
-    koi kr krc ks ksh ku kv kw ky la lad lb lbe lez lfn lg li lij lmo ln lo lrc lt
-    ltg lv lzh mad mai map-bms mdf mg mh mhr mi min minnan mk ml mn mo mr mrj ms mt
-    mus mwl my myv mzn na nah nan nap nb nds nds-nl ne new ng nia nl nn no nov nrm
-    nso nv ny oc olo om or os pa pag pam pap pcd pdc pfl pi pih pl pms pnb pnt ps
+    ff fi fiu-vro fj fo fr frp frr fur fy ga gag gan gcr gd gl glk gn gom gor got gsw
+    gu guw gv ha hak haw he hi hif ho hr hsb ht hu hy hyw hz ia id ie ig ii ik ilo
+    incubator inh io is it iu ja jam jbo jp jv ka kaa kab kcg kbd kbp kg ki kj kk kl km kn ko
+    koi kr krc ks ksh ku kv kw ky la lad lb lbe lez lfn lg li lij lld lmo ln lo lrc lt
+    ltg lv lzh mad mai map-bms mdf mg mh mhr mi min minnan mk ml mn mni mnw mo mr mrj ms mt
+    mus mwl my myv mzn na nah nan nap nb nds nds-nl ne new ng nia nl nn no nov nqo nrm
+    nso nv ny oc olo om or os pa pag pam pap pcd pcm pdc pfl pi pih pl pms pnb pnt ps
     pt pwn qu rm rmy rn ro roa-rup roa-tara ru rue rup rw sa sah sat sc scn sco sd se
-    sg sgs sh si simple sk sl sm sn so sq sr srn ss st stq su sv sw szl szy ta tay tcy te
+    sg sgs sh shi shn si simple sk skr sl sm smn sn so sq sr srn ss st stq su sv sw szl szy ta tay tcy te
     tet tg th ti tk tl tn to tpi tr trv ts tt tum tw ty tyv udm ug uk ur uz ve
     vec vep vi vls vo vro w wa war wikipedia wo wuu xal xh xmf yi yo yue za
     zea zh zh-cfr zh-classical zh-cn zh-min-nan zh-tw zh-yue zu
