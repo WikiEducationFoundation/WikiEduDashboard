e7436a1af2e1e256528aaf9094a2afd12f435f08
Sage Ross <ragesoss@gmail.com>
2022-10-13 10:29:25 -0700

    Handle removal of all tracked namespaces
    This was preventing updates for the case where all the tracked namespaces for a course are removed.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -285,8 +285,7 @@ class CoursesController < ApplicationController
   end
 
   def update_course_wiki_namespaces
-    namespaces = course_wiki_namespaces_params
-    return unless namespaces.present?
+    namespaces = course_wiki_namespaces_params || []
     # Each entry in namespaces uses wiki domain and namespace
     # Eg.: for cookbook, its "en.wikibooks.org-namespace-102"
     @course.courses_wikis.each do |course_wiki|
