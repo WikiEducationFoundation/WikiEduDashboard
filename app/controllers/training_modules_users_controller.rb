e94e8a36539c2c8bbfa736f3ba656673ab691c0a
Sharon Mwenda <smkathambi@gmail.com>
2022-10-26 07:40:53 +0300

    making changes on training slide users controllers

--- app/controllers/training_modules_users_controller.rb
+++ app/controllers/training_modules_users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
-  respond_to :json
+  
 class TrainingModulesUsersController < ApplicationController
-
+  respond_to :json
   before_action :require_signed_in, only: [:create_or_update, :mark_exercise_complete]
 
   def index
