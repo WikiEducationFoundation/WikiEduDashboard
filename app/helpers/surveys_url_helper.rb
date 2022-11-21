351d8d9478e50221c04d3a2a405cc2a893ab7de5
Sage Ross <ragesoss@gmail.com>
2022-11-21 09:53:44 -0800

    Add https to survey link

--- app/helpers/surveys_url_helper.rb
+++ app/helpers/surveys_url_helper.rb
@@ -2,6 +2,7 @@
 
 module SurveysUrlHelper
   def course_survey_url(notification)
-    "#{survey_url(notification.survey)}?course_slug=#{notification.course.slug.gsub('&', '%26')}"
+    slug = notification.course.slug.gsub('&', '%26')
+    "https://#{survey_url(notification.survey)}?course_slug=#{slug}"
   end
 end
