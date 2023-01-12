b32732e4711a01fedd78eff4fd00e0573806e775
kunal <10kunalji@gmail.com>
2023-01-13 00:46:18 +0530

    Final commit for issue#4859

--- lib/alerts/deleted_timeline_alert_manager.rb
+++ lib/alerts/deleted_timeline_alert_manager.rb
@@ -7,8 +7,8 @@ class DeletedTimelineAlertManager
 
   def create_alerts
     return unless @course.approved?
-    # it will not create an alert if course has more than 1 week
-    return if @course.weeks.count > 1
+    # it will not create an alert if course doesn't have any training module
+    return if @course.training_module_ids.any?
     alert = Alert.create(type: 'DeletedUploadsAlert', course_id: @course.id, message: "It appears you have removed either the timeline, week or block from your course.")
     alert.email_content_expert
   end
--- spec/lib/alerts/deleted_timeline_alert_manager_spec.rb
+++ spec/lib/alerts/deleted_timeline_alert_manager_spec.rb
@@ -5,25 +5,41 @@ require "#{Rails.root}/lib/alerts/deleted_timeline_alert_manager"
 
 describe DeletedTimelineAlertManager do
   let(:course) { create(:course) }
-  let(:week) { create(:week, course_id: course.id, title: "Week1", created_at: 1.month.ago, updated_at: 5.days.from_now ) }
-  let!(:block) { create(:block, week_id: week.id, training_module_ids: [1]) }
+  let(:week) { create(:week, course_id: course.id, title: "Week1", created_at: 1.month.ago, updated_at: 5.days.from_now ) 
+               create(:week, course_id: course.id, title: "Week2", created_at: 1.month.ago, updated_at: 5.days.from_now )}
   let (:course_approved) do
     course.campaigns << Campaign.first
+  end  
+  let (:adding_blocks_without_training_module) do
+    create(:block, week_id: week.id)
+  end  
+    let (:adding_blocks_with_training_module) do
+    create(:block, week_id: week.id, training_module_ids: [1])
   end  
   let(:subject) { described_class.new(course) }
   
   context 'when course is not approved' do
     it 'does not create an alert' do
+      adding_blocks_without_training_module
       subject.create_alerts
       expect(Alert.count).to eq(0)
     end
   end
 
-  context 'when course is approved' do
+  context 'when course is approved but has 0 training modules' do
     it 'create an alert' do
+      adding_blocks_without_training_module
       course_approved
       subject.create_alerts
       expect(Alert.count).to eq(1)
+    end
+  end
+  context 'when course is approved but has training modules' do
+    it 'does not create an alert' do
+      adding_blocks_with_training_module
+      course_approved
+      subject.create_alerts
+      expect(Alert.count).to eq(0)
     end
   end
 end
