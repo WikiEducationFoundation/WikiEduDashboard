0531e8ec27b8ab41652740a57aaf7ee21cff1e34
kunal <10kunalji@gmail.com>
2023-01-12 22:13:17 +0530

    added check for weeks removal

--- lib/alerts/deleted_timeline_alert_manager.rb
+++ lib/alerts/deleted_timeline_alert_manager.rb
@@ -6,9 +6,9 @@ class DeletedTimelineAlertManager
   end
 
   def create_alerts
-    # return unless @course.approved? 
-    # it will not create an alert if course has 1 or no week
-    return if @course.weeks.count >= 1
+    return unless @course.approved?
+    # it will not create an alert if course has more than 1 week
+    return if @course.weeks.count > 1
     alert = Alert.create(type: 'DeletedUploadsAlert', course_id: @course.id, message: "It appears you have removed either the timeline, week or block from your course.")
     alert.email_content_expert
   end
--- spec/lib/alerts/deleted_timeline_alert_manager_spec.rb
+++ spec/lib/alerts/deleted_timeline_alert_manager_spec.rb
@@ -5,25 +5,25 @@ require "#{Rails.root}/lib/alerts/deleted_timeline_alert_manager"
 
 describe DeletedTimelineAlertManager do
   let(:course) { create(:course) }
-  let(:week) { create(:week, course_id: course.id, title: "Week1", created_at: 1.month.ago, updated_at: 5.days.from_now )
-               create(:week, course_id: course.id, title: "Week2", created_at: 2.month.ago, updated_at: 15.days.from_now ) }
-  let!(:block) { create(:block, week_id: week.id, training_module_ids: [1]) }  
+  let(:week) { create(:week, course_id: course.id, title: "Week1", created_at: 1.month.ago, updated_at: 5.days.from_now ) }
+  let!(:block) { create(:block, week_id: week.id, training_module_ids: [1]) }
+  let (:course_approved) do
+    course.campaigns << Campaign.first
+  end  
   let(:subject) { described_class.new(course) }
   
   context 'when course is not approved' do
-    # let(:approved?) { false }
-
     it 'does not create an alert' do
       subject.create_alerts
       expect(Alert.count).to eq(0)
     end
   end
-  # context 'when course is approved' do
-  #   # let(:campaigns?) { true }
 
-  #   it 'create an alert' do
-  #     subject.create_alerts
-  #     expect(Alert.count).to eq(1)
-  #   end
-  # end
+  context 'when course is approved' do
+    it 'create an alert' do
+      course_approved
+      subject.create_alerts
+      expect(Alert.count).to eq(1)
+    end
+  end
 end
