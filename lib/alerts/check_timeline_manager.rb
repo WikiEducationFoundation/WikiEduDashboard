30170aab490d8114f8bbdb19eb797d3f997f3517
kunal <10kunalji@gmail.com>
2023-01-24 01:22:58 +0530

    ruby lint fix

--- lib/alerts/check_timeline_manager.rb
+++ lib/alerts/check_timeline_manager.rb
@@ -11,7 +11,7 @@ class CheckTimelineManager
     # if course doesn't have any training module, it will not create an alert
     return if @course.training_module_ids.any?
     alert = Alert.create(type: 'CheckTimeline', course_id: @course.id,
-                         message: 'It appears you have removed either the timeline, week or block from your course.')
+                         message: 'It appears you have removed timeline from your course.')
     alert.email_content_expert
   end
 end
