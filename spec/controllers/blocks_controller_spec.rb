0bc6657ffb7221861717a806b9e482c9117cb9f6
kunal <10kunalji@gmail.com>
2023-01-27 02:03:05 +0530

    Checking for specific alert type in BlocksController

--- spec/controllers/blocks_controller_spec.rb
+++ spec/controllers/blocks_controller_spec.rb
@@ -31,7 +31,7 @@ describe BlocksController, type: :request do
         expect(Block.count).to eq(1)
         delete "/blocks/#{block.id}", params: { id: block.id, format: :json }
         expect(Block.count).to eq(0)
-        expect(Alert.count).to eq(1)
+        expect(Alert.first.type).to eq('CheckTimeline')
       end
     end
   end
