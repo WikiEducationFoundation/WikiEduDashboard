5d8918f3feec3ec0e1fd5ef0b731b0b6b9d7270e
kunal <10kunalji@gmail.com>
2023-01-28 15:55:48 +0530

    Added check if alert is already existing

--- lib/alerts/check_timeline_alert_manager.rb
+++ lib/alerts/check_timeline_alert_manager.rb
@@ -10,14 +10,14 @@ class CheckTimelineAlertManager
     return unless @course.approved?
     # if course doesn't have any training module, it will not create an alert
     return if @course.training_module_ids.any?
-    return if alert_already_exists?(@course.id)
+    return if alert_already_exists?
     alert = Alert.create(type: 'CheckTimelineAlert',
                          course_id: @course.id,
                          message: CheckTimelineAlert.default_message)
     alert.email_content_expert
   end
 
-  def alert_already_exists?(id)
-    CheckTimelineAlert.exists?(course_id: id, resolved: false)
+  def alert_already_exists?
+    CheckTimelineAlert.exists?(course_id: @course.id, resolved: false)
   end
 end
