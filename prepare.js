3c9693f44b629b946ae49dfa2d70bf0b70ca3c04
Shashwat <10794178+TheTrio@users.noreply.github.com>
2023-02-02 03:48:08 +0530

    cache i18n files
    for some reason, generating i18n files is incredibly slow. Until we figure out why, we'll cache them.
    The way this works is simple - if the hash of the files in config/locales changes, then we'll regenerate the i18n files. Otherwise, we'll just use the cached ones.

--- prepare.js
+++ prepare.js
@@ -6,19 +6,48 @@ function elapsed(hrtime) {
   return seconds;
 }
 
+function getHashOfI18nFiles() {
+  return execSync('cat config/locales/* | sha1sum').toString().split('-')[0].trim();
+}
+
 let start = process.hrtime();
 const execSync = require('child_process').execSync;
+const exec = require('child_process').exec;
+
 const config = require('./config');
+const fs = require('fs');
+
+let fullRebuild = false;
+try {
+  const lastHash = fs.readFileSync(`${config.buildCache}/i18n/hash.txt`).toString();
+  const currentHash = getHashOfI18nFiles();
+  fullRebuild = lastHash.trim() !== currentHash.trim();
+} catch (e) {
+  execSync(`mkdir -p ${config.buildCache}/i18n`);
+  fullRebuild = true;
+}
 
 // cleans the stale assets
 console.log('Started cleaning stale assets');
 execSync(`rm -rf ${config.outputPath}`);
 console.log('\x1b[33m%s\x1b[0m', `Finished cleaning stale assets after ${elapsed(process.hrtime(start))} seconds`);
 
-// export i18n files
-console.log('Started emitting i18n translations');
-execSync('bundle exec i18n export -c ./config/i18n-js.yml');
-console.log('\x1b[33m%s\x1b[0m', `Finished emitting i18n translations after ${elapsed(process.hrtime(start))} seconds`);
+if (fullRebuild) {
+  // export i18n files
+  console.log('Started emitting i18n translations');
+  execSync('bundle exec i18n export -c ./config/i18n-js.yml');
+  console.log('\x1b[33m%s\x1b[0m', `Finished emitting i18n translations after ${elapsed(process.hrtime(start))} seconds`);
+
+  const hash = getHashOfI18nFiles();
+  exec(`cp ${config.outputPath}/${config.jsDirectory}/i18n.js ${config.buildCache}/i18n/i18n.js`);
+  exec(`cp ${config.outputPath}/${config.jsDirectory}/i18n ${config.buildCache}/i18n/i18n -r`);
+  fs.writeFile(`${config.buildCache}/i18n/hash.txt`, hash, () => {
+    console.log('Updated cache');
+  });
+} else {
+  console.log('\x1b[33m%s\x1b[0m', 'Skipping i18n export - found cached version');
+  execSync(`mkdir -p ${config.outputPath}/${config.jsDirectory}`);
+}
 
 // copies static assets from the source to assets folder
 const copyPaths = [{
@@ -34,20 +63,42 @@ const copyPaths = [{
   from: './node_modules/tinymce/skins',
   to: `${config.outputPath}/${config.jsDirectory}/skins`,
 }];
+
+if (!fullRebuild) {
+  copyPaths.push({
+    from: `${config.buildCache}/i18n/i18n.js`,
+    to: `${config.outputPath}/${config.jsDirectory}`,
+  });
+  copyPaths.push({
+    from: `${config.buildCache}/i18n/i18n`,
+    to: `${config.outputPath}/${config.jsDirectory}`,
+  });
+}
+
 copyPaths.forEach((entry, idx) => {
   switch (idx) {
     case 0: console.log('Started copying jQuery'); break;
     case 1: console.log('Started copying images'); break;
     case 2: console.log('Started copying fonts'); break;
     case 3: console.log('Started copying tinymce skins'); break;
+    case 4:
+      console.log('Started copying i18n javascript');
+      break;
+    case 5:
+      console.log('Started copying i18n locales');
+      break;
     default: console.log('Started copying static assets'); break;
   }
-  execSync(`cp -r ${entry.from} ${entry.to}`);
-  switch (idx) {
-    case 0: console.log('\x1b[33m%s\x1b[0m', `Finished copying jQuery after ${elapsed(process.hrtime(start))} seconds`); break;
-    case 1: console.log('\x1b[33m%s\x1b[0m', `Finished copying images after ${elapsed(process.hrtime(start))} seconds`); break;
-    case 2: console.log('\x1b[33m%s\x1b[0m', `Finished copying fonts after ${elapsed(process.hrtime(start))} seconds`); break;
-    case 3: console.log('\x1b[33m%s\x1b[0m', `Finished copying tinymce skins after ${elapsed(process.hrtime(start))} seconds`); break;
-    default: console.log('\x1b[33m%s\x1b[0m', `Finished copying static assets after ${elapsed(process.hrtime(start))} seconds`); break;
-  }
+
+  exec(`cp -r ${entry.from} ${entry.to}`, () => {
+    switch (idx) {
+      case 0: console.log('\x1b[33m%s\x1b[0m', `Finished copying jQuery after ${elapsed(process.hrtime(start))} seconds`); break;
+      case 1: console.log('\x1b[33m%s\x1b[0m', `Finished copying images after ${elapsed(process.hrtime(start))} seconds`); break;
+      case 2: console.log('\x1b[33m%s\x1b[0m', `Finished copying fonts after ${elapsed(process.hrtime(start))} seconds`); break;
+      case 3: console.log('\x1b[33m%s\x1b[0m', `Finished copying tinymce skins after ${elapsed(process.hrtime(start))} seconds`); break;
+      case 4: console.log('\x1b[33m%s\x1b[0m', `Finished copying i18n javascript after ${elapsed(process.hrtime(start))} seconds`); break;
+      case 5: console.log('\x1b[33m%s\x1b[0m', `Finished copying i18n locales after ${elapsed(process.hrtime(start))} seconds`); break;
+      default: console.log('\x1b[33m%s\x1b[0m', `Finished copying static assets after ${elapsed(process.hrtime(start))} seconds`); break;
+    }
+  });
 });
