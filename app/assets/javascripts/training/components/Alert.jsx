39abbaecd1a5778d26a33c91e7991393a409374a
Chaitanya Rai <Chaitanyarai899@gmail.com>
2023-02-06 20:47:26 +0530

    configured alert closing conditions again

--- app/assets/javascripts/training/components/Alert.jsx
+++ app/assets/javascripts/training/components/Alert.jsx
@@ -6,14 +6,16 @@ export default function Alert() {
   return (
     <>
       {open && (
-        <div className="alert-box-container">
-          <div className="alert-box">
-            <h2 className="alert-title">Please take your time!</h2>
-            <p className="alert-content">It is very important that you learn the training content thoroughly.</p>
-            <div className="alert-button-container">
-              <button className="alert-button" onClick={() => setOpen(false)}>CLOSE</button>
-            </div>
-          </div>
+        <div  className="backdrop" onClick={() => setOpen(false)}>
+         <div className="alert-box-container">
+           <div className="alert-box">
+             <h2 className="alert-title">Please take your time!</h2>
+             <p className="alert-content">It is very important that you learn the training content thoroughly.</p>
+             <div className="alert-button-container">
+               <button className="alert-button" onClick={() => setOpen(false)}>CLOSE</button>
+             </div>
+           </div>
+         </div>
         </div>
       )};
     </>
