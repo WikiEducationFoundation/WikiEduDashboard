3d58d8726da5c0e9b5da5889447e856738034fbc
Chaitanya Rai <Chaitanyarai899@gmail.com>
2023-02-07 10:31:43 +0530

    indentation errors fixed

--- app/assets/javascripts/training/components/Alert.jsx
+++ app/assets/javascripts/training/components/Alert.jsx
@@ -8,14 +8,14 @@ export default function Alert() {
       {open && (
         <div className="backdrop" onClick={() => setOpen(false)}>
           <div className="alert-box-container">
-           <div className="alert-box">
-             <h2 className="alert-title">Please take your time!</h2>
-             <p className="alert-content">It is very important that you learn the training content thoroughly.</p>
-             <div className="alert-button-container">
-               <button className="alert-button" onClick={() => setOpen(false)}>CLOSE</button>
-             </div>
-           </div>
-         </div>
+            <div className="alert-box">
+              <h2 className="alert-title">Please take your time!</h2>
+              <p className="alert-content">It is very important that you learn the training content thoroughly.</p>
+              <div className="alert-button-container">
+                <button className="alert-button" onClick={() => setOpen(false)}>CLOSE</button>
+              </div>
+            </div>
+          </div>
         </div>
       )};
     </>
