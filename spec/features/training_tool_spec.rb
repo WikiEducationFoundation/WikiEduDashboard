7d6d9d8d63a61ccb5dcdc7aad615d0b1433ac117
Chaitanya Rai <Chaitanyarai899@gmail.com>
2023-02-16 02:24:32 +0530

    modifying test to support training alert

--- spec/features/training_tool_spec.rb
+++ spec/features/training_tool_spec.rb
@@ -264,8 +264,9 @@ def check_slide_contents(slide, slide_number, slide_count)
 end
 
 def proceed_to_next_slide
-  if driver.find_element(:class, 'alert-button').displayed?
-    driver.find_element(:class, 'alert-button').click
+  if page.has_selector?('button.alert-button')
+    alert_button = page.find('button.alert-button')
+    alert_button.click
     button = page.first('button.ghost-button', minimum: 0)
     find_correct_answer_by_trial_and_error unless button.nil?
     page.first('a.slide-nav.btn.btn-primary.icon-rt_arrow').click
