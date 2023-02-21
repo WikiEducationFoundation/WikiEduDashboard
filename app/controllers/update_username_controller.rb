e7c025c0acd6075fdd92e2dbf4c8524a5bd549c2
kunal <10kunalji@gmail.com>
2023-02-22 01:19:52 +0530

    Added new test case and fixed liniting bugs

--- app/controllers/update_username_controller.rb
+++ app/controllers/update_username_controller.rb
@@ -3,16 +3,15 @@
 require_dependency "#{Rails.root}/lib/importers/user_importer"
 
 class UpdateUsernameController < ApplicationController
-    before_action :require_signed_in
-    respond_to :html, :json
-    def index
-        @user = User
-    end
+  before_action :require_signed_in
+  respond_to :html, :json
+  def index
+    @user = User
+  end
 
-    def update
-        @username = params['username']
-        puts @username
-        UserImporter.update_username_for_global_id(@username)
-        redirect_to '/'
-    end
+  def update
+    @username = params['username']
+    UserImporter.update_username_for_global_id(@username)
+    redirect_to '/'
+  end
 end
--- spec/features/update_username_spec.rb
+++ spec/features/update_username_spec.rb
@@ -1,31 +1,30 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require "#{Rails.root}/app/controllers/update_username_controller.rb"
 
-describe UpdateUsernameController, type: :feature, js: true do
-    let(:user) { create(:user, id: 1, username: 'Old Username', global_id: 1234) }
-    before { login_as user }
-    
-    describe 'Update Username page' do
-        it 'checks if page is visited' do
-           
-            visit '/update_username'
-            # fill_in 'username', with: 'hash'
-            # click_button 'Update Username'
-            expect(page).to have_content('Enter Username to update')
-        end
-        # it 'checks if submit button is clicked' do
-        #     info = OpenStruct.new(name: 'RageSock')
-        #     credentials = OpenStruct.new(token: 'foo', secret: 'bar')
-        #     hash = OpenStruct.new(uid: '1234',
-        #                           info:,
-        #                           credentials:)
-        #     visit '/update_username'
-        #     fill_in('username', with: info.name)
-        #     click_button 'Update Username'
-        #     expect(User.find(1).id).to eq(user.id)
-        #     expect(page).to have_content('My Dashboard')
-        # end
+describe 'Update username controller', type: :feature, js: true do
+  let!(:user) { create(:user) }
+
+  before do
+    login_as user
+  end
+
+  context 'Update Username page' do
+    it 'checks if page is visited' do
+      visit '/update_username'
+      expect(page).to have_content('Enter Username to update')
     end
+
+    it 'visits and enter updated username' do
+      VCR.use_cassette('update_username') do
+        create(:user, id: 500, username: 'Ragesoss')
+        new_username = 'New Username'
+        User.find(500).update_attribute(:username, new_username)
+        visit '/update_username'
+        fill_in('username', with: new_username)
+        click_button 'Update Username'
+        expect(User.find(500).username).to eq(new_username)
+      end
+    end
+  end
 end
