455f618b360d111e0fae028228c72e332ad586cc
Shashwat <10794178+TheTrio@users.noreply.github.com>
2023-03-10 05:35:07 +0530

    improve styling

--- app/assets/javascripts/components/assignments/new_assignment_input.jsx
+++ app/assets/javascripts/components/assignments/new_assignment_input.jsx
@@ -1,46 +1,51 @@
 import React from 'react';
 import SelectedWikiOption from '../common/selected_wiki_option';
-import ArticleUtils from '../../utils/article_utils';
 
 const NewAssignmentInput = ({
   language, project, title,
   assign, handleChangeTitle, handleWikiChange, trackedWikis
-}) => (
-  <form
-    onSubmit={assign}
-    style={{
+}) => {
+  const articles = title.split('\n').filter(Boolean).length;
+  const multipleArticles = articles > 1;
+  return (
+    <form
+      onSubmit={assign}
+      style={{
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'flex-end',
       gap: '5px'
     }}
-  >
-    <textarea
-      placeholder={ArticleUtils.I18n('title_example', project)}
-      value={title}
-      onSubmit={assign}
-      onChange={handleChangeTitle}
-      style={{
-        padding: '0',
+    >
+      <textarea
+        placeholder={I18n.t('assignments.add_available_placeholder')}
+        value={title}
+        onSubmit={assign}
+        onChange={handleChangeTitle}
+        rows={`${articles <= 1 ? 1 : 3}`}
+        style={{
+        padding: '10px',
         width: '100%',
+        minWidth: '275px',
       }}
-    />
-    <button
-      className="button border"
-      type="submit"
-      style={{
+      />
+      <button
+        className="button border"
+        type="submit"
+        style={{
         width: 'max-content',
       }}
-    >
-      {I18n.t('assignments.label')}
-    </button>
-    <SelectedWikiOption
-      language={language}
-      project={project}
-      handleWikiChange={handleWikiChange}
-      trackedWikis={trackedWikis}
-    />
-  </form>
+      >
+        {multipleArticles ? 'Assign All' : I18n.t('assignments.label')}
+      </button>
+      <SelectedWikiOption
+        language={language}
+        project={project}
+        handleWikiChange={handleWikiChange}
+        trackedWikis={trackedWikis}
+      />
+    </form>
 );
+};
 
 export default NewAssignmentInput;
