943dafe24af6440fa6367066cd093093535dc2a9
Kunal Arora <72390769+kunalthedev@users.noreply.github.com>
2023-03-14 03:34:06 +0530

    Added area-label to Button without text to make it readable by screen readers (#5289)
    Resolves #2531
    
    * Added label to Remove Button and changed styling
    
    * added area label to buttons without text

--- app/assets/javascripts/components/overview/tag_editable.jsx
+++ app/assets/javascripts/components/overview/tag_editable.jsx
@@ -70,7 +70,7 @@ const TagEditable = createReactClass({
     // In editable mode we'll show a list of tags and a remove button plus a selector to add new tags
     const tagList = this.props.tags.map((tag) => {
       const removeButton = (
-        <button className="button border plus" onClick={this.removeTag.bind(this, tag.tag)}>-</button>
+        <button className="button border plus" aria-label="Remove tag" onClick={this.removeTag.bind(this, tag.tag)}>-</button>
       );
       return (
         <tr key={`${tag.id}_tag`}>
