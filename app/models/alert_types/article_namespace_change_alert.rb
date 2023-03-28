04694d37fd52d88e15d579555d938ea63a1370f3
sharon-victor <sharon.odhiambo100@gmail.com>
2023-03-28 02:52:59 -0400

    add alerts for when an article is moved from mainspace to a different namespace

---
+++ app/models/alert_types/article_namespace_change_alert.rb
@@ -0,0 +1,17 @@
+class ArticleNamespaceChangeAlertManager
+    def self.create_alert(article, previous_namespace, new_namespace, course)
+      # Create a new alert record of the "Article Namespace Change" type
+      alert = Alert.create(type: Alert::ARTICLE_NAMESPACE_CHANGE, course: course, article: article)
+  
+      # Set the necessary attributes for the alert
+      alert.previous_namespace = previous_namespace
+      alert.new_namespace = new_namespace
+  
+      # Save the alert record
+      alert.save
+  
+      # Notify the Wiki Expert for the associated course
+      WikiEduDashboardMailer.alert_notification(course, alert).deliver_now
+    end
+  end
+  
