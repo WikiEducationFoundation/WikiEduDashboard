cf60c72ee362a1afc422b58aa6f65809c4e2a71c
kessio <sharonkessio6@gmail.com>
2023-03-30 22:51:26 +0300

    autocorrect layout/indentation linter erros caused by changes in file

--- spec/features/invalid_oauth_spec.rb
+++ spec/features/invalid_oauth_spec.rb
@@ -3,31 +3,31 @@
 require 'rails_helper'
 
 describe 'user authentication with oauth credentials', type: :feature do
-describe 'a user with invalid oauth credentials' do
-  before do
-    user = create(:user, wiki_token: 'invalid')
-    login_as user
+  describe 'a user with invalid oauth credentials' do
+    before do
+      user = create(:user, wiki_token: 'invalid')
+      login_as user
+    end
+
+    it 'gets logged out and see a message about the problem' do
+      error_message = I18n.t('error.oauth_invalid')
+      visit root_path
+      expect(page).to have_content error_message
+      expect(page).to have_content 'Log in'
+    end
   end
 
-  it 'gets logged out and see a message about the problem' do
-    error_message = I18n.t('error.oauth_invalid')
-    visit root_path
-    expect(page).to have_content error_message
-    expect(page).to have_content 'Log in'
+  describe 'a user whose oauth credentials expire', type: :feature do
+    let(:admin)  { create(:admin) }
+    let(:course) { create(:course, end: 1.year.from_now, submitted: true) }
+
+    it 'is logged out upon visiting a course' do
+      stub_token_request_failure
+
+      login_as admin
+      visit "/courses/#{escaped_slug course.slug}"
+      expect(page).to have_current_path(root_path)
+      expect(page).to have_content 'Your Wikipedia authorization has expired'
+    end
   end
-end
-
-describe 'a user whose oauth credentials expire', type: :feature do
-  let(:admin)  { create(:admin) }
-  let(:course) { create(:course, end: 1.year.from_now, submitted: true) }
-
-  it 'is logged out upon visiting a course' do
-    stub_token_request_failure
-
-    login_as admin
-    visit "/courses/#{escaped_slug course.slug}"
-    expect(page).to have_current_path(root_path)
-    expect(page).to have_content 'Your Wikipedia authorization has expired'
-  end
-end
 end
