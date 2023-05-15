a788e9074ffe51037190b4d498b128b9671f9aaa
Amine Hassou <japnoo@gmail.com>
2023-05-16 00:50:15 +0100

    Converted admin alerts component from class to functional, used redux hooks

--- app/assets/javascripts/components/alerts/admin_alerts.jsx
+++ app/assets/javascripts/components/alerts/admin_alerts.jsx
@@ -1,31 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import AlertsHandler from './alerts_handler.jsx';
 import { fetchAdminAlerts } from '../../actions/alert_actions';
 
-class AdminAlerts extends React.Component {
-  componentDidMount() {
-    // This adds ALL alerts to the state, to be used in AlertsHandler
-    this.props.fetchAdminAlerts();
-  }
+const AdminAlerts = () => {
+  const dispatch = useDispatch();
 
-  render() {
-    return (
-      <AlertsHandler
-        alertLabel={I18n.t('alerts.alert_label')}
-        noAlertsLabel={I18n.t('alerts.no_alerts')}
-        adminAlert={true}
-      />
-    );
-  }
-}
+  useEffect(() => {
+    // This adds ALL alerts to the state, to be used in AlertsHandler
+    dispatch(fetchAdminAlerts());
+  }, [dispatch]);
+
+
+  return (
+    <AlertsHandler
+      alertLabel={I18n.t('alerts.alert_label')}
+      noAlertsLabel={I18n.t('alerts.no_alerts')}
+      adminAlert={true}
+    />
+  );
+};
 
 AdminAlerts.propTypes = {
   fetchAdminAlerts: PropTypes.func,
 };
 
-const mapDispatchToProps = { fetchAdminAlerts };
-
-export default connect(null, mapDispatchToProps)(AdminAlerts);
+export default (AdminAlerts);
