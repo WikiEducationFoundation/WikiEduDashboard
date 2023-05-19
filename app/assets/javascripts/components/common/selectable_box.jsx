7ea9551adc084b19a133b733349b2779e034cdc9
Shashwat <10794178+TheTrio@users.noreply.github.com>
2023-05-19 14:33:43 +0530

    reference image by url instead of importing svg
    as always, blame jest :-|

--- app/assets/javascripts/components/common/selectable_box.jsx
+++ app/assets/javascripts/components/common/selectable_box.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import checkboxSvg from '../../../svg/check.svg';
 
 const SelectableBox = ({ onClick, heading, description, style, selected }) => {
   return (
     <div key={heading} onClick={onClick} className={`program-description ${selected ? 'selected' : ''}`} style={style}>
-      {selected && <img src={checkboxSvg} alt="checkbox" className="checkbox-image" />}
+      {selected && <img src="/assets/images/check.svg" alt="checkbox" className="checkbox-image" />}
       <h4><strong>{heading}</strong></h4>
       <p>
         {description}
