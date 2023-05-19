4c569ec0079a1312404710650415e0bbe36c699e
Shashwat <10794178+TheTrio@users.noreply.github.com>
2023-05-19 13:43:41 +0530

    add checkbox to selected scoping methods

--- app/assets/javascripts/components/common/selectable_box.jsx
+++ app/assets/javascripts/components/common/selectable_box.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import checkboxSvg from '../../../svg/check.svg';
 
 const SelectableBox = ({ onClick, heading, description, style, selected }) => {
   return (
     <div key={heading} onClick={onClick} className={`program-description ${selected ? 'selected' : ''}`} style={style}>
+      {selected && <img src={checkboxSvg} alt="checkbox" className="checkbox-image" />}
       <h4><strong>{heading}</strong></h4>
       <p>
         {description}
