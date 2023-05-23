e0af86cb5b2d8fb089156267fb4aef4d2e32d38e
Amine Hassou <japnoo@gmail.com>
2023-05-23 05:30:56 +0100

    Updated MarkAsIncompleteButton testr + snapshot to account for use of redux hooks

--- test/components/overview/my_articles/components/Categories/List/Assignment/Header/Actions/MarkAsIncompleteButton.spec.jsx
+++ test/components/overview/my_articles/components/Categories/List/Assignment/Header/Actions/MarkAsIncompleteButton.spec.jsx
@@ -1,24 +1,37 @@
+// @ts-nocheck
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import '../../../../../../../../../testHelper';
-
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import MarkAsIncompleteButton from '../../../../../../../../../../app/assets/javascripts/components/overview/my_articles/components/Categories/List/Assignment/Header/Actions/MarkAsIncompleteButton';
+import { Provider } from 'react-redux';
 
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 describe('MarkAsIncompleteButton', () => {
+  const store = mockStore({
+  });
+
+  const MockProvider = (mockProps) => {
+    return (
+      <Provider store={store}>
+        <MarkAsIncompleteButton {...mockProps} />
+      </Provider >
+    );
+  };
   const update = jest.fn();
   const props = {
     assignment: { assignment_all_statuses: [] },
     courseSlug: 'course/slug',
-    handleUpdateAssignment: update,
-    refreshAssignments: jest.fn()
   };
   it('should show the button', () => {
-    const component = shallow(<MarkAsIncompleteButton {...props} />);
+    const component = mount(<MockProvider {...props} />);
     expect(component).toMatchSnapshot();
   });
 
   it('should update the assignment on button click', async () => {
-    const component = shallow(<MarkAsIncompleteButton {...props} />);
+    const component = mount(<MockProvider {...props} />);
     const button = component.find('button');
 
     await button.props().onClick();
