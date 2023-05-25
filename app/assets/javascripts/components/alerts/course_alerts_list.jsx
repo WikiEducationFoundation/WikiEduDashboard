1be107c57b6ce44edf15fc94c977d3b48fdba11d
Amine Hassou <japnoo@gmail.com>
2023-05-25 04:55:57 +0100

    Converted CourseAlertsList to functional component + added redux hooks use

--- app/assets/javascripts/components/alerts/course_alerts_list.jsx
+++ app/assets/javascripts/components/alerts/course_alerts_list.jsx
@@ -1,32 +1,25 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 import AlertsHandler from './alerts_handler.jsx';
 import { fetchCourseAlerts } from '../../actions/alert_actions';
 
-class CourseAlertsList extends React.Component {
-  componentDidMount() {
+const CourseAlertsList = (props) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
     // sets the title of this tab
-    document.title = `${this.props.course.title} - ${I18n.t('courses.alerts')}`;
+    document.title = `${props.course.title} - ${I18n.t('courses.alerts')}`;
     // This adds the specific course alerts to the state, to be used in AlertsHandler
-    this.props.fetchCourseAlerts(this.props.course_id);
-  }
+    dispatch(fetchCourseAlerts(props.course_id));
+  }, []);
 
-  render() {
-    return (
-      <AlertsHandler
-        alertLabel={I18n.t('alerts.alert_label')}
-        noAlertsLabel={I18n.t('alerts.no_alerts')}
-      />
-    );
-  }
-}
-
-CourseAlertsList.propTypes = {
-  fetchCourseAlerts: PropTypes.func,
+  return (
+    <AlertsHandler
+      alertLabel={I18n.t('alerts.alert_label')}
+      noAlertsLabel={I18n.t('alerts.no_alerts')}
+    />
+  );
 };
 
-const mapDispatchToProps = { fetchCourseAlerts };
-
-export default connect(null, mapDispatchToProps)(CourseAlertsList);
+export default (CourseAlertsList);
