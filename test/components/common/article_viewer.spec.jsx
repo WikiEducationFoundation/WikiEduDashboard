06e4ac4e99bf6d9a6947a98fae064be1aa84b40a
Amine Hassou <japnoo@gmail.com>
2023-06-01 20:30:59 +0100

    Fixed articleViewer test

--- test/components/common/article_viewer.spec.jsx
+++ test/components/common/article_viewer.spec.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactTestUtils from 'react-dom/test-utils';
 
-import { ArticleViewer } from '@components/common/ArticleViewer/containers/ArticleViewer.jsx';
+import ArticleViewer from '@components/common/ArticleViewer/containers/ArticleViewer.jsx';
 import '../../testHelper';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 describe('ArticleViewer', () => {
+  const store = mockStore({
+  });
   it('renders', () => {
     const article = {
       id: 99,
@@ -17,12 +24,15 @@ describe('ArticleViewer', () => {
       id: 1
     };
     const TestArticleViewer = ReactTestUtils.renderIntoDocument(
-      <ArticleViewer
-        alertStatus={{}}
-        article={article}
-        course={course}
-        fetchArticleDetails={() => null}
-      />
+      <Provider store={store} >
+        <ArticleViewer
+          alertStatus={{}}
+          article={article}
+          course={course}
+          fetchArticleDetails={() => null}
+        />
+      </Provider>
+
     );
     expect(TestArticleViewer).toExist;
   });
