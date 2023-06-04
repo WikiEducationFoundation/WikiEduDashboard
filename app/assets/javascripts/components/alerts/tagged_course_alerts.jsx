1156dd3c76d7e160d711c5d0cbc0287b54f62b4e
Amine Hassou <japnoo@gmail.com>
2023-06-04 22:03:53 +0100

    Converted TaggedCourseAlerts into functional component + used redux hooks

--- app/assets/javascripts/components/alerts/tagged_course_alerts.jsx
+++ app/assets/javascripts/components/alerts/tagged_course_alerts.jsx
@@ -1,40 +1,21 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import withRouter from '../util/withRouter';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import AlertsHandler from './alerts_handler.jsx';
-import { fetchTaggedCourseAlerts, filterAlerts } from '../../actions/alert_actions';
+import { fetchTaggedCourseAlerts } from '../../actions/alert_actions';
+import { useParams } from 'react-router-dom';
 
-class TaggedCourseAlerts extends React.Component {
-  constructor(props) {
-    super(props);
+const TaggedCourseAlerts = () => {
+  const { tag } = useParams();
+  const dispatch = useDispatch();
 
-    this.getTag = this.getTag.bind(this);
-  }
+  useEffect(() => dispatch(fetchTaggedCourseAlerts(tag)), []);
 
-  componentDidMount() {
-    // This adds the specific campaign alerts to the state, to be used in AlertsHandler
-    this.props.fetchTaggedCourseAlerts(this.getTag());
-  }
-
-  getTag() {
-    return `${this.props.router.params.tag}`;
-  }
-
-  render() {
-    return (
-      <AlertsHandler
-        alertLabel={I18n.t('campaign.alert_label')}
-        noAlertsLabel={I18n.t('campaign.no_alerts')}
-      />
-    );
-  }
-}
-
-TaggedCourseAlerts.propTypes = {
-  fetchTaggedCourseAlerts: PropTypes.func
+  return (
+    <AlertsHandler
+      alertLabel={I18n.t('campaign.alert_label')}
+      noAlertsLabel={I18n.t('campaign.no_alerts')}
+    />
+  );
 };
 
-const mapDispatchToProps = { fetchTaggedCourseAlerts, filterAlerts };
-
-export default withRouter(connect(null, mapDispatchToProps)(TaggedCourseAlerts));
+export default (TaggedCourseAlerts);
