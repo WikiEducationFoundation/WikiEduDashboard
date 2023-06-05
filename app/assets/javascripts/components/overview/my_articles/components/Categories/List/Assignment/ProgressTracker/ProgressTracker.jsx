e501604b3afdf8e4bf6eecba28f7989a7db598d5
Amine Hassou <japnoo@gmail.com>
2023-06-05 01:21:49 +0100

    Convered ProgressTracker into functional component

--- app/assets/javascripts/components/overview/my_articles/components/Categories/List/Assignment/ProgressTracker/ProgressTracker.jsx
+++ app/assets/javascripts/components/overview/my_articles/components/Categories/List/Assignment/ProgressTracker/ProgressTracker.jsx
@@ -1,64 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import processes from '@components/overview/my_articles/step_processes';
 
-// components
 import Step from './Step/Step.jsx';
 import NavigationElements from './NavigationElements/NavigationElements';
 
-export class ProgressTracker extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      show: false
-    };
+const ProgressTracker = ({ assignment, course }) => {
+  const [showTracker, setShowTracker] = useState(false);
 
-    this.toggle = this.toggle.bind(this);
-  }
+  const toggle = () => {
+    setShowTracker(prevState => !prevState);
+  };
 
-  toggle() {
-    this.setState({ show: !this.state.show });
-  }
+  const steps = processes(assignment, course).map((content, index) => (
+    <Step
+      {...content}
+      assignment={assignment}
+      course={course}
+      index={index}
+      key={index}
+    />
+  ));
 
-  render() {
-    const { assignment, course } = this.props;
-    const { show } = this.state;
-
-    const steps = processes(assignment, course).map((content, index) => (
-      <Step
-        {...content}
-        assignment={assignment}
-        course={course}
-        index={index}
-        key={index}
-      />
-    ));
-
-    return (
-      <div className="progress-tracker">
-        <button
-          className="screen-reader"
-          onClick={this.toggle}
-        >
-          Click to hide or show progress tracker
-        </button>
-        <nav
-          aria-label="Click to hide or show progress tracker"
-          className="toggle-progress-tracker"
-          onClick={this.toggle}
-        >
-          <NavigationElements assignment={assignment} show={show} course={course} />
-        </nav>
-        <section className="flow">
-          {show ? steps : null}
-        </section>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="progress-tracker">
+      <button
+        className="screen-reader"
+        onClick={toggle}
+      >
+        Click to hide or show progress tracker
+      </button>
+      <nav
+        aria-label="Click to hide or show progress tracker"
+        className="toggle-progress-tracker"
+        onClick={toggle}
+      >
+        <NavigationElements assignment={assignment} showTracker={showTracker} course={course} />
+      </nav>
+      <section className="flow">
+        {showTracker ? steps : null}
+      </section>
+    </div>
+  );
+};
 
 ProgressTracker.propTypes = {
-  // props
   assignment: PropTypes.object.isRequired,
   course: PropTypes.object.isRequired,
 };
