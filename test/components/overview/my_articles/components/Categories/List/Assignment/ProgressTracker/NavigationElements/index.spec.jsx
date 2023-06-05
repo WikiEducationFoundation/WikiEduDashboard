46a2fbe53ef01663bd458590886c14e4509508d0
Amine Hassou <japnoo@gmail.com>
2023-06-05 01:21:49 +0100

    Convered ProgressTracker into functional component

--- test/components/overview/my_articles/components/Categories/List/Assignment/ProgressTracker/NavigationElements/index.spec.jsx
+++ test/components/overview/my_articles/components/Categories/List/Assignment/ProgressTracker/NavigationElements/index.spec.jsx
@@ -5,8 +5,8 @@ import '../../../../../../../../../testHelper';
 import NavigationElements from '@components/overview/my_articles/components/Categories/List/Assignment/ProgressTracker/NavigationElements/NavigationElements.jsx';
 
 describe('NavigationElements', () => {
-  const props = { assignment: {}, show: false };
-  it('should show the down arrow when `show` is false', () => {
+  const props = { assignment: {}, showTracker: false };
+  it('should show the down arrow when `showTracker` is false', () => {
     const component = shallow(<NavigationElements {...props} />);
     const li = component.find('li');
 
@@ -14,8 +14,8 @@ describe('NavigationElements', () => {
     expect(li.props().className).toContain('icon-arrow');
   });
 
-  it('should show the up arrow when `show` is true', () => {
-    const component = shallow(<NavigationElements {...props} show={true} />);
+  it('should show the up arrow when `showTracker` is true', () => {
+    const component = shallow(<NavigationElements {...props} showTracker={true} />);
     const li = component.find('li');
 
     expect(li.length).toEqual(1);
@@ -24,7 +24,7 @@ describe('NavigationElements', () => {
 
   it('displays list items when given an assignment', () => {
     const component = shallow(
-      <NavigationElements assignment={{ sandboxUrl: 'url' }} show={true} />
+      <NavigationElements assignment={{ sandboxUrl: 'url' }} showTracker={true} />
     );
     const listItem = component.find('ListItem');
     expect(listItem.length).toBeTruthy;
