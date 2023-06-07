c53d2767a248ae84e0338ae041a63eae8d2dc8aa
Amine Hassou <japnoo@gmail.com>
2023-06-08 00:03:28 +0100

    Converted TicketsHandler into functional component + used redux hooks

--- app/assets/javascripts/components/tickets/tickets_handler.jsx
+++ app/assets/javascripts/components/tickets/tickets_handler.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Row from './tickets_table_row';
 import Pagination from './pagination';
@@ -9,169 +9,158 @@ import SearchBar from '../common/search_bar';
 import TicketOwnersFilter from './ticket_owners_filter';
 import TicketStatusesFilter from './ticket_statuses_filter';
 import SearchTypeSelector from './search_type_selector';
-import { fetchTickets, sortTickets, setInitialTicketFilters, emptyList } from '../../actions/tickets_actions';
+import { fetchTickets, sortTickets, setInitialTicketFilters } from '../../actions/tickets_actions';
 import { getFilteredTickets } from '../../selectors';
 
-export class TicketsHandler extends React.Component {
-  constructor(props) {
-    super(props);
-    this.SearchByCourseParamInURL = this.getCourseSearchParamInURL();
-    this.state = {
-      page: 0,
-      mode: 'filter',
-      searchType: this.SearchByCourseParamInURL ? 'by_course' : 'no_search',
-      searchText: this.SearchByCourseParamInURL || ''
-    };
-    this.searchBarRef = React.createRef();
-    this.searchTypeRef = React.createRef();
-  }
+const TicketsHandler = () => {
+  const [page, setPage] = useState(0);
+  const [mode, setMode] = useState('filter');
+  const [searchType, setSearchType] = useState('no_search');
+  const [searchText, setSearchText] = useState('');
 
-  componentDidMount() {
-    if (this.SearchByCourseParamInURL) {
-      this.props.fetchTickets({ search: this.SearchByCourseParamInURL, what: 'by_course' });
-    } else if (!this.props.tickets.all.length) {
-        this.props.fetchTickets();
-        this.props.setInitialTicketFilters();
-      }
-  }
+  const dispatch = useDispatch();
+  const tickets = useSelector(state => state.tickets);
+  const filteredTickets = useSelector(state => getFilteredTickets(state));
 
-  getCourseSearchParamInURL() {
+  const searchBarRef = useRef();
+  const searchTypeRef = useRef();
+
+  useEffect(() => {
+    const searchByCourseParamInURL = getCourseSearchParamInURL();
+
+    if (searchByCourseParamInURL) {
+      setSearchType('by_course');
+      setSearchText(searchByCourseParamInURL);
+      dispatch(fetchTickets({ search: searchByCourseParamInURL, what: 'by_course' }));
+    } else if (!tickets.all.length) {
+      dispatch(fetchTickets());
+      dispatch(setInitialTicketFilters());
+    }
+  }, []);
+
+  const getCourseSearchParamInURL = () => {
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get('search_by_course');
-  }
-
-  doSearch = () => {
-    this.props.fetchTickets({ search: this.searchBarRef?.current.value, what: this.state.searchType });
-    this.setState({ searchText: this.searchBarRef?.current.value });
   };
 
-  changeMode = (e) => {
-    this.setState({ searchType: e.value, searchText: '' });
+  const doSearch = () => {
+    dispatch(fetchTickets({ search: searchBarRef?.current.value, what: searchType }));
+    setSearchText(searchBarRef?.current.value);
+  };
+
+  const changeMode = (e) => {
+    setSearchType(e.value);
+    setSearchText('');
+
     if (e.value === 'no_search') {
-      this.setState({ mode: 'filter' });
-      this.props.fetchTickets();
+      setMode('filter');
+      dispatch(fetchTickets());
     } else {
-      this.setState({ mode: 'search' });
+      setMode('search');
     }
   };
 
-  getTickets = () => {
-    if (this.state.mode === 'filter') {
-      return this.props.filteredTickets;
+  const getTickets = () => {
+    if (mode === 'filter') {
+      return filteredTickets;
     }
 
-    return this.props.tickets.all;
+    return tickets.all;
   };
 
-  goToPage(page) {
-    this.setState({ page });
+  const goToPage = (newPage) => {
+    setPage(newPage);
+  };
+
+  if (tickets.loading) return <Loading />;
+
+  const keys = {
+    sender: {
+      label: 'Sender',
+      desktop_only: false,
+      sortable: true
+    },
+    subject: {
+      label: 'Subject',
+      desktop_only: false,
+      sortable: true
+    },
+    course_title: {
+      label: 'Course',
+      desktop_only: false,
+      sortable: true
+    },
+    status: {
+      label: 'Status',
+      desktop_only: false,
+      sortable: true
+    },
+    owner: {
+      label: 'Ticket Owner',
+      desktop_only: true,
+      sortable: true
+    },
+    actions: {
+      label: 'Actions',
+      desktop_only: false,
+      sortable: false
+    }
+  };
+
+  const TICKETS_PER_PAGE = 10;
+
+  const pagesLength = Math.floor((filteredTickets.length - 1) / TICKETS_PER_PAGE) + 1;
+  const elements = getTickets()
+    .slice(page * TICKETS_PER_PAGE, page * TICKETS_PER_PAGE + TICKETS_PER_PAGE)
+    .map(ticket => <Row key={ticket.id} ticket={ticket} />);
+
+  // Since this is used multiple places (student_list.jsx), we should
+  // refactor this a bit.
+  if (tickets.sort.key && keys[tickets.sort.key]) {
+    const order = (tickets.sort.sortKey) ? 'asc' : 'desc';
+    keys[tickets.sort.key].order = order;
   }
 
-  render() {
-    if (this.props.tickets.loading) return <Loading />;
-
-
-    const keys = {
-      sender: {
-        label: 'Sender',
-        desktop_only: false,
-        sortable: true
-      },
-      subject: {
-        label: 'Subject',
-        desktop_only: false,
-        sortable: true
-      },
-      course_title: {
-        label: 'Course',
-        desktop_only: false,
-        sortable: true
-      },
-      status: {
-        label: 'Status',
-        desktop_only: false,
-        sortable: true
-      },
-      owner: {
-        label: 'Ticket Owner',
-        desktop_only: true,
-        sortable: true
-      },
-      actions: {
-        label: 'Actions',
-        desktop_only: false,
-        sortable: false
-      }
-    };
-
-    const TICKETS_PER_PAGE = 10;
-
-    const pagesLength = Math.floor((this.props.filteredTickets.length - 1) / TICKETS_PER_PAGE) + 1;
-    const elements = this.getTickets()
-      .slice(this.state.page * TICKETS_PER_PAGE, this.state.page * TICKETS_PER_PAGE + TICKETS_PER_PAGE)
-      .map(ticket => <Row key={ticket.id} ticket={ticket} />);
-
-    // Since this is used multiple places (student_list.jsx), we should
-    // refactor this a bit.
-    if (this.props.tickets.sort.key && keys[this.props.tickets.sort.key]) {
-      const order = (this.props.tickets.sort.sortKey) ? 'asc' : 'desc';
-      keys[this.props.tickets.sort.key].order = order;
-    }
-
-    return (
-      <main className="container ticket-dashboard">
-        <h1 className="mt4">Ticketing Dashboard</h1>
+  return (
+    <main className="container ticket-dashboard">
+      <h1 className="mt4">Ticketing Dashboard</h1>
+      <div>
         <div>
-          <div>
-            <span className="pull-left w10">Status: </span>
-            <TicketStatusesFilter disabled={this.state.mode === 'search'}/>
-            <span className="pull-left w10">Owner: </span>
-            <TicketOwnersFilter disabled={this.state.mode === 'search'}/>
-          </div>
-          <div>
-            <SearchTypeSelector
-              value={this.state.searchType}
-              ref={this.searchTypeRef}
-              handleChange={this.changeMode}
-            />
-            <SearchBar
-              name="tickets_search"
-              value={this.state.searchText}
-              onClickHandler={this.doSearch} ref={this.searchBarRef}
-              placeholder={I18n.t('tickets.search_bar_placeholder')}
-            />
-          </div>
+          <span className="pull-left w10">Status: </span>
+          <TicketStatusesFilter disabled={mode === 'search'} />
+          <span className="pull-left w10">Owner: </span>
+          <TicketOwnersFilter disabled={mode === 'search'} />
         </div>
-        <hr/>
-        <List
-          className="table--expandable table--hoverable"
-          elements={elements}
-          keys={keys}
-          sortBy={this.props.sortTickets}
-          sortable={true}
-          table_key="tickets"
-        />
-        <Pagination
-          currentPage={this.state.page}
-          goToPage={this.goToPage.bind(this)}
-          length={pagesLength}
-        />
-      </main>
-    );
-  }
-}
-
-const mapStateToProps = state => ({
-  tickets: state.tickets,
-  filteredTickets: getFilteredTickets(state)
-});
-
-const mapDispatchToProps = {
-  fetchTickets,
-  sortTickets,
-  setInitialTicketFilters,
-  emptyList
+        <div>
+          <SearchTypeSelector
+            value={searchType}
+            ref={searchTypeRef}
+            handleChange={changeMode}
+          />
+          <SearchBar
+            name="tickets_search"
+            value={searchText}
+            onClickHandler={doSearch} ref={searchBarRef}
+            placeholder={I18n.t('tickets.search_bar_placeholder')}
+          />
+        </div>
+      </div>
+      <hr />
+      <List
+        className="table--expandable table--hoverable"
+        elements={elements}
+        keys={keys}
+        sortBy={key => dispatch(sortTickets(key))}
+        sortable={true}
+        table_key="tickets"
+      />
+      <Pagination
+        currentPage={page}
+        goToPage={goToPage}
+        length={pagesLength}
+      />
+    </main>
+  );
 };
 
-const connector = connect(mapStateToProps, mapDispatchToProps);
-export default connector(TicketsHandler);
+export default (TicketsHandler);
--- test/components/tickets/tickets_handler.spec.jsx
+++ test/components/tickets/tickets_handler.spec.jsx
@@ -1,47 +1,34 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
-import { MemoryRouter } from 'react-router';
+import thunk from 'redux-thunk';
 import configureMockStore from 'redux-mock-store';
 
-import { TicketsHandler } from '../../../app/assets/javascripts/components/tickets/tickets_handler';
+import TicketsHandler from '../../../app/assets/javascripts/components/tickets/tickets_handler';
 import '../../testHelper';
+import { MemoryRouter } from 'react-router-dom';
 
-const mockStore = configureMockStore()({
+const middlewares = [thunk];
+const filteredTickets = [
+  { id: 1, owner: {}, project: {}, sender: {}, subject: 'Subject 1' },
+  { id: 2, owner: {}, project: {}, sender: {}, subject: 'Subject 2' }
+];
+const tickets = { all: filteredTickets, sort: {}, owners: {}, filters: { owners: [], statuses: [] } };
+
+const mockStore = configureMockStore(middlewares)({
   admins: [],
-  tickets: {
-    filters: {
-      owners: [],
-      statuses: []
-    }
-  }
+  tickets: tickets
 });
-
 describe('Tickets', () => {
   describe('TicketsHandler', () => {
     it('should display the standard information', () => {
-      const filteredTickets = [
-        { id: 1, owner: {}, project: {}, sender: {}, subject: 'Subject 1' },
-        { id: 2, owner: {}, project: {}, sender: {}, subject: 'Subject 2' }
-      ];
-      const tickets = { all: filteredTickets, sort: {} };
-
-      const fetchTickets = sinon.stub().resolves();
-      const setInitialTicketFilters = sinon.stub().resolves();
-      const props = {
-        fetchTickets,
-        filteredTickets,
-        setInitialTicketFilters,
-        tickets
-      };
       const handler = mount(
         <Provider store={mockStore}>
           <MemoryRouter>
-            <TicketsHandler {...props} />
-          </MemoryRouter>
+            <TicketsHandler />
+          </ MemoryRouter>
         </Provider>
       );
-
       const heading = handler.find('h1');
       expect(heading.length).toBeTruthy;
       expect(heading.text()).toEqual('Ticketing Dashboard');
@@ -52,7 +39,7 @@ describe('Tickets', () => {
       const ticketOwnersFilter = handler.find('TicketOwnersFilter');
       expect(ticketOwnersFilter.length).toBeTruthy;
 
-      const html = handler.html();
+      const html = handler.find('tbody').html();
       filteredTickets.forEach((ticket) => {
         expect(html).toContain(ticket.subject);
       });
