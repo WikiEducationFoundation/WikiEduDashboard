7c785d560e984ddaee25c19692cb8adca50cdeae
Abishek Das <abishekdascs@gmail.com>
2023-06-09 10:52:29 +0530

    Refactor line length in WatchlistController spec

--- spec/controllers/courses/watchlist_controller_spec.rb
+++ spec/controllers/courses/watchlist_controller_spec.rb
@@ -11,12 +11,12 @@ RSpec.describe Courses::WatchlistController, type: :controller do
     allow(controller).to receive(:current_user).and_return(user)
     allow(controller).to receive(:find_course_by_slug).and_return(course)
     allow(course).to receive(:home_wiki).and_return(wiki)
-    allow_any_instance_of(CoursesUsers).to receive(:user_page).with(course.students).and_return(array_of_users)
+    allow_any_instance_of(CoursesUsers).to receive(:user_page).with(course.students)
+                                                              .and_return(array_of_users)
   end
 
   describe 'POST #add_to_watchlist' do
     it 'watches the user pages for the students' do
-      puts wiki.inspect
       watchlist_edits = instance_double(WatchlistEdits)
       expect(WatchlistEdits).to receive(:new).with(wiki, user).and_return(watchlist_edits)
       expect(watchlist_edits).to receive(:watch_userpages).with(array_of_users)
