f207db0bb2f61f92dfc9d7e7036e12f573dfa5bd
Amine Hassou <japnoo@gmail.com>
2023-06-19 20:06:19 +0100

    Replace all uses of unsupported ReactDOM.render

--- app/assets/javascripts/components/app.jsx
+++ app/assets/javascripts/components/app.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Nav from './nav/nav.jsx';
 import { render } from './Main';
+import { createRoot } from 'react-dom/client';
 
 // The navbar is its own React element, independent of the
 // main React Router-based component tree.
 // `nav_root` is present throughout the app, via the Rails view layouts.
 const navBar = document.getElementById('nav_root');
+const root = createRoot(navBar); // createRoot(container!) if you use TypeScript
+
 if (navBar) {
-  ReactDOM.render((<Nav/>), navBar);
+  root.render((<Nav />));
 }
 
 const reactRoot = document.getElementById('react_root');
--- app/assets/javascripts/surveys/survey-results.jsx
+++ app/assets/javascripts/surveys/survey-results.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import QuestionResults from './components/QuestionResults.jsx';
+import { createRoot } from 'react-dom/client';
 
 $('[data-question-results]').each((i, result) => {
   const data = $(result).data('question-results');
-  ReactDOM.render(<QuestionResults {...data} />, result);
+  const root = createRoot(result);
+  root.render(<QuestionResults {...data} />);
 });
