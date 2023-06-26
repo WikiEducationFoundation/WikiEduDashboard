3561dd396aab8b796f098130734483e3bfa11e84
Sage Ross <ragesoss@gmail.com>
2023-06-26 11:00:52 -0700

    Add redlink behavior for draft sandbox links based on sandbox status

--- app/assets/javascripts/components/common/AssignmentLinks/SandboxLink.jsx
+++ app/assets/javascripts/components/common/AssignmentLinks/SandboxLink.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const SandboxLink = ({ assignment, editMode }) => {
+  const sandboxExists = assignment.draft_sandbox_status !== 'does_not_exist';
   let url = assignment.sandboxUrl || assignment.sandbox_url;
-  if (Features.wikiEd && editMode) {
-    url += '?veaction=edit&preload=Template:Dashboard.wikiedu.org_draft_template';
+  let linkClass = '';
+
+  if (Features.wikiEd && !sandboxExists) {
+    if (editMode) { url += '?veaction=edit&preload=Template:Dashboard.wikiedu.org_draft_template'; }
+    linkClass += 'redlink';
   }
+
   return (
-    <a href={url} target="_blank">
+    <a href={url} className={linkClass} target="_blank">
       {I18n.t('assignments.sandbox_draft_link')}
     </a>
   );
