54f3a91b3ab91a1d6c7ab0a6d34652cd815daf26
Amine Hassou <japnoo@gmail.com>
2023-06-29 01:09:44 +0100

    Updated useOutsideClick to deal with event bubbling edge case

--- app/assets/javascripts/hooks/useOutsideClick.js
+++ app/assets/javascripts/hooks/useOutsideClick.js
@@ -11,10 +11,10 @@ const useOutsideClick = (callback) => {
       }
     };
 
-    document.addEventListener('click', handleClick);
+    document.addEventListener('click', handleClick, true);
 
     return () => {
-      document.removeEventListener('click', handleClick);
+      document.removeEventListener('click', handleClick, true);
     };
   }, [ref]);
 
