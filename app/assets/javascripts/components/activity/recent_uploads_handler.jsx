e22d4633bddfaf08f6962a686e6cad05c7fde87a
Amine Hassou <japnoo@gmail.com>
2023-07-07 22:34:27 +0100

    Converted RecentUploadsHandlerBase into functional component

--- app/assets/javascripts/components/activity/recent_uploads_handler.jsx
+++ app/assets/javascripts/components/activity/recent_uploads_handler.jsx
@@ -1,56 +1,45 @@
-import React from 'react';
-import createReactClass from 'create-react-class';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchRecentUploads, sortRecentUploads } from '../../actions/recent_uploads_actions.js';
 import { GALLERY_VIEW } from '../../constants';
 import UploadList from '../uploads/upload_list.jsx';
 
 
-export const RecentUploadsHandlerBase = createReactClass({
-  displayName: 'RecentUploadsHandler',
+const RecentUploadsHandlerBase = () => {
+  const dispatch = useDispatch();
+  const uploads = useSelector(state => state.recentUploads.uploads);
 
-  propTypes: {
-    fetchRecentUploads: PropTypes.func,
-    sortRecentUploads: PropTypes.func,
-    uploads: PropTypes.array,
-    loading: PropTypes.bool
-   },
+  useEffect(() => {
+    dispatch(fetchRecentUploads());
+  }, []);
 
-  componentDidMount() {
-    return this.props.fetchRecentUploads();
-  },
+  const sortBy = (e) => {
+    dispatch(sortRecentUploads(e.target.value));
+  };
 
-  sortBy(e) {
-    this.props.sortRecentUploads(e.target.value);
-  },
-
-  render() {
-    return (
-      <div id="uploads">
-        <div className="section-header">
-          <h3>{I18n.t('uploads.header')}</h3>
-          <div className="sort-select">
-            <select className="sorts" name="sorts" onChange={this.sortBy}>
-              <option value="uploaded_at">{I18n.t('uploads.uploaded_at')}</option>
-              <option value="uploader">{I18n.t('uploads.uploaded_by')}</option>
-              <option value="usage_count">{I18n.t('uploads.usage_count')}</option>
-            </select>
-          </div>
+  return (
+    <div id="uploads">
+      <div className="section-header">
+        <h3>{I18n.t('uploads.header')}</h3>
+        <div className="sort-select">
+          <select className="sorts" name="sorts" onChange={sortBy}>
+            <option value="uploaded_at">{I18n.t('uploads.uploaded_at')}</option>
+            <option value="uploader">{I18n.t('uploads.uploaded_by')}</option>
+            <option value="usage_count">{I18n.t('uploads.usage_count')}</option>
+          </select>
         </div>
-        <UploadList uploads={this.props.uploads} view={GALLERY_VIEW} />
       </div>
-    );
-  }
-});
-
-const mapStateToProps = state => ({
-  uploads: state.recentUploads.uploads
-});
-
-const mapDispatchToProps = {
-  fetchRecentUploads,
-  sortRecentUploads
+      <UploadList uploads={uploads} view={GALLERY_VIEW} />
+    </div>
+  );
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(RecentUploadsHandlerBase);
+RecentUploadsHandlerBase.propTypes = {
+  fetchRecentUploads: PropTypes.func,
+  sortRecentUploads: PropTypes.func,
+  uploads: PropTypes.array,
+  loading: PropTypes.bool
+};
+
+export default (RecentUploadsHandlerBase);
