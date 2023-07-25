eefa424fb80ef0edff5690e4fe16941c190d9108
Abishek Das <abishekdascs@gmail.com>
2023-07-26 01:22:56 +0530

    Improve Default Sorting and Sorting Retention for Students List

--- app/assets/javascripts/reducers/users.js
+++ app/assets/javascripts/reducers/users.js
@@ -5,7 +5,7 @@ const initialState = {
   users: [],
   sort: {
     sortKey: null,
-    key: null,
+    key: 'last_name',
   },
   isLoaded: false,
   lastRequestTimestamp: 0 // UNIX timestamp of last request - in milliseconds
@@ -24,20 +24,32 @@ const SORT_DESCENDING = {
 export default function users(state = initialState, action) {
   switch (action.type) {
     case RECEIVE_USERS: {
-      // Get the sorting key from the Redux store, defaulting to 'last_name' if not set
-      const sort_key = state.sort.sortKey || 'last_name';
-      // Transform the user data to include 'first_name' and 'last_name' properties based on 'real_name'
-      const updatedUsers = action.data.course.users.map((user) => {
-        const [first_name, ...rest] = (user.real_name?.trim().toLowerCase() || '').split(' ');
-        return { ...user, first_name, last_name: rest.join(' ') };
-      }).sort((a, b) => {
-          // Compare the 'sort_key' properties (e.g., 'last_name') using 'localeCompare'
-          // If the 'sort_key' values are equal or falsy, compare 'username' as a fallback
-          return (a[sort_key] || '').localeCompare(b[sort_key] || '') || a.username.localeCompare(b.username);
-      });
+      // Get the sorting key from the Redux store
+      let sort_key = state.sort.key;
+      let updatedUsers;
+      // Get the user list from the action's data
+      const user_list = action.data.course.users;
+
+      // Check if at least one user has a 'real_name', if so, perform data transformation
+       if (user_list.some(user => user.real_name)) {
+         updatedUsers = user_list.map((user) => {
+          // Split 'real_name' into 'first_name' and the rest of the name ('rest')
+           const [first_name, ...rest] = (user.real_name?.trim().toLowerCase() || '').split(' ');
+           // Return the user object with updated 'first_name' and 'last_name' properties
+           return { ...user, first_name, last_name: rest.join(' ') };
+         });
+       } else {
+         // If no user has 'real_name', use the original user list as-is and sort by 'username'
+         updatedUsers = action.data.course.users;
+         sort_key = state.sort.key === 'last_name' ? 'username' : state.sort.key;
+        }
+
+      // Sort the 'updatedUsers' array based on the 'sort_key'
+      const sorted = sortByKey(updatedUsers, sort_key, null, SORT_DESCENDING[sort_key]);
+
     return {
       ...state,
-      users: updatedUsers,
+      users: sorted.newModels,
       isLoaded: true,
       lastRequestTimestamp: Date.now()
     };
