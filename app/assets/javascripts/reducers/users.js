67bfad7f0d3e89ca07dfc44e84fa6cf79710d0c9
Abishek Das <abishekdascs@gmail.com>
2023-07-26 15:35:28 +0530

    Refactor Code

--- app/assets/javascripts/reducers/users.js
+++ app/assets/javascripts/reducers/users.js
@@ -26,30 +26,31 @@ export default function users(state = initialState, action) {
     case RECEIVE_USERS: {
       // Get the sorting key from the Redux store
       let sort_key = state.sort.key;
-      let updatedUsers;
-      // Get the user list from the action's data
-      const user_list = action.data.course.users;
 
-      // Check if at least one user has a 'real_name', if so, perform data transformation
-       if (user_list.some(user => user.real_name)) {
-         updatedUsers = user_list.map((user) => {
-          // Split 'real_name' into 'first_name' and the rest of the name ('rest')
+       // Initialize a variable to hold the user list. If there are existing users in the state,
+      //  use that list; otherwise, use the user list from the action data.
+      let user_list = state.users.length ? state.users : action.data.course.users;
+
+       // Check if the user list is empty in the state and if any user in the action data has 'real_name'
+       if (!state.users.length && action.data.course.users.some(user => user.real_name)) {
+         // Perform data transformation on the user list: Split 'real_name' into 'first_name' and the rest of the name ('rest')
+         user_list = action.data.course.users.map((user) => {
            const [first_name, ...rest] = (user.real_name?.trim().toLowerCase() || '').split(' ');
-           // Return the user object with updated 'first_name' and 'last_name' properties
+            // Return the user object with updated 'first_name' and 'last_name' properties
            return { ...user, first_name, last_name: rest.join(' ') };
          });
-       } else {
-         // If no user has 'real_name', use the original user list as-is and sort by 'username'
-         updatedUsers = action.data.course.users;
-         sort_key = state.sort.key === 'last_name' ? 'username' : state.sort.key;
+       } else if (!state?.users.length) {
+         // If there are no users in the state and none of the users in the action data have 'real_name',
+        // set the sorting key to 'username'
+         sort_key = 'username';
         }
 
-      // Sort the 'updatedUsers' array based on the 'sort_key'
-      const sorted = sortByKey(updatedUsers, sort_key, null, SORT_DESCENDING[sort_key]);
+      // Sort the 'user_list' array based on the 'sort_key' if the user list is empty in the state.
+      if (!state.users.length) user_list = sortByKey(user_list, sort_key, null, SORT_DESCENDING[sort_key]);
 
     return {
       ...state,
-      users: sorted.newModels,
+      users: user_list.newModels ? user_list.newModels : user_list,
       isLoaded: true,
       lastRequestTimestamp: Date.now()
     };
