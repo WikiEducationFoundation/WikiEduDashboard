c12b296b9ac8b7708187fa88a7f22b5ad9fabd62
Abishek Das <abishekdascs@gmail.com>
2023-07-31 19:40:10 +0530

    Update course_page_spec test

--- spec/features/course_page_spec.rb
+++ spec/features/course_page_spec.rb
@@ -412,10 +412,10 @@ describe 'the course page', type: :feature, js: true do
     it 'shows a number of most recent revisions for a student' do
       js_visit "/courses/#{slug}/students"
       sleep 1
-      expect(page).to have_content(User.last.username)
+      expect(page).to have_content(User.last_name.username)
       student_row = 'table.users tbody tr.students:first-child'
       within(student_row) do
-        expect(page).to have_content User.first.username
+        expect(page).to have_content User.first_name.username
         within 'td:nth-of-type(4)' do
           expect(page.text).to eq('1')
         end
