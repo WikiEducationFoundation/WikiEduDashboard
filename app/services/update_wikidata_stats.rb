24c6871adc35f1b45aa66560076764a8f5020b9d
Sulagna Saha <saha23s@mtholyoke.edu>
2023-07-31 13:55:57 +0600

    solving linting problems

--- app/services/update_wikidata_stats.rb
+++ app/services/update_wikidata_stats.rb
@@ -7,7 +7,7 @@ require 'wikidata-diff-analyzer'
 class UpdateWikidataStats
   def initialize(course)
     @course = course
-    #import_summaries
+    # import_summaries
     update_summary_with_stats
     update_wikidata_statistics
   end
@@ -22,8 +22,10 @@ class UpdateWikidataStats
 
   def update_summary_with_stats
     return if wikidata_revisions_without_summaries.empty?
+
     revision_ids = wikidata_revisions_without_summaries.pluck(:mw_rev_id)
     analyzed_revisions = WikidataDiffAnalyzer.analyze(revision_ids)[:diffs]
+
     revision_ids.each do |rev_id|
       individual_stat = analyzed_revisions[rev_id]
       # Serialize the individual_stat to JSON format
@@ -36,19 +38,19 @@ class UpdateWikidataStats
       revision.update(summary: serialized_stat)
     end
   end
+
   def import_summaries
     return if wikidata_revisions_without_summaries.empty?
-    WikidataSummaryImporter.new.import_missing_summaries wikidata_revisions_without_summaries
+
+    WikidataSummaryImporter.new.import_missing_summaries(wikidata_revisions_without_summaries)
   end
 
   # Get Wikidata stats based on revision's summaries
 
   def update_wikidata_statistics
     return if course_revisions.empty?
+
     crs_stat = CourseStat.find_by(course_id: @course.id) || CourseStat.create(course_id: @course.id)
-    # crs_stat.stats_hash[wikidata.domain] = stats
-    # crs_stat.save
-    stats_hash = crs_stat.stats_hash || {}
 
     # Initialize arrays to store revisions with edit summaries and serialized stats
     revisions_with_summary = []
@@ -66,41 +68,57 @@ class UpdateWikidataStats
 
     summary_stats = WikidataSummaryParser.analyze_revisions(revisions_with_summary)
     serialized_stats = get_stats_from_serialized_stats(revisions_with_serialized_stats)
-    stats_hash = merge_stats(summary_stats, serialized_stats)
+    stats = merge_stats(summary_stats, serialized_stats)
     # Update the stats_hash in the CourseStat model and save it
-    crs_stat.stats_hash = stats_hash
+    crs_stat.stats_hash[wikidata.domain] = stats
     crs_stat.save
   end
 
   def merge_stats(summary_stats, serialized_stats)
     # Create a set of all unique keys from both 'summary_stats' and 'serialized_stats'
     all_keys = summary_stats.keys.concat(serialized_stats.keys).uniq
-  
+
     # Initialize a new hash to store the merged stats
     merged_stats = {}
-  
+
     # Iterate through all unique keys and add the values from 'summary_stats' and 'serialized_stats'
     all_keys.each do |key|
       summary_value = summary_stats[key].to_i # gracefully handle nil values
       serialized_value = serialized_stats[key].to_i
       merged_stats[key] = summary_value + serialized_value
     end
-  
+
     merged_stats
   end
-  
 
   def get_stats_from_serialized_stats(revisions_with_serialized_stats)
     # Initialize a hash with the same order of attributes like they exist in the serialized stats
-    strings = ['claims created', 'claims removed', 'claims changed', 'references added', 'references removed', 'references changed', 'qualifiers added', 'qualifiers removed', 'qualifiers changed', 'aliases added', 'aliases removed', 'aliases changed', 'labels added', 'labels removed', 'labels changed', 'descriptions added', 'descriptions removed', 'descriptions changed', 'interwiki links added', 'interwiki links removed', 'interwiki links updated', 'merged to', 'merged from', 'redirects created', 'reverts performed', 'restorations performed', 'items cleared', 'items created', 'lemmas added', 'lemmas removed', 'lemmas changed', 'forms added', 'forms removed', 'forms changed', 'senses added', 'senses removed', 'senses changed', 'properties created', 'lexeme items created', 'representations added', 'representations removed', 'representations changed', 'glosses added', 'glosses removed', 'glosses changed', 'form claims added', 'form claims removed', 'form claims changed', 'sense claims added', 'sense claims removed', 'sense claims changed']
-    stats = Hash[strings.map { |string| [string, 0] }]
+    strings = [
+    'claims created', 'claims removed', 'claims changed',
+    'references added', 'references removed', 'references changed',
+    'qualifiers added', 'qualifiers removed', 'qualifiers changed',
+    'aliases added', 'aliases removed', 'aliases changed',
+    'labels added', 'labels removed', 'labels changed',
+    'descriptions added', 'descriptions removed', 'descriptions changed',
+    'interwiki links added', 'interwiki links removed', 'interwiki links updated',
+    'merged to', 'merged from', 'redirects created',
+    'reverts performed', 'restorations performed', 'items cleared',
+    'items created', 'lemmas added', 'lemmas removed', 'lemmas changed',
+    'forms added', 'forms removed', 'forms changed',
+    'senses added', 'senses removed', 'senses changed',
+    'properties created', 'lexeme items created',
+    'representations added', 'representations removed', 'representations changed',
+    'glosses added', 'glosses removed', 'glosses changed',
+    'form claims added', 'form claims removed', 'form claims changed',
+    'sense claims added', 'sense claims removed', 'sense claims changed']
+    stats = strings.index_with { 0 }
 
-    # create a sum of stats after deserilizing the stats for each revision object
+    # create a sum of stats after deserializing the stats for each revision object
     revisions_with_serialized_stats.each do |revision|
       # Deserialize the summary field to get the stats
       deserialized_stat = revision.diff_stats
       # create a stats which sums up each field of the deserialized_stat and create a stats hash
-      deserialized_stat.each_with_index do |(key, value), index|
+      deserialized_stat.each_with_index do |(_key, value), index|
         stats[strings[index]] += value
       end
     end
@@ -119,3 +137,4 @@ class UpdateWikidataStats
     @wikidata ||= Wiki.get_or_create(language: nil, project: 'wikidata')
   end
 end
+
