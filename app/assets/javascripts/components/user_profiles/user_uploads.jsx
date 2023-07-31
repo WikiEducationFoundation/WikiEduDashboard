a8f78f4a3c8c568c3bbffc55ebca9b3060d4e90c
Amine Hassou <japnoo@gmail.com>
2023-07-31 21:43:42 +0100

    Converted UserUploads into functional component

--- app/assets/javascripts/components/user_profiles/user_uploads.jsx
+++ app/assets/javascripts/components/user_profiles/user_uploads.jsx
@@ -1,27 +1,24 @@
 import React from 'react';
-import createReactClass from 'create-react-class';
 import PropTypes from 'prop-types';
 import { GALLERY_VIEW } from '../../constants';
 import UploadList from '../uploads/upload_list.jsx';
 
 
-const UserUploads = createReactClass({
-  propTypes: {
-    uploads: PropTypes.array
-  },
-
-  render() {
-    let uploadList = (<UploadList uploads={this.props.uploads} view={GALLERY_VIEW} />);
-    if (this.props.uploads.length === 0) {
-      uploadList = (<span>{I18n.t('courses.user_uploads_none')}</span>);
-    }
-    return (
-      <div id="recent-uploads">
-        <h3>Recent Uploads</h3>
-        {uploadList}
-      </div>
-    );
+const UserUploads = ({ uploads }) => {
+  let uploadList = (<UploadList uploads={uploads} view={GALLERY_VIEW} />);
+  if (uploads.length === 0) {
+    uploadList = (<span>{I18n.t('courses.user_uploads_none')}</span>);
   }
-});
+  return (
+    <div id="recent-uploads">
+      <h3>Recent Uploads</h3>
+      {uploadList}
+    </div>
+  );
+};
+
+UserUploads.propTypes = {
+  uploads: PropTypes.array
+};
 
 export default UserUploads;
