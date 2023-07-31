828892b11bbeb24f8867a243fcdc614cb2f2c7a0
Amine Hassou <japnoo@gmail.com>
2023-07-31 22:00:00 +0100

    Converted UserTrainingStatus into functional component

--- app/assets/javascripts/components/user_profiles/user_training_status.jsx
+++ app/assets/javascripts/components/user_profiles/user_training_status.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import createReactClass from 'create-react-class';
 import TrainingStatus from '@components/students/shared/StudentList/StudentDrawer/TrainingStatus/TrainingStatus.jsx';
 
-const UserTrainingStatus = createReactClass({
-  propTypes: {
-    trainingModules: PropTypes.array.isRequired
-  },
-  render() {
-    let status;
-    if (this.props.trainingModules.length > 0) {
-      status = <TrainingStatus exercises={{}} trainingModules={this.props.trainingModules} />;
-    } else {
-      status = <span>{I18n.t('users.user_no_training_status')}</span>;
-    }
-    return (
-      <div id="training-status">
-        <h3>Training Status</h3>
-        {status}
-      </div>
-    );
+const UserTrainingStatus = ({ trainingModules }) => {
+  let status;
+  if (trainingModules.length > 0) {
+    status = <TrainingStatus exercises={{}} trainingModules={trainingModules} />;
+  } else {
+    status = <span>{I18n.t('users.user_no_training_status')}</span>;
   }
-});
+  return (
+    <div id="training-status">
+      <h3>Training Status</h3>
+      {status}
+    </div>
+  );
+};
+
+UserTrainingStatus.propTypes = {
+  trainingModules: PropTypes.array.isRequired
+};
 
 export default UserTrainingStatus;
