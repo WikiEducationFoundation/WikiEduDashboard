4e92e65d3e9712348350f27ad411a199cfbeb63f
Amine Hassou <japnoo@gmail.com>
2023-08-02 22:13:40 +0100

    Converted CustomLink into functional component

--- app/assets/javascripts/components/nav/CustomLink.jsx
+++ app/assets/javascripts/components/nav/CustomLink.jsx
@@ -1,31 +1,21 @@
-// Replacement for non-custom Link component. The react Link component doesn't highlight the active link if it is not handeled by react router, CustomLink is used to provide that functioning.
+// Replacement for non-custom Link component. The react Link component doesn't highlight the active link if it is not handled by react router
+// CustomLink is used to provide that functioning.
 import React from 'react';
-import createReactClass from 'create-react-class';
 import PropTypes from 'prop-types';
 
-const CustomLink = createReactClass({
-  displayName: 'CustomLink',
-
-  propTypes: {
-    to: PropTypes.string,
-    location: PropTypes.object,
-    name: PropTypes.string,
-    clickedElement: PropTypes.string,
-    target: PropTypes.string
-  },
-  isActive() {
-    let bool;
+const CustomLink = ({ clickedElement, to, target, name }) => {
+  const isActive = () => {
     const path = location.pathname.split('/')[1];
-    if (path === this.props.clickedElement) {
-      bool = true;
-    } else {
-      bool = false;
-    }
-    return bool;
-  },
-  render() {
-    return <a href = {this.props.to} className={this.isActive() ? 'active' : ''} target={this.props.target}> {this.props.name} </a>;
-  }
-});
+    return path === clickedElement;
+  };
+  return <a href={to} className={isActive() ? 'active' : ''} target={target}> {name} </a>;
+};
+
+CustomLink.propTypes = {
+  to: PropTypes.string,
+  name: PropTypes.string,
+  clickedElement: PropTypes.string,
+  target: PropTypes.string
+};
 
 export default CustomLink;
