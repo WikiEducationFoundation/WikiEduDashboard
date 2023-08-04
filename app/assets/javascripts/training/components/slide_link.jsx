d9c65d48b6538e6607f6cc491d5cc4803af42012
Amine Hassou <japnoo@gmail.com>
2023-08-04 21:36:07 +0100

    Converted SlideLink into functional component

--- app/assets/javascripts/training/components/slide_link.jsx
+++ app/assets/javascripts/training/components/slide_link.jsx
@@ -1,44 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import createReactClass from 'create-react-class';
 import { Link } from 'react-router-dom';
 
-const SlideLink = createReactClass({
-  displayName: 'SlideLink',
+const SlideLink = ({ button, params, slideId, onClick, disabled, buttonText }) => {
+  const linkClass = `slide-nav ${button ? 'btn btn-primary icon icon-rt_arrow' : ''}`;
+  const href = `/training/${params.library_id}/${params.module_id}/${slideId}`;
+  return (
+    <Link data-href={href} onClick={onClick} disabled={disabled} className={linkClass} to={href}>
+      {buttonText}
+    </Link>
+  );
+};
 
-  propTypes: {
-    button: PropTypes.bool,
-    disabled: PropTypes.bool,
-    buttonText: PropTypes.string,
-    params: PropTypes.object.isRequired,
-    slideId: PropTypes.string.isRequired,
-    onClick: PropTypes.func.isRequired
-  },
-
-  linkParams(props) {
-    return {
-      library_id: props.params.library_id,
-      module_id: props.params.module_id,
-      slide_id: props.slideId
-    };
-  },
-
-  _slideLink(props) {
-    return `/training/${props.library_id}/${props.module_id}/${props.slide_id}`;
-  },
-
-  render() {
-    const linkParams = this.linkParams(this.props);
-    const buttonClasses = ' btn btn-primary icon icon-rt_arrow';
-    let linkClass = 'slide-nav';
-    linkClass += this.props.button ? buttonClasses : '';
-    const href = this._slideLink(linkParams);
-    return (
-      <Link data-href={href} onClick={this.props.onClick} disabled={this.props.disabled} className={linkClass} to={href}>
-        {this.props.buttonText}
-      </Link>
-    );
-  }
-});
+SlideLink.propTypes = {
+  button: PropTypes.bool,
+  params: PropTypes.object.isRequired,
+  slideId: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+  buttonText: PropTypes.string,
+};
 
 export default SlideLink;
