731864cd52c890449d039457136b40653bddb543
Abishek Das <abishekdascs@gmail.com>
2023-08-10 11:42:34 +0530

    Remove previousSortKey state from redux store

--- app/assets/javascripts/reducers/users.js
+++ app/assets/javascripts/reducers/users.js
@@ -6,7 +6,6 @@ const initialState = {
   sort: {
     sortKey: null,
     key: null,
-    previousSortKey: null,
   },
   isLoaded: false,
   lastRequestTimestamp: 0 // UNIX timestamp of last request - in milliseconds
@@ -27,7 +26,7 @@ export default function users(state = initialState, action) {
     case RECEIVE_USERS: {
       // Get the sorting key from if available from Redux store or else use last_name
       let sort_key = state.sort.key || 'last_name';
-
+      let previousKey = null;
       // Initialize a variable to hold the user list.
       let user_list = action.data.course.users;
 
@@ -41,8 +40,10 @@ export default function users(state = initialState, action) {
           sort_key = 'username';
         }
 
+        if (state.sort.key) { previousKey = state.sort.sortKey ? null : sort_key; }
+
         // Sort the 'user_list' array based on the 'sort_key'
-        user_list = sortByKey(user_list, sort_key, state.sort.previousSortKey, SORT_DESCENDING[sort_key]);
+        user_list = sortByKey(user_list, sort_key, previousKey, SORT_DESCENDING[sort_key]);
     return {
       ...state,
       users: user_list.newModels, // Update 'users' with the sorted user list.
@@ -66,8 +67,6 @@ export default function users(state = initialState, action) {
         sort: {
           sortKey: sorted.newKey,
           key: action.key,
-          // store the previous sortKey to use if the instructor switches between tabs and comes back to the students/editors tab
-          previousSortKey: state.sort.sortKey,
         }
       };
     }
