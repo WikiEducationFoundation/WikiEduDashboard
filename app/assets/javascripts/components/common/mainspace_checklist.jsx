0d889d680671f7a638da450dd30482c1726f8c95
Amine Hassou <japnoo@gmail.com>
2023-08-10 01:23:14 +0100

    Converted MainspaceChecklist into functional component

--- app/assets/javascripts/components/common/mainspace_checklist.jsx
+++ app/assets/javascripts/components/common/mainspace_checklist.jsx
@@ -1,67 +1,57 @@
-import React from 'react';
-import createReactClass from 'create-react-class';
-import OnClickOutside from 'react-onclickoutside';
+import React, { useState } from 'react';
+import useOutsideClick from '../../hooks/useOutsideClick';
 
-const MainspaceChecklist = createReactClass({
-  displayName: 'MainspaceChecklist',
+const MainspaceChecklist = () => {
+  const [isVisible, setIsVisible] = useState(false);
 
-  getInitialState() {
-    return {
-      show: false
-    };
-  },
+  const show = () => {
+    setIsVisible(true);
+  };
 
-  show() {
-    this.setState({ show: true });
-  },
+  const hide = () => {
+    setIsVisible(false);
+  };
 
-  hide() {
-    this.setState({ show: false });
-  },
+  const ref = useOutsideClick(hide);
 
-  handleClickOutside() {
-    this.hide();
-  },
 
-  render() {
-    let button;
-    if (this.state.show) {
-      button = <button onClick={this.hide} className="button dark small">Okay</button>;
-    } else {
-      button = <a onClick={this.show} className="button dark small">Quality checklist</a>;
-    }
+  let button;
+  if (isVisible) {
+    button = <button onClick={hide} className="button dark small">Okay</button>;
+  } else {
+    button = <a onClick={show} className="button dark small">Quality checklist</a>;
+  }
 
-    let modal;
-    if (!this.state.show) {
-      modal = <div className="empty" />;
-    } else {
-      modal = (
-        <div className="article-viewer my-assignment-checklist">
-          <h2>Quality checklist</h2>
-          <p>
-            Before your article can become a live Wikipedia
-            article, make sure meets all these criteria.
-          </p>
-          <dl>
-            <dd><input type="checkbox" /> It starts with a clear definition of the topic, with the title in the first sentence in <strong>bold</strong>.</dd>
-            <dd><input type="checkbox" /> It has a lead section — which comes before any section headers — that provides an overview of the topic.</dd>
-            <dd><input type="checkbox" /> If it has additional sections after the lead, they have content, not just a placeholder.</dd>
-            <dd><input type="checkbox" /> The content has inline citations, not just a bibliography.</dd>
-            <dd><input type="checkbox" /> It has a &quot;References&quot; section after the body of the article.</dd>
-            <dd><input type="checkbox" /> All comments, notes, and outlines have been removed from the draft.</dd>
-          </dl>
-          {button}
-        </div>
-      );
-    }
-
-    return (
-      <div>
+  let modal;
+  if (!isVisible) {
+    modal = <div className="empty" />;
+  } else {
+    modal = (
+      <div ref={ref} className="article-viewer my-assignment-checklist">
+        <h2>Quality checklist</h2>
+        <p>
+          Before your article can become a live Wikipedia
+          article, make sure meets all these criteria.
+        </p>
+        <dl>
+          <dd><input type="checkbox" /> It starts with a clear definition of the topic, with the title in the first sentence in <strong>bold</strong>.</dd>
+          <dd><input type="checkbox" /> It has a lead section — which comes before any section headers — that provides an overview of the topic.</dd>
+          <dd><input type="checkbox" /> If it has additional sections after the lead, they have content, not just a placeholder.</dd>
+          <dd><input type="checkbox" /> The content has inline citations, not just a bibliography.</dd>
+          <dd><input type="checkbox" /> It has a &quot;References&quot; section after the body of the article.</dd>
+          <dd><input type="checkbox" /> All comments, notes, and outlines have been removed from the draft.</dd>
+        </dl>
         {button}
-        {modal}
       </div>
     );
   }
-});
 
-export default OnClickOutside(MainspaceChecklist);
+  return (
+    <div>
+      {button}
+      {modal}
+    </div>
+  );
+};
+
+export default MainspaceChecklist;
