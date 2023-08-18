ab2184e0af9e42851a57a4c9be93f9ef6e434d43
Amine Hassou <japnoo@gmail.com>
2023-08-18 23:28:28 +0100

    Converted Campaign into functional component + cleaned up related campaign components

--- app/assets/javascripts/components/campaign/campaign.jsx
+++ app/assets/javascripts/components/campaign/campaign.jsx
@@ -1,73 +1,55 @@
-import { Route, Routes } from 'react-router-dom';
-import withRouter from '../util/withRouter';
-import React from 'react';
-import createReactClass from 'create-react-class';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import { Route, Routes, useParams } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { getCampaign } from '../../actions/campaign_view_actions';
 import CampaignAlerts from '../alerts/campaign_alerts.jsx';
 import CampaignOresPlot from './campaign_ores_plot.jsx';
-import CampaignOverviewHandler from './campaign_overview_handler';
 import CampaignNavbar from '../common/campaign_navbar';
 import CampaignStats from './campaign_stats';
 import WikidataOverviewStats from '../common/wikidata_overview_stats';
+import CampaignStatsDownloadModal from './campaign_stats_download_modal';
 
-export const Campaign = createReactClass({
-  displayName: 'Campaign',
+export const Campaign = () => {
+  const dispatch = useDispatch();
+  const campaign = useSelector(state => state.campaign);
 
-  propTypes: {
-    campaign: PropTypes.object.isRequired,
-    match: PropTypes.object,
-  },
+  const { campaign_slug } = useParams();
 
-  componentDidMount() {
-    const campaignSlug = this.props.router.params.campaign_slug;
-    return this.props.getCampaign(campaignSlug);
-  },
+  useEffect(() => { dispatch(getCampaign(campaign_slug)); }, []);
 
-  render() {
-    if (this.props.campaign.loading) {
-      return <div />;
-    }
+  if (campaign.loading) {
+    return <div />;
+  }
 
-    let campaignHandler;
-    if (window.location.href.match(/overview/)) {
-      campaignHandler = (
-        <div className="high-modal">
-          <CampaignOverviewHandler {...this.props} />
-        </div>
-        );
-    }
-
-    return (
-      <div>
-        <CampaignNavbar
-          campaign={this.props.campaign}
-        />
-        <div className="container campaign_main">
-          <section className="overview container">
-            <CampaignStats campaign={this.props.campaign} />
-            {this.props.campaign.course_stats && <WikidataOverviewStats
-              statistics={this.props.campaign.course_stats['www.wikidata.org']}
-            />}
-          </section>
-          {campaignHandler}
-          <Routes>
-            <Route path="ores_plot" element={<CampaignOresPlot />} />
-            <Route path="alerts" element={<CampaignAlerts />} />
-          </Routes>
-        </div>
-      </div >
+  let campaignHandler;
+  if (window.location.href.match(/overview/)) {
+    campaignHandler = (
+      <div className="high-modal">
+        <CampaignStatsDownloadModal campaign_slug />
+      </div>
     );
   }
-});
 
-const mapStateToProps = state => ({
-  campaign: state.campaign,
-});
-
-const mapDispatchToProps = {
-  getCampaign
+  return (
+    <div>
+      <CampaignNavbar
+        campaign={campaign}
+      />
+      <div className="container campaign_main">
+        <section className="overview container">
+          <CampaignStats campaign={campaign} />
+          {campaign.course_stats && <WikidataOverviewStats
+            statistics={campaign.course_stats['www.wikidata.org']}
+          />}
+        </section>
+        {campaignHandler}
+        <Routes>
+          <Route path="ores_plot" element={<CampaignOresPlot />} />
+          <Route path="alerts" element={<CampaignAlerts />} />
+        </Routes>
+      </div>
+    </div >
+  );
 };
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Campaign));
+export default (Campaign);
--- app/assets/javascripts/components/campaign/campaign_overview_handler.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import CampaignStatsDownloadModal from './campaign_stats_download_modal';
-
-const CampaignOverviewHandler = (props) => {
-    return <CampaignStatsDownloadModal {...props} />;
-};
-
-export default CampaignOverviewHandler;
