8c8eaf70b6fd1b839e36602c316a3e05c661b9f3
Amine Hassou <japnoo@gmail.com>
2023-08-20 23:26:57 +0100

    Converted AssignmentList into functional component

--- app/assets/javascripts/components/assignments/assignment_list.jsx
+++ app/assets/javascripts/components/assignments/assignment_list.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import createReactClass from 'create-react-class';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { sortBy, groupBy, compact } from 'lodash-es';
 
@@ -8,87 +7,81 @@ import Assignment from './assignment.jsx';
 import { getFiltered } from '../../utils/model_utils.js';
 import ArticleUtils from '../../utils/article_utils.js';
 
-const AssignmentList = createReactClass({
-  displayName: 'AssignmentList',
-
-  propTypes: {
-    articles: PropTypes.array,
-    assignments: PropTypes.array,
-    course: PropTypes.object,
-    current_user: PropTypes.object,
-    wikidataLabels: PropTypes.object
-  },
-
-  componentDidMount() {
+const AssignmentList = ({ course, assignments, articles, current_user, wikidataLabels, }) => {
+  useEffect(() => {
     // sets the title of this tab
-    const project = this.props.course.home_wiki.project;
-    document.title = `${this.props.course.title} - ${ArticleUtils.I18n('assigned', project)}`;
-  },
+    const project = course.home_wiki.project;
+    document.title = `${course.title} - ${ArticleUtils.I18n('assigned', project)}`;
+  }, []);
 
-  hasAssignedUser(group) {
+  const hasAssignedUser = (group) => {
     return group.some((assignment) => {
       return assignment.user_id;
     });
-  },
+  };
 
-  render() {
-    const allAssignments = this.props.assignments;
-    const sortedAssignments = sortBy(allAssignments, assignment => assignment.article_title);
-    const grouped = groupBy(sortedAssignments, assignment => assignment.article_title);
-    let elements = Object.keys(grouped).map((title) => {
-      const group = grouped[title];
-      if (!this.hasAssignedUser(group)) { return null; }
-      const article = getFiltered(this.props.articles, { title })[0];
-      return (
-        <Assignment
-          key={group[0].id}
-          assignmentGroup={group}
-          article={article || null}
-          course={this.props.course}
-          current_user={this.props.current_user}
-          wikidataLabel={this.props.wikidataLabels[title]}
-        />
-      );
-    });
-    elements = compact(elements);
-
-    const keys = {
-      rating_num: {
-        label: I18n.t('articles.rating'),
-        desktop_only: true
-      },
-      title: {
-        label: I18n.t('articles.title'),
-        desktop_only: false
-      },
-      assignee: {
-        label: I18n.t('assignments.assignees'),
-        desktop_only: true
-      },
-      reviewer: {
-        label: I18n.t('assignments.reviewers'),
-        desktop_only: true
-      }
-    };
-
-    const project = this.props.course.home_wiki.project;
-
+  const sortedAssignments = sortBy(assignments, assignment => assignment.article_title);
+  const grouped = groupBy(sortedAssignments, assignment => assignment.article_title);
+  let elements = Object.keys(grouped).map((title) => {
+    const group = grouped[title];
+    if (!hasAssignedUser(group)) { return null; }
+    const article = getFiltered(articles, { title })[0];
     return (
-      <div id="assignments" className="mt4">
-        <div className="section-header">
-          <h3>{ArticleUtils.I18n('assigned', project)}</h3>
-        </div>
-        <List
-          elements={elements}
-          keys={keys}
-          table_key={'assignments'}
-          none_message={ArticleUtils.I18n('assignments_none', project)}
-          sortable={false}
-        />
-      </div>
+      <Assignment
+        key={group[0].id}
+        assignmentGroup={group}
+        article={article || null}
+        course={course}
+        current_user={current_user}
+        wikidataLabel={wikidataLabels[title]}
+      />
     );
-  }
-}
-);
+  });
+  elements = compact(elements);
+
+  const keys = {
+    rating_num: {
+      label: I18n.t('articles.rating'),
+      desktop_only: true
+    },
+    title: {
+      label: I18n.t('articles.title'),
+      desktop_only: false
+    },
+    assignee: {
+      label: I18n.t('assignments.assignees'),
+      desktop_only: true
+    },
+    reviewer: {
+      label: I18n.t('assignments.reviewers'),
+      desktop_only: true
+    }
+  };
+
+  const project = course.home_wiki.project;
+
+  return (
+    <div id="assignments" className="mt4">
+      <div className="section-header">
+        <h3>{ArticleUtils.I18n('assigned', project)}</h3>
+      </div>
+      <List
+        elements={elements}
+        keys={keys}
+        table_key={'assignments'}
+        none_message={ArticleUtils.I18n('assignments_none', project)}
+        sortable={false}
+      />
+    </div>
+  );
+};
+
+AssignmentList.propTypes = {
+  articles: PropTypes.array,
+  assignments: PropTypes.array,
+  course: PropTypes.object,
+  current_user: PropTypes.object,
+  wikidataLabels: PropTypes.object
+};
 
 export default AssignmentList;
