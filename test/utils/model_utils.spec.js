d0af7c1d3a3413aa72aa7d96a32cf77c670def72
Abishek Das <abishekdascs@gmail.com>
2023-08-24 19:12:55 +0530

    Fix Test

--- test/utils/model_utils.spec.js
+++ test/utils/model_utils.spec.js
@@ -1,25 +1,21 @@
 import { transformUsers } from '../../app/assets/javascripts/utils/model_utils';
 
-describe('transformUsers', () => {
-  it('transforms user names correctly', () => {
+describe('transformUsers function', () => {
+  it('transforms users with real names and maintains sorting order', () => {
     const users = [
-      { real_name: 'John Doe Smith' },
-      { real_name: 'Bob Charles Johnson' },
-      { real_name: 'Alice' },
-      { real_name: 'Smith Time' },
-      { real_name: '' },
+      { id: 1, real_name: 'John Doe' },
+      { id: 2, real_name: 'Alice Mary Smith' },
+      { id: 3, real_name: 'Bob Johnson' },
+      { id: 4 },
     ];
 
     const transformedUsers = transformUsers(users);
 
-    const expectedTransformedUsers = [
-      { real_name: 'John Doe Smith', first_name: 'John', last_name: 'Smith' },
-      { real_name: 'Bob Charles Johnson', first_name: 'Bob', last_name: 'Johnson' },
-      { real_name: 'Alice', first_name: 'Alice' },
-      { real_name: 'Smith Time', first_name: 'Smith', last_name: 'Time' },
-      { real_name: '' },
-    ];
-
-    expect(transformedUsers).toEqual(expectedTransformedUsers);
+    expect(transformedUsers).toEqual([
+      { id: 1, real_name: 'John Doe', first_name: 'John', last_name: 'Doe' },
+      { id: 2, real_name: 'Alice Mary Smith', first_name: 'Alice', last_name: 'Smith' },
+      { id: 3, real_name: 'Bob Johnson', first_name: 'Bob', last_name: 'Johnson' },
+      { id: 4 },
+    ]);
   });
 });
