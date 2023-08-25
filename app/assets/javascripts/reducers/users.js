139d5ff2378f976a43ec1857ca4c8f365052decc
Abishek Das <abishekdascs@gmail.com>
2023-08-25 11:44:07 +0530

    Refactor code for sorting logic

--- app/assets/javascripts/reducers/users.js
+++ app/assets/javascripts/reducers/users.js
@@ -24,33 +24,19 @@ const SORT_DESCENDING = {
 export default function users(state = initialState, action) {
   switch (action.type) {
     case RECEIVE_USERS: {
-      // Get the sorting key if available from Redux store or else use last_name
-      let sort_key = state.sort.key || 'last_name';
-      /* Use the previousKey to determine the directions of sorting.
-         Note: Direction of the sorting only matter after the user/instructor sort the students/editors
-         list using the dropdown menu or one of the student/editors header and then navigates between
-         tabs and then a data refresh occurs(usually after one minute).
-      */
-      let previousKey = null;
-      // Initialize a variable to hold the user list.
-      let user_list = action.data.course.users;
+      // Transform the 'real_name' in 'action' into separate 'first_name' and 'last_name' properties
+      // if 'real_name' is available by using transformUsers.
+      let user_list = transformUsers(action.data.course.users);
 
-      // Transform the 'real_name' in user_list into separate 'first_name' and 'last_name' properties
-      // if 'real_name' is available by using transformUsers
-      user_list = transformUsers(user_list);
+      // Get the sorting 'key' if available from Redux store or else use last_name or
+      // username as fallback if last_name not available.
+      const sort_key = state.sort.key || (!user_list.some(user => user.last_name) ? 'username' : 'last_name');
 
-      // If there are no users with last_name and key in the store is null then set sort_key by username
-      if (!state.sort.key && !user_list.some(user => user.last_name)) {
-        sort_key = 'username';
-      }
-
-      /* If 'key' in the store is not null which implies instructor/user had sort the student/editors list either using
-      the dropdown menu or one of the student/editor tab and if 'sortKey' state is null it means sorting was done in
-      reverse so set the previousKey to the 'sort_key' or else null. */
-      if (state.sort.key) { previousKey = state.sort.sortKey ? null : sort_key; }
+      // Determine if sorting direction should be in reversed or not
+      const isReversed = (state.sort.key && !state.sort.sortKey) ? sort_key : null;
 
       // Sort the 'user_list' array based on the 'sort_key'
-      user_list = sortByKey(user_list, sort_key, previousKey, SORT_DESCENDING[sort_key]);
+      user_list = sortByKey(user_list, sort_key, isReversed, SORT_DESCENDING[sort_key]);
 
     return {
       ...state,
