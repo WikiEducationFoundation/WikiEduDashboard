75129a562881e26ed6935af8101635de2a8a8f31
gabina <gabinaluz@gmail.com>
2023-10-08 01:39:01 -0300

    Small stylistic change in controller

--- app/controllers/training_modules_controller.rb
+++ app/controllers/training_modules_controller.rb
@@ -13,15 +13,10 @@ class TrainingModulesController < ApplicationController
   end
 
   def find
-    training_module = TrainingModule.find_by(id: params[:module_id])
-
-    raise ActiveRecord::RecordNotFound unless training_module
-
-    training_module_slug = "%slug: " + training_module.slug + "\n%"
+    training_module = TrainingModule.find(params[:module_id])
+    training_module_slug = "%slug: #{training_module.slug}\n%"
     training_library = TrainingLibrary.find_by("categories LIKE ?", training_module_slug)
-
     raise ActiveRecord::RecordNotFound unless training_library
-
     redirect_to "/training/#{training_library.slug}/#{training_module.slug}"
   end
 end
