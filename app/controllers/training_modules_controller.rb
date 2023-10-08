dcfd285eab03939165ef51ee2a87e6491077d832
gabina <gabinaluz@gmail.com>
2023-10-08 00:34:05 -0300

    Add a new find_training_module route and its corresponding controller to be able to reach a training module by their id

--- app/controllers/training_modules_controller.rb
+++ app/controllers/training_modules_controller.rb
@@ -11,4 +11,17 @@ class TrainingModulesController < ApplicationController
   def show
     @training_module = TrainingModule.find_by(slug: params[:module_id])
   end
+
+  def find
+    training_module = TrainingModule.find_by(id: params[:module_id])
+
+    raise ActiveRecord::RecordNotFound unless training_module
+
+    training_module_slug = "%slug: " + training_module.slug + "\n%"
+    training_library = TrainingLibrary.find_by("categories LIKE ?", training_module_slug)
+
+    raise ActiveRecord::RecordNotFound unless training_library
+
+    redirect_to "/training/#{training_library.slug}/#{training_module.slug}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -328,6 +328,8 @@ Rails.application.routes.draw do
   get 'training_modules' => 'training_modules#index'
   get 'training_module' => 'training_modules#show'
 
+  # To find training modules by id
+  get 'find_training_module/:module_id' => 'training_modules#find'
 
   # Misc
   # get 'courses' => 'courses#index'
