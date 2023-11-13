075111c29fc22493bc08d4c409f0df4a5adb6791
Sage Ross <ragesoss@gmail.com>
2023-11-13 13:53:21 -0800

    Add several new languages

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -38,12 +38,14 @@ class Wiki < ApplicationRecord
   ].freeze
   validates_inclusion_of :project, in: PROJECTS
 
+  # One place to look for recently-added languages:
+  # https://incubator.wikimedia.org/wiki/Incubator:News
   LANGUAGES = %w[
     aa ab ace ady af ak als alt am ami an ang ar arc ary arz as ast atj av avk ay awa az azb
-    ba ban bar bat-smg bcl be be-tarask be-x-old bg bh bi bjn blk bm bn bo bpy br bs
+    ba ban bar bat-smg bbc bcl be be-tarask be-x-old bg bh bi bjn blk bm bn bo bpy br bs btm
     bug bxr ca cbk-zam cdo ce ceb ch cho chr chy ckb cmn co commons cr crh cs csb cu
-    cv cy da dag de din diq dk dsb dty dv dz ee egl el eml en eo epo es et eu ext fa fat
-    ff fi fiu-vro fj fo fon fr frp frr fur fy ga gag gan gcr gd gl glk gn gom gor got gsw
+    cv cy da dag de dga din diq dk dsb dty dv dz ee egl el eml en eo epo es et eu ext fa fat
+    ff fi fiu-vro fj fo fon fr frp frr fur fy ga gag gan gcr gd gl glk gn gom gor got gpe gsw
     gu guc gur guw gv ha hak haw he hi hif ho hr hsb ht hu hy hyw hz ia id ie ig ii ik ilo
     incubator inh io is it iu ja jam jbo jp jv ka kaa kab kcg kbd kbp kg ki kj kk kl km kn ko
     koi kr krc ks ksh ku kv kw ky la lad lb lbe lez lfn lg li lij lld lmo ln lo lrc lt
@@ -52,9 +54,9 @@ class Wiki < ApplicationRecord
     nso nv ny oc olo om or os pa pag pam pap pcd pcm pdc pfl pi pih pl pms pnb pnt ps
     pt pwn qu rm rmy rn ro roa-rup roa-tara ru rue rup rw sa sah sat sc scn sco sd se
     sg sgs sh shi shn shy si simple sk skr sl sm smn sn so sq sr srn ss st stq su sv sw szl
-    szy ta tay tcy te tet tg th ti tk tl tn to tpi tr trv ts tt tum tw ty tyv udm ug uk
+    szy ta tay tcy te tet tg th ti tk tl tly tn to tpi tr trv ts tt tum tw ty tyv udm ug uk
     ur uz ve vec vep vi vls vo vro w wa war wikipedia wo wuu xal xh xmf yi yo yue za
-    zea zh zh-cfr zh-classical zh-cn zh-min-nan zh-tw zh-yue zu
+    zea zgh zh zh-cfr zh-classical zh-cn zh-min-nan zh-tw zh-yue zu
   ].freeze
   validates_inclusion_of :language, in: LANGUAGES + [nil]
 
