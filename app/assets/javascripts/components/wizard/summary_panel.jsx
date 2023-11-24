8f223ea80a8ea44b4162b4c605d8a62f1f2d4c39
ujjwalpathaak <pathak2002ujjwal@gmail.com>
2023-11-24 11:51:16 +0530

    converting summary panel to func component

--- app/assets/javascripts/components/wizard/summary_panel.jsx
+++ app/assets/javascripts/components/wizard/summary_panel.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import createReactClass from 'create-react-class';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import withRouter from '../util/withRouter';
 import Panel from './panel.jsx';
@@ -7,78 +6,96 @@ import Panel from './panel.jsx';
 const answersFromPanels = (panels) => {
   const answers = [];
   panels.forEach((panel, i) => {
-    if (i === panels.length - 1) { return; }
+    if (i === panels.length - 1) {
+      return;
+    }
     const answer = { title: panel.title, selections: [] };
     if (panel.options !== undefined && panel.options.length > 0) {
-      panel.options.map((option) => {
-        if (option.selected) { return answer.selections.push(option.title); }
-        return undefined;
+      panel.options.forEach((option) => {
+        if (option.selected) {
+          answer.selections.push(option.title);
+        }
       });
-      if (answer.selections.length === 0) { answer.selections = ['No selections']; }
+      if (answer.selections.length === 0) {
+        answer.selections = ['No selections'];
+      }
     }
-    return answers.push(answer);
+    answers.push(answer);
   });
   return answers;
 };
 
-const SummaryPanel = createReactClass({
-  displayName: 'SummaryPanel',
+const SummaryPanel = ({ courseId, course, panels, submitWizard, goToWizard, router, active }) => {
+  useEffect(() => {
+    const handleBeforeUnload = () => {
+      window.onbeforeunload = window.onbeforeunloadcache;
+    };
 
-  propTypes: {
-    courseId: PropTypes.string,
-    course: PropTypes.object.isRequired,
-    panels: PropTypes.array.isRequired
-  },
+    return () => {
+      window.onbeforeunload = handleBeforeUnload;
+    };
+  }, []);
 
-  submit() {
-    this.props.submitWizard(this.props.courseId);
-    window.onbeforeunload = window.onbeforeunloadcache;
-    return this.props.router.navigate(`/courses/${this.props.courseId}/timeline`);
-  },
-  rewind(toIndex) {
-    this.props.goToWizard(toIndex);
+  const submit = () => {
+    submitWizard(courseId);
+    router.navigate(`/courses/${courseId}/timeline`);
+  };
+
+  const rewind = (toIndex) => {
+    goToWizard(toIndex);
     window.location.hash = `step${toIndex + 1}`; // Sync Step Changes
     document.title = document.title.replace(/\d+$/, toIndex + 1); // Sync Title
-  },
-  render() {
-    let answers = [];
-    if (this.props.active) {
-      answers = answersFromPanels(this.props.panels);
-    }
-    const rawOptions = answers.map((answer, i) => {
-      // summary of the Course Dates panel
-      let details;
-      if (i === 0) {
-        details = [
-          <p key={'course_dates_summary'}>
-            {I18n.t('timeline.course_start')} — {this.props.course.start} <br />
-            {I18n.t('timeline.course_end')} — {this.props.course.end} <br />
-            {I18n.t('courses.assignment_start')} — {this.props.course.timeline_start} <br />
-            {I18n.t('courses.assignment_end')} — {this.props.course.timeline_end}
-          </p>
-        ];
-      } else {
-        details = answer.selections.map((selection, j) => <p key={`detail${i}${j}`}>{selection}</p>);
-      }
-      return (
-        <button key={`answer${i}`} className="wizard__option summary" onClick={this.rewind.bind(this, i)}>
-          <h3>{answer.title}</h3>
-          {details}
-          <p className="edit">Edit</p>
-        </button>
+  };
+
+  let answers = [];
+  if (active) {
+    answers = answersFromPanels(panels);
+  }
+
+  const rawOptions = answers.map((answer, i) => {
+    let details;
+    if (i === 0) {
+      details = (
+        <p key={'course_dates_summary'}>
+          {I18n.t('timeline.course_start')} — {course.start} <br />
+          {I18n.t('timeline.course_end')} — {course.end} <br />
+          {I18n.t('courses.assignment_start')} — {course.timeline_start} <br />
+          {I18n.t('courses.assignment_end')} — {course.timeline_end}
+        </p>
       );
+    } else {
+      details = answer.selections.map((selection, j) => <p key={`detail${i}${j}`}>{selection}</p>);
     }
-    );
 
     return (
-      <Panel
-        {...this.props}
-        advance={this.submit}
-        raw_options={rawOptions}
-        button_text="Generate Timeline"
-      />
+      <button key={`answer${i}`} className="wizard__option summary" onClick={() => rewind(i)}>
+        <h3>{answer.title}</h3>
+        {details}
+        <p className="edit">Edit</p>
+      </button>
     );
-  }
-});
+  });
+
+  return (
+    <Panel
+      courseId={courseId}
+      course={course}
+      panels={panels}
+      advance={submit}
+      raw_options={rawOptions}
+      button_text="Generate Timeline"
+    />
+  );
+};
+
+SummaryPanel.propTypes = {
+  courseId: PropTypes.string,
+  course: PropTypes.object.isRequired,
+  panels: PropTypes.array.isRequired,
+  submitWizard: PropTypes.func.isRequired,
+  goToWizard: PropTypes.func.isRequired,
+  router: PropTypes.object.isRequired,
+  active: PropTypes.bool.isRequired,
+};
 
 export default withRouter(SummaryPanel);
