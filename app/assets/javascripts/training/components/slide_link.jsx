e89c15005587f674294cb8d363c6728d44ac4dcf
ujjwalpathaak <pathak2002ujjwal@gmail.com>
2023-12-04 15:03:23 +0530

    fix/test failing

--- app/assets/javascripts/training/components/slide_link.jsx
+++ app/assets/javascripts/training/components/slide_link.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const SlideLink = ({ button, params, slideId, onClick, disabled, buttonText }) => {
-  const linkClass = `slide-nav ${button ? 'btn btn-primary' : ''}`;
+  const linkClass = `slide-nav ${button ? 'btn btn-primary next-button' : ''}`;
   const href = `/training/${params.library_id}/${params.module_id}/${slideId}`;
   return (
     <Link data-href={href} onClick={onClick} disabled={disabled} className={linkClass} to={href}>
