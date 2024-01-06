bdb0a29d0bb9ffdaa1e39d6ab1522d30e916068e
Sage Ross <ragesoss@gmail.com>
2024-01-05 16:03:45 -0800

    Add script for getting pages that ended up in draft space

--- docs/analytics_scripts/draft_space_per_term.rb
+++ docs/analytics_scripts/draft_space_per_term.rb
@@ -1,3 +1,5 @@
+# Get stats on how much per term has ended up in draft space
+
 stats = {}
 Campaign.all.each do |campaign|
   puts campaign.slug
@@ -9,3 +11,21 @@ Campaign.all.each do |campaign|
 end
 
 puts stats
+
+# Get the titles of all the edited articles for a term that ended up in Draft space
+
+term = Campaign.find_by_slug 'fall_2023'
+drafts = []
+
+term.courses.each do |course|
+  pp course.slug
+  article_ids = course.revisions.pluck(:article_id).uniq
+  drafts << Article.where(id: article_ids, namespace: 118).pluck(:title)
+end
+
+drafts.flatten!
+puts drafts.count
+
+File.open("/home/sage/drafts_2023.txt", "w+") do |f| 
+  f.puts(drafts) 
+end
