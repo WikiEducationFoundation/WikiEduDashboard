9a51c2734f5949910503b050d565b5924ac4d56d
Abishek Das <abishekdascs@gmail.com>
2024-01-21 16:33:16 +0530

    Implement NotesHandler component for course notes management
    This commit introduces the 'NotesHandler' component, responsible for managing the display and interaction flow of course notes. It utilizes state management to handle different modal types, allowing users to view and edit course notes. The component includes integration with 'NotesPanel' and 'NotesEditor' for displaying notes and enabling note editing, respectively.

---
+++ app/assets/javascripts/components/notes/notes_handler.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { nameHasChanged } from '../../actions/course_actions';
+import { resetCourseNote, persistCourseNote } from '../../actions/course_notes_action';
+import { getCurrentUser } from '../../selectors/index';
+import NotesEditor from './notes_editor';
+import NotesPanel from './notes_panel';
+
+const NotesHandler = () => {
+  const [modalType, setModalType] = useState(null);
+  const [noteId, setNoteId] = useState(null);
+  const course = useSelector(state => state.course);
+  const currentUser = useSelector(getCurrentUser);
+  const dispatch = useDispatch();
+
+  const dispatchNameHasChanged = () => {
+    dispatch(nameHasChanged());
+  };
+  const dispatchPersistCourseNote = () => {
+    dispatch(persistCourseNote(course.id, currentUser.username));
+  };
+  const dispatchResetCourseNote = () => {
+    dispatch(resetCourseNote());
+  };
+
+  const setState = (id = null, type = 'DefaultPanel') => {
+    setNoteId(id);
+    setModalType(type);
+  };
+
+  const defaultAdminNotesPanel = (
+    <NotesPanel
+      setState={setState}
+      modalType={modalType}
+      currentUser={currentUser}
+      courseId={course.id}
+      buttonText={'notes.admin.button_text'}
+      headerText={'notes.admin.header_text'}
+    />
+  );
+
+  const AdminNotesEditPanel = (
+    <NotesEditor
+      title={course.title}
+      course_id={course.slug}
+      current_user={currentUser}
+      note_id={noteId}
+      resetState={dispatchResetCourseNote}
+      persistCourse={dispatchPersistCourseNote}
+      nameHasChanged={dispatchNameHasChanged}
+      setState={setState}
+    />
+  );
+
+  switch (modalType) {
+    case 'NoteEditor':
+      return AdminNotesEditPanel;
+    default:
+      return defaultAdminNotesPanel;
+  }
+};
+
+export default (NotesHandler);
