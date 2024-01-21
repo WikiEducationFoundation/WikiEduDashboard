d41cd1390f57c878e54bd3ce7122303322c5305b
Abishek Das <abishekdascs@gmail.com>
2024-01-21 15:55:17 +0530

    Introduce NotesPanel and NotesPanelEditButton components
    This commit adds the 'NotesPanel' component, responsible for managing a panel displaying notes for a course. It fetches and updates the list of notes and includes an edit button. The 'NotesPanelEditButton' component is introduced to handle creating new and deleting individual notes within the panel.

---
+++ app/assets/javascripts/components/notes/notes_panel.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchAllCourseNotes } from '../../actions/course_notes_action';
+import NotesPanelEditButton from './notes_panel_edit_button';
+import NotesList from './notes_list';
+
+const NotesPanel = ({ setState, modalType, currentUser, courseId, buttonText, headerText }) => {
+  const notesList = useSelector(state => state.courseNotes.notes_list);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchData = () => {
+      dispatch(fetchAllCourseNotes(courseId));
+    };
+
+    fetchData();
+
+    const pollInterval = setInterval(fetchData, 60000);
+
+    return () => clearInterval(pollInterval);
+  }, []);
+
+  if (modalType !== 'DefaultPanel' && modalType === null) {
+    return <button onClick={() => setState(null)} className="button">{I18n.t(buttonText)}</button>;
+  }
+
+  return (
+    <div className="basic-modal">
+      <button onClick={() => setState(undefined, null)} className="pull-right article-viewer-button icon-close" />
+      <div className="list__wrapper">
+        <div className="section-header">
+          <h3>{I18n.t(headerText)}</h3>
+        </div>
+        <div className="users-control" style={{ marginBottom: '30px' }}>
+          <NotesPanelEditButton setState={setState} currentUser={currentUser} notesList={notesList} />
+        </div>
+        <NotesList setState={setState} courseId={courseId} notesList={notesList}/>
+      </div>
+    </div>
+   );
+};
+
+export default NotesPanel;
