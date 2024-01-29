f33fdd9d11a1e76b61981983b0e6130feea42d31
fillingtothemomo <rockingpenny4@gmail.com>
2024-01-30 01:31:18 +0530

    fixes lint issue

--- app/assets/javascripts/components/common/campaign_navbar.jsx
+++ app/assets/javascripts/components/common/campaign_navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import CourseUtils from '../../utils/course_utils';
