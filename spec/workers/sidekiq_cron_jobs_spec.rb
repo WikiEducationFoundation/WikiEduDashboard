784693a4e68bc997843239b1cab150d29d13ea79
gabina <gabinaluz@gmail.com>
2024-02-07 20:58:03 -0300

    Schedule default campaign update at the beginning of spring and fall (northern hemisphere)

--- spec/workers/sidekiq_cron_jobs_spec.rb
+++ spec/workers/sidekiq_cron_jobs_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 require "#{Rails.root}/app/workers/daily_update_worker"
 require "#{Rails.root}/app/workers/survey_update_worker"
 require "#{Rails.root}/app/workers/ticket_notifications_worker"
+require "#{Rails.root}/app/workers/default_campaign_update_worker.rb"
 
 describe 'workers scheduled via sidekiq-cron' do
   it 'run daily updates' do
@@ -19,4 +20,21 @@ describe 'workers scheduled via sidekiq-cron' do
     expect(TicketNotificationEmails).to receive(:notify)
     TicketNotificationsWorker.perform_async
   end
+
+  context 'when WikiEd Feature enabled' do
+    before { allow(Features).to receive(:wiki_ed?).and_return(true) }
+    it 'run default campaign update' do
+      expect(DefaultCampaignUpdate).to receive(:new)
+      DefaultCampaignUpdateWorker.perform_async
+    end
+  end
+
+  context 'when WikiEd Feature disabled' do
+    before { allow(Features).to receive(:wiki_ed?).and_return(false) }
+    it 'run default campaign update' do
+      expect(DefaultCampaignUpdate).not_to receive(:new)
+      DefaultCampaignUpdateWorker.perform_async
+    end
+  end
+
 end
