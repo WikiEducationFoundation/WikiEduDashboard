06abf56e69b5b3a1bf420f671e12242e204f6cd0
Pranjal Rajput <pranjal.rajput.che21@itbhu.ac.in>
2024-02-07 12:51:01 +0530

    Refactored Block.jsx

--- app/assets/javascripts/components/timeline/block.jsx
+++ app/assets/javascripts/components/timeline/block.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import createReactClass from 'create-react-class';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-
 import TextInput from '../common/text_input.jsx';
 import DatePicker from '../common/date_picker.jsx';
 import TextAreaInput from '../common/text_area_input.jsx';
@@ -12,83 +10,54 @@ import TrainingModulesViewMode from './TrainingModules/TrainingModulesViewMode.j
 import { BLOCK_KIND_RESOURCES } from '../../constants';
 import { initiateConfirm } from '../../actions/confirm_actions.js';
 
-const DEFAULT_POINTS = 10;
-
-const Block = createReactClass({
-  displayName: 'Block',
-
-  propTypes: {
-    block: PropTypes.object,
-    editableBlockIds: PropTypes.array,
-    editPermissions: PropTypes.bool,
-    saveBlockChanges: PropTypes.func,
-    cancelBlockEditable: PropTypes.func,
-    updateBlock: PropTypes.func,
-    toggleFocused: PropTypes.func,
-    isDragging: PropTypes.bool,
-    all_training_modules: PropTypes.array,
-    weekStart: PropTypes.object,
-    trainingLibrarySlug: PropTypes.string.isRequired,
-    current_user: PropTypes.object,
-    initiateConfirm: PropTypes.func.isRequired
-  },
-
-  updateBlock(valueKey, value) {
-    const toPass = { ...this.props.block };
+const Block = (props) => {
+const updateBlock = (valueKey, value) => {
+    const toPass = { ...props.block };
     toPass[valueKey] = value;
     delete toPass.deleteBlock;
-    return this.props.updateBlock(toPass);
-  },
+    return props.updateBlock(toPass);
+  };
 
-  passedUpdateBlock(selectedIds) {
-    const newBlock = Object.assign({}, this.props.block);
+  const passedUpdateBlock = (selectedIds) => {
+    const newBlock = Object.assign({}, props.block);
     newBlock.training_module_ids = selectedIds;
-    return this.props.updateBlock(newBlock);
-  },
+    return props.updateBlock(newBlock);
+  };
 
-  deleteBlock() {
+  const deleteBlocks = () => {
     const confirmMessage = 'Are you sure you want to delete this block? This will delete the block and all of its content.\n\nThis cannot be undone.';
-    const deleteBlock = this.props.deleteBlock;
-    const onConfirm = () => deleteBlock(this.props.block.id);
-    return this.props.initiateConfirm({ confirmMessage, onConfirm });
-  },
+    const deleteBlock = props.deleteBlock;
+    const onConfirm = () => deleteBlock(props.block.id);
+    return props.initiateConfirm({ confirmMessage, onConfirm });
+  };
 
-  _setEditable() {
-    return this.props.setBlockEditable(this.props.block.id);
-  },
+  const _setEditable = () => {
+    return props.setBlockEditable(props.block.id);
+  };
 
-  _isEditable() {
-    if (this.props.editableBlockIds) {
-      return this.props.editableBlockIds.indexOf(this.props.block.id) >= 0;
+  const _isEditable = () => {
+    if (props.editableBlockIds) {
+      return props.editableBlockIds.indexOf(props.block.id) >= 0;
     }
     return false;
-  },
+  };
 
-  _hidden() {
+  const _hidden = () => {
     // Resources blocks are hidden on the timeline, except for instructors and admins.
     // They show up in the Resources tab instead, where there is no Week and so no weekStart.
-    return this.props.weekStart && !this.props.editPermissions && this.props.block.kind === BLOCK_KIND_RESOURCES;
-  },
+    return props.weekStart && !props.editPermissions && props.block.kind === BLOCK_KIND_RESOURCES;
+  };
 
-  updateGradeable(_valueKey, value) {
-    if (value) {
-      this.updateBlock('points', DEFAULT_POINTS);
-    } else {
-      this.updateBlock('points', null);
-    }
-  },
-
-  render() {
-    const block = this.props.block;
-    const isEditable = this._isEditable();
-    const isStudent = this.props.current_user && this.props.current_user.isStudent;
-    if (this._hidden()) { return null; }
+    const block = props.block;
+    const isEditable = _isEditable();
+    const isStudent = props.current_user && props.current_user.isStudent;
+    if (_hidden()) { return null; }
 
     let className = 'block';
     className += ` block-kind-${block.kind}`;
 
     let blockTypeClassName = 'block__block-type';
-    if (this.props.editPermissions) {
+    if (props.editPermissions) {
       blockTypeClassName += ' editable';
     }
 
@@ -96,8 +65,8 @@ const Block = createReactClass({
     if (isEditable) {
       blockActions = (
         <div className="float-container block__block-actions">
-          <button onClick={this.props.saveBlockChanges.bind(null, block.id)} className="button dark pull-right no-clear">Save</button>
-          <span role="button" tabIndex={0} onClick={this.props.cancelBlockEditable.bind(null, block.id)} className="span-link pull-right no-clear">Cancel</span>
+          <button onClick={props.saveBlockChanges.bind(null, block.id)} className="button dark pull-right no-clear">Save</button>
+          <span role="button" tabIndex={0} onClick={props.cancelBlockEditable.bind(null, block.id)} className="span-link pull-right no-clear">Cancel</span>
         </div>
       );
     }
@@ -106,14 +75,14 @@ const Block = createReactClass({
     if (block.due_date !== null && block.kind === 1) {
       dueDateRead = (
         <TextInput
-          onChange={this.updateBlock}
+          onChange={updateBlock}
           value={block.due_date}
           value_key={'due_date'}
           editable={false}
           label="Due"
           show={Boolean(block.due_date)}
-          onFocus={this.props.toggleFocused}
-          onBlur={this.props.toggleFocused}
+          onFocus={props.toggleFocused}
+          onBlur={props.toggleFocused}
           p_tag_classname="block__read__due-date"
         />
       );
@@ -128,10 +97,10 @@ const Block = createReactClass({
     // let graded;
     if (isEditable) {
       if (!block.is_new) {
-        deleteBlock = (<div className="delete-block-container"><button className="danger" onClick={this.deleteBlock}>Delete Block</button></div>);
+        deleteBlock = (<div className="delete-block-container"><button className="danger" onClick={deleteBlocks}>Delete Block</button></div>);
       }
       className += ' editable';
-      if (this.props.isDragging) { className += ' dragging'; }
+      if (props.isDragging) { className += ' dragging'; }
     }
 
     let modules = [];
@@ -139,34 +108,34 @@ const Block = createReactClass({
       if (isEditable) {
         const length = block.training_modules.length;
         modules.push(<TrainingModules
-          all_modules={this.props.all_training_modules}
+          all_modules={props.all_training_modules}
           block_modules={block.training_modules}
           block={block}
           editable={isEditable}
           header={length > 1 && 'Training'}
           key=""
-          onChange={this.passedUpdateBlock}
-          trainingLibrarySlug={this.props.trainingLibrarySlug}
+          onChange={passedUpdateBlock}
+          trainingLibrarySlug={props.trainingLibrarySlug}
         />);
       } else {
         modules = (<TrainingModulesViewMode
-          all_modules={this.props.all_training_modules}
+          all_modules={props.all_training_modules}
           block={block}
           editable={isEditable}
           isStudent={isStudent}
-          trainingLibrarySlug={this.props.trainingLibrarySlug}
+          trainingLibrarySlug={props.trainingLibrarySlug}
         />);
       }
     } else {
       modules.push(<TrainingModules
-        all_modules={this.props.all_training_modules}
+        all_modules={props.all_training_modules}
         block_modules={[]}
         block={block}
         editable={isEditable}
         header={length > 1 && 'Training'}
         key="training-modules"
-        onChange={this.passedUpdateBlock}
-        trainingLibrarySlug={this.props.trainingLibrarySlug}
+        onChange={passedUpdateBlock}
+        trainingLibrarySlug={props.trainingLibrarySlug}
       />);
     }
 
@@ -174,13 +143,13 @@ const Block = createReactClass({
       <div className="block__editor-container">
         {modules}
         <TextAreaInput
-          onChange={this.updateBlock}
+          onChange={updateBlock}
           value={block.content}
           value_key="content"
           editable={isEditable}
           rows="4"
-          onFocus={this.props.toggleFocused}
-          onBlur={this.props.toggleFocused}
+          onFocus={props.toggleFocused}
+          onBlur={props.toggleFocused}
           wysiwyg={true}
           className="block__block-content"
         />
@@ -192,9 +161,9 @@ const Block = createReactClass({
       <span className="block__due-date-spacer"> - </span>
     ) : undefined;
 
-    const editButton = this.props.editPermissions ? (
+    const editButton = props.editPermissions ? (
       <div className="block__edit-button-container">
-        <button className="pull-right button ghost-button block__edit-block" onClick={this._setEditable}>Edit</button>
+        <button className="pull-right button ghost-button block__edit-block" onClick={_setEditable}>Edit</button>
       </div>
     ) : undefined;
 
@@ -210,7 +179,7 @@ const Block = createReactClass({
         <div className="block__edit-container">
           <h3 className={headerClass}>
             <TextInput
-              onChange={this.updateBlock}
+              onChange={updateBlock}
               value={block.title}
               value_key="title"
               editable={isEditable}
@@ -218,11 +187,11 @@ const Block = createReactClass({
               show={Boolean(block.title) && !isEditable}
               className="title pull-left"
               spacer=""
-              onFocus={this.props.toggleFocused}
-              onBlur={this.props.toggleFocused}
+              onFocus={props.toggleFocused}
+              onBlur={props.toggleFocused}
             />
             <TextInput
-              onChange={this.updateBlock}
+              onChange={updateBlock}
               value={block.title}
               value_key={'title'}
               editable={isEditable}
@@ -231,13 +200,13 @@ const Block = createReactClass({
               className="pull-left"
               spacer=""
               show={isEditable}
-              onFocus={this.props.toggleFocused}
-              onBlur={this.props.toggleFocused}
+              onFocus={props.toggleFocused}
+              onBlur={props.toggleFocused}
             />
           </h3>
           <div className={blockTypeClassName}>
             <BlockTypeSelect
-              onChange={this.updateBlock}
+              onChange={updateBlock}
               value={block.kind}
               value_key={'kind'}
               editable={isEditable}
@@ -250,7 +219,7 @@ const Block = createReactClass({
         <div className="block__edit-container">
           <div className="block__block-due-date">
             <DatePicker
-              onChange={this.updateBlock}
+              onChange={updateBlock}
               value={block.due_date}
               value_key="due_date"
               editable={isEditable}
@@ -259,9 +228,9 @@ const Block = createReactClass({
               placeholder="Due date"
               isClearable={true}
               show={isEditable && parseInt(block.kind) === 1}
-              date_props={{ minDate: this.props.weekStart }}
-              onFocus={this.props.toggleFocused}
-              onBlur={this.props.toggleFocused}
+              date_props={{ minDate: props.weekStart }}
+              onFocus={props.toggleFocused}
+              onBlur={props.toggleFocused}
             />
           </div>
           {blockKindNote}
@@ -270,10 +239,24 @@ const Block = createReactClass({
         {deleteBlock}
       </li>
     );
-  }
-}
-);
+  };
+Block.displayName = 'Block';
 
+Block.propTypes = {
+    block: PropTypes.object,
+    editableBlockIds: PropTypes.array,
+    editPermissions: PropTypes.bool,
+    saveBlockChanges: PropTypes.func,
+    cancelBlockEditable: PropTypes.func,
+    updateBlock: PropTypes.func,
+    toggleFocused: PropTypes.func,
+    isDragging: PropTypes.bool,
+    all_training_modules: PropTypes.array,
+    weekStart: PropTypes.object,
+    trainingLibrarySlug: PropTypes.string.isRequired,
+    current_user: PropTypes.object,
+    initiateConfirm: PropTypes.func.isRequired
+  };
 const mapDispatchToProps = { initiateConfirm };
 
 export default connect(null, mapDispatchToProps)(
