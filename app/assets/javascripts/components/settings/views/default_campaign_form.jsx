112541acdc4cd75f014b7319aea1346e61668944
gabina <gabinaluz@gmail.com>
2024-02-09 20:20:10 -0300

    Refactor DefaultCampaignForm component to functional component

--- app/assets/javascripts/components/settings/views/default_campaign_form.jsx
+++ app/assets/javascripts/components/settings/views/default_campaign_form.jsx
@@ -1,48 +1,36 @@
-import createReactClass from 'create-react-class';
-import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import TextInput from '../../common/text_input';
 
-const DefaultCampaignForm = createReactClass({
-  propTypes: {
-    updateDefaultCampaign: PropTypes.func,
-    handlePopoverClose: PropTypes.func,
-  },
+function DefaultCampaignForm({ updateDefaultCampaign, handlePopoverClose }) {
+  const [defaultCampaign, setDedaultCampaign] = useState('');
 
-  getInitialState() {
-    return {};
-  },
+  const handleChange = (_key, campaign) => {
+    setDedaultCampaign(campaign);
+  };
 
-  handleChange(key, value) {
-    return this.setState({ [key]: value });
-  },
-
-  handleSubmit(e) {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    this.props.updateDefaultCampaign(this.state.default_campaign_slug);
-    this.props.handlePopoverClose(e);
-  },
+    updateDefaultCampaign(defaultCampaign);
+    handlePopoverClose(e);
+  };
 
-  render() {
-    return (
-      <tr>
-        <td>
-          <form onSubmit={this.handleSubmit}>
-            <TextInput
-              id="default_campaign_slug"
-              editable
-              onChange={this.handleChange}
-              value={this.state.default_campaign_slug}
-              value_key="default_campaign_slug"
-              type="text"
-              label="Default Campaign Slug"
-            />
-            <button className="button border" type="submit" value="Submit">{I18n.t('application.submit')}</button>
-          </form>
-        </td>
-      </tr>
-    );
-  }
-});
+  return (
+    <tr>
+      <td>
+        <form onSubmit={handleSubmit}>
+          <TextInput
+            id="default_campaign_slug"
+            editable
+            onChange={handleChange}
+            value={defaultCampaign}
+            type="text"
+            label="Default Campaign Slug"
+          />
+          <button className="button border" type="submit" value="Submit">{I18n.t('application.submit')}</button>
+        </form>
+      </td>
+    </tr>
+  );
+}
 
 export default DefaultCampaignForm;
