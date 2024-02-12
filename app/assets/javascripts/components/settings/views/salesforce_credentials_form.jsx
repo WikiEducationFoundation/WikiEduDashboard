1e3e9d9e62d3600a5ceba9373c391c9794dce412
gabina <gabinaluz@gmail.com>
2024-02-12 16:06:00 -0300

    Refactor SalesforceCredentialsForm to functional component

--- app/assets/javascripts/components/settings/views/salesforce_credentials_form.jsx
+++ app/assets/javascripts/components/settings/views/salesforce_credentials_form.jsx
@@ -1,57 +1,54 @@
-import createReactClass from 'create-react-class';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import TextInput from '../../common/text_input';
 
-const SalesforceCredentialsForm = createReactClass({
-  propTypes: {
-    updateSalesforceCredentials: PropTypes.func,
-    handlePopoverClose: PropTypes.func,
-  },
-
-  getInitialState() {
-    return {};
-  },
-
-  handleChange(key, value) {
-    return this.setState({ [key]: value });
-  },
-
-  handleSubmit(e) {
+const SalesforceCredentialsForm = (props) => {
+  const [password, setPassword] = useState('');
+  const [token, setToken] = useState('');
+  const handlePasswordChange = (_key, value) => {
+    return setPassword(value);
+  };
+  const handleTokenChange = (_key, value) => {
+    return setToken(value);
+  };
+  const handleSubmit = (e) => {
     e.preventDefault();
-    this.props.updateSalesforceCredentials(this.state.salesforce_password, this.state.salesforce_token);
-    this.props.handlePopoverClose(e);
-  },
+    props.updateSalesforceCredentials(password, token);
+    props.handlePopoverClose(e);
+  };
 
-  render() {
-    return (
-      <tr>
-        <td>
-          <form onSubmit={this.handleSubmit}>
-            <TextInput
-              id="salesforce_password"
-              editable
-              onChange={this.handleChange}
-              value={this.state.salesforce_password}
-              value_key="salesforce_password"
-              type="password"
-              label="Password"
-            />
-            <TextInput
-              id="salesforce_token"
-              editable
-              onChange={this.handleChange}
-              value={this.state.salesforce_token}
-              value_key="salesforce_token"
-              type="password"
-              label="Security Token"
-            />
-            <button className="button border" type="submit" value="Submit">{I18n.t('application.submit')}</button>
-          </form>
-        </td>
-      </tr>
-    );
-  }
-});
+  return (
+    <tr>
+      <td>
+        <form onSubmit={handleSubmit}>
+          <TextInput
+            id="salesforce_password"
+            editable
+            onChange={handlePasswordChange}
+            value={password}
+            value_key="salesforce_password"
+            type="password"
+            label="Password Gab"
+          />
+          <TextInput
+            id="salesforce_token"
+            editable
+            onChange={handleTokenChange}
+            value={token}
+            value_key="salesforce_token"
+            type="password"
+            label="Security Token Gab"
+          />
+          <button className="button border" type="submit" value="Submit">{I18n.t('application.submit')}</button>
+        </form>
+      </td>
+    </tr>
+  );
+};
+
+SalesforceCredentialsForm.propTypes = {
+  updateSalesforceCredentials: PropTypes.func,
+  handlePopoverClose: PropTypes.func,
+};
 
 export default SalesforceCredentialsForm;
