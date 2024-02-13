20f37ccd8489d4779b6b7b3aa991563b07f86ef4
Abishek Das <abishekdascs@gmail.com>
2024-02-13 15:44:32 +0530

    Remove unwanted comment

--- db/migrate/20240105094818_create_course_notes.rb
+++ db/migrate/20240105094818_create_course_notes.rb
@@ -1,7 +1,7 @@
 class CreateCourseNotes < ActiveRecord::Migration[7.0]
   def change
     create_table :course_notes do |t|
-      t.references :courses, foreign_key: true, type: :integer # Make sure to specify the type
+      t.references :courses, foreign_key: true, type: :integer
       t.string :title
       t.text :text
       t.string :edited_by
