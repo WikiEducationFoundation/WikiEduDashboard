77aacc2e7414bc9f1b885b3e85d1026ed8d630c3
om-chauhan1 <omchauhan64408@gmail.com>
2024-03-21 07:55:44 +0530

    Minor change in rspec test

--- spec/features/sitenotice_spec.rb
+++ spec/features/sitenotice_spec.rb
@@ -6,6 +6,8 @@ describe 'sitenotice', type: :feature, js: true do
   let(:super_admin) { create(:super_admin) }
 
   before do
+    # Delete the specific key from cache before each test
+    Rails.cache.delete('site_notice')
     login_as(super_admin, scope: :user)
     visit '/settings'
   end
@@ -23,6 +25,10 @@ describe 'sitenotice', type: :feature, js: true do
     end
 
     it 'Does not display sitenotice because it is disabled' do
+      click_button 'Update Site Notice'
+      fill_in('site_notice', with: notice)
+      click_button 'Submit'
+      click_button 'Enable'
       click_button 'Disable'
       visit root_path
       expect(first('.notification', minimum: 0)).to be_nil
