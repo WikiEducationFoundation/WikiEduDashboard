edc7660b0670a3078890d4756bae72929ce89d27
Pratham Vaidya <iamprathamvaidya@gmail.com>
2024-03-30 18:04:24 +0530

    refactor: class to functional component

--- app/assets/javascripts/components/common/wiki_select.jsx
+++ app/assets/javascripts/components/common/wiki_select.jsx
@@ -1,18 +1,143 @@
 import React from 'react';
-import createReactClass from 'create-react-class';
 import AsyncSelect from 'react-select/async';
 import PropTypes from 'prop-types';
 import { map } from 'lodash-es';
 import ArrayUtils from '../../utils/array_utils';
 import WIKI_OPTIONS from '../../utils/wiki_options';
-import { formatOption } from '../../utils/wiki_utils';
 
 /**
  *  A Wiki Selector Component that combines both language and project into a singular searchable
  *  component that works for both single-wiki and multi-wiki.
  */
-const WikiSelect = createReactClass({
-  propTypes: {
+const WikiSelect = ({
+  multi,
+  onChange,
+  styles,
+  wikis,
+  homeWiki,
+  readOnly,
+  label,
+  id,
+  options
+}) => {
+  // const [selectedWikis, setSelectedWikis] = useState([]);
+
+  const formatOption = (wiki) => {
+    return {
+      value: JSON.stringify(wiki),
+      label: url(wiki)
+    };
+  };
+
+  const url = (wiki) => {
+    const subdomain = wiki.language || 'www';
+    return `${subdomain}.${wiki.project}.org`;
+  };
+
+  if (readOnly) {
+    const lastIndex = wikis.length - 1;
+
+    const wikiList = map(wikis, (wiki, index) => {
+      const comma = (index !== lastIndex) ? ', ' : '';
+      const wikiUrl = url(wiki);
+      return <span key={wikiUrl}>{wikiUrl}{comma}</span>;
+    });
+    return (
+      <>
+        {wikiList}
+      </>
+    );
+  }
+
+  // Used to set the already available wikis
+  let selectedWikis = [];
+  if (wikis) {
+    selectedWikis = wikis.map((wiki) => {
+      wiki.language = wiki.language || 'www'; // for multilingual wikis language is null
+      return formatOption(wiki);
+    });
+  }
+
+  // Home Wiki should appear first in the list of tracked wikis as in any other place it blocks
+  // the removal of a wiki via backspace. Removing a wiki by pressing 'X' still works
+  // but is bad UI and home wiki appearing first makes more sense anyway.
+  let home_wiki = homeWiki;
+  if (multi) {
+    home_wiki = formatOption(home_wiki);
+    selectedWikis = ArrayUtils.removeObject(selectedWikis, home_wiki);
+    selectedWikis.unshift(home_wiki);
+  }
+
+  const preprocess = (wiki) => {
+    if (wiki) {
+      if (multi) {
+        wiki = wiki.map((w) => { return { value: JSON.parse(w.value), label: w.label }; });
+      } else {
+        const value = JSON.parse(wiki.value);
+        wiki = { label: wiki.label, value };
+      }
+      onChange(wiki);
+    } else {
+      onChange(multi ? [] : {});
+    }
+  };
+
+  // If the component was passed a set of wikis as options, limit the options to those wikis.
+  // Otherwise, allow any wiki to be selected.
+  const availableOptions = (options && options.length) ? options : WIKI_OPTIONS;
+
+  // If the input is less than three characters, it will be matched from the beginning of the string.
+
+  const filterOptionsLong = function (val) {
+    return availableOptions.filter(wiki =>
+      wiki.label.toLowerCase().includes(val.toLowerCase())
+    ).slice(0, 10); // limit the options for better performance
+  };
+
+  // If the input is at least three characters, it will be matched anywhere in the string
+
+  const filterOptionsShort = function (val) {
+    return availableOptions.filter(wiki =>
+      wiki.label.toLowerCase().includes(val.toLowerCase())
+    ).slice(0, 10);
+  };
+
+  const loadOptions = function (inputValue, callback) {
+    if (inputValue.length < 3) {
+      callback(filterOptionsShort(inputValue));
+    }
+    callback(filterOptionsLong(inputValue));
+  };
+
+ return (
+   <>
+     <label
+       id={`${id}-label`}
+       htmlFor={id} className="text-input-component__label"
+     >
+       <strong>
+         {label}:&nbsp;
+       </strong>
+     </label>
+     <AsyncSelect
+       id={id}
+       isMulti={multi}
+       placeholder={I18n.t('multi_wiki.selector_placeholder')}
+       noOptionsMessage={() => I18n.t('multi_wiki.selector_placeholder')}
+       value={selectedWikis.length ? selectedWikis : undefined}
+       loadOptions={loadOptions}
+       onChange={preprocess}
+       styles={styles}
+       isClearable={false}
+       defaultValue={home_wiki && formatOption(home_wiki)}
+       className="multi-wiki-selector"
+       aria-labelledby={`${id}-label`}
+     />
+   </>
+ );
+};
+
+WikiSelect.propTypes = {
     /**
      *  If true multiple wiki can be selected.
      */
@@ -50,128 +175,10 @@ const WikiSelect = createReactClass({
 
     /**
      * Wikis for options, in article finder (through selected_wiki_options)
-     * and assign button (through new assigment input and selected wiki options)  tracked Wikis,
-     * in details and couse_form all Wikis
+     * and assign button (through new assignment input and selected wiki options)  tracked Wikis,
+     * in details and course_form all Wikis
     */
     options: PropTypes.array
-  },
-
-  formatOption(wiki) {
-    return {
-      value: JSON.stringify(wiki),
-      label: this.url(wiki)
-    };
-  },
-
-  url(wiki) {
-    const subdomain = wiki.language || 'www';
-    return `${subdomain}.${wiki.project}.org`;
-  },
-
-  render() {
-    if (this.props.readOnly) {
-      const lastIndex = this.props.wikis.length - 1;
-
-      const wikiList = map(this.props.wikis, (wiki, index) => {
-        const comma = (index !== lastIndex) ? ', ' : '';
-        const wikiUrl = this.url(wiki);
-        return <span key={wikiUrl}>{wikiUrl}{comma}</span>;
-      });
-      return (
-        <>
-          {wikiList}
-        </>
-      );
-    }
-
-    // Used to set the already available wikis
-    let wikis = [];
-    if (this.props.wikis) {
-      wikis = this.props.wikis.map((wiki) => {
-        wiki.language = wiki.language || 'www'; // for multilingual wikis language is null
-        return this.formatOption(wiki);
-      });
-    }
-
-    // Home Wiki should appear first in the list of tracked wikis as in any other place it blocks
-    // the removal of a wiki via backspace. Removing a wiki by pressing 'X' still works
-    // but is bad UI and home wiki appearing first makes more sense anyway.
-    let home_wiki = this.props.homeWiki;
-    if (this.props.multi) {
-      home_wiki = this.formatOption(home_wiki);
-      wikis = ArrayUtils.removeObject(wikis, home_wiki);
-      wikis.unshift(home_wiki);
-    }
-
-    const preprocess = (wiki) => {
-      if (wiki) {
-        if (this.props.multi) {
-          wiki = wiki.map((w) => { return { value: JSON.parse(w.value), label: w.label }; });
-        } else {
-          const value = JSON.parse(wiki.value);
-          wiki = { label: wiki.label, value };
-        }
-        this.props.onChange(wiki);
-      } else {
-        this.props.onChange(this.props.multi ? [] : {});
-      }
-    };
-
-    // If the component was passed a set of wikis as options, limit the options to those wikis.
-    // Otherwise, allow any wiki to be selected.
-    const options = (this.props.options && this.props.options.length) ? this.props.options : WIKI_OPTIONS;
-
-    // If the input is less than three characters, it will be matched from the beginning of the string.
-
-    const filterOptionsLong = function (val) {
-      return options.filter(wiki =>
-        wiki.label.toLowerCase().includes(val.toLowerCase())
-      ).slice(0, 10); // limit the options for better performance
-    };
-
-  // If the input is at least three characters, it will be matched anywhere in the string
-
-    const filterOptionsShort = function (val) {
-      return options.filter(wiki =>
-        wiki.label.toLowerCase().includes(val.toLowerCase())
-      ).slice(0, 10);
-    };
-
-    const loadOptions = function (inputValue, callback) {
-      if (inputValue.length < 3) {
-        callback(filterOptionsShort(inputValue));
-      }
-      callback(filterOptionsLong(inputValue));
-    };
-
-   return (
-     <>
-       <label
-         id={`${this.props.id}-label`}
-         htmlFor={this.props.id} className="text-input-component__label"
-       >
-         <strong>
-           {this.props.label}:&nbsp;
-         </strong>
-       </label>
-       <AsyncSelect
-         id={this.props.id}
-         isMulti={this.props.multi}
-         placeholder={I18n.t('multi_wiki.selector_placeholder')}
-         noOptionsMessage={() => I18n.t('multi_wiki.selector_placeholder')}
-         value={wikis.length ? wikis : undefined}
-         loadOptions={loadOptions}
-         onChange={preprocess}
-         styles={this.props.styles}
-         isClearable={false}
-         defaultValue={home_wiki && formatOption(home_wiki)}
-         className="multi-wiki-selector"
-         aria-labelledby={`${this.props.id}-label`}
-       />
-     </>
-   );
-  }
-}
-);
+};
 
 export default WikiSelect;
