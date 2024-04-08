903c47472794b82779d95d6727b586723a14e047
Sage Ross <ragesoss@gmail.com>
2024-04-08 11:02:35 -0700

    Fix broken NoTaSupportAlertManager
    Whoops, I clearly didn't test this well enough before merging it; not sure how that slipped through, but this was never working and has broken the alert cycle.

--- lib/alerts/ta_support_alert_manager.rb
+++ lib/alerts/ta_support_alert_manager.rb
@@ -7,20 +7,20 @@ class NoTaSupportAlertManager
 
   def create_alerts
     @tagged_courses.each do |course|
-      instructors = get_course_instructors(course.course_id)
+      instructors = get_course_instructors(course)
       next unless within_no_ta_alert_period?(course.created_at)
       next unless instructors.size <= 1
 
-      next if Alert.exists?(course_id: course.course_id, type: 'NoTaEnrolledAlert')
-      alert = Alert.create(type: 'NoTaEnrolledAlert', course_id: course.course_id)
+      next if Alert.exists?(course:, type: 'NoTaEnrolledAlert')
+      alert = Alert.create(type: 'NoTaEnrolledAlert', course:)
       alert.send_email
     end
   end
 
   private
 
-  def get_course_instructors(course_id)
-    CoursesUsers.where(course_id:, role: CoursesUsers::Roles::INSTRUCTOR_ROLE).pluck(:user_id)
+  def get_course_instructors(course)
+    CoursesUsers.where(course:, role: CoursesUsers::Roles::INSTRUCTOR_ROLE).pluck(:user_id)
   end
 
   # Only create an alert if has been at least MIN_DAYS (7days)
