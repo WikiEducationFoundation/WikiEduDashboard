16454a4b617c2f30fa8a881ae204b0f435c61b71
om-chauhan1 <omchauhan64408@gmail.com>
2024-04-26 23:31:02 +0530

    Refactored add_category_button.jsx from class to functional component

--- app/assets/javascripts/components/categories/add_category_button.jsx
+++ app/assets/javascripts/components/categories/add_category_button.jsx
@@ -1,11 +1,9 @@
-import createReactClass from 'create-react-class';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 import React from 'react';
+import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
 import { initiateConfirm } from '../../actions/confirm_actions';
 import Popover from '../common/popover.jsx';
-import Expandable from '../high_order/expandable.jsx';
-
+import useExpandablePopover from '../../hooks/useExpandablePopover';
 import CategoriesScoping from '../course_creator/scoping_methods/categories_scoping';
 import PagePileScoping from '../course_creator/scoping_methods/pagepile_scoping';
 import PetScanScoping from '../course_creator/scoping_methods/petscan_scoping';
@@ -13,120 +11,112 @@ import TemplatesScoping from '../course_creator/scoping_methods/templates_scopin
 import { getAddCategoriesPayload } from '../util/scoping_methods';
 import { resetScopingMethod } from '../../actions/scoping_methods';
 
-const AddCategoryButton = createReactClass({
-  displayName: 'AddCategoryButton',
+const AddCategoryButton = ({
+  course,
+  source,
+  addCategory,
+}) => {
+  const scopingMethods = useSelector(state => state.scopingMethods);
+  const dispatch = useDispatch();
 
-  propTypes: {
-    course: PropTypes.object.isRequired,
-    is_open: PropTypes.bool,
-    open: PropTypes.func.isRequired,
-    initiateConfirm: PropTypes.func,
-    addCategory: PropTypes.func,
-    source: PropTypes.string.isRequired
-  },
+  const getKey = () => `add_${source}_button`;
+  const { isOpen, ref, open } = useExpandablePopover(getKey);
 
-  getKey() {
-    return `add_${this.props.source}_button`;
-  },
-
-  addCategory(e) {
+  const addCategoryHandler = (e) => {
     e.preventDefault();
-    const addCategory = this.props.addCategory;
     const payload = {
       ...getAddCategoriesPayload({
-        sourceType: this.props.source,
-        scopingMethods: this.props.scopingMethods,
+        sourceType: source,
+        scopingMethods,
       }),
-      course_id: this.props.course.id,
-      source: this.props.source,
+      course_id: course.id,
+      source,
     };
-    const resetScoping = this.props.resetScopingMethod;
-    const open = this.props.open;
-    const onConfirm = function () {
+    const onConfirm = () => {
       addCategory(payload);
-      resetScoping();
+      dispatch(resetScopingMethod());
       open(null);
     };
     const confirmMessage = I18n.t('categories.confirm_add_n_scoping_method', { n: payload.categories.items.length });
-    return this.props.initiateConfirm({ confirmMessage, onConfirm });
-  },
+    dispatch(initiateConfirm({ confirmMessage, onConfirm }));
+  };
 
+  let description;
 
-  render() {
-    let description;
-      if (this.props.source === 'pileid') {
-        description = (
-          <p>
-            Make sure the PagePile&apos;s wiki is one of the tracked wikis for this program.
-          </p>
-        );
-      } else if (this.props.source === 'psid') {
-        description = (
-          <p>
-            PetScan queries only work with the Dashboard one wiki at a time. Ensure that your PetScan query includes only articles from a single wiki, and that it matches the wiki set below.
-          </p>
-        );
-      }
-    const permitted = true;
-    let className = 'button border small assign-button';
-    if (this.props.is_open) { className += ' dark'; }
-
-    const buttonText = I18n.t(`categories.add_${this.props.source}`);
-    const showButton = <button className={`${className}`} onClick={this.props.open}>{buttonText}</button>;
-
-    let editRow;
-    if (permitted) {
-      let inputField;
-      if (this.props.source === 'category') {
-        inputField = <CategoriesScoping vertical/>;
-      } else if (this.props.source === 'psid') {
-        inputField = <PetScanScoping/>;
-      } else if (this.props.source === 'pileid') {
-        inputField = <PagePileScoping />;
-      } else {
-        inputField = <TemplatesScoping />;
-      }
-      editRow = (
-        <tr className="edit">
-          <td>
-            <form
-              onSubmit={this.addCategory} style={{
-              display: 'flex',
-              flexDirection: 'column',
-              gap: '1em'
-            }}
-              className="category-add-form"
-            >
-              {description}
-              {inputField}
-              <button className="button border" type="submit">
-                {I18n.t(`categories.add_this_${this.props.source}`)}
-              </button>
-            </form>
-          </td>
-        </tr>
-      );
-    }
-
-    return (
-      <div className="pop__container">
-        {showButton}
-        <Popover
-          is_open={this.props.is_open}
-          edit_row={editRow}
-          styles={{
-            width: '500px'
-          }}
-        />
-      </div>
+  if (source === 'pileid') {
+    description = (
+      <p>
+        {I18n.t('categories.pileid_description')}
+      </p>
+    );
+  } else if (source === 'psid') {
+    description = (
+      <p>
+        {I18n.t('categories.psid_description')}
+      </p>
     );
   }
-});
 
-const mapDispatchToProps = { initiateConfirm, resetScopingMethod };
-const mapStateToProps = state => ({
-  scopingMethods: state.scopingMethods,
-});
-export default connect(mapStateToProps, mapDispatchToProps)(
-  Expandable(AddCategoryButton)
-);
+  const permitted = true;
+  let className = 'button border small assign-button';
+  if (isOpen) { className += ' dark'; }
+
+  const buttonText = I18n.t(`categories.add_${source}`);
+  const showButton = <button className={`${className}`} onClick={open}>{buttonText}</button>;
+
+  let editRow = null;
+  if (permitted) {
+    let inputField;
+    if (source === 'category') {
+      inputField = <CategoriesScoping vertical />;
+    } else if (source === 'psid') {
+      inputField = <PetScanScoping />;
+    } else if (source === 'pileid') {
+      inputField = <PagePileScoping />;
+    } else {
+      inputField = <TemplatesScoping />;
+    }
+    editRow = (
+      <tr className="edit">
+        <td>
+          <form
+            onSubmit={addCategoryHandler}
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '1em',
+            }}
+            className="category-add-form"
+          >
+            {description}
+            {inputField}
+            <button className="button border" type="submit">
+              {I18n.t(`categories.add_this_${source}`)}
+            </button>
+          </form>
+        </td>
+      </tr>
+    );
+  }
+
+  return (
+    <div className="pop__container" ref={ref}>
+      {showButton}
+      <Popover
+        is_open={isOpen}
+        edit_row={editRow}
+        styles={{
+          width: '500px',
+        }}
+      />
+    </div>
+  );
+};
+
+AddCategoryButton.propTypes = {
+  course: PropTypes.object.isRequired,
+  source: PropTypes.string.isRequired,
+  addCategory: PropTypes.func,
+};
+
+export default AddCategoryButton;
