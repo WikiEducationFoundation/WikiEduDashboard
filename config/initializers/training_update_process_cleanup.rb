38707b0cb0e567861a428db54b630be4f8f6adcb
Ujjwal Pathak <pathak2002ujjwal@email.com>
2024-04-26 15:10:17 +0530

    trying to resolve testing

--- config/initializers/training_update_process_cleanup.rb
+++
@@ -1,8 +0,0 @@
-require_dependency "#{Rails.root}/lib/data_cycle/batch_update_logging"
-require_dependency "#{Rails.root}/lib/training/training_base"
-
-include BatchUpdateLogging
-
-delete_pid_file(:training)
-TrainingBase.new.update_process_state(0)
-TrainingBase.new.clear_error_messages
