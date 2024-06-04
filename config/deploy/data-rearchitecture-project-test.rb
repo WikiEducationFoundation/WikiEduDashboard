bd9c2eb410a9707ed8f2a835b67108d7995f6302
gabina <gabinaluz@gmail.com>
2024-06-04 18:29:24 -0300

    Change rails environment to production

--- config/deploy/data-rearchitecture-project-test.rb
+++ config/deploy/data-rearchitecture-project-test.rb
@@ -1,5 +1,5 @@
 set :branch, 'data-rearchitecture-for-dashboard'
-set :rails_env, 'development'
+set :rails_env, 'production'
 
 role :app, %w(gabina@data-rearchitecture-project-test.globaleducation.eqiad1.wikimedia.cloud)
 role :web, %w(gabina@data-rearchitecture-project-test.globaleducation.eqiad1.wikimedia.cloud)
