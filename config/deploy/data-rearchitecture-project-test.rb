f524483138ac579b9755814d76a3256725af8708
gabina <gabinaluz@gmail.com>
2024-06-07 15:38:29 -0300

    Add PASSENGER_INSTANCE_REGISTRY_DIR environment variable to the data-rearchitecture-project-test deploy stage

--- config/deploy/data-rearchitecture-project-test.rb
+++ config/deploy/data-rearchitecture-project-test.rb
@@ -9,3 +9,4 @@ set :user, 'gabina'
 set :address, 'data-rearchitecture-project-test.globaleducation.eqiad1.wikimedia.cloud'
 
 set :deploy_to, '/var/www/dashboard'
+set :default_env, { 'PASSENGER_INSTANCE_REGISTRY_DIR' => '/var/www/dashboard/shared/tmp/pids' }
