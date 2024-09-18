a6f90a0e1bc866c6d61df1c4d103d1130d2e8254
FormasitChijoh324 <manoica2023@gmail.com>
2024-09-18 23:19:46 +0100

    fix: added test for sort courses and removed schema and Gemfile

--- test/reducers/active_courses.spec.js
+++ test/reducers/active_courses.spec.js
@@ -12,6 +12,38 @@ describe('active_course reducer', () => {
         expect(newState.courses).toEqual([]);
         expect(newState.isLoaded).toBe(false);
     });
+    test('should return the same state when RECEIVE_ACTIVE_COURSES is dispatched multiple times with the same data', () => {
+        const initialState = {
+            courses: [],
+            isLoaded: false,
+            sort: { key: null, sortKey: null },
+        };
+        deepFreeze(initialState);
+        const mockedAction = {
+            type: RECEIVE_ACTIVE_COURSES,
+            data: { courses: [{ id: 1, title: 'Course 1' }] },
+        };
+        const firstState = active_courses(initialState, mockedAction);
+        const secondState = active_courses(firstState, mockedAction);
+        expect(firstState).toEqual(secondState);
+        expect(secondState.courses).toEqual([{ id: 1, title: 'Course 1' }]);
+        expect(secondState.isLoaded).toBe(true);
+    });
+    test('should handle an empty list of courses in RECEIVE_ACTIVE_COURSES', () => {
+        const initialState = {
+            courses: [{ id: 1, title: 'Existing Course' }],
+            isLoaded: true,
+            sort: { key: null, sortKey: null },
+        };
+        deepFreeze(initialState);
+        const mockedAction = {
+            type: RECEIVE_ACTIVE_COURSES,
+            data: { courses: [] },
+        };
+        const newState = active_courses(initialState, mockedAction);
+        expect(newState.courses).toEqual([]);
+        expect(newState.isLoaded).toBe(true);
+    });
 
     test('should return active course data  with RECEIVE_ACTIVE_COURSES and set isLoaded to true', () => {
         const initialState = {};
@@ -25,7 +57,7 @@ describe('active_course reducer', () => {
         expect(newState).toEqual(expectedState);
     });
 
-    test('should return active course campaign data with RECEIVE_CAMPAIGN_ACTIVE_COURSES ', () => {
+    test('handles RECEIVE_CAMPAIGN_ACTIVE_COURSES by updating active courses with campaign data ', () => {
         const initialState = { courses: [{ title: 'title' }] };
         deepFreeze(initialState);
         const mockedAction = {
@@ -39,7 +71,7 @@ describe('active_course reducer', () => {
 
     test('sort active courses via SORT_ACTIVE_COURSES ', () => {
         const initialState = {
-            courses: [{ id: 1, title: 'title one' }, { id: 2, title: 'title two' }],
+            courses: [{ id: 2, title: 'title two' }, { id: 1, title: 'title one' }],
             sort: { sorKey: null }
         };
         deepFreeze(initialState);
@@ -51,6 +83,20 @@ describe('active_course reducer', () => {
         expect(newState.sort.key).toBe('id');
         expect(newState.courses[0].id).toBe(1);
         expect(newState.courses[1].id).toBe(2);
+    });
+    test('should not modify courses when SORT_ACTIVE_COURSES is dispatched with an invalid key', () => {
+        const initialState = {
+            courses: [{ id: 2, title: 'Course 2' }, { id: 1, title: 'Course 1' }],
+            sort: { sortKey: null, key: null },
+        };
+        deepFreeze(initialState);
+        const mockedAction = {
+            type: SORT_ACTIVE_COURSES,
+            key: 'nonExistentKey',
+        };
+        const newState = active_courses(initialState, mockedAction);
+        expect(newState.courses).toEqual(initialState.courses); // Should remain unchanged
+        expect(newState.sort.key).toBe('nonExistentKey');
     });
 });
 
