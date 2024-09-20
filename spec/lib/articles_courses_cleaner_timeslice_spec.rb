28902e2ae2e97c9652fe6a42cb717449fb287970
gabina <gabinaluz@gmail.com>
2024-09-20 16:52:00 -0300

    Remove article course timeslices when removing articles courses

--- spec/lib/articles_courses_cleaner_timeslice_spec.rb
+++ spec/lib/articles_courses_cleaner_timeslice_spec.rb
@@ -7,6 +7,7 @@ describe ArticlesCoursesCleanerTimeslice do
   let(:enwiki) { Wiki.get_or_create(project: 'wikipedia', language: 'en') }
   let(:wikidata) { Wiki.get_or_create(project: 'wikidata', language: nil) }
   let(:course) { create(:course, start: '2024-01-01', end: '2024-04-20') }
+  let(:manager) { TimesliceManager.new(course) }
   let(:article1) { create(:article, wiki: enwiki) }
   let(:article2) { create(:article, wiki: wikidata) }
   let(:article3) { create(:article, wiki: wikidata) }
@@ -33,12 +34,19 @@ describe ArticlesCoursesCleanerTimeslice do
       create(:articles_course, course:, article: article1, user_ids: [1, 45])
       create(:articles_course, course:, article: article3, user_ids: [47, 45, 5])
       create(:articles_course, course:, article: article2, user_ids: [455])
+      manager.create_timeslices_for_new_article_course_records(
+        [{ article_id: article1.id, course_id: course.id },
+         { article_id: article2.id, course_id: course.id },
+         { article_id: article3.id, course_id: course.id }]
+      )
     end
 
-    it 'removes ArticlesCourses that were edited only by removed users' do
+    it 'removes ArticlesCourses and their timeslices that were edited only by removed users' do
+      expect(course.article_course_timeslices.size).to eq(342)
       expect(course.articles_courses.size).to eq(3)
       # User ids 5, 45 and 47 were deleted
       described_class.clean_articles_courses_for_user_ids(course, [5, 45, 47])
+      expect(course.article_course_timeslices.size).to eq(228)
       expect(course.articles_courses.size).to eq(2)
       expect(course.articles_courses.first.user_ids).to eq([1, 45])
       expect(course.articles_courses.second.user_ids).to eq([455])
