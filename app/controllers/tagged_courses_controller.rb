c0099b9186fda45bd8d680c6a027e66793cf4496
Abishek Das <abishekdascs@gmail.com>
2024-09-21 20:40:11 +0530

    Move  load_wiki_experts from presenter to tagged courses controller.

--- app/controllers/tagged_courses_controller.rb
+++ app/controllers/tagged_courses_controller.rb
@@ -25,6 +25,7 @@ class TaggedCoursesController < ApplicationController
   def programs
     set_page
     set_courses_and_presenter
+    load_wiki_experts
     render 'tagged_courses/programs'
   end
 
@@ -43,5 +44,19 @@ class TaggedCoursesController < ApplicationController
     @courses = Tag.courses_tagged_with(@tag)
     @presenter = CoursesPresenter.new(current_user:, tag: @tag,
                                       courses_list: @courses, page: @page)
+  end
+
+  # Loads CoursesUsers records with role 4 and filters by wiki experts, avoiding N+1 queries
+  def load_wiki_experts
+    return @wiki_experts if @wiki_experts # Avoid re-loading if already loaded
+
+    course_ids = @courses.pluck(:id)
+    wiki_experts_set = SpecialUsers.special_users[:wikipedia_experts]&.to_set
+
+    @wiki_experts = CoursesUsers
+                    .where(course_id: course_ids, role: 4)
+                    .includes(:user)
+                    .select { |course_user| wiki_experts_set.include?(course_user.user.username) }
+                    .map { |course_user| { course_id: course_user.course_id, username: course_user.user.username } } # rubocop:disable Layout/LineLength
   end
 end
