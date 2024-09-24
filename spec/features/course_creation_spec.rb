b8cc22d9e3680b17b2ec3aeea55a80bed482be5f
Sage Ross <ragesoss@gmail.com>
2024-09-24 11:26:54 -0700

    Add 'keeping track of work' module block to timelines

--- spec/features/course_creation_spec.rb
+++ spec/features/course_creation_spec.rb
@@ -124,7 +124,7 @@ describe 'New course creation and editing', type: :feature do
   end
 
   describe 'course workflow', js: true do
-    let(:expected_course_blocks) { 25 }
+    let(:expected_course_blocks) { 26 }
     let(:module_name) { 'Get started on Wikipedia' }
 
     it 'allows the user to create a course' do
