139c05fdb007b7efbef3e31deba757ca47034383
Abishek Das <abishekdascs@gmail.com>
2024-10-01 19:57:49 +0530

    Refactor WikiExpertNotificationMailer to access alert properties directly

--- app/mailers/wiki_expert_notification_mailer.rb
+++ app/mailers/wiki_expert_notification_mailer.rb
@@ -3,15 +3,14 @@
 class WikiExpertNotificationMailer < ApplicationMailer
   def self.send_email(alert)
     return unless Features.email?
+
     email(alert).deliver_now
   end
 
   def email(alert)
-    @alert = alert
-    @course = @alert.course
-    @wiki_experts = @alert.wiki_experts_email
-    @course_link = @alert.url
+    @course = alert.course
+    @course_link = alert.url
 
-    mail(to: @wiki_experts, subject: @alert.main_subject)
+    mail(to: alert.wiki_experts_email, subject: alert.main_subject)
   end
 end
