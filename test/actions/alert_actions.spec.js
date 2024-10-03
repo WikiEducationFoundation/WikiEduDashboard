19b43e4e9fc49f3a352e3d3e1e4ac2de06adb6f1
Formasitchijoh <formasitf@gmail.com>
2024-10-03 03:28:17 +0100

    fix: reverted fix made to alert_actions failing test

--- test/actions/alert_actions.spec.js
+++ test/actions/alert_actions.spec.js
@@ -4,20 +4,16 @@ import '../testHelper';
 describe('AlertActions', () => {
   const initialState = { submitting: false, created: false };
   const expectedState = { submitting: false, created: true };
-  const expectedNewState = { submitting: true, created: false };
-  const messageData = {
-    target_user_id: 2,
-    message: 'new Message',
-    course_id: 1
-  };
-  test('submits and creates an alert, then resets it', async () => {
+  test('submits and creates an alert, then resets it', () => {
     expect(reduxStore.getState().needHelpAlert).toEqual(initialState);
     sinon.stub($, 'ajax').yieldsTo('success', { success: true });
-    await reduxStore.dispatch(actions.submitNeedHelpAlert({}));
-    expect(reduxStore.getState().needHelpAlert).not.toEqual(expectedState);
-    await reduxStore.dispatch(actions.submitNeedHelpAlert(messageData));
-    expect(reduxStore.getState().needHelpAlert).toEqual(expectedNewState);
-    reduxStore.dispatch(actions.resetNeedHelpAlert());
-    expect(reduxStore.getState().needHelpAlert).toEqual(initialState);
+    reduxStore.dispatch(actions.submitNeedHelpAlert({}))
+      .then(() => {
+        expect(reduxStore.getState().needHelpAlert).toEqual(expectedState);
+      })
+      .then(() => {
+        reduxStore.dispatch(actions.resetNeedHelpAlert());
+        expect(reduxStore.getState().needHelpAlert).toEqual(initialState);
+      });
   });
 });
