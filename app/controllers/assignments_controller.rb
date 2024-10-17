1ba2695f86f26fbedf9d9e33980580931b98dbcd
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2024-10-17 01:20:27 +0100

    update parameter name

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -49,7 +49,7 @@ class AssignmentsController < ApplicationController
 
   # Select an Available Article as a new Assignment for a user
   def claim
-    @claimed_assignment = Assignment.find(params[:id])
+    @claimed_assignment = Assignment.find(params[:assignment_id])
     @course = @claimed_assignment.course
     check_permissions(assignment_params[:user_id].to_i)
     check_participation # prevents a user from a different course claiming an assignment
