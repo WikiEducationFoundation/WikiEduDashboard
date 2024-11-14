9a158aaf1307d82f003c95e73e2455199867d7b2
shishiro <bannu1302@gmail.com>
2024-11-14 22:43:38 +0530

    added the real names to the activity tab

--- app/assets/javascripts/components/revisions/revision.jsx
+++ app/assets/javascripts/components/revisions/revision.jsx
@@ -3,13 +3,15 @@ import PropTypes from 'prop-types';
 import DiffViewer from './diff_viewer.jsx';
 import CourseUtils from '../../utils/course_utils.js';
 import { formatDateWithTime } from '../../utils/date_utils.js';
+import { trunc } from '../../utils/strings.js';
 
-const Revision = ({ revision, index, wikidataLabel, course, setSelectedIndex, lastIndex, selectedIndex }) => {
+const Revision = ({ revision, index, wikidataLabel, course, setSelectedIndex, lastIndex, selectedIndex, student }) => {
   const ratingClass = `rating ${revision.rating}`;
   const ratingMobileClass = `${ratingClass} tablet-only`;
   const formattedTitle = CourseUtils.formattedArticleTitle({ title: revision.title, project: revision.wiki.project, language: revision.wiki.language }, course.home_wiki, wikidataLabel);
   const subtitle = wikidataLabel ? `(${CourseUtils.removeNamespace(revision.title)})` : '';
   const isWikipedia = revision.wiki.project === 'wikipedia';
+  const showRealName = student.first_name && student.last_name;
   return (
     <tr className="revision">
       <td className="tooltip-trigger desktop-only-tc">
@@ -24,7 +26,24 @@ const Revision = ({ revision, index, wikidataLabel, course, setSelectedIndex, la
         {isWikipedia && <div className={ratingMobileClass}><p>{revision.pretty_rating || '-'}</p></div>}
         <a href={revision.article_url} target="_blank" className="inline"><p className="title">{formattedTitle}&nbsp;<small>{subtitle}</small></p></a>
       </td>
-      <td className="desktop-only-tc">{revision.revisor}</td>
+      <td className="desktop-only-tc">
+        {showRealName ? (
+          <span>
+            <strong>{trunc(student.real_name)}</strong>&nbsp;
+            (
+            <a onClick={e => e.preventDefault()}>
+              {revision.revisor}
+            </a>
+            )
+          </span>
+        ) : (
+          <span>
+            <a onClick={e => e.preventDefault()}>
+              {revision.revisor}
+            </a>
+          </span>
+        )}
+      </td>
       <td className="desktop-only-tc">{revision.characters}</td>
       <td className="desktop-only-tc">{revision.references_added}</td>
       <td className="desktop-only-tc date"><a href={revision.url}>{formatDateWithTime(revision.date)}</a></td>
@@ -50,7 +69,8 @@ Revision.propTypes = {
   course: PropTypes.object,
   setSelectedIndex: PropTypes.func,
   lastIndex: PropTypes.number,
-  selectedIndex: PropTypes.number
+  selectedIndex: PropTypes.number,
+  student: PropTypes.object
 };
 
 export default Revision;
