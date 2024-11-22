d2cb82af2519180fde74e7480bcf203cb09b3745
gabina <gabinaluz@gmail.com>
2024-11-22 00:59:56 -0300

    Make articles.json view calculate view_count based on first revision and average_views for article

--- app/helpers/article_helper.rb
+++ app/helpers/article_helper.rb
@@ -56,4 +56,10 @@ module ArticleHelper
       return nil
     end
   end
+
+  def view_count(first_revision, average_views)
+    return 0 if first_revision.nil? || average_views.nil?
+    days = (Time.now.utc.to_date - first_revision.to_date).to_i
+    (days * average_views).to_i
+  end
 end
