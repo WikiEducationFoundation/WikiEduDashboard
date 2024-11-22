ae11c0a40950b644d3fe517251befaea12566989
gabina <gabinaluz@gmail.com>
2024-11-22 01:09:18 -0300

    Calculate view_sum cache for an entire course based on first revision and average views for every article course. Notice that this is done in the SQL side because that way is much faster.

--- spec/lib/course_cache_manager_spec.rb
+++ spec/lib/course_cache_manager_spec.rb
@@ -5,7 +5,8 @@ require "#{Rails.root}/lib/course_cache_manager"
 
 describe CourseCacheManager do
   let(:wiki) { Wiki.get_or_create(language: 'en', project: 'wikipedia') }
-  let(:article) { create(:article, id: 1, namespace: 0) }
+  let(:article1) { create(:article, namespace: 0, average_views: 10) }
+  let(:article2) { create(:article, namespace: 0, average_views: 5) }
   let(:course) do
     create(:course, start: Time.zone.today - 1.month, end: Time.zone.today + 1.month)
   end
@@ -14,12 +15,13 @@ describe CourseCacheManager do
     create(:user, id: 1, username: 'Ragesoss')
     create(:user, id: 2, username: 'Gatoespecie')
 
-    create(:articles_course, id: 1, article:, course:, view_count: 4, new_article: true)
-    create(:articles_course, id: 2, article:, course:, view_count: 4)
+    create(:articles_course, article: article1, course:,
+           first_revision: 10.days.ago, new_article: true)
+    create(:articles_course, article: article2, course:, first_revision: 8.days.ago)
 
-    create(:courses_user, id: 1, course:, user_id: 1)
-    create(:courses_user, id: 2, course:, user_id: 2)
-    create(:courses_user, id: 3, course:, user_id: 2, role: CoursesUsers::Roles::INSTRUCTOR_ROLE)
+    create(:courses_user, course:, user_id: 1)
+    create(:courses_user, course:, user_id: 2)
+    create(:courses_user, course:, user_id: 2, role: CoursesUsers::Roles::INSTRUCTOR_ROLE)
 
     create(:commons_upload, user_id: 1, uploaded_at: 10.days.ago, usage_count: 3)
     create(:commons_upload, user_id: 2, uploaded_at: 10.days.ago, usage_count: 4)
@@ -69,8 +71,7 @@ describe CourseCacheManager do
 
     it 'updates caches based on existing articles courses records' do
       described_class.new(course).update_cache_from_timeslices []
-      # TODO: view_sum should be 8. See issue #5911
-      expect(course.view_sum).to eq(4)
+      expect(course.view_sum).to eq(140)
       expect(course.article_count).to eq(2)
       expect(course.new_article_count).to eq(1)
     end
@@ -85,7 +86,7 @@ describe CourseCacheManager do
       expect(course.trained_count).to eq(2)
     end
 
-    it 'updates uploads based on existing article courses records' do
+    it 'updates uploads based on existing common uploads records' do
       described_class.new(course).update_cache_from_timeslices []
       # TODO: modify to be calculated from course wiki timeslices values
       expect(course.upload_count).to eq(2)
