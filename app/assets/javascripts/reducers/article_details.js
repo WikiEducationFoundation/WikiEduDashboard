21c377cd5964d63a5e02022010eaf3fa1d2b2f5f
Sage Ross <ragesoss@gmail.com>
2024-12-04 15:59:59 -0800

    [WIP] Bypass Revisions table queries for DiffViewer

--- app/assets/javascripts/reducers/article_details.js
+++ app/assets/javascripts/reducers/article_details.js
@@ -6,7 +6,7 @@ export default function articleDetails(state = initialState, action) {
   switch (action.type) {
     case RECEIVE_ARTICLE_DETAILS: {
       const newState = { ...state };
-      newState[action.articleId] = action.data.article_details;
+      newState[action.articleId] = { ...action.details, ...action.revisionRange };
       return newState;
     }
     default:
