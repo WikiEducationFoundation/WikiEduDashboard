1deeccc56dc27f1afc9d697781f1e31a95579b68
bhushan354 <deshmukhumesh354@gamil.com>
2024-12-18 14:05:56 +0530

    Implement pagination with 'continue' to load all linked pages

--- lib/training/wiki_training_loader.rb
+++ lib/training/wiki_training_loader.rb
@@ -93,15 +93,23 @@ class WikiTrainingLoader
 
   # Gets a list of page titles linked from the base page
   def wiki_source_pages
-    # To handle more than 500 pages linked from the source page,
-    # we'll need to update this to use 'continue'.
+    source_pages = []
     query_params = { prop: 'links', titles: @wiki_base_page, pllimit: 500 }
-    response = WikiApi.new(MetaWiki.new).query(query_params)
-    begin
-      response.data['pages'].values[0]['links'].map { |page| page['title'] }
-    rescue StandardError
-      raise InvalidWikiContentError, "could not get links from '#{@wiki_base_page}'"
+    loop do
+      response = WikiApi.new(MetaWiki.new).query(query_params)
+  
+      begin
+        source_pages.concat(response.data['pages'].values[0]['links'].map { |page| page['title'] })
+      rescue StandardError
+        raise InvalidWikiContentError, "could not get links from '#{@wiki_base_page}'"
+      end
+  
+      break unless response.data['continue']
+  
+      query_params.merge!(response.data['continue'])
     end
+  
+    source_pages
   end
 
   def listed_wiki_source_pages
