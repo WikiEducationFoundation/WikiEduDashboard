d795da3c18f76c5373dfc92f6f4206e9333f58bd
Abishek Das <abishekdascs@gmail.com>
2024-12-20 18:42:44 +0530

    fix(DiffViewer & ArticleViewer): handle moved articles using MediaWiki page ID
    - Added "crossCheckArticleTitle" function to verify and update article titles and URLs using the MediaWiki API based on the page ID.
    - Introduced "UPDATE_ARTICLE_TITLE_AND_URL" action in the reducer to update article titles and URLs in the Redux store when article title is
      changed.
    - Modified "fetchMissingArticleDetails" of Article used by DiffViewer & ArticleViewer to pass article.title and article.mw_page_id for title
      verification.
    - Updated "fetchArticleDetails" to integrate "crossCheckArticleTitle" for dynamic title validation and updates before fetching article details.
    - Enhanced handling of moved articles by verifying and updating titles via MediaWiki API by using "crossCheckArticleTitle" in ArticleViewer.

--- app/assets/javascripts/actions/article_actions.js
+++ app/assets/javascripts/actions/article_actions.js
@@ -3,19 +3,20 @@ import API from '../utils/api.js';
 import { getRevisionRange } from '../utils/mediawiki_revisions_utils';
 
 // This action uses the Thunk middleware pattern: instead of returning a plain
-// action object, it returns a function that takes the store dispatch fucntion —
+// action object, it returns a function that takes the store dispatch function —
 // which Thunk automatically provides — and can then dispatch a series of plain
 // actions to be handled by the store.
 // This is how actions with side effects — such as API calls — are handled in
 // Redux.
-export function fetchArticleDetails(articleId, courseId) {
-  return function (dispatch) {
+export function fetchArticleDetails(articleId, courseId, articleTitle, article_mw_page_id) {
+  return async function (dispatch) {
+    const crossCheckedArticleTitle = await dispatch(crossCheckArticleTitle(articleId, articleTitle, article_mw_page_id));
+    const wikipediaArticleTitle = crossCheckedArticleTitle === articleTitle ? articleTitle : crossCheckedArticleTitle;
+
     return API.fetchArticleDetails(articleId, courseId)
       .then((response) => {
-        // eslint-disable-next-line no-console
         const details = response.article_details;
-        return getRevisionRange(details.apiUrl, details.articleTitle, details.editors, details.startDate, details.endDate)
-          // eslint-disable-next-line no-console
+        return getRevisionRange(details.apiUrl, wikipediaArticleTitle, details.editors, details.startDate, details.endDate)
           .then(revisionRange => dispatch({ type: types.RECEIVE_ARTICLE_DETAILS, articleId, details, revisionRange }));
       })
       .catch(response => (dispatch({ type: types.API_FAIL, data: response })));
@@ -32,3 +33,37 @@ export function updateArticleTrackedStatus(articleId, courseId, tracked) {
     }))).catch(response => (dispatch({ type: types.API_FAIL, data: response })));
   };
 }
+
+export const crossCheckArticleTitle = (articleId, articleTitle, article_mw_page_id) => {
+  return async (dispatch) => {
+    try {
+      // Fetch the page title from Wikipedia API
+      const response = await fetch(
+        `https://en.wikipedia.org/w/api.php?action=query&pageids=${article_mw_page_id}&format=json&origin=*`
+      );
+
+      if (!response.ok) {
+        throw new Error(`API request failed with status ${response.status}`);
+      }
+
+      const apiResponse = await response.json();
+      const wikipediaArticleTitle = apiResponse.query.pages[article_mw_page_id]?.title;
+
+      if (wikipediaArticleTitle && wikipediaArticleTitle !== articleTitle) {
+        const baseUrl = 'https://en.wikipedia.org/wiki/';
+        const updatedUrl = `${baseUrl}${wikipediaArticleTitle.replace(/ /g, '_')}`;
+
+        dispatch({
+          type: types.UPDATE_ARTICLE_TITLE_AND_URL,
+          payload: { articleId, title: wikipediaArticleTitle, url: updatedUrl },
+        });
+
+        return wikipediaArticleTitle;
+      }
+
+      return articleTitle;
+    } catch (error) {
+      dispatch({ type: types.API_FAIL, data: error });
+    }
+  };
+};
