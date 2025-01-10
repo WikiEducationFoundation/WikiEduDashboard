94a36b6af6daaf939816cf0c466856afa52b8c57
Formasitchijoh <formasitf@gmail.com>
2025-01-10 14:29:52 +0100

    removed chack for loading text on the page

--- spec/features/assigned_articles_spec.rb
+++ spec/features/assigned_articles_spec.rb
@@ -23,7 +23,6 @@ describe 'Assigned Articles view', type: :feature, js: true do
       visit "/courses/#{course.slug}/articles/assigned"
       expect(page).to have_content('Nancy Tuana')
       find('a', text: 'Feedback').click
-      expect(page).to have_no_content(I18n.t('courses.feedback_loading'), wait: 10)
       expect(page).to have_selector('textarea.feedback-form')
       find('textarea.feedback-form').fill_in with: 'This is a great article!'
       click_button 'Add Suggestion'
