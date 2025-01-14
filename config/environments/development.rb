2479eb4ab333ea6e7eff189db8d2c29a72bf920a
Formasitchijoh <formasitf@gmail.com>
2025-01-14 14:42:55 +0100

    Set Paperclip’s command path to /usr/bin

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  
+  Paperclip.options[:command_path] = "/usr/bin"
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  
+  Paperclip.options[:command_path] = "/usr/bin"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
