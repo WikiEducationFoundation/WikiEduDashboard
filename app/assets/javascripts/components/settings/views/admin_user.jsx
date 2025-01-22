44c165e6b7a238621acb2dc8ec811bc1e4f66459
Om Chauhan <omchauhan@Oms-MacBook-Pro.local>
2025-01-22 19:21:08 +0530

    refactored: admin_user.jsx from class to functional component

--- app/assets/javascripts/components/settings/views/admin_user.jsx
+++ app/assets/javascripts/components/settings/views/admin_user.jsx
@@ -1,21 +1,12 @@
-import createReactClass from 'create-react-class';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import React from 'react';
 
-const AdminUser = createReactClass({
-  propTypes: {
-    downgradeAdmin: PropTypes.func,
-    user: PropTypes.shape({
-      id: PropTypes.number,
-      username: PropTypes.string.isRequired,
-      real_name: PropTypes.string,
-      permissions: PropTypes.number.isRequired,
-    }),
-  },
+const AdminUser = ({ user, downgradeAdmin, revokingAdmin }) => {
+  const [confirming, setConfirming] = useState(false);
 
-  getInitialState() {
-    return { confirming: false };
-  },
+  const isRevoking = () => {
+    return revokingAdmin.status && revokingAdmin.username === user.username;
+  };
 
   /*
     returns the current state of the revoking button
@@ -23,83 +14,82 @@ const AdminUser = createReactClass({
     2) "confirming"
     3) "submitting"
   */
-  getButtonState() {
-    if (this.isRevoking()) {
+  const getButtonState = () => {
+    if (isRevoking()) {
       return 'revoking';
-    } else if (this.state.confirming) {
+    } else if (confirming) {
       return 'confirming';
     }
     return 'not confirming';
-  },
+  };
 
-  handleClick() {
-    if (this.state.confirming) {
+  const handleClick = () => {
+    if (confirming) {
       // user has clicked button while confirming. Process!
-      if (!this.isRevoking()) {
+      if (!isRevoking()) {
         // only process if not currently revoking
-        this.props.downgradeAdmin(this.props.user.username);
+        downgradeAdmin(user.username);
       }
-      this.setState({ confirming: false });
+      setConfirming(false);
     } else {
-      this.setState({ confirming: true });
+      setConfirming(true);
     }
-  },
+  };
 
-  isRevoking() {
-    const { user, revokingAdmin } = this.props;
-    return revokingAdmin.status && revokingAdmin.username === user.username;
-  },
+  const adminLevel = user.permissions === 3 ? 'Super Admin' : 'Admin';
 
-  render() {
-    const { user } = this.props;
-    const adminLevel = user.permissions === 3
-      ? 'Super Admin'
-      : 'Admin';
+  let buttonText;
+  let buttonClass = 'button';
+  switch (getButtonState()) {
+    case 'confirming':
+      buttonClass += ' danger';
+      buttonText = I18n.t('settings.admin_users.remove.revoke_button_confirm', { username: user.username });
+      break;
+    case 'revoking':
+      buttonText = I18n.t('settings.admin_users.remove.revoking_button_working');
+      buttonClass += ' border';
+      break;
+    default:
+      // not confirming
+      buttonClass += ' danger';
+      buttonText = I18n.t('settings.admin_users.remove.revoke_button');
+      break;
+  }
 
-    let buttonText;
-    let buttonClass = 'button';
-    switch (this.getButtonState()) {
-      case 'confirming':
-        buttonClass += ' danger';
-        buttonText = I18n.t('settings.admin_users.remove.revoke_button_confirm', { username: user.username });
-        break;
-      case 'revoking':
-        buttonText = I18n.t('settings.admin_users.remove.revoking_button_working');
-        buttonClass += ' border';
-        break;
-      default:
-        // not confirming
-        buttonClass += ' danger';
-        buttonText = I18n.t('settings.admin_users.remove.revoke_button');
-        break;
-    }
+  return (
+    <tr className="user">
+      <td className="user__username">
+        <p>{user.username}</p>
+      </td>
+      <td className="user__real_name">
+        <p>{user.real_name}</p>
+      </td>
+      <td className="user__adminLevel">
+        <p>{adminLevel}</p>
+      </td>
+      <td className="user__revoke">
+        <p>
+          <button className={buttonClass} onClick={handleClick}>
+            {buttonText}
+          </button>
+        </p>
+      </td>
+    </tr>
+  );
+};
 
-    return (
-      <tr className="user">
-        <td className="user__username">
-          <p>{user.username}</p>
-        </td>
-        <td className="user__real_name">
-          <p>{user.real_name}</p>
-        </td>
-        <td className="user__adminLevel">
-          <p>{adminLevel}</p>
-        </td>
-        <td className="user__revoke">
-          <p>
-            <button
-              className={buttonClass}
-              onClick={this.handleClick}
-            >
-              {buttonText}
-            </button>
-          </p>
-
-        </td>
-
-      </tr>
-    );
-  },
-});
+AdminUser.propTypes = {
+  downgradeAdmin: PropTypes.func,
+  revokingAdmin: PropTypes.shape({
+    status: PropTypes.bool,
+    username: PropTypes.string
+  }),
+  user: PropTypes.shape({
+    id: PropTypes.number,
+    username: PropTypes.string.isRequired,
+    real_name: PropTypes.string,
+    permissions: PropTypes.number.isRequired,
+  }),
+};
 
 export default AdminUser;
