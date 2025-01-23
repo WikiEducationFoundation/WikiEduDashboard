ad0f26a0b90d3db16f7d86d0680d06681bf95528
Jiya Gupta <coderaholic21@gmail.com>
2025-01-23 19:44:08 +0530

    added both json and csv routes to download personal data

--- config/routes.rb
+++ config/routes.rb
@@ -64,7 +64,8 @@ Rails.application.routes.draw do
 
   #PersonalDataController
   controller :personal_data do
-    get 'download_personal_data' => 'personal_data#personal_data_csv', as: :download_personal_data
+    get 'download_personal_data' => 'personal_data#show'
+    get 'download_personal_data_csv' => 'personal_data#personal_data_csv'
   end
 
   # Users
