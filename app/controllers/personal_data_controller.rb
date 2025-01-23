d12381418baa2e82cdb0cc5656bca1d2a8d50485
Jiya Gupta <coderaholic21@gmail.com>
2025-01-23 19:46:11 +0530

    added option for json in the controller to download the personal data

--- app/controllers/personal_data_controller.rb
+++ app/controllers/personal_data_controller.rb
@@ -5,7 +5,7 @@ require_dependency "#{Rails.root}/lib/personal_data/personal_data_csv_builder.rb
 # Allows users to download the personal data about them stored on the Dashboard
 class PersonalDataController < ApplicationController
   before_action :require_signed_in
-  respond_to :csv, only: [:personal_data_csv]
+  respond_to :json, :csv
 
   def show
     @user = current_user
