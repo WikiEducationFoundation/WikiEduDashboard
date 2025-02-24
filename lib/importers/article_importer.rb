17d3e702108e554773ebe641702198083f086457
empty-codes <morayotimothy@gmail.com>
2025-02-24 19:22:41 +0100

    Fixes `414 request-uri too long` for `import_articles_by_title` by using a POST request to send the query parameters in the `request.body` instead of a GET request: - Adds `kn.wikipedia.org` to list of stubbed wiki validations - Adds 3 API post methods, `api_post`, `do_post` and `too_many_requests` for creating and sending the post requests - Refactors `import_articles_by_title` since response is now a hash - Adds a test that confirms `414 request-uri too long` no longer occurs

--- lib/importers/article_importer.rb
+++ lib/importers/article_importer.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 require_dependency "#{Rails.root}/lib/replica"
+require_dependency "#{Rails.root}/lib/errors/api_error_handling"
 
 #= Imports articles from Wikipedia into the dashboard database
 class ArticleImporter
+  include ApiErrorHandling
+
   def initialize(wiki)
     @wiki = wiki
     @articles = []
@@ -25,11 +28,14 @@ class ArticleImporter
     # Slice size is limited by max URI length.
     # 40 is too much for some languages, such as bn.wipedia.org
     titles.each_slice(30) do |some_article_titles|
-      query = { prop: 'info', titles: some_article_titles }
-      response = WikiApi.new(@wiki).query(query)
-      results = response&.data
-      next if results.blank?
-      results = results['pages']
+      params = {
+        action: 'query',
+        format: 'json',
+        prop: 'info',
+        titles: some_article_titles.join('|')
+      }
+      response = api_post(@wiki, params)
+      results = response.dig('query', 'pages')
       next if results.blank?
       import_articles_from_title_query(results)
     end
@@ -59,5 +65,41 @@ class ArticleImporter
                               namespace: page_data['ns'].to_i)
     end
     Article.import articles, on_duplicate_key_update: [:title, :namespace]
+  end
+
+  ###############
+  # API methods #
+  ###############
+
+  def api_post(wiki, params)
+    tries ||= 3
+    response = do_post(wiki, params)
+    return unless response
+    parsed_body = JSON.parse(response.body)
+    return if parsed_body.empty?
+    parsed_body
+  rescue StandardError => e
+    tries -= 1
+    sleep 1 if too_many_requests?(e)
+    retry unless tries.zero?
+    log_error(e, update_service: @update_service,
+              sentry_extra: { params:, api_url: @api_url })
+    return nil
+  end
+
+  def do_post(wiki, params)
+    uri = URI(wiki.api_url)
+    https = Net::HTTP.new(uri.host, uri.port)
+    https.use_ssl = true
+
+    request = Net::HTTP::Post.new(uri)
+    request['Content-Type'] = 'application/x-www-form-urlencoded'
+    request.body = URI.encode_www_form(params)
+    https.request(request)
+  end
+
+  def too_many_requests?(e)
+    return false unless e.instance_of?(MediawikiApi::HttpError)
+    e.status == 429
   end
 end
