e24ee1d0f21dd4d7ce83e48a9f63bf81f0e21dd1
Cvm <cvmcosta@ltiaas.com>
2025-03-06 10:02:13 -0300

    Adding comments

--- app/models/lti_context.rb
+++ app/models/lti_context.rb
@@ -5,10 +5,10 @@
 #
 #  id                 :integer          not null, primary key
 #  user_id            :integer          not null, foreign_key
-#  user_lti_id        :string(255)      not null
-#  context_id         :string(255)      not null
-#  lms_id             :string(255)      not null
-#  lms_family         :string(255)
+#  user_lti_id        :string(255)      not null - LMS User ID
+#  context_id         :string(255)      not null - LMS Context ID (Course ID + Resource Link ID)
+#  lms_id             :string(255)      not null - LMS ID
+#  lms_family         :string(255) - LMS Family Code (e.g. 'canvas')
 #
 
 class LtiContext < ApplicationRecord
