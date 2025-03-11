cfb0f502433ee5be4ef83c6dde7350b12d075cbe
Sage Ross <ragesoss@gmail.com>
2025-03-11 10:35:33 -0700

    Take sentry sample rate from application.yml
    Traces come in very rapidly at 1.0 sampling. I'm setting Programs & Events Dashboard to 0.01 for now.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
   config.dsn = ENV['sentry_dsn']
   config.enabled_environments = %w[development staging production test]
-  config.traces_sample_rate = 1.0
+  config.traces_sample_rate = ENV['sentry_sample_rate'].to_f
 end
