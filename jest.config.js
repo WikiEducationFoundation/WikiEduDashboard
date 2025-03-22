caed535c4ec0266a3f9a31a27e2b19cbd84ed62e
Abishek Das <abishekdascs@gmail.com>
2025-03-22 19:17:17 +0530

    test: improve Jest setup for better compatibility and stability
    - Polyfilled 'fetch', 'Response', and 'Request' using 'whatwg-fetch'
    - Added global 'Text Encoder' and 'TextDecoder' to prevent encoding-related errors
    - Polyfilled 'TransformStream' from 'stream/web' to support APIs that require it
    - Mocked 'I18n.t()' to prevent translation errors during testing
    - Added a mock 'BroadcastChannel' for compatibility with Jest 26
    - Updated 'jest.config.js' to correctly transform ESM dependencies (`@bundled-es-modules`)
    - Ensured Jest uses 'jsdom' as the test environment for React component tests

--- jest.config.js
+++ jest.config.js
@@ -14,7 +14,10 @@ module.exports = {
 
   // all these are ESM modules which must be converted to CJS
   transformIgnorePatterns: [
-    '/node_modules/(?!@reduxjs/toolkit|@react-dnd|react-dnd|dnd-core|react-dnd-html5-backend|lodash-es|i18n-js)',
+    '/node_modules/(?!@reduxjs/toolkit|@react-dnd|react-dnd|dnd-core|react-dnd-html5-backend|lodash-es|i18n-js|@bundled-es-modules)',
   ],
   setupFiles: ['./test/setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js', '<rootDir>/test/setup_env.js'],
+  // Ensures a browser-like environment for React tests
+  testEnvironment: 'jsdom',
 };
