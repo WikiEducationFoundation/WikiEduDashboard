4c486ce347e99cf213ddd6136c4bede7ea7a3250
Abishek Das <abishekdascs@gmail.com>
2025-04-18 23:45:42 +0530

    Improve tests for AverageViewsImporter with broader article scenarios

--- spec/lib/importers/average_views_importer_spec.rb
+++ spec/lib/importers/average_views_importer_spec.rb
@@ -18,10 +18,22 @@ describe AverageViewsImporter do
   end
 
   describe '.update_outdated_average_views' do
+    let!(:article_never_updated) do
+      create(:article, title: 'Petrichor', average_views: 1, average_views_updated_at: nil)
+    end
+
+    let!(:article_very_old) do
+      create(:article, title: 'ObsoleteTech', average_views: 1, average_views_updated_at: 2.months.ago) # rubocop:disable Layout/LineLength
+    end
+
     it 'does not update recently-updated records' do
       course = create(:course)
+      articles = [article, article_never_updated, article_very_old]
+      articles.each { |a| create(:articles_course, article: a, course:) }
       described_class.update_outdated_average_views(course)
       expect(article.reload.average_views).to eq(1)
+      expect(article_never_updated.reload.average_views).to eq(1)
+      expect(article_very_old.reload.average_views).to eq(1)
     end
   end
 end
