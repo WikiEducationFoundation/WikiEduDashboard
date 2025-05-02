19602c5c1a08e2f937c191e98b19b91454bc8384
gabina <gabinaluz@gmail.com>
2025-05-01 23:16:32 -0300

    Delete CourseRevisionUpdater.import_revisions

--- lib/course_revision_updater.rb
+++ lib/course_revision_updater.rb
@@ -8,23 +8,9 @@ class CourseRevisionUpdater
   ###############
   # Entry point #
   ###############
-  def self.import_revisions(course, all_time:, update_service: nil)
-    updater = new(course, update_service:)
-    return if updater.no_point_in_importing_revisions?
-    updater.update_revisions_for_relevant_wikis(all_time)
-    ArticlesCourses.update_from_course(course)
-  end
-
   def initialize(course, update_service: nil)
     @course = course
     @update_service = update_service
-  end
-
-  def update_revisions_for_relevant_wikis(all_time)
-    @course.wikis.each do |wiki|
-      RevisionImporter.new(wiki, @course, update_service: @update_service)
-                      .import_revisions_for_course(all_time:)
-    end
   end
 
   def no_point_in_importing_revisions?
