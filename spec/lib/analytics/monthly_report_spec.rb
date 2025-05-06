10001e1603269032e26baef3ab75f306cedc7ac0
gabina <gabinaluz@gmail.com>
2025-05-06 18:47:23 -0300

    Update article_course_timeslices_during_month to only consider non-empty timeslices as they were created due to revisions with system = true, and we don't want to count an article as edited if it was only modified due to a system revision.

--- spec/lib/analytics/monthly_report_spec.rb
+++ spec/lib/analytics/monthly_report_spec.rb
@@ -19,11 +19,11 @@ describe MonthlyReport do
       # Create two articles
       create(:article, id:, title: "Article_#{id}", namespace: Article::Namespaces::MAINSPACE)
       # Create timeslices
-      create(:article_course_timeslice, course_id: id, article_id: id,
+      create(:article_course_timeslice, course_id: id, article_id: id, revision_count:,
       start: 1.month.ago, end: 1.month.ago + 1.day)
-      create(:article_course_timeslice, course_id: id, article_id: id,
+      create(:article_course_timeslice, course_id: id, article_id: id, revision_count:,
       start: 1.month.ago + 1.day, end: 1.month.ago + 2.days)
-      create(:article_course_timeslice, course_id: id, article_id: id,
+      create(:article_course_timeslice, course_id: id, article_id: id, revision_count:,
       start: 2.months.ago, end: 2.months.ago + 1.day)
       # Create uploads
       create(:commons_upload, id:, user_id: id, uploaded_at: 1.month.ago, usage_count: 1)
@@ -35,7 +35,7 @@ describe MonthlyReport do
       # Create article
       create(:article, id: id2, title: "Article_#{id2}")
       # Create timeslice
-      create(:article_course_timeslice, course_id: id, article_id: id2,
+      create(:article_course_timeslice, course_id: id, article_id: id2, revision_count:,
       start: 13.months.ago, end: 13.months.ago + 1.day)
       # Create uploads
       create(:commons_upload, user_id: id2, uploaded_at: 13.months.ago, usage_count: 1)
@@ -48,11 +48,24 @@ describe MonthlyReport do
   end
 
   describe '.run' do
-    let(:subject) { described_class.run }
+    context 'when timeslices have revisions' do
+      let(:revision_count) { 15 }
+      let(:subject) { described_class.run }
 
-    it 'counts articles and uploads' do
-      expect(subject[:'2025-4']).to(eq({ articles_edited: 6, uploads: 6 }))
-      expect(subject[:'2024-4']).to(eq({ articles_edited: 4, uploads: 8 }))
+      it 'counts articles and uploads' do
+        expect(subject[:'2025-4']).to(eq({ articles_edited: 6, uploads: 6 }))
+        expect(subject[:'2024-4']).to(eq({ articles_edited: 4, uploads: 8 }))
+      end
+    end
+
+    context 'when timeslices are empty' do
+      let(:revision_count) { 0 }
+      let(:subject) { described_class.run }
+
+      it 'only counts uploads' do
+        expect(subject[:'2025-4']).to(eq({ articles_edited: 0, uploads: 6 }))
+        expect(subject[:'2024-4']).to(eq({ articles_edited: 0, uploads: 8 }))
+      end
     end
   end
 end
