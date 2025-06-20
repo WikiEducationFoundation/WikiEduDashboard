f6c9ddd75d9e4cedf75dffac2200f7f0412acf9c
Sage Ross <ragesoss@gmail.com>
2025-06-20 09:47:53 -0700

    Fix experiments presenter spec
    There are no active experiments, so this is hard to test.

--- spec/presenters/experiments_presenter_spec.rb
+++ spec/presenters/experiments_presenter_spec.rb
@@ -15,27 +15,27 @@ describe ExperimentsPresenter do
       end
     end
 
-    context 'when the course is in an active experiment' do
-      let(:flags) { { spring_2018_cmu_experiment: 'email_sent' } }
+    # context 'when the course is in an active experiment' do
+    #   let(:flags) { { spring_2018_cmu_experiment: 'email_sent' } }
 
-      it 'retuns the experiment class' do
-        expect(subject.experiment).to eq(Spring2018CmuExperiment)
-      end
-    end
+    #   it 'retuns the experiment class' do
+    #     expect(subject.experiment).to eq(Spring2018CmuExperiment)
+    #   end
+    # end
   end
 
-  describe '#notification' do
-    context 'when the course is in an active experiment' do
-      let(:flags) do
-        {
-          spring_2018_cmu_experiment: 'email_sent',
-          spring_2018_cmu_experiment_email_code: 'secret'
-        }
-      end
+  # describe '#notification' do
+  #   context 'when the course is in an active experiment' do
+  #     let(:flags) do
+  #       {
+  #         spring_2018_cmu_experiment: 'email_sent',
+  #         spring_2018_cmu_experiment_email_code: 'secret'
+  #       }
+  #     end
 
-      it 'includes an opt_in_link with the email code' do
-        expect(subject.notification[:opt_in_link]).to match(/secret/)
-      end
-    end
-  end
+  #     it 'includes an opt_in_link with the email code' do
+  #       expect(subject.notification[:opt_in_link]).to match(/secret/)
+  #     end
+  #   end
+  # end
 end
