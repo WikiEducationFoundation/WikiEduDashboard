7b265ef38b68ba11af140d550813a6f5ae930dd3
gabina <gabinaluz@gmail.com>
2025-07-09 11:50:11 -0300

    Remove unnecessary revisions from spec

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -7,14 +7,6 @@ describe ArticlesController, type: :request do
   let(:user) { create(:user) }
   let(:second_user) { create(:user, username: 'SecondUser') }
   let(:course) { create(:course) }
-  let!(:revision1) do
-    create(:revision, article_id: article.id, user_id: user.id,
-                      date: course.start + 1.day, mw_rev_id: 123)
-  end
-  let!(:revision2) do
-    create(:revision, article_id: article.id, user_id: second_user.id,
-                      date: course.end - 1.day, mw_rev_id: 234)
-  end
 
   before do
     create(:courses_user, user_id: user.id, course_id: course.id)
