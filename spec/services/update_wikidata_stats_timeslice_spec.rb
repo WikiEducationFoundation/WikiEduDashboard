2a3f7843b64e4ee309e60af732539388c82fbf7e
Gabina Luz Bianchi <gabinaluz@gmail.com>
2025-07-22 17:12:30 -0300

    Handle errors from wikidata diff analyzer gem (#6409)
    * Add RevisionOnMemory class including ActiveModel to remove Revision class that inherits from ApplicationRecord
    
    * Update UpdateWikidataStatsTimeslice class to work with RevisionOnMemory
    
    * Start using RevisionOnMemory instead of Revision class.
    
    * Set characters attribute as integer to fix CourseRevisionUpdater specs
    
    * Fix CourseWikiTimeslice specs
    
    * Fix CourseUserWikiTimeslice  specs
    
    * Fix ArticleCourseTimeslice specs
    
    * Fix TimesliceManager specs
    
    * Fix ArticlesCourses specs
    
    * Fix IndividualStatisticsTimeslicePresenter specs
    
    * Force features to be an empty hash instead of nil. Fix RevisionScoreApiHandler specs.
    
    * Fix UpdateWikidataStatsTimeslice and RevisionScoreApiHandler specs
    
    * Fix ruby linting
    
    * Remove unnecessary revisions from spec
    
    * Remove unnecessary revisions from article viewer spec
    
    * Remove unnecessary revisions from articles edited view spec
    
    * Use RevisionOnMemory class in course page spec
    
    * Remove unnecessary revisions from students page spec
    
    * Remove unnecessary revisions from user profile page spec
    
    * Remove unnecessary revisions from wikidata course spec
    
    * Remove unnecessary revisions from histogram plotter spec
    
    * Remove unnecessary revisions from suspected plagiarism mailer spec
    
    * Remove belong_to relationship between alert and revision.
    
    * Remove unnecessary revisions from course spec
    
    * Remove unused views
    
    * Remove unused infer_courses_from_user method
    
    * Remove url method from Revision class. Move url specs from revision specs to alert specs.
    
    * Remove unused references_added method from Revision class and move its  specs to revision on memory.
    
    * Removed unused edit_summaryand diff_stats methods from Revision class. Remove revision_spec file.
    
    * Remove Revision scopes, attr_accessor, and requirements
    
    * Clarify assumptions for revision_url method
    
    * Remove table comments in RevisionOnMemory class
    
    * Add retry logic when fetching wikidata stats
    
    * Add specs
    
    * Mark revisions with error to reprocess in the future and do not raise error in update_revisions_with_stats
    
    * Fix rubocop for update_revisions_with_stats

--- spec/services/update_wikidata_stats_timeslice_spec.rb
+++ spec/services/update_wikidata_stats_timeslice_spec.rb
@@ -46,5 +46,39 @@ describe UpdateWikidataStatsTimeslice do
       expect(CourseStat.last.stats_hash).not_to be_nil
       expect(CourseStat.last.course_id).to eq(Course.last.id)
     end
+
+    context 'request fails' do
+      it 'retries 3 times', :vcr do
+        call_count = 0
+        allow(WikidataDiffAnalyzer).to receive(:analyze)
+          .and_wrap_original do |original_method, *args, &block|
+          if call_count < 2
+            call_count += 1
+            raise MediawikiApi::HttpError
+          else
+            original_method.call(*args, &block)
+          end
+        end
+
+        updater.update_revisions_with_stats(revisions)
+        expect(revision1.summary).not_to be_nil
+        expect(revision1.summary).not_to eq('null')
+        expect(revision2.summary).not_to be_nil
+        expect(revision2.summary).not_to eq('null')
+      end
+
+      it 'logs error once and marks revisions with error if request fails 3 times' do
+        allow(WikidataDiffAnalyzer).to receive(:analyze)
+          .and_raise(MediawikiApi::HttpError, '')
+        expect(updater).to receive(:log_error).once
+        updater.update_revisions_with_stats(revisions)
+        expect(revision1.summary).to be_nil
+        expect(revision1.error).to eq(true)
+        expect(revision2.summary).to be_nil
+        expect(revision2.error).to eq(true)
+        expect(unscoped_revision.summary).to be_nil
+        expect(unscoped_revision.error).to be_nil
+      end
+    end
   end
 end
