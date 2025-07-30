9df15675d75fbbe7159c0653786edf23ec35bb61
gabina <gabinaluz@gmail.com>
2025-07-30 16:17:23 -0300

    Fix typo

--- docs/cleanup_scripts/deduplicate_articles_courses.rb
+++ docs/cleanup_scripts/deduplicate_articles_courses.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 # If two (or more) course updates run at the same time, we end up with duplicate articles courses.
-# This script deduplicate articles courses, deleting the first one.
+# This script deduplicates articles courses, deleting the first one.
 # Note that you may need to deduplicate an article several times if the article appears three
 # times or more.
 dups = ArticlesCourses.all.group(:course_id, :article_id).having('count(*) > 1').count
