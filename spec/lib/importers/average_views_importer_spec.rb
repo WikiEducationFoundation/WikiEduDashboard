86db91bed8dc302caab90f8b9c69dc5718163ac7
Solly <solobarine@gmail.com>
2025-10-14 14:49:42 +0100

    feat: update test to reflect update

--- spec/lib/importers/average_views_importer_spec.rb
+++ spec/lib/importers/average_views_importer_spec.rb
@@ -60,8 +60,8 @@ describe AverageViewsImporter do
         described_class.update_outdated_average_views(course)
       end
       expect(articles_course.reload.average_views).to eq(1)
-      expect(article_course_never_updated.reload.average_views).to be > 1400
-      expect(article_course_very_old.reload.average_views).to eq(0)
+      expect(article_course_never_updated.reload.average_views).to eq(1396.2)
+      expect(article_course_very_old.reload.average_views).to eq(1)
     end
 
     it 'does not update AC record if no first_revision' do
