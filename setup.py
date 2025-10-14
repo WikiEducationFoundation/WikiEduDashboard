0bbcfabf98fb749067279e694f191276e61cdf23
Ajay Singh <37625112+Ajay-singh1@users.noreply.github.com>
2025-10-14 22:08:47 +0530

    Fix: Setup script for Fedora OS (#6486)
    * Fix: Setup script for Fedora OS
    
    Signed-off-by: Ajay Singh <ajaykripa8736968359@gmail.com>
    
    * Add a comment explaining current platform naming convention
    
    Signed-off-by: Ajay Singh <ajaykripa8736968359@gmail.com>
    
    * Trigger CI to test flaky spec
    
    ---------
    
    Signed-off-by: Ajay Singh <ajaykripa8736968359@gmail.com>

--- setup.py
+++ setup.py
@@ -42,7 +42,9 @@ if platform.platform().lower().find('ubuntu') != -1 \
         or platform.platform().lower().find('elementary') != -1 \
         or platform.uname().version.lower().find('ubuntu') != -1:
     deb_setup()
-elif platform.platform().lower().find('fedora') != -1:
+# If the current operating system is Fedora (identified by 'fedora' or 'fc' in the platform string),
+# then run the dnf_setup() function to configure dependencies specific to Fedora-based systems.
+elif platform.platform().lower().find('fedora') != -1 or platform.platform().lower().find('fc') != -1:
     dnf_setup()
 elif platform.platform().lower().find('darwin') != -1 \
         or platform.platform().lower().find('mac') != -1: 
