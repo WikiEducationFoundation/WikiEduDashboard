bf9a687d472b83bb2cabd1873e31a4278c78e22d
boyani-Otieno <boyani.ombogo7@gmailcom>
2025-10-17 10:43:48 +0300

    Fix ESLint errors in SurveyAssignmentAdmin.js

--- app/assets/javascripts/surveys/modules/SurveyAssignmentAdmin.js
+++ app/assets/javascripts/surveys/modules/SurveyAssignmentAdmin.js
@@ -7,23 +7,23 @@ const SurveyAssignmentAdmin = {
   },
 
   listeners() {
-    document.querySelectorAll('[data-toggle-courses-table]').forEach(button => {
-      button.addEventListener('click', (e) => this.toggleCoursesTable(e));
+    document.querySelectorAll('[data-toggle-courses-table]').forEach((button) => {
+      button.addEventListener('click', e => this.toggleCoursesTable(e));
     });
   },
 
   sortableTables() {
-    document.querySelectorAll('[data-sortable-courses]').forEach(sortableCourses => {
+    document.querySelectorAll('[data-sortable-courses]').forEach((sortableCourses) => {
       const options = {
         valueNames: ['title', 'id']
       };
-      new List(sortableCourses, options);
+      // eslint-disable-next-line no-new
+      const list = new List(sortableCourses, options);
     });
   },
 
     toggleCoursesTable(event) {
     const target = event.currentTarget;
-    // Find the closest parent with .block and toggle the active class on the data-sortable-courses element inside
     const block = target.closest('.block');
     const table = block?.querySelector('[data-sortable-courses]');
     if (table) {
