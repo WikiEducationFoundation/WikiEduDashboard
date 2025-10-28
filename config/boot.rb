3314a6a86d9eb81682ce70ff98766cd4bc2c4217
Wahab Saheed Oluwatoyin <wahabsaheed@gmail.com>
2025-10-28 12:45:32 +0100

    Fix: rebuild Gemfile.lock to restore missing gems

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,5 @@
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-
+require 'logger'
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
 require 'bootsnap/setup'
