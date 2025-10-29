9ce49ed67ac63da504cebaf2b6ea3d39af86191a
gabina <gabinaluz@gmail.com>
2025-10-28 22:53:38 -0300

    Update comments in AiEditAlertsStatsController

--- app/controllers/ai_edit_alerts_stats_controller.rb
+++ app/controllers/ai_edit_alerts_stats_controller.rb
@@ -31,28 +31,28 @@ class AiEditAlertsStatsController < ApplicationController
     @followups = (@alerts.map { |a| a.followup_student || a.followups.values.first }).compact
   end
 
-  # Returns a hash of counts by page types.
+  # Sets a hash of counts by page types.
   # Example:
   # {:choose_an_article=>1, :evaluate_an_article=>2, :bibliography=>1, :outline=>1, :sandbox=>2}
   def set_count_by_page_type
     @count_by_page_type = @alerts.group_by(&:page_type).transform_values(&:count)
   end
 
-  # Returns the number of unique students with multiple alerts
+  # Sets the number of unique students with multiple alerts
   def set_student_count_with_multiple_alerts
     @student_count_with_multiple_alerts = @alerts
                                           .filter(&:prior_alert_id_for_user)
                                           .map(&:user_id).uniq.count
   end
 
-  # Returns the number of unique pages with multiple alerts
+  # Sets the number of unique pages with multiple alerts
   def set_page_count_with_multiple_alerts
     @page_count_with_multiple_alerts = @alerts
                                        .filter(&:prior_alert_id_for_page)
                                        .map(&:article_id).uniq.count
   end
 
-  # Returns a list of alerts with a followup completed in the last RECENT_ALERTS_DAYS days.
+  # Sets a list of alerts with a followup completed in the last RECENT_ALERTS_DAYS days.
   # For now, we rely on the updated_at alert field to detrmine when a followup was answered.
   def set_alerts_with_recent_followup
     @alerts_with_recent_followup = @alerts.where('alerts.updated_at > ?',
@@ -64,14 +64,14 @@ class AiEditAlertsStatsController < ApplicationController
     @alerts.where('alerts.created_at > ?', RECENT_ALERTS_DAYS.days.ago)
   end
 
-  # Returns a list of alerts created in the last RECENT_ALERTS_DAYS days
+  # Sets a list of alerts created in the last RECENT_ALERTS_DAYS days
   # for students with previous alerts in the same campaign.
   def set_recent_alerts_for_students_with_multiple_alerts
     @recent_alerts_for_students_with_multiple_alerts =
       recent_alerts.filter(&:prior_alert_id_for_user)
   end
 
-  # Returns alerts for articles in mainspace created in the last RECENT_ALERTS_DAYS days.
+  # Sets alerts for articles in mainspace created in the last RECENT_ALERTS_DAYS days.
   def set_recent_alerts_for_mainspace
     @recent_alerts_for_mainspace = recent_alerts
                                    .where('article.namespace': Article::Namespaces::MAINSPACE)
