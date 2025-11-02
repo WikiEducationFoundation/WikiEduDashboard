1422a94f51d4f0b20fec827e680658be5c7c388e
Wahab Saheed Oluwatoyin <wahabsaheed@gmail.com>
2025-11-02 13:52:03 +0100

    update schema.rb file

--- db/migrate/20250730205439_remove_index_articles_courses_on_course_id.rb
+++ db/migrate/20250730205439_remove_index_articles_courses_on_course_id.rb
@@ -1,7 +1,8 @@
 class RemoveIndexArticlesCoursesOnCourseId < ActiveRecord::Migration[7.0]
   def change
-    if index_exists?(:articles_courses, name: :index_articles_courses_on_course_id)
-      remove_index :articles_courses, name: :index_articles_courses_on_course_id
+    if index_exists?(:articles_courses, [:course_id], name: "index_articles_courses_on_course_id")
+      remove_index :articles_courses, name: "index_articles_courses_on_course_id"
+      
     end
   end
 end
