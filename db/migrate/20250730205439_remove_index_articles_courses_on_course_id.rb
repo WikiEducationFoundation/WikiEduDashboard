96ec75bdac035c31077fa95f37d2af09313184e1
Wahab Saheed Oluwatoyin <wahabsaheed@gmail.com>
2025-11-02 15:38:47 +0100

    fix: db migration related error

--- db/migrate/20250730205439_remove_index_articles_courses_on_course_id.rb
+++ db/migrate/20250730205439_remove_index_articles_courses_on_course_id.rb
@@ -2,7 +2,6 @@ class RemoveIndexArticlesCoursesOnCourseId < ActiveRecord::Migration[7.0]
   def change
     if index_exists?(:articles_courses, [:course_id], name: "index_articles_courses_on_course_id")
       remove_index :articles_courses, name: "index_articles_courses_on_course_id"
-      
     end
   end
 end
