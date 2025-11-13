5f5f0f0940cb2948e1ec0f61b729afeb237d9198
gabina <gabinaluz@gmail.com>
2025-11-12 21:44:06 -0300

    Pass in rev_date as an integer to avoid WARN: Job arguments to AiDetectionWorker do not serialize to JSON safely. This will raise an error in Sidekiq 7.0

--- app/workers/ai_detection_worker.rb
+++ app/workers/ai_detection_worker.rb
@@ -7,7 +7,7 @@ class AiDetectionWorker
   sidekiq_options lock: :until_executed
 
   def self.schedule_check(wiki:, revision:, course:)
-    perform_async(wiki.id, revision.mw_rev_id, revision.user_id, course.id, revision.date)
+    perform_async(wiki.id, revision.mw_rev_id, revision.user_id, course.id, revision.date.to_i)
   end
 
   def perform(wiki_id, mw_rev_id, user_id, course_id, rev_date)
