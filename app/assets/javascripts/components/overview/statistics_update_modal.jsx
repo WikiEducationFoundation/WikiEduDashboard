bc88d0b946afe2a6d95cbbd2d2e8d6d2f5ba52bb
Boyani <86007093+sylvia-boyani@users.noreply.github.com>
2025-12-17 20:25:52 +0300

    Add tracking description#1857 (#6523)
    * display tracking description on course overview page
    
    * updated tracking description into dedicated component
    
    * revert unintended change to format_wiki_namespaces in course_helper
    
    * Fix RuboCop layout issues in CourseHelper
    
    * replace filter_map for cleaner wiki filtering in CourseHelper
    
    * Refactor tracking description to show in the popover.
    
    Moved the tracking description logic to a service object.
    Updated the modal to display the tracking description after update status.
    Removed the old tracking_description component and related code.
    
    * added deleted tracking_description file
    
    * Simplified statistics_update_modal.jsx by delegating log summaries, counts, and next-update messages to the new updateLog component
    
    * Fixed eslint errors
    
    * fix rubocop style errors
    
    * add yarn.lock and minor edits on update.logs
    
    * Refactor the tracking description to be computed on the frontend
    
    * Remove TrackingDescriptionBuilder call from CoursesController#show to fix 500 errors
    
    * fix rubocop error
    
    ---------
    
    Co-authored-by: boyani-Otieno <boyani.ombogo7@gmailcom>

--- app/assets/javascripts/components/overview/statistics_update_modal.jsx
+++ app/assets/javascripts/components/overview/statistics_update_modal.jsx
@@ -1,58 +1,90 @@
 import React from 'react';
-import { nextUpdateExpected, getLastUpdateSummary, getTotaUpdatesMessage, getUpdateLogs } from '../../utils/statistic_update_info_utils';
 import ArticleUtils from '../../utils/article_utils';
 import { isAfter, format } from 'date-fns';
 import { getUTCDate, toDate } from '../../utils/date_utils';
+import { computeTrackingDescription } from '../../utils/statistic_update_info_utils';
+import TrackingDescription from './tracking_description';
+import UpdateLogs from './update_logs';
 
 const StatisticsUpdateModal = (props) => {
   const course = props.course;
-  const helpMessage = Features.wikiEd ? I18n.t('metrics.wiki_ed_help') : I18n.t('metrics.outreach_help');
+  const helpMessage = Features.wikiEd
+    ? I18n.t('metrics.wiki_ed_help') : I18n.t('metrics.outreach_help');
+
   const updatesEndMoment = toDate(course.update_until);
   const futureUpdatesRemaining = isAfter(updatesEndMoment, new Date());
-  const futureUpdatesMessage = futureUpdatesRemaining ? I18n.t('metrics.future_updates_remaining.updates_active', { date: format(getUTCDate(updatesEndMoment), 'MMMM do yyyy') }) : I18n.t('metrics.future_updates_remaining.updates_inactive');
-  const additionalUpdateMessage = course.needs_update ? I18n.t('metrics.non_updating_course_update') : '';
 
-  const lastUpdateSummary = getLastUpdateSummary(course);
-  const updateLogs = getUpdateLogs(course);
+  const futureUpdatesMessage = futureUpdatesRemaining
+    ? I18n.t('metrics.future_updates_remaining.updates_active', {
+        date: format(getUTCDate(updatesEndMoment), 'MMMM do yyyy')
+      })
+    : I18n.t('metrics.future_updates_remaining.updates_inactive');
 
-  const failureUpdatesCount = updateLogs.filter(log => log.orphan_lock_failure !== undefined).length;
-  const erroredUpdatesCount = updateLogs.filter(log => log.error_count !== undefined && log.error_count > 0).length;
-  const recentUpdatesSummary = I18n.t('metrics.recent_updates_summary', { total: updateLogs.length, failure_count: failureUpdatesCount, error_count: erroredUpdatesCount });
+  const additionalUpdateMessage = course.needs_update
+    ? I18n.t('metrics.non_updating_course_update')
+    : '';
 
-  // Update numbers (ids) are stored incrementally as counts in update_logs, so the
-  // last update number is the total number of updates till now
-
-  const totalUpdatesMessage = getTotaUpdatesMessage(course);
-
-  const isNextUpdateAfter = props.isNextUpdateAfter;
-  let nextUpdateMessage = props.nextUpdateMessage;
-
-  if (!isNextUpdateAfter) {
-    nextUpdateMessage = I18n.t('metrics.late_update', { late_update_time: nextUpdateExpected(course) });
-  }
+  const trackingDescription = computeTrackingDescription(course);
 
   return (
     <div className="statistics-update-modal-container">
       <div className="statistics-update-modal">
         <b>{I18n.t('metrics.update_status_heading')}</b>
-        <br/>
-        { lastUpdateSummary }
-        <ul>
-          <li>{ recentUpdatesSummary }</li>
-          <li>{ totalUpdatesMessage }</li>
-          <li>{nextUpdateMessage}</li>
-          <li>{futureUpdatesMessage} {additionalUpdateMessage}</li>
-        </ul>
+        <br />
+
+        <UpdateLogs
+          course={course}
+          isNextUpdateAfter={props.isNextUpdateAfter}
+          nextUpdateMessage={props.nextUpdateMessage}
+          futureUpdatesMessage={futureUpdatesMessage}
+          additionalUpdateMessage={additionalUpdateMessage}
+        />
+
+        {/* Tracking Description */}
+        {trackingDescription && (
+          <>
+            <b>
+              {I18n.t('metrics.tracking_status_title', {
+                defaultValue: 'Tracking Status'
+              })}
+            </b>
+            <TrackingDescription trackingDescription={trackingDescription} />
+          </>
+        )}
+
         <b>{I18n.t('metrics.missing_data_heading')}</b>
-        <br/>
-        { I18n.t('metrics.missing_data_info') }:
+        <br />
+        {I18n.t('metrics.missing_data_info')}:
         <ul>
-          <li>{ I18n.t('metrics.replag_info') } <a href="https://replag.toolforge.org/" target="_blank">{I18n.t('metrics.replag_link')}</a></li>
-          { course.type === 'ArticleScopedProgram' && <li>{ I18n.t(`metrics.${ArticleUtils.projectSuffix(course.home_wiki.project, 'article_scoped_program_info')}`) }</li> }
+          <li>
+            {I18n.t('metrics.replag_info')}{' '}
+            <a
+              href="https://replag.toolforge.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {I18n.t('metrics.replag_link')}
+            </a>
+          </li>
+
+          {course.type === 'ArticleScopedProgram' && (
+            <li>
+              {I18n.t(
+                `metrics.${ArticleUtils.projectSuffix(
+                  course.home_wiki.project,
+                  'article_scoped_program_info'
+                )}`
+              )}
+            </li>
+          )}
         </ul>
-        <small>{ helpMessage }</small>
-        <br/>
-        <button className="button dark" onClick={props.toggleModal}>{I18n.t('metrics.close_modal')}</button>
+
+        <small>{helpMessage}</small>
+        <br />
+
+        <button className="button dark" onClick={props.toggleModal}>
+          {I18n.t('metrics.close_modal')}
+        </button>
       </div>
     </div>
   );
