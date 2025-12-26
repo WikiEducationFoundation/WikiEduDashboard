fd0f1e144178a0ea19ea27ce1223b72dd1064534
gabina <gabinaluz@gmail.com>
2025-12-26 19:20:20 -0300

    Add post-backup scripts. They restart sidekiq process. Every server has to restart its sepcific processes according to the files found in etc/systemd/system

---
+++ docs/backup_scripts/post_db_backup_peony-sidekiq-medium.sh
@@ -0,0 +1,3 @@
+# Based on etc/systemd/system
+sudo systemctl restart sidekiq-medium
+sudo systemctl restart sidekiq-short-extra-2
---
+++ docs/backup_scripts/post_db_backup_peony-sidekiq.sh
@@ -0,0 +1,4 @@
+# Based on etc/systemd/system
+sudo systemctl restart sidekiq-constant
+sudo systemctl restart sidekiq-long
+sudo systemctl restart sidekiq-short-extra
