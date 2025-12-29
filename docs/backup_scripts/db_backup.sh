cc8d50ab73770acc2c96b9fc33f7092a77e3f523
gabina <gabinaluz@gmail.com>
2025-12-29 16:05:29 -0300

    Run post_db_backup on every sidekiq server through ssh

--- docs/backup_scripts/db_backup.sh
+++ docs/backup_scripts/db_backup.sh
@@ -14,5 +14,8 @@ log "Starting post DB backup process"
 # Restart sidekiq in web-server
 ./post_db_backup_peony-web.sh
 # Restart sidekiq processes in peony-sidekiq through ssh
+ssh peony-sidekiq.globaleducation.eqiad1.wikimedia.cloud /home/backup_user/post_db_backup_peony-sidekiq.sh
 # Restart sidekiq processes in peony-sidekiq-medium through ssh
+ssh peony-sidekiq-medium.globaleducation.eqiad1.wikimedia.cloud /home/backup_user/post_db_backup_peony-sidekiq-medium.sh
 # Restart sidekiq processes in peony-sidekiq-3 through ssh
+ssh peony-sidekiq-3.globaleducation.eqiad1.wikimedia.cloud /home/backup_user/post_db_backup_peony-sidekiq-3.sh
