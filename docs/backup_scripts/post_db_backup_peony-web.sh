b0d58a6164f2f6a273dd8fd17d8557e1c2676c77
gabina <gabinaluz@gmail.com>
2025-12-29 16:04:06 -0300

    Comment line for restarting sidekiq-constant on web server since we run it on one of the sidekiq servers.

--- docs/backup_scripts/post_db_backup_peony-web.sh
+++ docs/backup_scripts/post_db_backup_peony-web.sh
@@ -1,4 +1,4 @@
 # Based on etc/systemd/system
 sudo systemctl restart sidekiq-default
-sudo systemctl restart sidekiq-constant
+# sudo systemctl restart sidekiq-constant
 sudo systemctl restart sidekiq-daily
