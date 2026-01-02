fc18dfe36dc9719c377c80a4f5ea0849b661107f
gabina <gabinaluz@gmail.com>
2026-01-02 15:45:39 -0300

    Add backup model

---
+++ app/models/system/backup.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: backups
+#
+#  id           :bigint           not null, primary key
+#  scheduled_at :datetime
+#  start        :datetime
+#  end          :datetime
+#  status       :string(255)
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+class Backup < ApplicationRecord
+end
