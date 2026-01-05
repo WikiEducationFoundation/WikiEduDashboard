5cbd3b06fa783077b82658353697d33903cf918f
Abishekcs <abishekdascs@gmail.com>
2026-01-05 16:51:14 +0530

    Optimize campaign CSV generation with eager loading
    Performance improvments:
    - Add preload_course_data to batch-load related data for multiple
    courses
    - Extract course_ids, tags, revision_counts, new_editor_counts, and wiki
    methods
    - Memoize all data queries to prevent N+1 query issues
    
    Refactoring
    - Update CourseCsvBuilder to accept optional preloaded data
    - Modify revisions_by_namespace to use preloaded data when available
    
    This change significantly improves performance for campaigns with
    multiple courses by reducing databae queries from O(n) to O(1) per data
    type.

--- lib/analytics/campaign_csv_builder.rb
+++ lib/analytics/campaign_csv_builder.rb
@@ -10,13 +10,89 @@ class CampaignCsvBuilder
     @campaign = campaign || AllCourses
   end
 
+  # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity
   def courses_to_csv
     csv_data = [CourseCsvBuilder::CSV_HEADERS]
+
+    preload_course_data if @campaign.courses.size > 1
+
     @campaign.courses.find_each do |course|
-      csv_data << CourseCsvBuilder.new(course).row
+      csv_data << CourseCsvBuilder.new(
+        course,
+        tag: tags[course.id]&.first&.tag || 'unknown',
+        revision: revision_counts.select { |(id, _), _| id == course.id },
+        new_editors: new_editor_counts[course.id] || 0,
+        home_wiki: home_wiki_url(course)
+      ).row
     end
 
     CSV.generate { |csv| csv_data.uniq.each { |line| csv << line } }
+  end
+
+  def preload_course_data
+    course_ids
+    tags
+    revision_counts
+    new_editor_counts
+    wikis
+  end
+
+  def course_ids
+    return @course_ids if @course_ids
+
+    @course_ids = @campaign.courses.pluck(:id)
+  end
+
+  def tags
+    return @tags if @tags
+
+    @tags = Tag
+            .where(course_id: @course_ids, tag: %w[first_time_instructor returning_instructor])
+            .select(:tag, :course_id)
+            .group_by(&:course_id)
+  end
+
+  def revision_counts
+    return @revision_counts if @revision_counts
+
+    @revision_counts = ArticleCourseTimeslice
+                       .where(tracked: true, course_id: @course_ids)
+                       .select(:revision_count, :course_id)
+                       .joins(:article)
+                       .where(articles: { namespace: [0, 1, 2] })
+                       .group(:course_id, :namespace)
+                       .sum(:revision_count)
+  end
+
+  def new_editor_counts
+    return @new_editor_counts if @new_editor_counts
+
+    @new_editor_counts = User
+                         .where(registered_at: @campaign.courses.minimum(:start)..@campaign.courses.maximum(:end)) # rubocop:disable Layout/LineLength
+                         .joins(:courses_users)
+                         .where(courses_users: { course_id: @course_ids, role: CoursesUsers::Roles::STUDENT_ROLE }) # rubocop: disable Layout/LineLength
+                         .group(:course_id)
+                         .count
+  end
+
+  def wikis
+    return @wikis if @wikis
+
+    @wikis = Wiki.where(id: @campaign.courses.pluck(:home_wiki_id)).group_by(&:id)
+  end
+
+  def home_wiki_url(course)
+    wiki = wikis[course.home_wiki_id]&.first
+    return '' unless wiki
+
+    language = wiki.language
+    project = wiki.project
+
+    if language && project
+      "#{language}.#{project}.org"
+    else
+      Wiki::MULTILINGUAL_PROJECTS[project]
+    end
   end
 
   def articles_to_csv
--- lib/analytics/course_csv_builder.rb
+++ lib/analytics/course_csv_builder.rb
@@ -4,9 +4,14 @@ require 'csv'
 require_dependency "#{Rails.root}/lib/analytics/per_wiki_course_stats"
 
 class CourseCsvBuilder
-  def initialize(course, per_wiki: false)
+  # rubocop:disable Metrics/ParameterLists
+  def initialize(course, per_wiki: false, tag: nil, revision: nil, new_editors: nil, home_wiki: nil)
     @course = course
     @per_wiki = per_wiki
+    @tag = tag
+    @revisions = revision
+    @new_editors = new_editors
+    @home_wiki = home_wiki
   end
 
   CSV_HEADERS = %w[
@@ -42,13 +47,13 @@ class CourseCsvBuilder
     row << @course.title
     row << @course.school
     row << @course.term
-    row << @course.home_wiki.domain
+    row << (@home_wiki || @course.home_wiki&.domain)
     row << @course.created_at
     row << @course.start
     row << @course.end
-    row << new_or_returning_tag
+    row << (@tag || new_or_returning_tag)
     row << @course.user_count
-    row << new_editors.count
+    row << (@new_editors || new_editors)
     row << @course.article_count
     row << @course.new_article_count
     row << @course.character_sum
@@ -97,6 +102,8 @@ class CourseCsvBuilder
   end
 
   def revisions_by_namespace(namespace)
+    return @revisions[[@course.id, namespace]] if @revisions
+
     @course.scoped_article_timeslices
            .where(tracked: true)
            .joins(:article)
