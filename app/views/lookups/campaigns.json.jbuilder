a8e776f07ece892fa9e77f2d1ef9ca6e1a147c20
Jose Rafael Camejo <jose20camejo@gmail.com>
2026-01-29 13:59:21 -0300

    form search in courses, articles, students, modal and alerts in csv reports, pagination in campaing index

--- app/views/lookups/campaigns.json.jbuilder
+++ app/views/lookups/campaigns.json.jbuilder
@@ -2,4 +2,11 @@
 
 json.campaigns @values do |campaign|
   json.call(campaign, :id, :title, :slug)
+  json.courses campaign.courses.count
+  json.articles_created campaign.courses.sum(:new_article_count)
+  json.articles_edited campaign.courses.sum(:article_count)
+  json.words_added WordCount.from_characters(campaign.courses.sum(:character_sum))
+  json.references_added campaign.courses.sum(:references_count)
+  json.views campaign.courses.sum(:view_sum)
+  json.editors campaign.courses.sum(:user_count)
 end
--- spec/controllers/campaigns_controller_spec.rb
+++ spec/controllers/campaigns_controller_spec.rb
@@ -427,6 +427,86 @@ describe CampaignsController, type: :request do
         end
       end
 
+      context 'when searching by title' do
+        let(:article) { create(:article, title: 'Test Article') }
+        let(:article2) { create(:article, title: 'Another Article') }
+
+        before do
+          create(:articles_course, course:, article: article2, tracked: true)
+        end
+
+        it 'filters articles by title' do
+          get "/campaigns/#{campaign.slug}/articles", params: { title: 'Test' }
+
+          articles_courses = assigns(:presenter).campaign_articles[:articles_courses]
+          expect(articles_courses.length).to eq(1)
+          expect(articles_courses.first.article.title).to eq(article.title)
+        end
+      end
+
+      context 'when searching by course title' do
+        let(:other_course) { create(:course, title: 'Other Course', slug: 'Other_Course') }
+        let(:article) { create(:article) }
+
+        before do
+          create(:articles_course, course: other_course, article: article, tracked: true)
+          create(:courses_user, course: other_course, user: @organizer, role: 1)
+          create(:campaigns_course, campaign: campaign, course: other_course)
+        end
+
+        it 'filters articles by course title' do
+          get "/campaigns/#{campaign.slug}/articles", params: { course_title: other_course.title }
+
+          articles_courses = assigns(:presenter).campaign_articles[:articles_courses]
+          expect(response).to be_successful
+          expect(articles_courses).not_to be_empty
+          expect(articles_courses.first.course.title).to eq(other_course.title)
+        end
+      end
+
+      context 'when searching by references count' do
+        let(:article1) { create(:article, title: 'Article 1') }
+        let(:article2) { create(:article, title: 'Article 2') }
+
+        before do
+          create(:articles_course, course: course, article: article1, references_count: 5,
+tracked: true)
+          create(:articles_course, course: course, article: article2, references_count: 15,
+tracked: true)
+        end
+
+        it 'filters articles by references count' do
+          get "/campaigns/#{campaign.slug}/articles",
+              params: { references_count_from: 10, references_count_to: 20 }
+
+          articles_courses = assigns(:presenter).campaign_articles[:articles_courses]
+          expect(response).to be_successful
+          expect(articles_courses.count).to eq(1)
+          expect(articles_courses.first.article.title).to eq(article2.title)
+        end
+      end
+
+      context 'when searching by view count' do
+        let(:article1) { create(:article, title: 'Article 1') }
+        let(:article2) { create(:article, title: 'Article 2') }
+
+        before do
+          create(:articles_course, course: course, article: article1, view_count: 50, tracked: true)
+          create(:articles_course, course: course, article: article2, view_count: 150,
+tracked: true)
+        end
+
+        it 'filters articles by view count' do
+          get "/campaigns/#{campaign.slug}/articles",
+              params: { view_count_from: 100, view_count_to: 200 }
+
+          articles_courses = assigns(:presenter).campaign_articles[:articles_courses]
+          expect(response).to be_successful
+          expect(articles_courses.count).to eq(1)
+          expect(articles_courses.first.article.title).to eq(article2.title)
+        end
+      end
+
       context 'when campaign does not exist' do
         it 'raises routing error' do
           expect do
