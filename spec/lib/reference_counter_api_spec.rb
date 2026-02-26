6f0c57c9ac7f4ce61ebe111a236e96398b188339
Formasit-chijoh <formasitf@gmail.com>
2026-02-26 04:50:29 +0100

    Batches multiple errors and limits Sentry samples for reference_counter api

--- spec/lib/reference_counter_api_spec.rb
+++ spec/lib/reference_counter_api_spec.rb
@@ -78,28 +78,7 @@ describe ReferenceCounterApi do
       expect(response.dig('5006946', 'num_ref')).to be_nil
       expect(response.dig('5006946').key?('error')).to eq(false)
     end
-  end
-
-  # it 'logs the message if response is not 200 OK', vcr: true do
-  #   ref_counter_api = described_class.new(en_wikipedia)
-  #   expect(Sentry).to receive(:capture_message).with(
-  #     'Non-200 response hitting references counter API',
-  #     level: 'warning',
-  #     extra: {
-  #       project_code: 'wikipedia',
-  #       language_code: 'en',
-  #       rev_id: 708326238,
-  #       status_code: 403,
-  #       content: {
-  #           'description' =>
-  #           "mwapi error: permissiondenied - You don't have permission to view deleted " \
-  #           'text or changes between deleted revisions.'
-  #       }
-  #     }
-  #   )
-  #   response = ref_counter_api.get_number_of_references_from_revision_ids deleted_rev_ids
-  #   expect(response.dig('708326238')).to eq({ 'num_ref' => nil })
-  # end
+  end  
 
   it 'logs the error once if an unexpected error raises several times', vcr: true do
     reference_counter_api = described_class.new(es_wiktionary)
@@ -125,4 +104,58 @@ describe ReferenceCounterApi do
     expect(response.dig('5006940', 'num_ref')).to be_nil
     expect(response.dig('5006940', 'error')).not_to be_nil
   end
+
+  context 'when the API returns non-200 responses' do
+    let(:failing_ids) {
+[708326238, 123456789, 987654321, 111222333, 444555666, 777888999, 408326238]}
+    let(:subject) { described_class.new(en_wikipedia) }
+
+    it 'batches multiple errors and limits Sentry samples to exactly 5' do
+
+      # Sentry is called ONCE with total count 7 and 5 samples
+      expect(Sentry).to receive(:capture_message).once.with(
+        "Non-200 response hitting references counter API: 403",
+        hash_including(
+          level: 'warning',
+          fingerprint: ['references-counter-api-error', '403'],
+          extra: {
+            project_code: 'wikipedia',
+            language_code: 'en',
+            error_count: 7,
+            errors: [
+              { rev_id: 708326238, content: { "description" => "mwapi error: permissiondenied" } },
+              { rev_id: 123456789, content: { "description" => "mwapi error: permissiondenied" } },
+              { rev_id: 987654321, content: { "description" => "mwapi error: permissiondenied" } },
+              { rev_id: 111222333, content: { "description" => "mwapi error: permissiondenied" } },
+              { rev_id: 444555666, content: { "description" => "mwapi error: permissiondenied" } }
+            ]
+          }
+        )
+      )
+
+      # MOCK the API call to return a result that simulates a 403 error flow
+      allow(subject).to receive(:get_number_of_references_from_revision_id) do |rev_id|
+        subject.send(:batch_non_200_response_log, 403, 
+{ rev_id: rev_id, content: { "description" => "mwapi error: permissiondenied" } })
+        { 'num_ref' => nil }
+      end
+
+      #This triggers the mocked calls and then the Sentry report
+      results = subject.get_number_of_references_from_revision_ids(failing_ids)
+
+      expect(results.length).to eq(7)
+      expect(results.values.all? { |v| v['num_ref'].nil? }).to be true
+    end
+    it 'sends separate Sentry logs for each unique status code' do
+      # One call per status code
+      subject.send(:batch_non_200_response_log, 403, { rev_id: 1, content: {} })
+      subject.send(:batch_non_200_response_log, 400, { rev_id: 2, content: {} })
+      subject.send(:batch_non_200_response_log, 400, { rev_id: 3, content: {} })
+
+      expect(Sentry).to receive(:capture_message).twice
+      subject.send(:report_reference_counter_error_to_sentry)
+    end
+
+  end
+
 end
