bc28ff712a29a14fe6f98c2cf8a4badaf07c0588
gabina <gabinaluz@gmail.com>
2026-03-09 12:50:05 -0300

    Run API requests to different tools and models in parallel

--- app/controllers/ai_tools_controller.rb
+++ app/controllers/ai_tools_controller.rb
@@ -13,15 +13,44 @@ class AiToolsController < ApplicationController
     parse_url
     @diff_mode = params[:diff_mode] == 'true'
     @plain = GetRevisionPlaintext.new(@rev_id, @wiki, diff_mode: @diff_mode, from_rev: @from_rev)
-    @pangram_v2_result = PangramApi.v2.inference @plain.plain_text
-    @pangram_v3_result = PangramApi.v3.inference @plain.plain_text
-    @originality_turbo_result = OriginalityApi.turbo.inference @plain.plain_text
-    @originality_academic_result = OriginalityApi.academic.inference @plain.plain_text
-
+    detect_ai_from_multiple_resources @plain.plain_text
     render 'show'
   end
 
   private
+
+  MAX_CONCURRENCY = 4
+
+  PANGRAM_V2_KEY = 'pangram_v2'
+  PANGRAM_V3_KEY = 'pangram_v3'
+  ORIGINALITY_TURBO_KEY = 'originality_turbo'
+  ORIGINALITY_ACADEMIC_KEY = 'originality_academic'
+
+  DETECTORS = {
+    PANGRAM_V2_KEY => PangramApi.v2,
+    PANGRAM_V3_KEY => PangramApi.v3,
+    ORIGINALITY_TURBO_KEY => OriginalityApi.turbo,
+    ORIGINALITY_ACADEMIC_KEY => OriginalityApi.academic
+  }.freeze
+
+  def detect_ai_from_multiple_resources(text)
+    pool = Concurrent::FixedThreadPool.new(MAX_CONCURRENCY)
+    results = Concurrent::Hash.new
+
+    DETECTORS.each do |key, detector|
+      pool.post { detect_ai(key, detector, text, results) }
+    end
+    pool.shutdown && pool.wait_for_termination
+
+    @pangram_v2_result = results[PANGRAM_V2_KEY]
+    @pangram_v3_result = results[PANGRAM_V3_KEY]
+    @originality_turbo_result = results[ORIGINALITY_TURBO_KEY]
+    @originality_academic_result = results[ORIGINALITY_ACADEMIC_KEY]
+  end
+
+  def detect_ai(key, ai_detector, text, results)
+    results[key] = ai_detector.inference text
+  end
 
   def parse_url
     @url = params[:article_or_diff_url]
