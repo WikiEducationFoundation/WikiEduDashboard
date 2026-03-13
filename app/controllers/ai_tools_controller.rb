1c7ac129e4455ae5a7188d960fe016b7b09025bb
gabina <gabinaluz@gmail.com>
2026-03-13 00:21:02 -0300

    Handle article URL in AiToolsController. This involves manually fetching the latest revision

--- app/controllers/ai_tools_controller.rb
+++ app/controllers/ai_tools_controller.rb
@@ -11,7 +11,6 @@ class AiToolsController < ApplicationController
 
   def compare_pangrams
     parse_url
-    @diff_mode = params[:diff_mode] == 'true'
     @plain = GetRevisionPlaintext.new(@rev_id, @wiki, diff_mode: @diff_mode, from_rev: @from_rev)
     detect_ai_from_multiple_resources @plain.plain_text
     render 'show'
@@ -63,9 +62,34 @@ class AiToolsController < ApplicationController
     @url = params[:article_or_diff_url]
     parser = WikiUrlParser.new(@url)
     @wiki = parser.wiki
-    @article_title= parser.title
-    revs = [parser.oldid, parser.diff].compact
-    @rev_id = revs.max
-    @from_rev = revs.min if revs.count == 2
+    @article_title = parser.title
+
+    if parser.diff
+      revs = [parser.oldid, parser.diff].compact
+      @rev_id = revs.max
+      @from_rev = revs.min if revs.count == 2
+      @diff_mode = true
+    else
+      # If there is no diff revision in the url, it means it's just a single revision url.
+      @diff_mode = false
+      # If it does not contain an oldid either we have to manually fetch the latest revision.
+      # Example: https://en.wikipedia.org/wiki/Greater_Cooch_Behar_People%27s_Association
+      @rev_id = parser.oldid || latest_revision
+    end
+  end
+
+  def latest_revision
+    response = WikiApi.new(@wiki).query(query_params)
+    response.data['pages'].values.first['revisions'].first['revid']
+  end
+
+  # Query for the latest revision for a given article title
+  def query_params
+    {
+      action: 'query',
+      prop: 'revisions',
+      titles: CGI.unescape(@article_title),
+      rvprop: 'ids'
+    }
   end
 end
