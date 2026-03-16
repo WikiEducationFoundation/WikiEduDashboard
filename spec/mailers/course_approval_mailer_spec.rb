cd4721cd5f9eb8293f47b6570ae45f2670e4f5ce
John Costa <costajohnt@gmail.com>
2026-03-16 12:49:07 -0700

    test: add mailer tests for locale in enrollment links
    Verify that enrollment links include &locale= when the course's
    campaign has a default_language set, and omit it when not set.

--- spec/mailers/course_approval_mailer_spec.rb
+++ spec/mailers/course_approval_mailer_spec.rb
@@ -14,5 +14,21 @@ describe CourseApprovalMailer do
       expect(mail.html_part.body).to include(escaped_slug(course.slug) + '?enroll=')
       expect(mail.to).to eq([instructor.email])
     end
+
+    it 'includes locale parameter when campaign has a default language' do
+      allow(Features).to receive(:email?).and_return(true)
+      campaign = create(:campaign, default_language: 'es')
+      course.campaigns << campaign
+      mail = described_class.send_approval_notification(course, instructor)
+      expect(mail.html_part.body).to include('&locale=es')
+    end
+
+    it 'omits locale parameter when campaign has no default language' do
+      allow(Features).to receive(:email?).and_return(true)
+      campaign = create(:campaign, default_language: nil)
+      course.campaigns << campaign
+      mail = described_class.send_approval_notification(course, instructor)
+      expect(mail.html_part.body).not_to include('&locale=')
+    end
   end
 end
