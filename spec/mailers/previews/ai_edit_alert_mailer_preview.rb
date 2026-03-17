26f3ecf33d8b2692b5a320eaab5caf35a500d595
Sage Ross <ragesoss@gmail.com>
2026-03-17 15:33:06 -0700

    Update emails for Pangram 3.

--- spec/mailers/previews/ai_edit_alert_mailer_preview.rb
+++ spec/mailers/previews/ai_edit_alert_mailer_preview.rb
@@ -25,17 +25,17 @@ class AiEditAlertMailerPreview < ActionMailer::Preview
 
   def example_alert_details(title)
     { article_title: title,
-      pangram_prediction: 'Fully AI-Generated',
-      headline_result: 'AI Detected',
+      pangram_prediction: 'We are confident that this document is fully AI-generated',
+      headline_result: 'Fully AI Generated',
       average_ai_likelihood: 0.974378,
       max_ai_likelihood: 1.0,
       fraction_human_content: 0.0,
       fraction_ai_content: 1.0,
       fraction_mixed_content: 0.0,
-      window_likelihoods: [1.0, 1.0],
-      predicted_ai_window_count: 2,
+      window_likelihoods: [1.0, 0.9982278487261604, 0.9959831237792969],
+      predicted_ai_window_count: 3,
       pangram_share_link: 'https://www.pangram.com/history/88ba317f-8572-4000-911f-2aa8fbea68fa',
-      pangram_version: 'adaptive_boundaries',
+      pangram_version: '3.0',
       prior_alert_count_for_course: 0 }
   end
 
