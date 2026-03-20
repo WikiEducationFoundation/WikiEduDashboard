e9044af27bfdd3b9aa6cf3f2e3d0f63eeff2184e
sylvia boyani <sylviaboyani11@gmail.com>
2026-03-20 05:01:11 +0300

    Fix I18n warnings in fast_training_alert.jsx

--- app/assets/javascripts/training/components/fast_training_alert.jsx
+++ app/assets/javascripts/training/components/fast_training_alert.jsx
@@ -9,10 +9,10 @@ function FastTrainingAlert() {
         <div className="blur-backdrop-for-alert-box" onClick={() => setOpen(false)}>
           <div className="alert-box-container">
             <div className="alert-box">
-              <h2 className="alert-title">Please take your time!</h2>
-              <p className="alert-content">It is very important that you learn the training content thoroughly.</p>
+              <h2 className="alert-title">{I18n.t('training.take_your_time')}</h2>
+              <p className="alert-content">{I18n.t('training.alert_content')}</p>
               <div className="alert-button-container">
-                <button className="alert-button" onClick={() => setOpen(false)}>CLOSE</button>
+                <button className="alert-button" onClick={() => setOpen(false)}>{I18n.t('training.close')}</button>
               </div>
             </div>
           </div>
